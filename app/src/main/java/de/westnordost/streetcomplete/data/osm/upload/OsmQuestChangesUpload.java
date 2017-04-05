0ad3000b95e3d3c27ba00263357cdfc68eab3799
Tobias Zwick <newton@openclonk.org>
2017-04-05 17:44:32 +0200

    upload all quests of same quest type into same changeset; advanced conflict handling (#21)
    and add test cases

--- app/src/main/java/de/westnordost/streetcomplete/data/osm/upload/OsmQuestChangesUpload.java
+++ app/src/main/java/de/westnordost/streetcomplete/data/osm/upload/OsmQuestChangesUpload.java
@@ -1,8 +1,10 @@
 package de.westnordost.streetcomplete.data.osm.upload;
 
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,8 +12,20 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.inject.Inject;
 
+import de.westnordost.osmapi.changesets.ChangesetInfo;
+import de.westnordost.osmapi.changesets.ChangesetsDao;
+import de.westnordost.osmapi.map.data.Node;
+import de.westnordost.osmapi.map.data.OsmNode;
+import de.westnordost.osmapi.map.data.OsmRelation;
+import de.westnordost.osmapi.map.data.OsmWay;
+import de.westnordost.osmapi.map.data.Relation;
+import de.westnordost.osmapi.map.data.Way;
 import de.westnordost.streetcomplete.ApplicationConstants;
+import de.westnordost.streetcomplete.Prefs;
 import de.westnordost.streetcomplete.data.QuestStatus;
+import de.westnordost.streetcomplete.data.changesets.ManageChangesetInfo;
+import de.westnordost.streetcomplete.data.changesets.ManageChangesetsDao;
+import de.westnordost.streetcomplete.data.osm.OsmElementQuestType;
 import de.westnordost.streetcomplete.data.osm.OsmQuest;
 import de.westnordost.streetcomplete.data.osm.changes.StringMapChanges;
 import de.westnordost.streetcomplete.data.osm.persist.ElementGeometryDao;
@@ -21,7 +35,6 @@ import de.westnordost.streetcomplete.data.statistics.QuestStatisticsDao;
 import de.westnordost.osmapi.common.errors.OsmConflictException;
 import de.westnordost.osmapi.map.MapDataDao;
 import de.westnordost.osmapi.map.data.Element;
-import de.westnordost.osmapi.map.data.OsmElement;
 
 public class OsmQuestChangesUpload
 {
@@ -32,21 +45,33 @@ public class OsmQuestChangesUpload
 	private final MergedElementDao elementDB;
 	private final ElementGeometryDao elementGeometryDB;
 	private final QuestStatisticsDao statisticsDB;
+	private final ManageChangesetsDao manageChangesetsDB;
+	private final ChangesetsDao changesetsDao;
+	private final SharedPreferences prefs;
+
+	// The cache is just here so that uploading 500 quests of same quest type does not result in 500 DB requests.
+	private Map<String, Long> changesetIdsCache = new HashMap<>();
 
 	@Inject public OsmQuestChangesUpload(
 			MapDataDao osmDao, OsmQuestDao questDB, MergedElementDao elementDB,
-			ElementGeometryDao elementGeometryDB, QuestStatisticsDao statisticsDB)
+			ElementGeometryDao elementGeometryDB, QuestStatisticsDao statisticsDB,
+			ManageChangesetsDao manageChangesetsDB, ChangesetsDao changesetsDao,
+			SharedPreferences prefs)
 	{
 		this.osmDao = osmDao;
 		this.questDB = questDB;
 		this.elementDB = elementDB;
 		this.statisticsDB = statisticsDB;
 		this.elementGeometryDB = elementGeometryDB;
+		this.manageChangesetsDB = manageChangesetsDB;
+		this.changesetsDao = changesetsDao;
+		this.prefs = prefs;
 	}
 
-	public void upload(AtomicBoolean cancelState)
+	public synchronized void upload(AtomicBoolean cancelState)
 	{
 		int commits = 0, obsolete = 0;
+		changesetIdsCache = new HashMap<>();
 
 		for(OsmQuest quest : questDB.getAll(null, QuestStatus.ANSWERED))
 		{
@@ -54,7 +79,8 @@ public class OsmQuestChangesUpload
 
 			Element element = elementDB.get(quest.getElementType(), quest.getElementId());
 
-			if (uploadQuestChanges(quest, element, false))
+			long changesetId = getChangesetIdOrCreate(quest.getOsmElementQuestType());
+			if (uploadQuestChange(changesetId, quest, element, false, false))
 			{
 				commits++;
 			}
@@ -73,58 +99,62 @@ public class OsmQuestChangesUpload
 		}
 
 		Log.i(TAG, logMsg);
+
+		// TODO check if we can close any changesets now
 	}
 
-	boolean uploadQuestChanges(OsmQuest quest, Element element, boolean alreadyHandlingConflict)
+	private long getChangesetIdOrCreate(OsmElementQuestType questType)
 	{
-		// The element can be null if it has been deleted in the meantime (outside this app usually)
-		if(element == null)
+		String questTypeName = questType.getClass().getSimpleName();
+
+		Long cachedChangesetId = changesetIdsCache.get(questTypeName);
+		if(cachedChangesetId != null) return cachedChangesetId;
+
+		ManageChangesetInfo changesetInfo = manageChangesetsDB.get(questTypeName);
+		long result;
+		if (changesetInfo != null && changesetInfo.changesetId != null)
 		{
-			questDB.delete(quest.getId());
-			Log.v(TAG, "Dropped quest " + getQuestStringForLog(quest) +
-					" because the associated element has already been deleted");
-			return false;
+			result = changesetInfo.changesetId;
+		}
+		else
+		{
+			result = createChangeset(questType);
 		}
 
-		StringMapChanges changes = quest.getChanges();
-		Map<String,String> elementTags = element.getTags();
-		if(elementTags == null)
-		{
-			((OsmElement) element).setTags(new HashMap<String, String>());
-			elementTags = element.getTags();
-		}
-		if(changes.hasConflictsTo(elementTags))
+		changesetIdsCache.put(questTypeName, result);
+		return result;
+	}
+
+	private long createChangeset(OsmElementQuestType questType)
+	{
+		String questTypeName = questType.getClass().getSimpleName();
+		long changesetId = osmDao.openChangeset(createChangesetTags(questType));
+		manageChangesetsDB.assignChangesetId(questTypeName, changesetId);
+		return changesetId;
+	}
+
+	boolean uploadQuestChange(long changesetId, OsmQuest quest, Element element,
+							  boolean alreadyHandlingElementConflict,
+							  boolean alreadyHandlingChangesetConflict)
+	{
+		Element elementWithChangesApplied = changesApplied(element, quest);
+		if(elementWithChangesApplied == null)
 		{
 			questDB.delete(quest.getId());
-			Log.v(TAG, "Dropped quest " + getQuestStringForLog(quest) +
-					" because there has been a conflict while applying the changes");
 			return false;
 		}
-		changes.applyTo(element.getTags());
 
 		try
 		{
-			Map<String,String> changesetTags = createChangesetTags(quest);
-
-			osmDao.updateMap( changesetTags, Collections.singleton(element), null);
+			osmDao.uploadChanges( changesetId, Collections.singleton(elementWithChangesApplied), null);
 			/* A diff handler is not (yet) necessary: The local copy of an OSM element is updated
 			 * automatically on conflict. A diff handler would be necessary if elements could be
 			 * created or deleted through quests because IDs of elements would then change. */
 		}
 		catch(OsmConflictException e)
 		{
-			/* should not be necessary but as a safeguard against a stack overflow in case
-			   the OSM server always answers with OsmConflictException (which is then very probably
-			   a programming error on our side */
-			if(alreadyHandlingConflict)
-			{
-				throw new RuntimeException(
-						"OSM server continues to report a conflict on uploading the changes for " +
-						"the quest " + getQuestStringForLog(quest) + ". The local version is " +
-						element.getVersion(), e);
-			}
-			element = updateElementFromServer(quest.getElementType(), quest.getElementId());
-			return uploadQuestChanges(quest, element, true);
+			return handleConflict(changesetId, quest, element, alreadyHandlingElementConflict,
+					alreadyHandlingChangesetConflict, e);
 		}
 
 		questDB.delete(quest.getId());
@@ -133,10 +163,113 @@ public class OsmQuestChangesUpload
 		return true;
 	}
 
+	private Element changesApplied(Element element, OsmQuest quest)
+	{
+		// The element can be null if it has been deleted in the meantime (outside this app usually)
+		if(element == null)
+		{
+			Log.v(TAG, "Dropping quest " + getQuestStringForLog(quest) +
+					" because the associated element has already been deleted");
+			return null;
+		}
+
+		Element copy = copyElement(element);
+
+		StringMapChanges changes = quest.getChanges();
+		if(changes.hasConflictsTo(copy.getTags()))
+		{
+			Log.v(TAG, "Dropping quest " + getQuestStringForLog(quest) +
+					" because there has been a conflict while applying the changes");
+			return null;
+		}
+		changes.applyTo(copy.getTags());
+		return copy;
+	}
+
+	private boolean handleConflict(long changesetId, OsmQuest quest, Element element,
+								   boolean alreadyHandlingElementConflict,
+								   boolean alreadyHandlingChangesetConflict, OsmConflictException e)
+	{
+		/* Conflict can either happen because of the changeset or because of the element(s) uploaded.
+		   Let's find out. */
+
+		ChangesetInfo changesetInfo = changesetsDao.get(changesetId);
+
+		Long myUserId = prefs.getLong(Prefs.OSM_USER_ID, -1);
+		// can happen if the user changes his OAuth identity in the settings while having an open changeset
+		boolean changesetWasOpenedByDifferentUser =
+				myUserId == -1 || changesetInfo.user == null || changesetInfo.user.id != myUserId;
+
+		if(!changesetInfo.isOpen || changesetWasOpenedByDifferentUser)
+		{
+			// safeguard against stack overflow in case of programming error
+			if(alreadyHandlingChangesetConflict)
+			{
+				throw new RuntimeException("OSM server continues to report a changeset " +
+						"conflict for changeset id " + changesetId, e);
+			}
+			return handleChangesetConflict(quest, element, alreadyHandlingElementConflict);
+		}
+		else
+		{
+			// safeguard against stack overflow in case of programming error
+			if(alreadyHandlingElementConflict)
+			{
+				throw new RuntimeException("OSM server continues to report an element " +
+						"conflict on uploading the changes for the quest " +
+						getQuestStringForLog(quest) + ". The local version is " +
+						element.getVersion(), e);
+			}
+			return handleElementConflict(changesetId, quest, alreadyHandlingChangesetConflict);
+		}
+	}
+
+	private boolean handleChangesetConflict(OsmQuest quest, Element element,
+											boolean alreadyHandlingElementConflict)
+	{
+		OsmElementQuestType questType = quest.getOsmElementQuestType();
+
+		long changesetId = createChangeset(questType);
+		String questTypeName = questType.getClass().getSimpleName();
+		changesetIdsCache.put(questTypeName, changesetId);
+
+		return uploadQuestChange(changesetId, quest, element, alreadyHandlingElementConflict, true);
+	}
+
+	private boolean handleElementConflict(long changesetId, OsmQuest quest,
+										  boolean alreadyHandlingChangesetConflict)
+	{
+		Element element = updateElementFromServer(quest.getElementType(), quest.getElementId());
+		return uploadQuestChange(changesetId, quest, element, true, alreadyHandlingChangesetConflict);
+	}
+
 	private static String getQuestStringForLog(OsmQuest quest)
 	{
 		return quest.getType().getClass().getSimpleName() + " for " +
 				quest.getElementType().name().toLowerCase() + " #" + quest.getElementId();
+	}
+
+	private static Element copyElement(Element e)
+	{
+		if(e == null) return null;
+		Map<String,String> tagsCopy = new HashMap<>();
+		if(e.getTags() != null) tagsCopy.putAll(e.getTags());
+
+		if(e instanceof Node)
+		{
+			return new OsmNode(e.getId(), e.getVersion(), ((Node)e).getPosition(), tagsCopy);
+		}
+		if(e instanceof Way)
+		{
+			return new OsmWay(e.getId(), e.getVersion(),
+					new ArrayList<>(((Way)e).getNodeIds()), tagsCopy);
+		}
+		if(e instanceof Relation)
+		{
+			return new OsmRelation(e.getId(), e.getVersion(),
+					new ArrayList<>(((Relation)e).getMembers()), tagsCopy);
+		}
+		return null;
 	}
 
 	private Element updateElementFromServer(Element.Type elementType, long id)
@@ -168,16 +301,16 @@ public class OsmQuestChangesUpload
 		return element;
 	}
 
-	private Map<String,String> createChangesetTags(OsmQuest quest)
+	private Map<String,String> createChangesetTags(OsmElementQuestType questType)
 	{
 		Map<String,String> changesetTags = new HashMap<>();
-		String commitMessage = quest.getCommitMessage();
+		String commitMessage = questType.getCommitMessage();
 		if(commitMessage != null)
 		{
 			changesetTags.put("comment", commitMessage);
 		}
 		changesetTags.put("created_by", ApplicationConstants.USER_AGENT);
-		String questTypeName = quest.getType().getClass().getSimpleName();
+		String questTypeName = questType.getClass().getSimpleName();
 		changesetTags.put(ApplicationConstants.QUESTTYPE_TAG_KEY, questTypeName);
 		changesetTags.put("source", "survey");
 		return changesetTags;
--- app/src/test/java/de/westnordost/streetcomplete/data/osm/upload/OsmQuestChangesUploadTest.java
+++ app/src/test/java/de/westnordost/streetcomplete/data/osm/upload/OsmQuestChangesUploadTest.java
@@ -1,5 +1,6 @@
 package de.westnordost.streetcomplete.data.osm.upload;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import junit.framework.TestCase;
@@ -14,15 +15,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import de.westnordost.osmapi.changesets.ChangesetInfo;
+import de.westnordost.osmapi.changesets.ChangesetsDao;
 import de.westnordost.osmapi.common.Handler;
 import de.westnordost.osmapi.common.errors.OsmConflictException;
 import de.westnordost.osmapi.map.MapDataDao;
-import de.westnordost.osmapi.map.changes.DiffElement;
 import de.westnordost.osmapi.map.data.Element;
 import de.westnordost.osmapi.map.data.OsmLatLon;
 import de.westnordost.osmapi.map.data.OsmNode;
-import de.westnordost.streetcomplete.ApplicationConstants;
+import de.westnordost.osmapi.user.User;
+import de.westnordost.streetcomplete.Prefs;
 import de.westnordost.streetcomplete.data.QuestStatus;
+import de.westnordost.streetcomplete.data.changesets.ManageChangesetsDao;
 import de.westnordost.streetcomplete.data.osm.OsmQuest;
 import de.westnordost.streetcomplete.data.osm.OverpassQuestType;
 import de.westnordost.streetcomplete.data.osm.changes.StringMapChanges;
@@ -41,6 +45,8 @@ import static org.mockito.Mockito.*;
 public class OsmQuestChangesUploadTest extends TestCase
 {
 
+	private static long A_NODE_ID = 5;
+
 	public void testCancel() throws InterruptedException
 	{
 		OsmQuestDao questDb = mock(OsmQuestDao.class);
@@ -58,7 +64,8 @@ public class OsmQuestChangesUploadTest extends TestCase
 					}
 				});
 
-		final OsmQuestChangesUpload u = new OsmQuestChangesUpload(null, questDb, elementDB, elementGeometryDao, null);
+		final OsmQuestChangesUpload u = new OsmQuestChangesUpload(null, questDb, elementDB,
+				elementGeometryDao, null, null, null, null);
 		final AtomicBoolean cancel = new AtomicBoolean(false);
 
 		Thread t = new Thread(new Runnable()
@@ -82,106 +89,201 @@ public class OsmQuestChangesUploadTest extends TestCase
 
 	public void testDropChangeWhenElementDeleted()
 	{
-		OsmQuest quest = createQuest(null);
+		OsmQuest quest = createAnsweredQuest(null);
 		OsmQuestDao questDb = mock(OsmQuestDao.class);
-		OsmQuestChangesUpload u = new OsmQuestChangesUpload(null, questDb, null, null, null);
+		OsmQuestChangesUpload u = new OsmQuestChangesUpload(null, questDb, null, null, null, null, null, null);
 
-		assertFalse(u.uploadQuestChanges(quest, null, false));
+		assertFalse(u.uploadQuestChange(-1, quest, null, false, false));
 
 		verify(questDb).delete(quest.getId());
 	}
 
-	public void testDropChangeWhenUnresolvableConflict()
+	public void testDropChangeWhenUnresolvableElementChange()
 	{
-		StringMapEntryChange nonPossibleChange = new StringMapEntryDelete("somekey","value");
-
-		StringMapChanges changes = new StringMapChanges(Collections.singletonList(nonPossibleChange));
-		OsmQuest quest = createQuest(changes);
+		OsmQuest quest = createAnsweredQuestWithNonAppliableChange();
 		Element element = createElement();
 
 		OsmQuestDao questDb = mock(OsmQuestDao.class);
-		OsmQuestChangesUpload u = new OsmQuestChangesUpload(null, questDb, null, null, null);
-		assertFalse(u.uploadQuestChanges(quest, element, false));
+		MergedElementDao elementDao = mock(MergedElementDao.class);
+		OsmQuestChangesUpload u = new OsmQuestChangesUpload(null, questDb, null, null, null, null, null, null);
+		assertFalse(u.uploadQuestChange(123, quest, element, false, false));
 		verify(questDb).delete(quest.getId());
 	}
 
-	public void testHandleConflictAndThenDeleted()
+	/* Simulates an element conflict while uploading the element, when updating the element from
+	   mock server, it turns out that it has been deleted */
+	public void testHandleElementConflictAndThenDeleted()
 	{
-		StringMapEntryChange aPossibleChange = new StringMapEntryAdd("somekey","value");
-		StringMapChanges changes = new StringMapChanges(Collections.singletonList(aPossibleChange));
-		OsmQuest quest = createQuest(changes);
+		final long changesetId = 123;
+		final long userId = 10;
+
+		OsmQuest quest = createAnsweredQuestWithAppliableChange();
 		Element element = createElement();
 
 		MergedElementDao elementDb = mock(MergedElementDao.class);
 		OsmQuestDao questDb = mock(OsmQuestDao.class);
-		MapDataDao mapDataDao = mock(MapDataDao.class);
-		when(mapDataDao.updateMap(any(Map.class), any(Iterable.class), isNull(Handler.class)))
-				.thenThrow(OsmConflictException.class);
-		when(mapDataDao.getNode(5)).thenReturn(null);
-		OsmQuestChangesUpload u = new OsmQuestChangesUpload(mapDataDao, questDb, elementDb, null, null);
 
-		assertFalse(u.uploadQuestChanges(quest, element, false));
+		MapDataDao mapDataDao = createMapDataDaoThatReportsConflictOnUploadAndNodeDeleted();
+
+		// a changeset dao+prefs that report that the changeset is open and the changeset is owned by the user
+		ChangesetsDao changesetsDao = mock(ChangesetsDao.class);
+		when(changesetsDao.get(changesetId)).thenReturn(createOpenChangesetForUser(userId));
+		SharedPreferences prefs = mock(SharedPreferences.class);
+		when(prefs.getLong(Prefs.OSM_USER_ID, -1)).thenReturn(userId);
+
+		OsmQuestChangesUpload u = new OsmQuestChangesUpload(mapDataDao, questDb, elementDb, null,
+				null, null, changesetsDao, prefs);
+
+		assertFalse(u.uploadQuestChange(changesetId, quest, element, false, false));
 		verify(questDb).delete(quest.getId());
-		verify(elementDb).delete(Element.Type.NODE, 5);
+		verify(elementDb).delete(Element.Type.NODE, A_NODE_ID);
+	}
+
+	/* Simulates the changeset that is about to be used was created by a different user, so a new
+	*  changeset needs to be created. (after that, it runs into the same case above, for simplicity
+	*  sake*/
+	public void testHandleChangesetConflictDifferentUser()
+	{
+		final long userId = 10;
+		final long otherUserId = 15;
+		final long firstChangesetId = 123;
+		final long secondChangesetId = 124;
+		// reports that the changeset is open but does belong to another user
+		ChangesetsDao changesetsDao = mock(ChangesetsDao.class);
+		when(changesetsDao.get(firstChangesetId)).thenReturn(createOpenChangesetForUser(otherUserId));
+		when(changesetsDao.get(secondChangesetId)).thenReturn(createOpenChangesetForUser(userId));
+
+		doTestHandleChangesetConflict(changesetsDao, userId, firstChangesetId, secondChangesetId);
+	}
+
+	/* Simulates the changeset that is about to be used is already closed, so a new changeset needs
+	to be created. (after that, it runs into the same case above, for simplicity sake*/
+	public void testHandleChangesetConflictAlreadyClosed()
+	{
+		final long userId = 10;
+		final long firstChangesetId = 123;
+		final long secondChangesetId = 124;
+		// reports that the changeset is open but does belong to another user
+		ChangesetsDao changesetsDao = mock(ChangesetsDao.class);
+		when(changesetsDao.get(firstChangesetId)).thenReturn(createClosedChangesetForUser(userId));
+		when(changesetsDao.get(secondChangesetId)).thenReturn(createOpenChangesetForUser(userId));
+
+		doTestHandleChangesetConflict(changesetsDao, userId, firstChangesetId, secondChangesetId);
+	}
+
+	private void doTestHandleChangesetConflict(ChangesetsDao changesetsDao, long userId,
+											   long firstChangesetId, long secondChangesetId)
+	{
+		OsmQuest quest = createAnsweredQuestWithAppliableChange();
+		Element element = createElement();
+
+		MergedElementDao elementDb = mock(MergedElementDao.class);
+		OsmQuestDao questDb = mock(OsmQuestDao.class);
+
+		ManageChangesetsDao manageChangesetsDb = mock(ManageChangesetsDao.class);
+
+		MapDataDao mapDataDao = createMapDataDaoThatReportsConflictOnUploadAndNodeDeleted();
+		when(mapDataDao.openChangeset(any(Map.class))).thenReturn(secondChangesetId);
+
+		SharedPreferences prefs = createPreferencesForUser(userId);
+
+		OsmQuestChangesUpload u = new OsmQuestChangesUpload(mapDataDao, questDb, elementDb, null,
+				null, manageChangesetsDb, changesetsDao, prefs);
+
+		assertFalse(u.uploadQuestChange(firstChangesetId, quest, element, false, false));
+
+		verify(manageChangesetsDb).assignChangesetId("TestQuestType", secondChangesetId);
+		verify(questDb).delete(quest.getId());
+		verify(elementDb).delete(Element.Type.NODE, A_NODE_ID);
+	}
+
+	private static SharedPreferences createPreferencesForUser(long userId)
+	{
+		SharedPreferences prefs = mock(SharedPreferences.class);
+		when(prefs.getLong(Prefs.OSM_USER_ID, -1)).thenReturn(userId);
+		return prefs;
+	}
+
+	// this map data dao ensures that the program is running into a conflict and finally into
+	// an unresolvable one for the node (because it has been deleted).
+	private static MapDataDao createMapDataDaoThatReportsConflictOnUploadAndNodeDeleted()
+	{
+		MapDataDao mapDataDao = mock(MapDataDao.class);
+		doThrow(OsmConflictException.class).when(mapDataDao)
+				.uploadChanges(any(Long.class), any(Iterable.class), isNull(Handler.class));
+		when(mapDataDao.getNode(A_NODE_ID)).thenReturn(null);
+		return mapDataDao;
+	}
+
+	private static ChangesetInfo createOpenChangesetForUser(long id)
+	{
+		ChangesetInfo result = createChangesetForUser(id);
+		result.isOpen = true;
+		return result;
+	}
+
+	private static ChangesetInfo createClosedChangesetForUser(long id)
+	{
+		ChangesetInfo result = createChangesetForUser(id);
+		result.isOpen = false;
+		return result;
+	}
+
+	private static ChangesetInfo createChangesetForUser(long id)
+	{
+		ChangesetInfo result = new ChangesetInfo();
+		result.user = new User(id, "Hans Wurst");
+		return result;
 	}
 
 	public void testUploadNormally()
 	{
-		StringMapEntryChange aPossibleChange = new StringMapEntryAdd("somekey","value");
-		StringMapChanges changes = new StringMapChanges(Collections.singletonList(aPossibleChange));
-		OsmQuest quest = createQuest(changes);
+		OsmQuest quest = createAnsweredQuestWithAppliableChange();
 		Element element = createElement();
 
 		OsmQuestDao questDb = mock(OsmQuestDao.class);
-		MapDataDao mapDataDao = new TestMapDataDao(element);
+		MapDataDao mapDataDao = mock(MapDataDao.class);
 		QuestStatisticsDao statisticsDao = mock(QuestStatisticsDao.class);
-		OsmQuestChangesUpload u = new OsmQuestChangesUpload(mapDataDao, questDb, null, null, statisticsDao);
+		OsmQuestChangesUpload u = new OsmQuestChangesUpload(mapDataDao, questDb, null, null,
+				statisticsDao, null, null, null);
 
-		assertTrue(u.uploadQuestChanges(quest, element, false));
+		assertTrue(u.uploadQuestChange(1, quest, element, false, false));
 		verify(questDb).delete(quest.getId());
 		verify(statisticsDao).addOne("TestQuestType");
 	}
 
-	private class TestMapDataDao extends MapDataDao
-	{
-		Element element;
-
-		public TestMapDataDao(Element element)
-		{
-			super(null);
-			this.element = element;
-		}
-
-		@Override public long updateMap(Map<String, String> tags, Iterable<Element> elements,
-							  Handler<DiffElement> handler)
-		{
-			assertEquals(tags.get("created_by"), ApplicationConstants.USER_AGENT);
-			assertEquals(tags.get("comment"), "commit message");
-			assertEquals(tags.get("source"), "survey");
-			assertEquals(tags.get("StreetComplete:quest_type"), "TestQuestType");
-
-			assertEquals(elements.iterator().next(), element);
-			return 0;
-		}
-	}
-
-	private class TestQuestType extends OverpassQuestType
+	private static class TestQuestType extends OverpassQuestType
 	{
 		@Override protected String getTagFilters() { return "nodes"; }
-		@Override public Integer applyAnswerTo(Bundle answer, StringMapChangesBuilder changes) { return null; }
+		@Override public void applyAnswerTo(Bundle answer, StringMapChangesBuilder changes) { }
+		@Override public String getCommitMessage() { return null; }
 		@Override public int importance() { return 0; }
 		@Override public AbstractQuestAnswerFragment createForm() { return null; }
 		@Override public String getIconName() { return null; }
 	}
 
-	private OsmQuest createQuest(StringMapChanges changes)
+	private static OsmQuest createAnsweredQuestWithAppliableChange()
 	{
-		return new OsmQuest(3L, new TestQuestType(), Element.Type.NODE, 5,
-				QuestStatus.ANSWERED, changes, "commit message", null, null);
+		StringMapEntryChange aPossibleChange = new StringMapEntryAdd("somekey","value");
+		StringMapChanges changes = new StringMapChanges(Collections.singletonList(aPossibleChange));
+		return createAnsweredQuest(changes);
 	}
 
-	private Element createElement()
+	private static OsmQuest createAnsweredQuestWithNonAppliableChange()
 	{
-		return new OsmNode(5, 0, new OsmLatLon(1,2), new HashMap<String,String>(), null);
+		StringMapEntryChange nonPossibleChange = new StringMapEntryDelete("somekey","value");
+		StringMapChanges changes = new StringMapChanges(Collections.singletonList(nonPossibleChange));
+		return createAnsweredQuest(changes);
+	}
+
+	private static OsmQuest createAnsweredQuest(StringMapChanges changes)
+	{
+		return new OsmQuest(3L, new TestQuestType(), Element.Type.NODE, A_NODE_ID,
+				QuestStatus.ANSWERED, changes, null, null);
+	}
+
+	private static Element createElement()
+	{
+		return new OsmNode(A_NODE_ID, 0, new OsmLatLon(1,2), new HashMap<String,String>());
 	}
 }
