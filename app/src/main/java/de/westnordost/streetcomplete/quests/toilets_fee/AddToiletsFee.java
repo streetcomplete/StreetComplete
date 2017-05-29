45f097c4f6320c1251ebb913af4641b96c03dd82
Binnette <binnette@gmail.com>
2017-05-29 23:52:48 +0200

    Add quest type toilets fee

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/toilets_fee/AddToiletsFee.java
@@ -0,0 +1,51 @@
+package de.westnordost.streetcomplete.quests.toilets_fee;
+
+import android.os.Bundle;
+
+import javax.inject.Inject;
+
+import de.westnordost.streetcomplete.R;
+import de.westnordost.streetcomplete.data.QuestImportance;
+import de.westnordost.streetcomplete.data.osm.SimpleOverpassQuestType;
+import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder;
+import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataDao;
+import de.westnordost.streetcomplete.quests.AbstractQuestAnswerFragment;
+import de.westnordost.streetcomplete.quests.YesNoQuestAnswerFragment;
+
+public class AddToiletsFee extends SimpleOverpassQuestType
+{
+	@Inject public AddToiletsFee(OverpassMapDataDao overpassServer)
+	{
+		super(overpassServer);
+	}
+
+	@Override
+	protected String getTagFilters()
+	{
+		return "nodes with amenity=toilets and !fee";
+	}
+
+	@Override
+	public int importance()
+	{
+		return QuestImportance.MINOR;
+	}
+
+	public AbstractQuestAnswerFragment createForm()
+	{
+		return new ToiletsFeeForm();
+	}
+
+	public void applyAnswerTo(Bundle answer, StringMapChangesBuilder changes)
+	{
+		String yesno = answer.getBoolean(YesNoQuestAnswerFragment.ANSWER) ? "yes" : "no";
+		changes.add("fee", yesno);
+	}
+
+	@Override public String getCommitMessage()
+	{
+		return "Add toilets fee";
+	}
+
+	@Override public String getIconName() {	return "toilets"; }
+}
