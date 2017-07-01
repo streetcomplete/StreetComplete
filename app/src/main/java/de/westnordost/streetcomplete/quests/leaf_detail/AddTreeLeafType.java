matkoniecz/Zazolc

quest to add leaf_type to trees
images are from
https://commons.wikimedia.org/wiki/File:Ontluikend_blad_van_Meidoorn_(Crataegus)._Locatie,_Natuurterrein_De_Famberhorst_01.jpg
https://commons.wikimedia.org/wiki/File:Conif%C3%A8re_Dordogne.jpg

app/src/main/java/de/westnordost/streetcomplete/quests/leaf_detail/AddTreeLeafType.java
@@ -1,0 +1,54 @@
+package de.westnordost.streetcomplete.quests.leaf_detail;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import de.westnordost.streetcomplete.data.QuestImportance;
+import de.westnordost.streetcomplete.data.osm.SimpleOverpassQuestType;
+import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder;
+import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataDao;
+import de.westnordost.streetcomplete.quests.AbstractQuestAnswerFragment;
+
+public class AddTreeLeafType extends SimpleOverpassQuestType
+{
+	@Inject public AddTreeLeafType(OverpassMapDataDao overpassServer)
+	{
+		super(overpassServer);
+	}
+
+	@Override
+	protected String getTagFilters()
+	{
+		return "nodes with natural=tree and !leaf_type";
+	}
+
+	@Override
+	public int importance()
+	{
+		return QuestImportance.INSIGNIFICANT;
+	}
+
+	public AbstractQuestAnswerFragment createForm()
+	{
+        return new AddTreeLeafTypeForm();
+	}
+
+	public void applyAnswerTo(Bundle answer, StringMapChangesBuilder changes)
+	{
+		ArrayList<String> values = answer.getStringArrayList(AddTreeLeafTypeForm.OSM_VALUES);
+		if(values != null  && values.size() == 1)
+		{
+			changes.add("leaf_type", values.get(0));
+		}
+	}
+
+	@Override public String getCommitMessage()
+	{
+		return "Add leaf_type";
+	}
+
+	@Override public String getIconName() {	return "quest"; }
+}

streetcomplete/StreetComplete

app/src/main/java/de/westnordost/streetcomplete/quests/leaf_detail/AddTreeLeafType.java
@@ -1,0 +1,54 @@
+package de.westnordost.streetcomplete.quests.leaf_detail;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import de.westnordost.streetcomplete.data.QuestImportance;
+import de.westnordost.streetcomplete.data.osm.SimpleOverpassQuestType;
+import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder;
+import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataDao;
+import de.westnordost.streetcomplete.quests.AbstractQuestAnswerFragment;
+
+public class AddTreeLeafType extends SimpleOverpassQuestType
+{
+	@Inject public AddTreeLeafType(OverpassMapDataDao overpassServer)
+	{
+		super(overpassServer);
+	}
+
+	@Override
+	protected String getTagFilters()
+	{
+		return "nodes with natural=tree and !leaf_type";
+	}
+
+	@Override
+	public int importance()
+	{
+		return QuestImportance.INSIGNIFICANT;
+	}
+
+	public AbstractQuestAnswerFragment createForm()
+	{
+        return new AddTreeLeafTypeForm();
+	}
+
+	public void applyAnswerTo(Bundle answer, StringMapChangesBuilder changes)
+	{
+		ArrayList<String> values = answer.getStringArrayList(AddTreeLeafTypeForm.OSM_VALUES);
+		if(values != null  && values.size() == 1)
+		{
+			changes.add("leaf_type", values.get(0));
+		}
+	}
+
+	@Override public String getCommitMessage()
+	{
+		return "Add leaf_type";
+	}
+
+	@Override public String getIconName() {	return "leaf"; }
+}