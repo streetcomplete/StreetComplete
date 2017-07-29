59a8d3340d33ca807e74615ff0270f8905baab46
Krzysztof Krason <Krzysztof.Krason@sabre.com>
2017-07-29 12:31:30 +0200

    #84 added missing files

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/way_lit/AddWayLit.java
@@ -0,0 +1,66 @@
+package de.westnordost.streetcomplete.quests.way_lit;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+
+import java.util.Arrays;
+
+import javax.inject.Inject;
+
+import de.westnordost.streetcomplete.data.osm.SimpleOverpassQuestType;
+import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder;
+import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataDao;
+import de.westnordost.streetcomplete.quests.AbstractQuestAnswerFragment;
+import de.westnordost.streetcomplete.quests.YesNoQuestAnswerFragment;
+
+public class AddWayLit extends SimpleOverpassQuestType
+{
+	static final String[] ROADS_WITH_LIGHT = {
+			"primary", "secondary", "tertiary", "unclassified", "residential", "living_street",
+			"service"
+	};
+
+	static final String[] WAYS_WITH_LIGHT = {
+			"pedestrian", "footway", "track", "road", "cycleway", "path"
+	};
+
+	@Inject public AddWayLit(OverpassMapDataDao overpassServer)
+	{
+		super(overpassServer);
+	}
+
+	@Override
+	protected String getTagFilters()
+	{
+		return "ways with highway ~ " + TextUtils.join("|", ROADS_WITH_LIGHT)
+				+ "|" + TextUtils.join("|", WAYS_WITH_LIGHT) + " and !lit";
+	}
+
+	public AbstractQuestAnswerFragment createForm()
+	{
+		return new WayLightFragment();
+	}
+
+	public void applyAnswerTo(Bundle answer, StringMapChangesBuilder changes)
+	{
+		String other = answer.getString(WayLightFragment.OTHER_ANSWER);
+		if (other != null)
+		{
+			changes.add("lit", other);
+		}
+		else
+		{
+			changes.add("lit", answer.getBoolean(YesNoQuestAnswerFragment.ANSWER) ? "yes" : "no");
+		}
+	}
+
+	@Override public String getCommitMessage()
+	{
+		return "Add way lit";
+	}
+
+	@Override public String getIconName()
+	{
+		return "lantern";
+	}
+}
