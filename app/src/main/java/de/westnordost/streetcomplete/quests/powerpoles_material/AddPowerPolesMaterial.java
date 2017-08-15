e8a40eb42663ed32bb885b537e6eaf92e31c6270
L.Lemke <ent8r@users.noreply.github.com>
2017-08-15 21:21:22 +0200

    Add new quest to determine the material of a power pole

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/powerpoles_material/AddPowerPolesMaterial.java
@@ -0,0 +1,49 @@
+package de.westnordost.streetcomplete.quests.powerpoles_material;
+
+import android.os.Bundle;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import de.westnordost.streetcomplete.data.osm.SimpleOverpassQuestType;
+import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder;
+import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataDao;
+import de.westnordost.streetcomplete.quests.AbstractQuestAnswerFragment;
+
+public class AddPowerPolesMaterial extends SimpleOverpassQuestType
+{
+	@Inject public AddPowerPolesMaterial(OverpassMapDataDao overpassServer)
+	{
+		super(overpassServer);
+	}
+
+	@Override
+	protected String getTagFilters()
+	{
+		return "nodes, ways with power=poles and !material";
+	}
+
+	public AbstractQuestAnswerFragment createForm()
+	{
+		return new AddPowerPolesMaterialForm();
+	}
+
+	public void applyAnswerTo(Bundle answer, StringMapChangesBuilder changes)
+	{
+		List<String> values = answer.getStringArrayList(AddPowerPolesMaterialForm.OSM_VALUES);
+		if(values != null  && values.size() == 1)
+		{
+			changes.add("material", values.get(0));
+		}
+	}
+
+	@Override public String getCommitMessage()
+	{
+		return "Add powerpoles material type";
+	}
+
+	@Override public String getIconName() {
+		return "pedestrian_crossing";
+	}
+}
---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/powerpoles_material/AddPowerPolesMaterialForm.java
@@ -0,0 +1,36 @@
+package de.westnordost.streetcomplete.quests.powerpoles_material;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.westnordost.streetcomplete.R;
+import de.westnordost.streetcomplete.quests.ImageListQuestAnswerFragment;
+
+public class AddPowerPolesMaterialForm extends ImageListQuestAnswerFragment
+{
+	private final OsmItem[] TYPES = new OsmItem[] {
+			new OsmItem("wood", R.drawable.power_pole_wood, R.string.quest_powerPolesMaterial_wood),
+			new OsmItem("metal", R.drawable.power_pole_metal, R.string.quest_powerPolesMaterial_metal),
+			new OsmItem("concrete", R.drawable.power_pole_concrete, R.string.quest_powerPolesMaterial_concrete)
+	};
+
+	@Override protected OsmItem[] getItems()
+	{
+		return TYPES;
+	}
+
+	@Override protected int getItemsPerRow()
+	{
+		return 3;
+	}
+
+	@Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+									   Bundle savedInstanceState)
+	{
+		View view = super.onCreateView(inflater, container, savedInstanceState);
+		setTitle(R.string.quest_powerPolesMaterial_title);
+		return view;
+	}
+}
