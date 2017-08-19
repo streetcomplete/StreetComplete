0c1bc6096d065d4b44e06a3a6a8a77a235691ee6
L.Lemke <ent8r@users.noreply.github.com>
2017-08-19 11:09:01 +0200

    Add new quest to determine the availability of a toilet

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/toilet_availability/AddToiletAvailability.java
@@ -0,0 +1,36 @@
+package de.westnordost.streetcomplete.quests.toilet_availability;
+
+import android.os.Bundle;
+
+import javax.inject.Inject;
+
+import de.westnordost.streetcomplete.R;
+import de.westnordost.streetcomplete.data.osm.SimpleOverpassQuestType;
+import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder;
+import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataDao;
+import de.westnordost.streetcomplete.quests.AbstractQuestAnswerFragment;
+import de.westnordost.streetcomplete.quests.YesNoQuestAnswerFragment;
+
+public class AddToiletAvailability extends SimpleOverpassQuestType
+{
+	@Inject public AddToiletAvailability(OverpassMapDataDao overpassServer) { super(overpassServer); }
+
+	@Override protected String getTagFilters()
+	{
+		return "nodes, ways with (shop ~ mall|department_store) and !toilet";
+	}
+
+	public AbstractQuestAnswerFragment createForm()
+	{
+		return new AddToiletAvailabilityForm();
+	}
+
+	public void applyAnswerTo(Bundle answer, StringMapChangesBuilder changes)
+	{
+		String yesno = answer.getBoolean(YesNoQuestAnswerFragment.ANSWER) ? "yes" : "no";
+		changes.add("toilets", yesno);
+	}
+
+	@Override public String getCommitMessage() { return "Add toilet availability"; }
+	@Override public int getIcon() { return R.drawable.ic_quest_toilets; }
+}
---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/toilet_availability/AddToiletAvailabilityForm.java
@@ -0,0 +1,33 @@
+package de.westnordost.streetcomplete.quests.toilet_availability;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.westnordost.streetcomplete.R;
+import de.westnordost.streetcomplete.quests.YesNoQuestAnswerFragment;
+
+public class AddToiletAvailabilityForm extends YesNoQuestAnswerFragment
+{
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View view = super.onCreateView(inflater, container, savedInstanceState);
+		setTitle();
+		return view;
+	}
+
+	private void setTitle()
+	{
+		String name = getElementName();
+		if(name != null)
+		{
+			setTitle(R.string.quest_toiletAvailability_name_title, name);
+		}
+		else
+		{
+			setTitle(R.string.quest_toiletAvailability_title);
+		}
+	}
+}
