13ecb0c2b36acce0619b7851974885d9033234df
Tobias Zwick <newton@openclonk.org>
2017-08-23 22:04:19 +0200

    fix null pointer exception

--- app/src/main/java/de/westnordost/streetcomplete/tangram/QuestsMapFragment.java
+++ app/src/main/java/de/westnordost/streetcomplete/tangram/QuestsMapFragment.java
@@ -133,7 +133,7 @@ public class QuestsMapFragment extends MapFragment implements TouchInput.TapResp
 		if(labelPickResult == null
 				|| labelPickResult.getType() != LabelPickResult.LabelType.ICON
 				|| labelPickResult.getProperties() == null
-				|| !labelPickResult.getProperties().containsKey(MARKER_QUEST_ID))
+				|| labelPickResult.getProperties().get(MARKER_QUEST_ID) == null)
 		{
 			onClickedMap(positionX, positionY);
 			return;
