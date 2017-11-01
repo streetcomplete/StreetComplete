43e08e1c02f663ec3344a77b7c77600c3d2acdd7
Tobias Zwick <newton@openclonk.org>
2017-11-01 01:42:58 +0100

    rename a layout file

--- app/src/main/java/de/westnordost/streetcomplete/quests/AttachPhotoFragment.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/AttachPhotoFragment.java
@@ -47,7 +47,7 @@ public class AttachPhotoFragment extends Fragment
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState)
 	{
-		View view = inflater.inflate(R.layout.attach_photo_fragment, container, false);
+		View view = inflater.inflate(R.layout.fragment_attach_photo, container, false);
 
 		Button takePhoto = view.findViewById(R.id.buttonTakeImage);
 		takePhoto.setOnClickListener(new View.OnClickListener()
