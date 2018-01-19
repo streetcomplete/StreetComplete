e88b22f9023d0a8db3f0bc7bfd3c030a991ba61a
Mateusz Konieczny <matkoniecz@gmail.com>
2018-01-19 17:35:06 +0100

    protects agains crash if MAX_DISPLAYED_ITEMS is larger than number of items in list
    does not happen in unmodified StreetComplete but this change makes easier to avoid crashes if somebody modifies program
    
    # Conflicts:
    #	app/src/main/java/de/westnordost/streetcomplete/quests/sport/AddSportForm.java

--- app/src/main/java/de/westnordost/streetcomplete/quests/sport/AddSportForm.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/sport/AddSportForm.java
@@ -95,7 +95,7 @@ public class AddSportForm extends ImageListQuestAnswerFragment
 			}
 		}
 		// only first 24 items (6 rows)
-		return sportsList.subList(0,MAX_DISPLAYED_ITEMS).toArray(new OsmItem[MAX_DISPLAYED_ITEMS]);
+		return sportsList.subList(0,Math.min(sportsList.size(), MAX_DISPLAYED_ITEMS)).toArray(new OsmItem[MAX_DISPLAYED_ITEMS]);
 	}
 
 	@Override protected void onClickOk()
