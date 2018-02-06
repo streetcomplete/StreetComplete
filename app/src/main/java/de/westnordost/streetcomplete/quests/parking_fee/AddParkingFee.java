4d0528dc9406e7927f6f70132bf24fa452db2294
ENT8R <info.ent8r@gmail.com>
2018-02-06 21:02:54 +0100

    Use new parking fee icon

--- app/src/main/java/de/westnordost/streetcomplete/quests/parking_fee/AddParkingFee.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/parking_fee/AddParkingFee.java
@@ -33,6 +33,6 @@ public class AddParkingFee extends SimpleOverpassQuestType
 	}
 
 	@Override public String getCommitMessage() { return "Add whether there is a parking fee"; }
-	@Override public int getIcon() { return R.drawable.ic_quest_parking; }
+	@Override public int getIcon() { return R.drawable.ic_quest_parking_fee; }
 	@Override public int getTitle(@NonNull Map<String, String> tags) { return R.string.quest_parking_fee_title; }
 }
