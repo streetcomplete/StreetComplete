f99f01a5df68b7daad5b75dd58476ee9e237f688
Swampeyfrog <southernswampfrog@gmail.com>
2018-02-07 14:49:13 +0100

    New Quest: bridge structure (#709)
    * Added the essential files, finished AddBridgeStructureForm
    
    * Finished the new quest; current priority is 7; filter tags are "ways with bridge and !structure", may need revision.
    
    * Finished the new quest, current priority is 7
    
    * Addressed the issues of the first pull request; new images; less string-values; changed the form
    
    * * fix wrong tagging! (I require you to test your PRs :-( )
    
    * added better (properly scaled) images and in correct format
    * proper attribution for images
    * only show for man_made=bridge
    * fix indents
    * add certain man_made structures to area-detection
    
    * reenable footway-type-bridges again, leave it to the user
    
    * fix test

--- app/src/main/java/de/westnordost/streetcomplete/quests/QuestModule.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/QuestModule.java
@@ -14,6 +14,7 @@ import de.westnordost.streetcomplete.quests.baby_changing_table.AddBabyChangingT
 import de.westnordost.streetcomplete.quests.bike_parking_capacity.AddBikeParkingCapacity;
 import de.westnordost.streetcomplete.quests.bike_parking_cover.AddBikeParkingCover;
 import de.westnordost.streetcomplete.quests.bikeway.AddCycleway;
+import de.westnordost.streetcomplete.quests.bridge_structure.AddBridgeStructure;
 import de.westnordost.streetcomplete.quests.building_levels.AddBuildingLevels;
 import de.westnordost.streetcomplete.quests.bus_stop_shelter.AddBusStopShelter;
 import de.westnordost.streetcomplete.quests.car_wash_type.AddCarWashType;
@@ -97,6 +98,7 @@ public class QuestModule
 				new AddWayLit(o),
 				new AddWheelchairAccessBusiness(o),
 				new AddToiletAvailability(o),
+				new AddBridgeStructure(o),
 				new AddWheelChairAccessToilets(o),
 				new AddReligionToWaysideShrine(o),
 
