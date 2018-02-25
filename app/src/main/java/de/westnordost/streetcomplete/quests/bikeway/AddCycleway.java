16b39e04dacace328cfbc438dc60278b6f32f808
Tobias Zwick <newton@openclonk.org>
2018-02-25 19:39:54 +0100

    Cycleway quest: Don't show for private streets (#915)

--- app/src/main/java/de/westnordost/streetcomplete/quests/bikeway/AddCycleway.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/bikeway/AddCycleway.java
@@ -184,6 +184,7 @@ public class AddCycleway implements OsmElementQuestType
 			   "[surface !~ \"^("+ TextUtils.join("|", OsmTaggings.ANYTHING_UNPAVED)+")$\"]" +
 			   // not any explicitly tagged as no bicycles
 			   "[bicycle != no]" +
+			   "[access !~ \"^private|no$\"]" +
 			   " -> .streets;" +
 			"(" +
 			   "way[highway=cycleway](around.streets: "+d+");" +
