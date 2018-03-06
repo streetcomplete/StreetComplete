c566415ee3de719fc94e329d7cf6d45955c111c3
Jordane Pelloux-Prayer <jordane@damsy.net>
2018-03-06 10:00:15 +0100

    limit the quest area to the known countries

--- app/src/main/java/de/westnordost/streetcomplete/quests/collection_times/AddCollectionTimes.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/collection_times/AddCollectionTimes.java
@@ -3,7 +3,6 @@ package de.westnordost.streetcomplete.quests.collection_times;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
-import android.util.Log;
 
 import java.util.Map;
 
@@ -12,9 +11,9 @@ import javax.inject.Inject;
 import de.westnordost.streetcomplete.R;
 import de.westnordost.streetcomplete.data.osm.Countries;
 import de.westnordost.streetcomplete.data.osm.SimpleOverpassQuestType;
+import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder;
 import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataDao;
 import de.westnordost.streetcomplete.quests.AbstractQuestAnswerFragment;
-import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder;
 
 public class AddCollectionTimes extends SimpleOverpassQuestType
 {
@@ -45,8 +44,17 @@ public class AddCollectionTimes extends SimpleOverpassQuestType
 
 	@NonNull @Override public Countries getEnabledForCountries()
 	{
-		return Countries.allExcept(new String[]{
-			""
+		return Countries.noneExcept(new String[]{
+			"DE", // Germany
+			"GB", // UK
+			"DK", // Denmark
+			"AT", // Austria
+			"CH", // Switzerland
+			"FR", // France
+			"SE", // Sweden
+			"PL", // Poland
+			"NL", // Netherlands
+			"US"  // United States of America
 		});
 	}
 
