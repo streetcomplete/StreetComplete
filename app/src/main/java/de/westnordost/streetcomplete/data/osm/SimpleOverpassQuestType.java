7ff5c4a17fca6f53246021520f2dce36aaa1d44b
ENT8R <info.ent8r@gmail.com>
2018-03-08 20:07:39 +0100

    Remove unnecessary import statement

--- app/src/main/java/de/westnordost/streetcomplete/data/osm/SimpleOverpassQuestType.java
+++ app/src/main/java/de/westnordost/streetcomplete/data/osm/SimpleOverpassQuestType.java
@@ -2,7 +2,6 @@ package de.westnordost.streetcomplete.data.osm;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import java.util.Collections;
 
