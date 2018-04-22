0c7fd9c0dde1644ff7d65e9e659663bee884a553
Tobias Zwick <newton@openclonk.org>
2018-04-22 19:45:46 +0200

    lut.im service has been discontinued. Switching to framapic.org :-/

--- app/src/main/java/de/westnordost/streetcomplete/ApplicationConstants.java
+++ app/src/main/java/de/westnordost/streetcomplete/ApplicationConstants.java
@@ -28,7 +28,7 @@ public class ApplicationConstants
 	 *  before the time expired */
 	public static final int REFRESH_QUESTS_AFTER = 7*24*60*60*1000; // one week in ms
 
-	public static final String LUTIM_INSTANCE = "https://lut.im/"; // must have trailing /
+	public static final String LUTIM_INSTANCE = "https://framapic.org/"; // must have trailing /
 	public static final Integer LUTIM_DELETE_AFTER_DAYS = 365 / 2; // half a year
 
 	public static final int ATTACH_PHOTO_QUALITY = 60;
