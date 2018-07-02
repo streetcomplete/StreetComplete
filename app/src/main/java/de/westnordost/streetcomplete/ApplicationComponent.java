6d6df972f88cb4159e5dd11831a30718824800fe
Tobias Zwick <newton@openclonk.org>
2018-07-02 20:43:47 +0200

    fix NPE when saving/loading state of bus stop name form

--- app/src/main/java/de/westnordost/streetcomplete/ApplicationComponent.java
+++ app/src/main/java/de/westnordost/streetcomplete/ApplicationComponent.java
@@ -12,6 +12,7 @@ import de.westnordost.streetcomplete.oauth.OAuthModule;
 import de.westnordost.streetcomplete.oauth.OsmOAuthDialogFragment;
 import de.westnordost.streetcomplete.quests.AbstractQuestAnswerFragment;
 import de.westnordost.streetcomplete.quests.QuestModule;
+import de.westnordost.streetcomplete.quests.localized_name.AddLocalizedNameForm;
 import de.westnordost.streetcomplete.quests.oneway.AddOnewayForm;
 import de.westnordost.streetcomplete.quests.opening_hours.AddOpeningHoursForm;
 import de.westnordost.streetcomplete.quests.localized_name.AddRoadNameForm;
@@ -48,6 +49,7 @@ public interface ApplicationComponent
 	void inject(UnsyncedChangesCounter unsyncedChangesCounter);
 
 	void inject(AddOpeningHoursForm addOpeningHoursForm);
+	void inject(AddLocalizedNameForm addLocalizedNameForm);
 	void inject(AddRoadNameForm addRoadNameForm);
 	void inject(AddParkingFeeForm parkingFeeForm);
 	void inject(AddOnewayForm addOnewayForm);
