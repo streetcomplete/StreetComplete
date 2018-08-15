33afc1e52f867429ed0579f08a7849694f78be13
Tobias Zwick <newton@openclonk.org>
2018-08-15 18:51:09 +0200

    redesign bottom sheet to emphasize the question-answer-style of this app

--- app/src/main/java/de/westnordost/streetcomplete/quests/AbstractBottomSheetFragment.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/AbstractBottomSheetFragment.java
@@ -17,49 +17,16 @@ import de.westnordost.streetcomplete.view.dialogs.AlertDialogBuilder;
 
 public abstract class AbstractBottomSheetFragment extends Fragment
 {
-	private LinearLayout bottomSheet;
-	private View buttonClose;
-
 	@Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
 	{
 		super.onViewCreated(view, savedInstanceState);
 
-		bottomSheet = view.findViewById(R.id.bottomSheet);
-		bottomSheet.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) ->
-		{
-			// not immediately because this is called during layout change (view.getTop() == 0)
-			final Handler handler = new Handler();
-			handler.post(this::updateCloseButtonVisibility);
-		});
-
-		buttonClose = view.findViewById(R.id.close_btn);
-		buttonClose.setOnClickListener(v -> getActivity().onBackPressed());
-
-		BottomSheetBehavior.from(bottomSheet).setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback()
-		{
-			@Override public void onStateChanged(@NonNull View bottomSheet, int newState) { }
-
-			@Override public void onSlide(@NonNull View bottomSheet, float slideOffset)
-			{
-				updateCloseButtonVisibility();
-			}
-		});
+		LinearLayout bottomSheet = view.findViewById(R.id.bottomSheet);
 
 		if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
 		{
 			BottomSheetBehavior.from(bottomSheet).setState(BottomSheetBehavior.STATE_EXPANDED);
 		}
-	}
-
-	private void updateCloseButtonVisibility()
-	{
-		// this is called asynchronously. It may happen that the activity is already gone when this
-		// method is finally called
-		if(getActivity() == null) return;
-
-		int toolbarHeight = getActivity().findViewById(R.id.toolbar).getHeight();
-		boolean coversToolbar = bottomSheet.getTop() < toolbarHeight;
-		buttonClose.setVisibility(coversToolbar ? View.VISIBLE : View.GONE);
 	}
 
 	/** Request to close the form through user interaction (back button, clicked other quest,..),
