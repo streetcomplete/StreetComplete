a0d42c964ff8d43ffb3a93c0c84bd87c0c4dc14e
Tobias Zwick <newton@openclonk.org>
2018-11-22 20:58:32 +0100

    add some places to ask for wheelchair access

--- app/src/main/java/de/westnordost/streetcomplete/quests/wheelchair_access/AddWheelchairAccessBusiness.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/wheelchair_access/AddWheelchairAccessBusiness.java
@@ -26,26 +26,27 @@ public class AddWheelchairAccessBusiness extends SimpleOverpassQuestType
 		String[] leisures = {
 				"golf_course", "water_park", "miniature_golf", "dance",
 				"bowling_alley", "horse_riding", "sports_centre", "fitness_centre",
-				"amusement_arcade", "adult_gaming_centre", "tanning_salon" };
+				"amusement_arcade", "adult_gaming_centre", "tanning_salon"
+		};
 
 		String[] amenities = {
-				"restaurant", "cafe", "ice_cream", "fast_food",
-				"bar", "pub", "biergarten", "food_court",
-				"cinema", "library", "arts_centre", "nightclub",
-				"bank", "bureau_de_change",	"money_transfer", "post_office",
-				"courthouse", "embassy", "car_wash", "car_rental",
-				"marketplace", "fuel", "driving_school",
+				"restaurant", "cafe", "ice_cream", "fast_food", "bar", "pub", "biergarten", "food_court", "nightclub",
+				"cinema", "library", "theatre", "arts_centre", "casino", "conference_centre",
+				"bank", "bureau_de_change", "money_transfer", "post_office", "internet_cafe", "marketplace",
+				"police", "ranger_station", "courthouse", "embassy", "townhall", "community_centre", "youth_centre",
+				"car_wash", "car_rental", "fuel", "driving_school",
 				"doctors", "clinic", "pharmacy", "veterinary", "dentist",
-				"place_of_worship", "townhall", "theatre", "casino", "internet_cafe"};
+				"place_of_worship", "monastery",
+		};
 
 		String[] tourism = {
-				"zoo", "aquarium", "theme_park", "gallery",
-				"museum", "hotel", "guest_house", "hostel",
-				"motel", "viewpoint"
+				"zoo", "aquarium", "theme_park", "gallery", "attraction", "viewpoint",
+				"museum", "hotel", "guest_house", "hostel", "motel", "apartment", "chalet",
 		};
 
 		String[] offices = {
-			"insurance", "government"
+			"insurance", "government", "lawyer", "estate_agent", "political_party", "travel_agent",
+			"tax_advisor", "therapist",
 		};
 
 		return " nodes, ways, relations with ( shop and shop !~ no|vacant or" +
