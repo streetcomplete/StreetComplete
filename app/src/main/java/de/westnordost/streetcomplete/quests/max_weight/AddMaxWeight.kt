4d97a42d0f0a383fa68a2571ecdd67755999179c
Mateusz Konieczny <matkoniecz@gmail.com>
2019-07-01 18:16:10 +0200

    simplify code

--- app/src/main/java/de/westnordost/streetcomplete/quests/max_weight/AddMaxWeight.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/max_weight/AddMaxWeight.kt
@@ -1,18 +1,13 @@
 package de.westnordost.streetcomplete.quests.max_weight
 
-import de.westnordost.osmapi.map.data.BoundingBox
-import de.westnordost.osmapi.map.data.Element
 import de.westnordost.streetcomplete.R
-import de.westnordost.streetcomplete.data.osm.OsmElementQuestType
+import de.westnordost.streetcomplete.data.osm.SimpleOverpassQuestType
 import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder
-import de.westnordost.streetcomplete.data.osm.download.MapDataWithGeometryHandler
 import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataDao
-import de.westnordost.streetcomplete.data.osm.tql.FiltersParser
-import de.westnordost.streetcomplete.data.osm.tql.OverpassQLUtil
 
-class AddMaxWeight(private val overpassServer: OverpassMapDataDao) : OsmElementQuestType<MaxWeightAnswer> {
+class AddMaxWeight(o: OverpassMapDataDao) : SimpleOverpassQuestType<MaxWeightAnswer>(o) {
 
-    private val wayFilter by lazy { FiltersParser().parse("""
+    override val tagFilters = """
         ways with highway ~ trunk|trunk_link|primary|primary_link|secondary|secondary_link|tertiary|tertiary_link|unclassified|residential|living_street|service
          and !maxweight and maxweight:signed!=no
          and bridge and bridge != no
@@ -20,27 +15,13 @@ class AddMaxWeight(private val overpassServer: OverpassMapDataDao) : OsmElementQ
          and vehicle !~ private|no
          and (access !~ private|no or (foot and foot !~ private|no))
          and area != yes
-    """)}
+    """
 
     override val commitMessage = "Add maximum allowed weight"
     override val icon = R.drawable.ic_quest_max_weight
 
     override fun getTitle(tags: Map<String, String>): Int {
         return R.string.quest_maxweight_title
-    }
-
-    override fun isApplicableTo(element: Element) = wayFilter.matches(element)
-
-    override fun download(bbox: BoundingBox, handler: MapDataWithGeometryHandler): Boolean {
-        return overpassServer.getAndHandleQuota(getOverpassQuery(bbox), handler)
-    }
-
-    private fun getOverpassQuery(bbox: BoundingBox): String? {
-        return OverpassQLUtil.getGlobalOverpassBBox(bbox) +
-                "(" +
-                wayFilter.toOverpassQLString(null) +
-                ");" +
-                OverpassQLUtil.getQuestPrintStatement()
     }
 
     override fun createForm() = AddMaxWeightForm()
