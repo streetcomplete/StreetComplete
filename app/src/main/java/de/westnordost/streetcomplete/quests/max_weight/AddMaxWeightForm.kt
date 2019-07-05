5d96efa160581a1e5bb06cd4c143df1bb46bd341
Mateusz Konieczny <matkoniecz@gmail.com>
2019-07-05 12:44:57 +0200

    make clear that exceptions are worth making note

--- app/src/main/java/de/westnordost/streetcomplete/quests/max_weight/AddMaxWeightForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/max_weight/AddMaxWeightForm.kt
@@ -25,6 +25,7 @@ class AddMaxWeightForm : AbstractQuestFormAnswerFragment<MaxWeightAnswer>() {
 
     override val otherAnswers = listOf(
             OtherAnswer(R.string.quest_maxweight_answer_other_sign) { onUnsupportedSign() },
+            OtherAnswer(R.string.quest_maxweight_answer_sign_with_exceptions) { onUnsupportedSign() },
             OtherAnswer(R.string.quest_maxweight_answer_noSign) { applyAnswer(NoMaxWeightSign("dummy")) }
     )
 
