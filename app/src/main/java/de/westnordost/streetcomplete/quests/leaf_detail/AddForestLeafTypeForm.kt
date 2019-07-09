702ba4d8ec10db37a403d5d3b77d04e08539871a
Mateusz Konieczny <matkoniecz@gmail.com>
2019-07-09 13:02:40 +0200

    add self-made mixed photo

--- app/src/main/java/de/westnordost/streetcomplete/quests/leaf_detail/AddForestLeafTypeForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/leaf_detail/AddForestLeafTypeForm.kt
@@ -9,7 +9,7 @@ class AddForestLeafTypeForm : AImageListQuestAnswerFragment<String, String>() {
     override val items = listOf(
             Item("needleleaved", R.drawable.needleleaved, R.string.quest_leaf_type_needles),
             Item("broadleaved", R.drawable.broadleaved, R.string.quest_leaf_type_broadleaved),
-            Item("mixed", R.drawable.bicycle_parking_type_shed, R.string.quest_leaf_type_mixed)
+            Item("mixed", R.drawable.mixed, R.string.quest_leaf_type_mixed)
     )
 
     override val itemsPerRow = 3
