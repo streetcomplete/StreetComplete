1182d90a95063431723d7edda6f1ea0c916c1a65
Tobias Zwick <newton@westnordost.de>
2019-07-28 22:46:50 +0200

    fix typo
    Co-Authored-By: Mateusz Konieczny <matkoniecz@gmail.com>

--- app/src/main/java/de/westnordost/streetcomplete/tangram/QuestsMapFragment.java
+++ app/src/main/java/de/westnordost/streetcomplete/tangram/QuestsMapFragment.java
@@ -340,7 +340,7 @@ public class QuestsMapFragment extends MapFragment implements TouchInput.TapResp
 
 	private void updateQuestsInRect(Rect tilesRect)
 	{
-		// area to big -> skip ( see https://github.com/tangrams/tangram-es/issues/1492 )
+		// area too big -> skip ( see https://github.com/tangrams/tangram-es/issues/1492 )
 		if(tilesRect.width() * tilesRect.height() > 4)
 		{
 			return;
