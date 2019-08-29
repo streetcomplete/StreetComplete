85ef39d236a806a4a9f15cee250907a89ca2eca0
Mateusz Konieczny <matkoniecz@gmail.com>
2019-08-29 21:53:42 +0200

    ask about footway part of segregated way (#1544)
    * ask about footway part of segregated way
    
    together with #1493 fixes #1489
    
    * use special icon - thanks for making it!

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddFootwayPartSurface.kt
@@ -0,0 +1,24 @@
+package de.westnordost.streetcomplete.quests.surface
+
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.osm.SimpleOverpassQuestType
+import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder
+import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataDao
+
+class AddFootwayPartSurface(o: OverpassMapDataDao) : SimpleOverpassQuestType<String>(o) {
+
+	override val tagFilters = """
+        ways with highway ~ path|footway|cycleway|bridleway and surface = paved and foot ~ designated|yes and segregated=yes
+        and !footway:surface and !surface:footway
+    """
+	override val commitMessage = "Add path surfaces"
+	override val icon = R.drawable.ic_quest_footway_surface
+
+	override fun getTitle(tags: Map<String, String>) = R.string.quest_footwayPartSurface_title
+
+	override fun createForm() = AddPathSurfaceForm()
+
+	override fun applyAnswerTo(answer: String, changes: StringMapChangesBuilder) {
+		changes.add("footway:surface", answer)
+	}
+}
