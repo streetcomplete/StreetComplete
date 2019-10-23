c20ebc4ebd1ad188df84c127cace744b47a13022
Pirmin Braun <pirminbraun16@gmail.com>
2019-10-23 18:52:04 +0200

    Exclude handrail:*=* tags in handrail quest too
    Assume survey was valid even if main handrail=* tag is missing
    Cut down on comments, too

--- app/src/main/java/de/westnordost/streetcomplete/quests/handrail/AddHandrail.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/handrail/AddHandrail.kt
@@ -18,24 +18,24 @@ import de.westnordost.streetcomplete.quests.YesNoQuestAnswerFragment
  */
 class AddHandrail(overpassServer: OverpassMapDataDao) : SimpleOverpassQuestType<Boolean>(overpassServer) {
     // Do not include nodes and relations, even though these exist with the right tags, because
-    // according to the wiki page for `highway=steps` [2] it can only be applied to ways. It also
+    // according to the wiki page for `highway=steps` [1] it can only be applied to ways. It also
     // does not make much sense for other types of elements and it is more likely to be a tagging
     // mistake.
-    // Do not exclude elements tagged with `handrail:left`, `handrail:center` or `handrail:right`
-    // because according to the wiki page for `handrail=*` [1], the `handrail` tag should
-    // always have just `yes` or
-    // `no` as a value to indicate the existence of a handrail somewhere. If there is a mismatch of
-    // the tags afterwards, that can be recognized using one of the various
-    // Quality Assurance tools [3].
-    // [1] https://wiki.openstreetmap.org/wiki/Key:handrail
-    // [2] https://wiki.openstreetmap.org/wiki/Tag:highway%3Dsteps
+    // Exclude elements tagged with `handrail:left`, `handrail:center` or `handrail:right`
+    // even though according to the wiki page for `handrail=*` [1], the `handrail` tag should
+    // always have just `yes` or `no` as a value to indicate the existence of a handrail somewhere.
+    // If one of these tags is present but without a `handrail` tag, we still assume the survey data
+    // itself was valid, so we do not need to use survey time to add the implied `handrail` tag.
+    // If there is such a case, mappers can fix this from remote locations, maybe with the help of
+    // one of the various Quality Assurance tools [3].
+    // [1] https://wiki.openstreetmap.org/wiki/Tag:highway%3Dsteps
+    // [2] https://wiki.openstreetmap.org/wiki/Key:handrail
     // [3] https://wiki.openstreetmap.org/wiki/Quality_assurance
-    override val tagFilters = "nodes with highway = steps and !handrail"
+    override val tagFilters = """ways with highway = steps
+        and !handrail and !handrail:left and !handrail:center and !handrail:right"""
 
-    // changeset comment to be used for uploading changes made through this quest
     override val commitMessage = "Add whether steps have a handrail"
 
-    // icon for displaying quest on the map
     override val icon = R.drawable.ic_quest_handrail
 
     override fun getTitle(tags: Map<String, String>) = R.string.quest_handrail_title
@@ -43,7 +43,7 @@ class AddHandrail(overpassServer: OverpassMapDataDao) : SimpleOverpassQuestType<
     // use simple yes/no user interface without additional pictures for prompting user for answer
     override fun createForm() = YesNoQuestAnswerFragment()
 
-    override fun applyAnswerTo(answerHandrailExists: Boolean, changes: StringMapChangesBuilder) {
-        changes.add("handrail", if (answerHandrailExists) "yes" else "no")
+    override fun applyAnswerTo(answer: Boolean, changes: StringMapChangesBuilder) {
+        changes.add("handrail", if (answer) "yes" else "no")
     }
 }
