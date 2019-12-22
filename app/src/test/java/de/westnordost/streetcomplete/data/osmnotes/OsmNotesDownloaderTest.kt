56fdc3e9dd7639d0bf8c55f2bc4ea7cfc74fb3b9
Tobias Zwick <osm@westnordost.de>
2019-12-22 16:21:22 +0100

    fix test

--- app/src/test/java/de/westnordost/streetcomplete/data/osmnotes/OsmNotesDownloaderTest.kt
+++ app/src/test/java/de/westnordost/streetcomplete/data/osmnotes/OsmNotesDownloaderTest.kt
@@ -12,6 +12,7 @@ import de.westnordost.streetcomplete.any
 import de.westnordost.streetcomplete.data.QuestStatus
 import de.westnordost.streetcomplete.mock
 import de.westnordost.streetcomplete.on
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentMatchers.isNull
@@ -27,7 +28,6 @@ class OsmNotesDownloaderTest {
     private lateinit var createNoteDB: CreateNoteDao
     private lateinit var preferences: SharedPreferences
     private lateinit var avatarsDownloader: OsmAvatarsDownloader
-    private lateinit var downloader: OsmNotesDownloader
 
     @Before fun setUp() {
         noteDB = mock()
@@ -36,46 +36,57 @@ class OsmNotesDownloaderTest {
         createNoteDB = mock()
         preferences = mock()
         avatarsDownloader = mock()
-        downloader = OsmNotesDownloader(notesServer, noteDB, noteQuestDB, createNoteDB, preferences, OsmNoteQuestType(), avatarsDownloader)
-        downloader.questListener = mock()
     }
 
     @Test fun `delete obsolete quests`() {
         on(preferences.getBoolean(Prefs.SHOW_NOTES_NOT_PHRASED_AS_QUESTIONS, false)).thenReturn(true)
-        
         // in the quest database mock, there are quests for note 4 and note 5
+
         val note1 = createANote(4L)
         val note2 = createANote(5L)
+
         on(noteQuestDB.getAll(isNull(), any(), isNull())).thenReturn(listOf(
             OsmNoteQuest(12L, note1, QuestStatus.NEW, null, Date(), OsmNoteQuestType(), null),
             OsmNoteQuest(13L, note2, QuestStatus.NEW, null, Date(), OsmNoteQuestType(), null)
         ))
-        
-        // note dao mock will only "find" the note #4
+
         doAnswer { invocation: InvocationOnMock ->
-            val handler = invocation.arguments[1] as Handler<Note>
-            handler.handle(note1)
-        }.on(notesServer).getAll(any(), any(), any(), any())
+            val deletedQuests = invocation.arguments[0] as Collection<Long>
+            assertEquals(1, deletedQuests.size)
+            assertEquals(13L, deletedQuests.iterator().next())
+            1
+        }.on(noteQuestDB).deleteAllIds(any())
 
-        on(noteQuestDB.deleteAllIds(any())).thenReturn(1)
+        // note dao mock will only "find" the note #4
+        val noteServer: NotesDao = TestListBasedNotesDao(listOf(note1))
 
-        downloader.download(BoundingBox(0.0, 0.0, 1.0, 1.0), null, 1000)
+        val dl = OsmNotesDownloader(noteServer, noteDB, noteQuestDB, createNoteDB, preferences, OsmNoteQuestType(), avatarsDownloader)
+        dl.questListener = mock()
+        dl.download(BoundingBox(0.0, 0.0, 1.0, 1.0), null, 1000)
 
-        verify(noteQuestDB).deleteAllIds(listOf(13L))
-        verify(downloader.questListener!!).onQuestsRemoved(any(), any())
+        verify(noteQuestDB).deleteAllIds(any())
+        verify(dl.questListener!!).onQuestsRemoved(any(), any())
     }
+}
 
-    private fun createANote(id: Long): Note {
-        val note = Note()
-        note.id = id
-        note.position = OsmLatLon(6.0, 7.0)
-        note.status = Note.Status.OPEN
-        note.dateCreated = Date()
-        val comment = NoteComment()
-        comment.date = Date()
-        comment.action = NoteComment.Action.OPENED
-        comment.text = "hurp durp"
-        note.comments.add(comment)
-        return note
+private fun createANote(id: Long): Note {
+    val note = Note()
+    note.id = id
+    note.position = OsmLatLon(6.0, 7.0)
+    note.status = Note.Status.OPEN
+    note.dateCreated = Date()
+    val comment = NoteComment()
+    comment.date = Date()
+    comment.action = NoteComment.Action.OPENED
+    comment.text = "hurp durp"
+    note.comments.add(comment)
+    return note
+}
+
+private class TestListBasedNotesDao(val notes: List<Note>) :  NotesDao(null) {
+    override fun getAll(bounds: BoundingBox, handler: Handler<Note>, limit: Int, hideClosedNoteAfter: Int) {
+        for (note in notes) {
+            handler.handle(note)
+        }
     }
 }
