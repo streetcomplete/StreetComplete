571d33cd490662824b2ed79b1c6f98c12a6c6859
ferdinand0101 <ferdi98701@gmail.com>
2020-01-11 15:33:59 +0100

    Update AddVegan.kt
    add vegan tags to ice cream stores

--- app/src/main/java/de/westnordost/streetcomplete/quests/diet_type/AddVegan.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/diet_type/AddVegan.kt
@@ -8,8 +8,11 @@ import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataAndGeometr
 class AddVegan(o: OverpassMapDataAndGeometryDao) : SimpleOverpassQuestType<String>(o) {
 
     override val tagFilters = """
-        nodes, ways with amenity ~ restaurant|cafe|fast_food
-        and name and diet:vegetarian ~ yes|only and !diet:vegan
+        nodes, ways with (amenity ~ restaurant|cafe|fast_food
+        and name and diet:vegetarian ~ yes|only and !diet:vegan)
+        or
+        (amenity ~ ice_cream
+        and name and !diet:vegan)
     """
     override val commitMessage = "Add vegan diet type"
     override val icon = R.drawable.ic_quest_restaurant_vegan
