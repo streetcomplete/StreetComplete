457f4d962577a62781a97e43fbf65e89bc89003f
Tobias Zwick <osm@westnordost.de>
2020-03-03 14:37:41 +0100

    CarWashType: allow selecting self-service + staff cleans car (fixes #1738)

--- app/src/main/java/de/westnordost/streetcomplete/quests/car_wash_type/AddCarWashType.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/car_wash_type/AddCarWashType.kt
@@ -4,8 +4,9 @@ import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.osm.SimpleOverpassQuestType
 import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder
 import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataAndGeometryDao
+import de.westnordost.streetcomplete.quests.car_wash_type.CarWashType.*
 
-class AddCarWashType(o: OverpassMapDataAndGeometryDao) : SimpleOverpassQuestType<CarWashType>(o) {
+class AddCarWashType(o: OverpassMapDataAndGeometryDao) : SimpleOverpassQuestType<List<CarWashType>>(o) {
 
     override val tagFilters = "nodes, ways with amenity = car_wash and !automated and !self_service"
     override val commitMessage = "Add car wash type"
@@ -15,8 +16,16 @@ class AddCarWashType(o: OverpassMapDataAndGeometryDao) : SimpleOverpassQuestType
 
     override fun createForm() = AddCarWashTypeForm()
 
-    override fun applyAnswerTo(answer: CarWashType, changes: StringMapChangesBuilder) {
-        changes.add("automated", if (answer.isAutomated) "yes" else "no")
-        changes.add("self_service", if (answer.isSelfService) "yes" else "no")
+    override fun applyAnswerTo(answer: List<CarWashType>, changes: StringMapChangesBuilder) {
+        val isAutomated = answer.contains(AUTOMATED)
+        changes.add("automated", if (isAutomated) "yes" else "no")
+
+        val hasSelfService = answer.contains(SELF_SERVICE)
+        val selfService = when {
+            hasSelfService && answer.size == 1 -> "only"
+            hasSelfService -> "yes"
+            else -> "no"
+        }
+        changes.add("self_service", selfService)
     }
 }
--- app/src/main/java/de/westnordost/streetcomplete/quests/car_wash_type/AddCarWashTypeForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/car_wash_type/AddCarWashTypeForm.kt
@@ -1,15 +1,11 @@
 package de.westnordost.streetcomplete.quests.car_wash_type
 
-import android.os.Bundle
-import android.view.View
-
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.quests.AImageListQuestAnswerFragment
-import de.westnordost.streetcomplete.view.ImageSelectAdapter
 import de.westnordost.streetcomplete.view.Item
-import de.westnordost.streetcomplete.quests.car_wash_type.AddCarWashTypeForm.CarWashOption.*
+import de.westnordost.streetcomplete.quests.car_wash_type.CarWashType.*
 
-class AddCarWashTypeForm : AImageListQuestAnswerFragment<AddCarWashTypeForm.CarWashOption, CarWashType>() {
+class AddCarWashTypeForm : AImageListQuestAnswerFragment<CarWashType, List<CarWashType>>() {
 
     override val items = listOf(
         Item(AUTOMATED, R.drawable.car_wash_automated, R.string.quest_carWashType_automated),
@@ -18,33 +14,9 @@ class AddCarWashTypeForm : AImageListQuestAnswerFragment<AddCarWashTypeForm.CarW
     )
 
     override val itemsPerRow = 3
-    override val maxSelectableItems = 3
+    override val maxSelectableItems = -1
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        imageSelector.listeners.add(object : ImageSelectAdapter.OnItemSelectionListener {
-            override fun onIndexSelected(index: Int) {
-                val value = imageSelector.items[index].value!!
-                // service is exclusive with everything else
-                if (value == SERVICE) {
-                    imageSelector.deselect(imageSelector.indexOf(AUTOMATED))
-                    imageSelector.deselect(imageSelector.indexOf(SELF_SERVICE))
-                } else {
-                    imageSelector.deselect(imageSelector.indexOf(SERVICE))
-                }
-            }
-
-            override fun onIndexDeselected(index: Int) {}
-        })
+    override fun onClickOk(selectedItems: List<CarWashType>) {
+        applyAnswer(selectedItems)
     }
-
-    override fun onClickOk(selectedItems: List<CarWashOption>) {
-        applyAnswer(CarWashType(
-            isSelfService = selectedItems.contains(SELF_SERVICE),
-            isAutomated = selectedItems.contains(AUTOMATED)
-        ))
-    }
-
-    enum class CarWashOption { AUTOMATED, SELF_SERVICE, SERVICE }
 }
