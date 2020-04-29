76bff8587c7e3bcf10194668f0853cbcd67ced7b
Tobias Zwick <osm@westnordost.de>
2020-04-29 17:35:50 +0200

    correct offset

--- app/src/main/java/de/westnordost/streetcomplete/map/QuestsMapFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/map/QuestsMapFragment.kt
@@ -148,7 +148,7 @@ class QuestsMapFragment : LocationAwareMapFragment() {
 
         val marker = controller?.addMarker() ?: return null
         marker.setStylingFromString(
-            "{ style: 'quest-selection', color: 'white', size: [${w}px, ${h}px], flat: false, collide: false, offset: ['3px', '-78px'] }"
+            "{ style: 'quest-selection', color: 'white', size: [${w}px, ${h}px], flat: false, collide: false, offset: ['0px', '-77px'] }"
         )
         marker.setDrawable(frame)
         return marker
