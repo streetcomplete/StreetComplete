06a24261c5a03107a39de32f23ec7716a4c4ccd9
Tobias Zwick <osm@westnordost.de>
2020-04-29 18:55:31 +0200

    correct selection placement

--- app/src/main/java/de/westnordost/streetcomplete/map/QuestsMapFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/map/QuestsMapFragment.kt
@@ -148,7 +148,7 @@ class QuestsMapFragment : LocationAwareMapFragment() {
 
         val marker = controller?.addMarker() ?: return null
         marker.setStylingFromString(
-            "{ style: 'quest-selection', color: 'white', size: [${w}px, ${h}px], flat: false, collide: false, offset: ['0px', '-78px'] }"
+            "{ style: 'quest-selection', color: 'white', size: [${w}px, ${h}px], flat: false, collide: false, offset: ['1px', '-78px'] }"
         )
         marker.setDrawable(frame)
         return marker
