d10d12965715da85e4238f138f357ed693450986
Tobias Zwick <osm@westnordost.de>
2020-05-13 18:02:06 +0200

    make sure the shine view for achievements always covers the whole screen

--- app/src/main/java/de/westnordost/streetcomplete/user/AchievementInfoFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/user/AchievementInfoFragment.kt
@@ -23,7 +23,6 @@ import de.westnordost.streetcomplete.util.Transforms
 import de.westnordost.streetcomplete.util.animateFrom
 import de.westnordost.streetcomplete.util.animateTo
 import de.westnordost.streetcomplete.util.applyTransforms
-import de.westnordost.streetcomplete.view.CircularOutlineProvider
 import kotlinx.android.synthetic.main.fragment_achievement_info.*
 
 
