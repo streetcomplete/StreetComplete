9cc0603391dc25877e2e4630053091a1c2e5cc8a
Tobias Zwick <newton@westnordost.de>
2020-06-17 15:17:38 +0200

    MapData implements Iterable

--- app/src/main/java/de/westnordost/osmapi/map/MapData.kt
+++ app/src/main/java/de/westnordost/osmapi/map/MapData.kt
@@ -1,18 +1,24 @@
 package de.westnordost.osmapi.map
 
-import de.westnordost.osmapi.map.data.BoundingBox
-import de.westnordost.osmapi.map.data.Node
-import de.westnordost.osmapi.map.data.Relation
-import de.westnordost.osmapi.map.data.Way
+import de.westnordost.osmapi.map.data.*
 import de.westnordost.osmapi.map.handler.MapDataHandler
+import de.westnordost.streetcomplete.util.MultiIterable
 
 data class MapData(
     val nodes: MutableMap<Long, Node> = mutableMapOf(),
     val ways: MutableMap<Long, Way> = mutableMapOf(),
-    val relations: MutableMap<Long, Relation> = mutableMapOf()) : MapDataHandler {
+    val relations: MutableMap<Long, Relation> = mutableMapOf()) : MapDataHandler, Iterable<Element> {
 
     override fun handle(bounds: BoundingBox) {}
     override fun handle(node: Node) { nodes[node.id] = node }
     override fun handle(way: Way) { ways[way.id] = way }
     override fun handle(relation: Relation) { relations[relation.id] = relation }
+
+    override fun iterator(): Iterator<Element> {
+        val elements = MultiIterable<Element>()
+        elements.add(nodes.values)
+        elements.add(ways.values)
+        elements.add(relations.values)
+        return elements.iterator()
+    }
 }
