1db83a8ba452adf7cf73ef1e59c2256823a49a34
Mateusz Konieczny <matkoniecz@gmail.com>
2020-07-16 23:07:59 +0200

    open class -> data class
    Co-authored-by: Tobias Zwick <osm@westnordost.de>

--- app/src/main/java/de/westnordost/streetcomplete/quests/sidewalk/SidewalkAnswer.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/sidewalk/SidewalkAnswer.kt
@@ -1,5 +1,5 @@
 package de.westnordost.streetcomplete.quests.sidewalk
 
 sealed class SidewalkAnswer
-open class SidewalkSides(val left:Boolean, val right:Boolean) : SidewalkAnswer()
+data class SidewalkSides(val left:Boolean, val right:Boolean) : SidewalkAnswer()
 object SeparatelyMapped: SidewalkAnswer()
