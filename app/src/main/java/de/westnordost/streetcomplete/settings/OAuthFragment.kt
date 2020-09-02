9a4e94af5c838c090dc83c7ebcb2b2d39976205b
Tobias Zwick <osm@westnordost.de>
2020-09-02 22:47:28 +0200

    remove unused imports

--- app/src/main/java/de/westnordost/streetcomplete/settings/OAuthFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/settings/OAuthFragment.kt
@@ -4,7 +4,6 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.webkit.URLUtil.isValidUrl
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
