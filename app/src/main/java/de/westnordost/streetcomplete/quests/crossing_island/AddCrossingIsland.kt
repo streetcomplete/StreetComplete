e75f34495e69e9771a44bd3710859cb5eaf3625c
Tobias Zwick <newton@westnordost.de>
2020-10-05 21:38:57 +0200

    fix overpass query and a few other cosmetic things
    "way.crossedWays[highway][oneway != no];" found also ways that did not have a oneway tag.

--- app/src/main/java/de/westnordost/streetcomplete/quests/crossing_island/AddCrossingIsland.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/crossing_island/AddCrossingIsland.kt
@@ -3,12 +3,13 @@ package de.westnordost.streetcomplete.quests.crossing_island
 import de.westnordost.osmapi.map.data.BoundingBox
 import de.westnordost.osmapi.map.data.Element
 import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.elementfilter.getQuestPrintStatement
+import de.westnordost.streetcomplete.data.elementfilter.toGlobalOverpassBBox
 import de.westnordost.streetcomplete.data.osm.elementgeometry.ElementGeometry
 import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder
 import de.westnordost.streetcomplete.data.osm.mapdata.OverpassMapDataAndGeometryApi
 import de.westnordost.streetcomplete.data.osm.osmquest.OsmElementQuestType
-import de.westnordost.streetcomplete.data.tagfilters.getQuestPrintStatement
-import de.westnordost.streetcomplete.data.tagfilters.toGlobalOverpassBBox
+import de.westnordost.streetcomplete.ktx.toYesNo
 import de.westnordost.streetcomplete.quests.YesNoQuestAnswerFragment
 
 class AddCrossingIsland(private val overpass: OverpassMapDataAndGeometryApi)
@@ -18,8 +19,7 @@ class AddCrossingIsland(private val overpass: OverpassMapDataAndGeometryApi)
     override val wikiLink = "Key:crossing:island"
     override val icon = R.drawable.ic_quest_pedestrian_crossing_island
 
-    override fun getTitle(tags: Map<String, String>) =
-        R.string.quest_pedestrian_crossing_island
+    override fun getTitle(tags: Map<String, String>) = R.string.quest_pedestrian_crossing_island
 
     override fun isApplicableTo(element: Element): Boolean? = null
 
@@ -27,32 +27,22 @@ class AddCrossingIsland(private val overpass: OverpassMapDataAndGeometryApi)
         return overpass.query(getOverpassQuery(bbox), handler)
     }
 
-    private fun getOverpassQuery(bbox: BoundingBox): String {
-        val accessFilter = "\"access\"~\"^(private|no)\$\""
-        val onewayFilter = "\"oneway\"!=\"no\""
-        val pathFilter = "\"highway\"~\"^(path|footway|cycleway|pedestrian)\$\""
+    private fun getOverpassQuery(bbox: BoundingBox): String =
+        bbox.toGlobalOverpassBBox() + """
+        node[highway = crossing][crossing][crossing != island][!"crossing:island"] -> .crossings;
+        .crossings < -> .crossedWays;
+        (
+          way.crossedWays[highway][access ~ "^(private|no)$"];
+          way.crossedWays[highway][oneway][oneway != no];
+          way.crossedWays[highway ~ "^(path|footway|cycleway|pedestrian)$"];
+        ) -> .excludedWays;
 
-        return bbox.toGlobalOverpassBBox() + """
-            node
-              ["highway" = "crossing"]
-              ["crossing"]
-              ["crossing" != "island"]
-              [!"crossing:island"] -> .crossings;
-            .crossings < -> .crossedWays;
-
-            (
-              way.crossedWays["highway"][$accessFilter];
-              way.crossedWays["highway"][$onewayFilter];
-              way.crossedWays[$pathFilter];
-            ) -> .excludedWays;
-
-            ( .crossings; - node(w.excludedWays); );
-        """.trimIndent() + "\n" + getQuestPrintStatement()
-    }
+        ( .crossings; - node(w.excludedWays); );
+    """.trimIndent() + "\n" + getQuestPrintStatement()
 
     override fun createForm() = YesNoQuestAnswerFragment()
 
     override fun applyAnswerTo(answer: Boolean, changes: StringMapChangesBuilder) {
-        changes.add("crossing:island", if(answer) "yes" else "no")
+        changes.add("crossing:island", answer.toYesNo())
     }
 }
