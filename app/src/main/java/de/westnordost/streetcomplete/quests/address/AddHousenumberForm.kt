6bbb73fd55435f121707c8a951670fcade0cc4df
Tobias Zwick <newton@westnordost.de>
2020-10-15 14:54:35 +0200

    implement +/- buttons and pre-fill "hints"

--- app/src/main/java/de/westnordost/streetcomplete/quests/address/AddHousenumberForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/address/AddHousenumberForm.kt
@@ -1,5 +1,6 @@
 package de.westnordost.streetcomplete.quests.address
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import android.text.InputType
@@ -11,6 +12,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.core.content.getSystemService
+import androidx.core.view.isInvisible
 
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.quests.AbstractQuestFormAnswerFragment
@@ -34,7 +36,14 @@ class AddHousenumberForm : AbstractQuestFormAnswerFragment<HousenumberAnswer>() 
     private var streetNumberInput: EditText? = null
     private var blockNumberInput: EditText? = null
 
+    private var toggleKeyboardButton: Button? = null
+
+    private var addButton: View? = null
+    private var subtractButton: View? = null
+
     private var isHousename: Boolean = false
+
+    private var houseNumberInputTextColors: ColorStateList? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = super.onCreateView(inflater, container, savedInstanceState)
@@ -52,11 +61,16 @@ class AddHousenumberForm : AbstractQuestFormAnswerFragment<HousenumberAnswer>() 
 
     override fun onClickOk() {
         createAnswer()?.let { answer ->
-            confirmHousenumber(answer.looksInvalid()) {
+            confirmHousenumber(answer.looksInvalid(countryInfo.additionalValidHousenumberRegex)) {
                 applyAnswer(answer)
+                if (answer.isRealHouseNumberAnswer) lastRealHousenumberAnswer = answer
             }
         }
     }
+
+    override fun isFormComplete() = createAnswer() != null
+
+    /* ------------------------------------- Other answers -------------------------------------- */
 
     private fun switchToHouseName() {
         isHousename = true
@@ -91,26 +105,29 @@ class AddHousenumberForm : AbstractQuestFormAnswerFragment<HousenumberAnswer>() 
         }
     }
 
-    // i.e. "95-98" or "5,5a,6" etc. (but not: "1, 3" or "3 - 5" or "5,6-7")
-    private fun getValidHousenumberRegex(): Regex {
-        var regex = VALID_HOUSENUMBER_REGEX
-        val additionalRegex = countryInfo.additionalValidHousenumberRegex
-        if (additionalRegex != null) {
-            regex = "(($regex)|($additionalRegex))"
-        }
-        return "^$regex((-$regex)|(,$regex)+)?".toRegex()
-    }
-
-    override fun isFormComplete() = createAnswer() != null
+    /* -------------------------- Set (different) housenumber layout  --------------------------- */
 
     private fun setLayout(layoutResourceId: Int) {
         val view = setContentView(layoutResourceId)
 
+        toggleKeyboardButton = view.findViewById(R.id.toggleKeyboardButton)
         houseNumberInput = view.findViewById(R.id.houseNumberInput)
         houseNameInput = view.findViewById(R.id.houseNameInput)
         conscriptionNumberInput = view.findViewById(R.id.conscriptionNumberInput)
         streetNumberInput = view.findViewById(R.id.streetNumberInput)
         blockNumberInput = view.findViewById(R.id.blockNumberInput)
+        addButton = view.findViewById(R.id.addButton)
+        subtractButton = view.findViewById(R.id.subtractButton)
+
+        addButton?.setOnClickListener { addToHouseNumberInput(+1) }
+        subtractButton?.setOnClickListener { addToHouseNumberInput(-1) }
+
+        // must be called before registering the text changed watchers because it changes the text
+        prefillBlockNumber()
+
+        initKeyboardButton()
+        // must be after initKeyboardButton because it re-sets the onFocusListener
+        showHouseNumberHint()
 
         val onChanged = TextChangedWatcher { checkIsFormComplete() }
         houseNumberInput?.addTextChangedListener(onChanged)
@@ -118,39 +135,94 @@ class AddHousenumberForm : AbstractQuestFormAnswerFragment<HousenumberAnswer>() 
         conscriptionNumberInput?.addTextChangedListener(onChanged)
         streetNumberInput?.addTextChangedListener(onChanged)
         blockNumberInput?.addTextChangedListener(onChanged)
-
-        // streetNumber is always optional
-        val input = getFirstNonNull(blockNumberInput, houseNumberInput, houseNameInput, conscriptionNumberInput)
-        input?.requestFocus()
-
-        initKeyboardButton(view)
     }
 
-    private fun initKeyboardButton(view: View) {
-        val toggleKeyboardButton = view.findViewById<Button>(R.id.toggleKeyboardButton)
-        if (toggleKeyboardButton != null) {
-            toggleKeyboardButton.text = "abc"
-            toggleKeyboardButton.setOnClickListener {
-                val focus = requireActivity().currentFocus
-                if (focus != null && focus is EditText) {
-                    val start = focus.selectionStart
-                    val end = focus.selectionEnd
-                    if (focus.inputType and InputType.TYPE_CLASS_NUMBER != 0) {
-                        focus.inputType = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
-                        toggleKeyboardButton.text = "123"
-                    } else {
-                        focus.inputType = InputType.TYPE_CLASS_NUMBER
-                        focus.keyListener = DigitsKeyListener.getInstance("0123456789.,- /")
-                        toggleKeyboardButton.text = "abc"
-                    }
-                    // for some reason, the cursor position gets lost first time the input type is set (#1093)
-                    focus.setSelection(start, end)
+    private fun prefillBlockNumber() {
+        /* the block number likely does not change from one input to the other, so let's prefill it
+           with the last selected value */
+        val input = blockNumberInput ?: return
+        val blockNumberAnswer = lastRealHousenumberAnswer as? HouseAndBlockNumber ?: return
+        input.setText(blockNumberAnswer.blockNumber)
+    }
 
-                    val imm = activity?.getSystemService<InputMethodManager>()
-                    imm?.showSoftInput(focus, InputMethodManager.SHOW_IMPLICIT)
-                }
+    private fun showHouseNumberHint() {
+        val input = houseNumberInput ?: return
+        val prev = lastRealHousenumberAnswer?.realHouseNumber ?: return
+
+        /* The Auto fit layout does not work with hints, so we workaround this by setting the "real"
+        *  text instead and make it look like it is a hint. This little hack is much less effort
+        *  than to fork and fix the external dependency. We need to revert back the color both on
+        *  focus and on text changed (tapping on +/- button) */
+        houseNumberInputTextColors = input.textColors
+        input.setTextColor(input.hintTextColors)
+        input.setText(prev)
+        input.addTextChangedListener(TextChangedWatcher {
+            val colors = houseNumberInputTextColors
+            if (colors != null) input.setTextColor(colors)
+            houseNumberInputTextColors = null
+        })
+        input.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            updateKeyboardButtonVisibility()
+            if (hasFocus) showKeyboard(input)
+            val colors = houseNumberInputTextColors
+            if (hasFocus && colors != null) {
+                input.text = null
+                input.setTextColor(colors)
+                houseNumberInputTextColors = null
             }
         }
+    }
+
+    private fun addToHouseNumberInput(add: Int) {
+        val input = houseNumberInput ?: return
+        val prev = if (input.text.isEmpty()) {
+            lastRealHousenumberAnswer?.realHouseNumber
+        } else {
+            input.text.toString()
+        } ?: return
+        val newHouseNumber = prev.addToHouseNumber(add) ?: return
+        input.setText(newHouseNumber)
+        input.setSelection(newHouseNumber.length)
+    }
+
+    private fun initKeyboardButton() {
+        toggleKeyboardButton?.text = "abc"
+        toggleKeyboardButton?.setOnClickListener {
+            val focus = requireActivity().currentFocus
+            if (focus != null && focus is EditText) {
+                val start = focus.selectionStart
+                val end = focus.selectionEnd
+                if (focus.inputType and InputType.TYPE_CLASS_NUMBER != 0) {
+                    focus.inputType = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+                    toggleKeyboardButton?.text = "123"
+                } else {
+                    focus.inputType = InputType.TYPE_CLASS_NUMBER
+                    focus.keyListener = DigitsKeyListener.getInstance("0123456789.,- /")
+                    toggleKeyboardButton?.text = "abc"
+                }
+                // for some reason, the cursor position gets lost first time the input type is set (#1093)
+                focus.setSelection(start, end)
+                showKeyboard(focus)
+            }
+        }
+        updateKeyboardButtonVisibility()
+
+        val onFocusChange = View.OnFocusChangeListener { v, hasFocus ->
+            updateKeyboardButtonVisibility()
+            if (hasFocus) showKeyboard(v)
+        }
+        houseNumberInput?.onFocusChangeListener = onFocusChange
+        streetNumberInput?.onFocusChangeListener = onFocusChange
+    }
+
+    private fun showKeyboard(focus: View) {
+        val imm = activity?.getSystemService<InputMethodManager>()
+        imm?.showSoftInput(focus, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun updateKeyboardButtonVisibility() {
+        toggleKeyboardButton?.isInvisible =
+            !(houseNumberInput?.hasFocus() == true || streetNumberInput?.hasFocus() == true)
     }
 
     private fun confirmHousenumber(isUnusual: Boolean, onConfirmed: () -> Unit) {
@@ -188,38 +260,80 @@ class AddHousenumberForm : AbstractQuestFormAnswerFragment<HousenumberAnswer>() 
         }
         else null
 
-    private fun HousenumberAnswer.looksInvalid() = when(this) {
-        is ConscriptionNumber ->
-            !number.matches(VALID_CONSCRIPTIONNUMBER_REGEX.toRegex())
-                    || streetNumber != null && !streetNumber.matches(getValidHousenumberRegex())
-        is HouseNumber ->
-            !number.matches(getValidHousenumberRegex())
-        is HouseAndBlockNumber ->
-            !houseNumber.matches(getValidHousenumberRegex())
-                    || !blockNumber.matches(VALID_BLOCKNUMBER_REGEX.toRegex())
-        else ->
-            false
-    }
-
     private val EditText.nonEmptyInput:String? get() {
         val input = text.toString().trim()
         return if(input.isNotEmpty()) input else null
     }
 
     companion object {
+        private var lastRealHousenumberAnswer: HousenumberAnswer? = null
+
         private const val IS_HOUSENAME = "is_housename"
-        // i.e. 99999/a, 9/a, 99/9, 99a, 99 a, 9 / a
-        const val VALID_HOUSENUMBER_REGEX = "\\p{N}{1,5}((\\s?/\\s?\\p{N})|(\\s?/?\\s?\\p{L}))?"
-        // i.e. 9, 99, 999, 999, 9A, 9 A
-        const val VALID_BLOCKNUMBER_REGEX = "\\p{N}{1,4}(\\s?\\p{L})?"
+    }
+}
 
-        const val VALID_CONSCRIPTIONNUMBER_REGEX = "\\p{N}{1,6}"
+private val VALID_CONSCRIPTIONNUMBER_REGEX = Regex("\\p{N}{1,6}")
 
-        private fun getFirstNonNull(vararg view: EditText?): EditText? {
-            for (editText in view) {
-                if (editText != null) return editText
+// i.e. 99999/a, 9/a, 99/9, 99a, 99 a, 9 / a
+internal val VALID_HOUSENUMBER_REGEX = Regex("\\p{N}{1,5}((\\s?/\\s?\\p{N})|(\\s?/?\\s?\\p{L}))?")
+
+// i.e. 9, 99, 999, 999, 9A, 9 A
+internal val VALID_BLOCKNUMBER_REGEX = Regex("\\p{N}{1,4}(\\s?\\p{L})?")
+
+private fun HousenumberAnswer.looksInvalid(additionalValidHousenumberRegex: String?): Boolean {
+    val validHousenumberRegex = getValidHouseNumberRegex(additionalValidHousenumberRegex)
+    return when(this) {
+        is ConscriptionNumber ->
+            !number.matches(VALID_CONSCRIPTIONNUMBER_REGEX) || streetNumber != null && !streetNumber.matches(validHousenumberRegex)
+        is HouseNumber ->
+            !number.matches(validHousenumberRegex)
+        is HouseAndBlockNumber ->
+            !houseNumber.matches(validHousenumberRegex) || !blockNumber.matches(VALID_BLOCKNUMBER_REGEX)
+        else ->
+            false
+    }
+}
+
+// i.e. "95-98" or "5,5a,6" etc. (but not: "1, 3" or "3 - 5" or "5,6-7")
+private fun getValidHouseNumberRegex(additionalValidRegex: String?): Regex {
+    var regex = VALID_HOUSENUMBER_REGEX.pattern
+    if (additionalValidRegex != null) {
+        regex = "(($regex)|($additionalValidRegex))"
+    }
+    return "^$regex((-$regex)|(,$regex)+)?".toRegex()
+}
+
+private val HousenumberAnswer.isRealHouseNumberAnswer: Boolean get() = when(this) {
+    is HouseNumber -> true
+    is HouseAndBlockNumber -> true
+    else -> false
+}
+
+private val HousenumberAnswer.realHouseNumber: String? get() = when(this) {
+    is HouseNumber -> number
+    is HouseAndBlockNumber -> houseNumber
+    else -> null
+}
+
+private fun String.addToHouseNumber(add: Int): String? {
+    val parsed = parseHouseNumber(this) ?: return null
+    when {
+        add == 0 -> return this
+        add > 0  -> {
+            val last = when (val it = parsed.list.last()) {
+                is HouseNumbersPartsRange -> it.end
+                is SingleHouseNumbersPart -> it.single
             }
-            return null
+            return (last.number + add).toString()
         }
+        add < 0  -> {
+            val first = when (val it = parsed.list.first()) {
+                is HouseNumbersPartsRange -> it.start
+                is SingleHouseNumbersPart -> it.single
+            }
+            val result = first.number + add
+            return if (result < 1) null else result.toString()
+        }
+        else -> return null
     }
 }
