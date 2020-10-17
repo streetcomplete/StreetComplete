f73f5fd578dc6bdd028d73a0a85c95e598b8931d
Mateusz Konieczny <matkoniecz@gmail.com>
2020-10-17 21:42:32 +0200

    delete no longer needed code and abandoned functionality
    special casing default lists was later anyway overriden by last used surface list, so impact was minimal

--- app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddPathSurfaceForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddPathSurfaceForm.kt
@@ -14,55 +14,16 @@ import de.westnordost.streetcomplete.view.image_select.Item
 import de.westnordost.streetcomplete.util.TextChangedWatcher
 
 class AddPathSurfaceForm : AImageListQuestAnswerFragment<String, DetailSurfaceAnswer>() {
-
-    /*
-    TODO - lost functionality for now
-    is it going to work out of box?
-    override val topItems get() =
-        when (val pathType = determinePathType(osmElement!!.tags)) {
-            "bridleway" -> listOf(
-                DIRT, GRASS, SAND,
-                PEBBLES, FINE_GRAVEL, COMPACTED
-            )
-            "path" -> listOf(
-                DIRT, PEBBLES, COMPACTED,
-                ASPHALT, FINE_GRAVEL, PAVING_STONES
-            )
-            "footway" -> listOf(
-                PAVING_STONES, ASPHALT, CONCRETE,
-                COMPACTED, FINE_GRAVEL, DIRT
-            )
-            "cycleway" -> listOf(
-                PAVING_STONES, ASPHALT, CONCRETE,
-                COMPACTED, WOOD, METAL
-            )
-            "steps" -> listOf(
-                PAVING_STONES, ASPHALT, CONCRETE,
-                WOOD, SETT, UNHEWN_COBBLESTONE
-            )
-            else -> throw IllegalStateException("Unexpected path type $pathType")
-        }.toItems()
-     */
-
     override val items: List<Item<String>> get() =
-        //if (osmElement!!.tags["surface"] == "paved") - TODO: reimplement or remove
         (PAVED_SURFACES + UNPAVED_SURFACES + GROUND_SURFACES).toItems() +
             // TODO: have proper images for path (crop from panorama images)
             Item("paved", R.drawable.path_surface_paved, R.string.quest_surface_value_paved, null, listOf()) +
             Item("unpaved", R.drawable.path_surface_unpaved, R.string.quest_surface_value_unpaved, null, listOf()) +
             Item("ground", R.drawable.surface_ground, R.string.quest_surface_value_ground, null, listOf())
 
-    // TODO: everything below duplicates AddRoadSurfaceForm...
-    // TODO: DRY it?
-
     override val itemsPerRow = 3
 
     override fun onClickOk(selectedItems: List<String>) {
-        // this calls comes from onClickOk() in this class,
-        // through onClickOk() AImageListQuestAnswerFragment
-        // that calls onClickOk with parameters - that is
-        // overloaded here
-
         val value = selectedItems.single()
         if(value == "paved" || value == "unpaved" || value == "ground") {
             AlertDialog.Builder(requireContext())
@@ -111,5 +72,4 @@ class AddPathSurfaceForm : AImageListQuestAnswerFragment<String, DetailSurfaceAn
             setView(view)
         }
     }
-
 }
