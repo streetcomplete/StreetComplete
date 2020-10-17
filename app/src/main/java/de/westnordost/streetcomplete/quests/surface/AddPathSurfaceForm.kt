37847e83bc79e45522bac50ca6d48b00b88b0986
Mateusz Konieczny <matkoniecz@gmail.com>
2020-10-17 21:54:21 +0200

    extract to a separate file

--- app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddPathSurfaceForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddPathSurfaceForm.kt
@@ -41,35 +41,4 @@ class AddPathSurfaceForm : AImageListQuestAnswerFragment<String, DetailSurfaceAn
         }
         applyAnswer(SurfaceAnswer(value))
     }
-
-    class DescribeGenericSurfaceDialog(
-        context: Context,
-        onSurfaceDescribed: (txt:String) -> Unit
-    ) : AlertDialog(context, R.style.Theme_Bubble_Dialog) {
-        init {
-            val view = LayoutInflater.from(context).inflate(R.layout.quest_surface_detailed_answer_impossible, null)
-            val explanationInput = view.findViewById<EditText>(R.id.explanationInput)
-            // TODO enable/disable ok button based on whether commentInput is empty (with TextWatcher?)
-
-            setTitle(context.resources.getString(R.string.quest_surface_detailed_answer_impossible_title))
-
-            setButton(
-                DialogInterface.BUTTON_POSITIVE,
-                context.getString(android.R.string.yes)
-            ) { _, _ ->
-                val txt = explanationInput.text.toString().trim()
-                if (!txt.isEmpty()) {
-                    onSurfaceDescribed(txt)
-                }
-            }
-
-            setButton(
-                DialogInterface.BUTTON_NEGATIVE,
-                context.getString(android.R.string.cancel)
-            ) { _, _ ->
-                setView(view)
-            }
-            setView(view)
-        }
-    }
 }
---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/DescribeGenericSurfaceDialog.kt
@@ -0,0 +1,39 @@
+package de.westnordost.streetcomplete.quests.surface
+
+import android.content.Context
+import android.content.DialogInterface
+import android.view.LayoutInflater
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import de.westnordost.streetcomplete.R
+
+class DescribeGenericSurfaceDialog(
+    context: Context,
+    onSurfaceDescribed: (txt:String) -> Unit
+) : AlertDialog(context, R.style.Theme_Bubble_Dialog) {
+    init {
+        val view = LayoutInflater.from(context).inflate(R.layout.quest_surface_detailed_answer_impossible, null)
+        val explanationInput = view.findViewById<EditText>(R.id.explanationInput)
+        // TODO enable/disable ok button based on whether commentInput is empty (with TextWatcher?)
+
+        setTitle(context.resources.getString(R.string.quest_surface_detailed_answer_impossible_title))
+
+        setButton(
+            DialogInterface.BUTTON_POSITIVE,
+            context.getString(android.R.string.yes)
+        ) { _, _ ->
+            val txt = explanationInput.text.toString().trim()
+            if (!txt.isEmpty()) {
+                onSurfaceDescribed(txt)
+            }
+        }
+
+        setButton(
+            DialogInterface.BUTTON_NEGATIVE,
+            context.getString(android.R.string.cancel)
+        ) { _, _ ->
+            setView(view)
+        }
+        setView(view)
+    }
+}
