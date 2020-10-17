c701a14471908802acd6f2d1649c01e08b25bf01
Mateusz Konieczny <matkoniecz@gmail.com>
2020-10-17 22:06:51 +0200

    DRY Road Surface, code reuse for the win

--- app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddRoadSurfaceForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddRoadSurfaceForm.kt
@@ -12,68 +12,25 @@ import de.westnordost.streetcomplete.util.TextChangedWatcher
 import de.westnordost.streetcomplete.view.image_select.Item
 
 class AddRoadSurfaceForm  : AImageListQuestAnswerFragment<String, DetailSurfaceAnswer>() {
-
     override val items: List<Item<String>> get() =
-        //if (osmElement!!.tags["surface"] == "paved") - TODO: reimplement or remove
         (PAVED_SURFACES + UNPAVED_SURFACES + GROUND_SURFACES).toItems() +
-        Item("paved", R.drawable.surface_paved, R.string.quest_surface_value_paved, null, listOf()) +
-        Item("unpaved", R.drawable.surface_unpaved, R.string.quest_surface_value_unpaved, null, listOf()) +
-        Item("ground", R.drawable.surface_ground, R.string.quest_surface_value_ground, null, listOf())
+            Item("paved", R.drawable.surface_paved, R.string.quest_surface_value_paved, null, listOf()) +
+            Item("unpaved", R.drawable.surface_unpaved, R.string.quest_surface_value_unpaved, null, listOf()) +
+            Item("ground", R.drawable.surface_ground, R.string.quest_surface_value_ground, null, listOf())
 
     override val itemsPerRow = 3
 
-    private var isInExplanationMode = false
-    private var selectedGenericSurfaceValue : String? = null
-    private var explanationInput: EditText? = null
-
-    private fun setLayout(layoutResourceId: Int) {
-        val view = setContentView(layoutResourceId)
-
-        explanationInput = view.findViewById(R.id.explanationInput)
-        explanationInput?.addTextChangedListener(TextChangedWatcher { checkIsFormComplete() })
-    }
-
-    private val explanation: String get() = explanationInput?.text?.toString().orEmpty().trim()
-
-    override fun isFormComplete(): Boolean {
-        return if(isInExplanationMode) {
-            explanation.isNotEmpty()
-        } else {
-            super.isFormComplete()
-        }
-    }
-
-    override fun onClickOk() {
-        // we need to handle fact that we may be in a separate layout
-        // that is used to input explanation why surface may not be
-        // specified more accurately than just paved/unpaved/ground
-        if(isInExplanationMode) {
-            // clicked in an explanation mode, therefore
-            // user has ready answer prepared that we many use
-            applyAnswer(DetailingWhyOnlyGeneric(selectedGenericSurfaceValue!!, explanation))
-        } else {
-            // use regular onClickOk call chain
-            // used in typical ImageList quest
-            super.onClickOk()
-        }
-    }
-
     override fun onClickOk(selectedItems: List<String>) {
-        // must not happen in isInExplanationMode
-        // this onClickOk is called when user is selecting images from
-        // list of surfaces
-
-        // this calls comes from onClickOk() in this class,
-        // through onClickOk() AImageListQuestAnswerFragment
-        // that calls onClickOk with parameters - that is
-        // overloaded here
-
         val value = selectedItems.single()
         if(value == "paved" || value == "unpaved" || value == "ground") {
             AlertDialog.Builder(requireContext())
                 .setMessage(R.string.quest_surface_detailed_answer_impossible_confirmation)
                 .setPositiveButton(R.string.quest_generic_confirmation_yes) {
-                    _, _ -> switchToExplanationLayout(value)
+                    _, _ -> run {
+                    DescribeGenericSurfaceDialog(requireContext()) { description ->
+                        applyAnswer(DetailingWhyOnlyGeneric(value, description))
+                    }.show()
+                }
                 }
                 .setNegativeButton(android.R.string.cancel, null)
                 .show()
@@ -81,29 +38,4 @@ class AddRoadSurfaceForm  : AImageListQuestAnswerFragment<String, DetailSurfaceA
         }
         applyAnswer(SurfaceAnswer(value))
     }
-
-    private fun switchToExplanationLayout(value: String) {
-        selectedGenericSurfaceValue = value
-        isInExplanationMode = true
-        setLayout(R.layout.quest_surface_detailed_answer_impossible)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = super.onCreateView(inflater, container, savedInstanceState)
-
-        isInExplanationMode = savedInstanceState?.getBoolean(IS_IN_EXPLANATION_MODE) ?: false
-        setLayout(if (isInExplanationMode) R.layout.quest_surface_detailed_answer_impossible else R.layout.quest_generic_list)
-
-        return view
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putBoolean(IS_IN_EXPLANATION_MODE, isInExplanationMode)
-    }
-
-    companion object {
-        private const val IS_IN_EXPLANATION_MODE = "is_in_explanation_mode"
-    }
-
 }
