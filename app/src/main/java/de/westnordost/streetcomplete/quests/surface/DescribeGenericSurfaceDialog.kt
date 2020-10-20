5706d4116fce826425e65e9f756a5aef9b6f9ee5
Mateusz Konieczny <matkoniecz@gmail.com>
2020-10-20 13:42:08 +0200

    Revert "technically there is now color changing"
    This reverts commit 24092173fd5453f108860f50926628a1b264e25a.

--- app/src/main/java/de/westnordost/streetcomplete/quests/surface/DescribeGenericSurfaceDialog.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/DescribeGenericSurfaceDialog.kt
@@ -2,7 +2,6 @@ package de.westnordost.streetcomplete.quests.surface
 
 import android.content.Context
 import android.content.DialogInterface
-import android.graphics.Color
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
@@ -38,7 +37,6 @@ class DescribeGenericSurfaceDialog(
 
         setOnShowListener {
             getButton(DialogInterface.BUTTON_POSITIVE).isEnabled = false
-            getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(Color.GRAY)
             explanationInput.addTextChangedListener(object : TextWatcher {
                 override fun onTextChanged(s: CharSequence, start: Int, before: Int,
                                            count: Int) {
@@ -51,11 +49,6 @@ class DescribeGenericSurfaceDialog(
                 override fun afterTextChanged(s: Editable) {
                     val txt = explanationInput.text.toString().trim()
                     getButton(DialogInterface.BUTTON_POSITIVE).isEnabled = txt.isNotEmpty()
-                    if(txt.isNotEmpty()) {
-                        getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(Color.rgb(209, 64, 0)) // accent color is D14000
-                    } else {
-                        getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(Color.GRAY)
-                    }
                 }
             })
         }
