6f61f55db7e027bde69d12ecd0c50aa77af5a70c
Tobias Zwick <newton@westnordost.de>
2020-10-31 18:57:07 +0100

    use concurrent data structures during concurrent operation (fixes #2220)

--- app/src/main/java/de/westnordost/streetcomplete/data/osm/osmquest/CachingMapDataWithGeometry.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/osm/osmquest/CachingMapDataWithGeometry.kt
@@ -5,6 +5,7 @@ import de.westnordost.osmapi.map.MutableMapData
 import de.westnordost.streetcomplete.data.osm.elementgeometry.ElementGeometry
 import de.westnordost.streetcomplete.data.osm.elementgeometry.ElementGeometryCreator
 import de.westnordost.streetcomplete.data.osm.elementgeometry.ElementPointGeometry
+import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
 
 /** MapDataWithGeometry that lazily creates the element geometry. Will create incomplete (relation)
@@ -13,43 +14,22 @@ class CachingMapDataWithGeometry @Inject constructor(
     private val elementGeometryCreator: ElementGeometryCreator
 ) : MutableMapData(), MapDataWithGeometry {
 
-    private val nodeGeometriesById: MutableMap<Long, ElementPointGeometry?> = mutableMapOf()
-    private val wayGeometriesById: MutableMap<Long, ElementGeometry?> = mutableMapOf()
-    private val relationGeometriesById: MutableMap<Long, ElementGeometry?> = mutableMapOf()
+    private val nodeGeometriesById: ConcurrentHashMap<Long, ElementPointGeometry?> = ConcurrentHashMap()
+    private val wayGeometriesById: ConcurrentHashMap<Long, ElementGeometry?> = ConcurrentHashMap()
+    private val relationGeometriesById: ConcurrentHashMap<Long, ElementGeometry?> = ConcurrentHashMap()
 
     override fun getNodeGeometry(id: Long): ElementPointGeometry? {
         val node = nodesById[id] ?: return null
-        if (!nodeGeometriesById.containsKey(id)) {
-            synchronized(nodeGeometriesById) {
-                if (!nodeGeometriesById.containsKey(id)) {
-                    nodeGeometriesById[id] = elementGeometryCreator.create(node)
-                }
-            }
-        }
-        return nodeGeometriesById[id]
+        return nodeGeometriesById.getOrPut(id, { elementGeometryCreator.create(node) })
     }
 
     override fun getWayGeometry(id: Long): ElementGeometry? {
         val way = waysById[id] ?: return null
-        if (!wayGeometriesById.containsKey(id)) {
-            synchronized(wayGeometriesById) {
-                if (!wayGeometriesById.containsKey(id)) {
-                    wayGeometriesById[id] = elementGeometryCreator.create(way, this, true)
-                }
-            }
-        }
-        return wayGeometriesById[id]
+        return wayGeometriesById.getOrPut(id, { elementGeometryCreator.create(way, this, true) })
     }
 
     override fun getRelationGeometry(id: Long): ElementGeometry? {
         val relation = relationsById[id] ?: return null
-        if (!relationGeometriesById.containsKey(id)) {
-            synchronized(relationGeometriesById) {
-                if (!relationGeometriesById.containsKey(id)) {
-                    relationGeometriesById[id] = elementGeometryCreator.create(relation, this, true)
-                }
-            }
-        }
-        return relationGeometriesById[id]
+        return relationGeometriesById.getOrPut(id, { elementGeometryCreator.create(relation, this, true) })
     }
 }
