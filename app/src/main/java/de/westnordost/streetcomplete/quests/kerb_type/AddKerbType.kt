34baa2b499f1b34e3d729f061d0b4e79b047f561
svstoll <s.stoll@posteo.de>
2020-11-13 22:18:46 +0100

    Generate kerb quests for correct nodes

--- app/src/main/java/de/westnordost/streetcomplete/quests/kerb_type/AddKerbType.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/kerb_type/AddKerbType.kt
@@ -1,54 +1,91 @@
 package de.westnordost.streetcomplete.quests.kerb_type
 
+import de.westnordost.osmapi.map.MapDataWithGeometry
+import de.westnordost.osmapi.map.data.Element
+import de.westnordost.osmapi.map.data.Node
 import de.westnordost.streetcomplete.R
-import de.westnordost.streetcomplete.data.meta.ANYTHING_UNPAVED
+import de.westnordost.streetcomplete.data.elementfilter.toElementFilterExpression
 import de.westnordost.streetcomplete.data.meta.updateWithCheckDate
 import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder
-import de.westnordost.streetcomplete.data.osm.osmquest.OsmFilterQuestType
+import de.westnordost.streetcomplete.data.osm.osmquest.OsmElementQuestType
 
-class AddKerbType : OsmFilterQuestType<String>() {
+class AddKerbType : OsmElementQuestType<String> {
 
-    // TODO sst: adapt query or use different super class
-    override val elementFilter = """
-        ways with highway ~ path|footway|cycleway|bridleway|steps
-        and segregated != yes
-        and access !~ private|no
-        and (!conveying or conveying = no)
-        and (!indoor or indoor = no)
-        and (
-          !surface
-          or surface ~ ${ANYTHING_UNPAVED.joinToString("|")} and surface older today -4 years
-          or surface older today -8 years
-          or (
-            surface ~ paved|unpaved
-            and !surface:note
-            and !note:surface
-          )
-        )
-    """
-//    override val elementFilter = """
-//        nodes with highway = crossing
-//          and foot != no
-//          and (
-//            !crossing
-//            or crossing ~ island|unknown|yes
-//            or (
-//              crossing ~ traffic_signals|uncontrolled|zebra|marked|unmarked
-//              and crossing older today -8 years
-//            )
-//          )
-//    """
+    private val footwayCrossingfilter by lazy { """
+        ways with highway = footway
+         and footway = crossing
+         and (!barrier or barrier != kerb or (barrier = kerb and (!kerb or !kerb:left or !kerb:right)))
+    """.toElementFilterExpression() }
 
-    override val commitMessage = "Add kerb"
+    override val commitMessage = "Add kerb to crossing"
     override val wikiLink = "Key:kerb"
     override val icon = R.drawable.ic_quest_kerb
     override val isSplitWayEnabled = false
 
+    override fun getApplicableElements(mapData: MapDataWithGeometry): Iterable<Element> {
+        val footwayCrossingWays = mapData.ways.filter { footwayCrossingfilter.matches(it) }
+        val applicableNodes = mutableSetOf<Node>()
+        footwayCrossingWays.forEach { way ->
+            if (way.nodeIds.size < 3) {
+                // This crossing way is not tagged according to the specification --> omit
+                return@forEach
+            }
+
+            val nodeIdIterator = way.nodeIds.iterator()
+            var previousNode: Node? = null
+            while (nodeIdIterator.hasNext()) {
+                val currentNode = mapData.getNode(nodeIdIterator.next())
+
+                if (currentNode != null) {
+                    if (isCrossingNode(currentNode)) {
+                        // Kerbs on crossing islands should be tagged as well
+                        if (isCrossingIsland(currentNode) && !kerbAlreadyTagged(currentNode)) {
+                            applicableNodes.add(currentNode)
+                        }
+
+                        // If there is a previous node that does not have the kerb tags,
+                        // it should be added as long as it is not a crossing node itself.
+                        // If the previous node is a crossing node that is also a crossing island
+                        // then it was already added.
+                        if (previousNode != null && !isCrossingNode(previousNode) && !kerbAlreadyTagged(previousNode)) {
+                            applicableNodes.add(previousNode)
+                        }
+                    } else {
+                        // After a crossing node, the next node should be added if it is not a
+                        // crossing node itself and does not have the kerb tags already.
+                        if (previousNode != null && isCrossingNode(previousNode) && !kerbAlreadyTagged(previousNode)) {
+                            applicableNodes.add(currentNode)
+                        }
+                    }
+                }
+                previousNode = currentNode
+            }
+        }
+        return applicableNodes
+    }
+
+    private fun isCrossingNode(node: Node): Boolean {
+        return "crossing" == node.tags["highway"]
+    }
+
+    private fun isCrossingIsland(node: Node): Boolean {
+        return "island" == node.tags["traffic_calming"] || "yes" == node.tags["crossing:island"]
+    }
+
+    private fun kerbAlreadyTagged(node: Element): Boolean {
+        return "kerb" == node.tags["barrier"] && !node.tags["kerb"].isNullOrEmpty()
+    }
+
+    override fun isApplicableTo(element: Element): Boolean? = null
+
     override fun getTitle(tags: Map<String, String>) =  R.string.quest_kerb_type_title
 
-    override fun createForm(): AddKerbTypeForm = AddKerbTypeForm() // TODO sst: change form
+    override fun createForm(): AddKerbTypeForm = AddKerbTypeForm()
 
     override fun applyAnswerTo(answer: String, changes: StringMapChangesBuilder) {
+        changes.updateWithCheckDate("barrier", "kerb")
         changes.updateWithCheckDate("kerb", answer)
+        changes.deleteIfExists("kerb:left")
+        changes.deleteIfExists("kerb:right")
     }
 }
