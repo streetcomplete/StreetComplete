2721d3215500d94d1de5af63ffeeb4ed5041a451
Tobias Zwick <newton@westnordost.de>
2020-11-22 18:07:48 +0100

    update version and changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "de.westnordost.streetcomplete"
         minSdkVersion(17)
         targetSdkVersion(29)
-        versionCode = 2701
-        versionName = "27.0-beta2"
+        versionCode = 2702
+        versionName = "27.0-beta3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
--- buildSrc/src/main/java/UpdateStoreDescriptionsTask.kt
+++ buildSrc/src/main/java/UpdateStoreDescriptionsTask.kt
@@ -3,8 +3,6 @@ import com.beust.klaxon.Parser
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 import java.io.File
-import java.net.HttpURLConnection
-import java.net.URL
 import java.util.*
 
 /** Update the metadata that contain the store descriptions for the app (for F-Droid) */
@@ -21,14 +19,17 @@ open class UpdateStoreDescriptionsTask : AUpdateFromPOEditorTask() {
             if (languageCode.toLowerCase(Locale.US) == "en-us") continue
             println(languageCode)
             fetchLocalization(languageCode, "key_value_json") { inputStream ->
-                val translations = Parser.default().parse(inputStream) as JsonObject
+                val txt = inputStream.bufferedReader().use { it.readText() }
+                if (txt.isNotEmpty()) {
+                    val translations = Parser.default().parse(txt.reader()) as JsonObject
 
-                File("$targetDir/$languageCode").mkdirs()
-                translations.string("store_listing_short_description")?.let {
-                    File("$targetDir/$languageCode/short_description.txt").writeText(it)
-                }
-                translations.string("store_listing_full_description")?.let {
-                    File("$targetDir/$languageCode/full_description.txt").writeText(it)
+                    File("$targetDir/$languageCode").mkdirs()
+                    translations.string("store_listing_short_description")?.let {
+                        File("$targetDir/$languageCode/short_description.txt").writeText(it)
+                    }
+                    translations.string("store_listing_full_description")?.let {
+                        File("$targetDir/$languageCode/full_description.txt").writeText(it)
+                    }
                 }
             }
         }
