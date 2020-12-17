6785912338aea80c9edb8974eb16f2a1f707e7e3
Mateusz Konieczny <matkoniecz@gmail.com>
2020-12-17 11:02:46 +0100

    run reformat code

--- app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddPitchSurfaceForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddPitchSurfaceForm.kt
@@ -6,22 +6,23 @@ import de.westnordost.streetcomplete.quests.AImageListQuestAnswerFragment
 import de.westnordost.streetcomplete.view.image_select.Item
 
 class AddPitchSurfaceForm : AImageListQuestAnswerFragment<String, SurfaceAnswer>() {
-    override val items: List<Item<String>> get() =
-        listOf(Surface.GRASS, Surface.ASPHALT, Surface.SAND, Surface.CONCRETE,
-            Surface.CLAY, Surface.ARTIFICIAL_TURF, Surface.TARTAN, Surface.DIRT).toItems() +
-            Item("paved", R.drawable.surface_paved_area, R.string.quest_surface_value_paved, null, listOf()) +
-            Item("unpaved", R.drawable.surface_unpaved_area, R.string.quest_surface_value_unpaved, null, listOf()) +
-            Item("ground", R.drawable.surface_ground_area, R.string.quest_surface_value_ground, null, listOf())
+    override val items: List<Item<String>>
+        get() =
+            listOf(Surface.GRASS, Surface.ASPHALT, Surface.SAND, Surface.CONCRETE,
+                Surface.CLAY, Surface.ARTIFICIAL_TURF, Surface.TARTAN, Surface.DIRT).toItems() +
+                Item("paved", R.drawable.surface_paved_area, R.string.quest_surface_value_paved, null, listOf()) +
+                Item("unpaved", R.drawable.surface_unpaved_area, R.string.quest_surface_value_unpaved, null, listOf()) +
+                Item("ground", R.drawable.surface_ground_area, R.string.quest_surface_value_ground, null, listOf())
 
     override val itemsPerRow = 3
 
     override fun onClickOk(selectedItems: List<String>) {
         val value = selectedItems.single()
-        if(value == "paved" || value == "unpaved") {
+        if (value == "paved" || value == "unpaved") {
             AlertDialog.Builder(requireContext())
                 .setMessage(R.string.quest_surface_detailed_answer_impossible_confirmation)
-                .setPositiveButton(R.string.quest_generic_confirmation_yes) {
-                    _, _ -> DescribeGenericSurfaceDialog(requireContext()) { description ->
+                .setPositiveButton(R.string.quest_generic_confirmation_yes) { _, _ ->
+                    DescribeGenericSurfaceDialog(requireContext()) { description ->
                         applyAnswer(GenericSurfaceAnswer(value, description))
                     }.show()
                 }
