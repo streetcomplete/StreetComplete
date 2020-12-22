38e6e8b01ae3b24a1261398ee328b4eeb8cba016
Mateusz Konieczny <matkoniecz@gmail.com>
2020-12-22 13:32:31 +0100

    simplify tests

--- app/src/test/java/de/westnordost/streetcomplete/quests/surface/AddPitchSurfaceTest.kt
+++ app/src/test/java/de/westnordost/streetcomplete/quests/surface/AddPitchSurfaceTest.kt
@@ -13,6 +13,34 @@ class AddPitchSurfaceTest {
         assertIsNotApplicable("leisure" to "pitch")
     }
 
+    @Test fun `applicable to pitch area with single valid sport`() {
+        assertIsApplicable("leisure" to "pitch", "sport" to "soccer")
+    }
+
+    @Test fun `applicable to pitch area with single valid sport in the middle of a list`() {
+        assertIsApplicable("leisure" to "pitch", "sport" to "rhubarb;soccer;tree_planting")
+    }
+
+    @Test fun `applicable to pitch area with single valid sport in the end of a list`() {
+        assertIsApplicable("leisure" to "pitch", "sport" to "rhubarb;soccer")
+    }
+
+    @Test fun `applicable to pitch area with single valid sport in the start of a list`() {
+        assertIsApplicable("leisure" to "pitch", "sport" to "soccer;zażółć")
+    }
+
+    @Test fun `not applicable to pitch area with single invalid sport`() {
+        assertIsNotApplicable("leisure" to "pitch", "sport" to "table_tennis")
+    }
+
+    @Test fun `applicable to pitch area with both valid and invalid sport`() {
+        assertIsApplicable("leisure" to "pitch", "sport" to "multi;table_tennis")
+    }
+
+    @Test fun `not applicable to pitch area with with sport value that is substring of valid one`() {
+        assertIsNotApplicable("leisure" to "pitch", "sport" to "table_soccer")
+    }
+
     private fun assertIsApplicable(vararg pairs: Pair<String, String>) {
         assertTrue(questType.isApplicableTo(create(*pairs)))
     }
@@ -22,74 +50,4 @@ class AddPitchSurfaceTest {
     }
 
     private fun create(vararg pairs: Pair<String, String>) = OsmWay(1L, 1, listOf(1,2,3), mapOf(*pairs))
-
-    @Test fun `applicable to pitch area with single valid sport`() {
-        val mapData = TestMapDataWithGeometry(listOf(
-            OsmWay(1L, 1, listOf(1,2,3), mapOf(
-                "leisure" to "pitch",
-                "sport" to "soccer",
-            ))
-        ))
-        assertEquals(1, questType.getApplicableElements(mapData).toList().size)
-    }
-
-    @Test fun `applicable to pitch area with single valid sport in the middle of a list`() {
-        val mapData = TestMapDataWithGeometry(listOf(
-            OsmWay(1L, 1, listOf(1,2,3), mapOf(
-                "leisure" to "pitch",
-                "sport" to "rhubarb;soccer;tree_planting",
-            ))
-        ))
-        assertEquals(1, questType.getApplicableElements(mapData).toList().size)
-    }
-
-    @Test fun `applicable to pitch area with single valid sport in the end of a list`() {
-        val mapData = TestMapDataWithGeometry(listOf(
-            OsmWay(1L, 1, listOf(1,2,3), mapOf(
-                "leisure" to "pitch",
-                "sport" to "rhubarb;soccer",
-            ))
-        ))
-        assertEquals(1, questType.getApplicableElements(mapData).toList().size)
-    }
-
-    @Test fun `applicable to pitch area with single valid sport in the start of a list`() {
-        val mapData = TestMapDataWithGeometry(listOf(
-            OsmWay(1L, 1, listOf(1,2,3), mapOf(
-                "leisure" to "pitch",
-                "sport" to "soccer;filler",
-            ))
-        ))
-        assertEquals(1, questType.getApplicableElements(mapData).toList().size)
-    }
-
-    @Test fun `not applicable to pitch area with single invalid sport`() {
-        val mapData = TestMapDataWithGeometry(listOf(
-            OsmWay(1L, 1, listOf(1,2,3), mapOf(
-                "leisure" to "pitch",
-                "sport" to "table_tennis",
-            ))
-        ))
-        assertEquals(0, questType.getApplicableElements(mapData).toList().size)
-    }
-
-    @Test fun `applicable to pitch area with both valid and invalid sport`() {
-        val mapData = TestMapDataWithGeometry(listOf(
-            OsmWay(1L, 1, listOf(1,2,3), mapOf(
-                "leisure" to "pitch",
-                "sport" to "multi;table_tennis",
-            ))
-        ))
-        assertEquals(1, questType.getApplicableElements(mapData).toList().size)
-    }
-
-    @Test fun `not applicable to pitch area with with sport value that is substring of valid one`() {
-        val mapData = TestMapDataWithGeometry(listOf(
-            OsmWay(1L, 1, listOf(1,2,3), mapOf(
-                "leisure" to "pitch",
-                "sport" to "table_soccer",
-            ))
-        ))
-        assertEquals(0, questType.getApplicableElements(mapData).toList().size)
-    }
 }
