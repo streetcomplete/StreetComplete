7edaf245d995aaeeeece01dea61fb43816d14388
Mateusz Konieczny <matkoniecz@gmail.com>
2020-12-22 13:32:20 +0100

    add to achievements

--- app/src/main/java/de/westnordost/streetcomplete/data/user/achievements/AchievementsModule.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/user/achievements/AchievementsModule.kt
@@ -685,6 +685,7 @@ object AchievementsModule {
                 listOf(
                     "CheckExistence",
                     "AddSport",
+                    "AddPitchSurface",
                     "AddSummitRegister",
                     "AddReligionToWaysideShrine",
                     // from pedestrian
