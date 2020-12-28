59b5c221b8fd0b28ae81a46de2e871a3be83f117
Tobias Zwick <newton@westnordost.de>
2020-12-28 17:33:54 +0100

    version up and changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "de.westnordost.streetcomplete"
         minSdkVersion(17)
         targetSdkVersion(30)
-        versionCode = 2803
-        versionName = "28.1"
+        versionCode = 2900
+        versionName = "29.0-beta1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
