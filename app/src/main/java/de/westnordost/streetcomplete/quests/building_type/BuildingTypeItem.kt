644ee41b196beb7eebd2bc444a7d4e3e105a496c
Flo Edelmann <florian-edelmann@online.de>
2021-01-12 19:03:07 +0100

    Inline "get resource ID" functions into `asItem` function

--- app/src/main/java/de/westnordost/streetcomplete/quests/building_type/BuildingTypeItem.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/building_type/BuildingTypeItem.kt
@@ -4,157 +4,66 @@ import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.view.image_select.Item
 import de.westnordost.streetcomplete.quests.building_type.BuildingType.*
 
-fun BuildingType.asItem(): Item<String> {
-    return Item(this.osmTag, getIconResId(), getTitleResId(), getDescriptionResId(), this.subTypes?.toItems())
-}
-
 fun List<BuildingType>.toItems() = this.map { it.asItem() }
 
-private fun BuildingType.getIconResId(): Int = when(this) {
-    HOUSE -> R.drawable.ic_building_house
-    APARTMENTS -> R.drawable.ic_building_apartments
-    DETACHED -> R.drawable.ic_building_detached
-    SEMI_DETACHED -> R.drawable.ic_building_semi_detached
-    TERRACE -> R.drawable.ic_building_terrace
-    HOTEL -> R.drawable.ic_building_hotel
-    DORMITORY -> R.drawable.ic_building_dormitory
-    HOUSEBOAT -> R.drawable.ic_building_houseboat
-    BUNGALOW -> R.drawable.ic_building_bungalow
-    STATIC_CARAVAN -> R.drawable.ic_building_static_caravan
-    HUT -> R.drawable.ic_building_hut
-    INDUSTRIAL -> R.drawable.ic_building_industrial
-    RETAIL -> R.drawable.ic_building_retail
-    OFFICE -> R.drawable.ic_building_office
-    WAREHOUSE -> R.drawable.ic_building_warehouse
-    KIOSK -> R.drawable.ic_building_kiosk
-    STORAGE_TANK -> R.drawable.ic_building_storage_tank
-    KINDERGARTEN -> R.drawable.ic_building_kindergarten
-    SCHOOL -> R.drawable.ic_building_school
-    COLLEGE -> R.drawable.ic_building_college
-    SPORTS_CENTRE -> R.drawable.ic_sport_volleyball
-    HOSPITAL -> R.drawable.ic_building_hospital
-    STADIUM -> R.drawable.ic_sport_volleyball
-    TRAIN_STATION -> R.drawable.ic_building_train_station
-    TRANSPORTATION -> R.drawable.ic_building_transportation
-    UNIVERSITY -> R.drawable.ic_building_university
-    GOVERNMENT -> R.drawable.ic_building_historic
-    CHURCH -> R.drawable.ic_religion_christian
-    CHAPEL -> R.drawable.ic_religion_christian
-    CATHEDRAL -> R.drawable.ic_religion_christian
-    MOSQUE -> R.drawable.ic_religion_muslim
-    TEMPLE -> R.drawable.ic_building_temple
-    PAGODA -> R.drawable.ic_building_temple
-    SYNAGOGUE -> R.drawable.ic_religion_jewish
-    SHRINE -> R.drawable.ic_building_temple
-    CARPORT -> R.drawable.ic_building_carport
-    GARAGE -> R.drawable.ic_building_garage
-    GARAGES -> R.drawable.ic_building_garages
-    PARKING -> R.drawable.ic_building_parking
-    FARM -> R.drawable.ic_building_farm_house
-    FARM_AUXILIARY -> R.drawable.ic_building_barn
-    SILO -> R.drawable.ic_building_silo
-    GREENHOUSE -> R.drawable.ic_building_greenhouse
-    SHED -> R.drawable.ic_building_shed
-    ROOF -> R.drawable.ic_building_roof
-    TOILETS -> R.drawable.ic_building_toilets
-    SERVICE -> R.drawable.ic_building_service
-    HANGAR -> R.drawable.ic_building_hangar
-    BUNKER -> R.drawable.ic_building_bunker
-    HISTORIC -> R.drawable.ic_building_historic
-    ABANDONED -> R.drawable.ic_building_abandoned
-    RUINS -> R.drawable.ic_building_ruins
-    RESIDENTIAL -> R.drawable.ic_building_apartments
-    COMMERCIAL -> R.drawable.ic_building_office
-    CIVIC -> R.drawable.ic_building_civic
-    RELIGIOUS -> R.drawable.ic_building_temple
-    FOR_CARS -> R.drawable.ic_building_car
-    FOR_FARMS -> R.drawable.ic_building_farm
-    OTHER -> R.drawable.ic_building_other
-}
-
-private fun BuildingType.getTitleResId(): Int = when(this) {
-    HOUSE -> R.string.quest_buildingType_house
-    APARTMENTS -> R.string.quest_buildingType_apartments
-    DETACHED -> R.string.quest_buildingType_detached
-    SEMI_DETACHED -> R.string.quest_buildingType_semi_detached
-    TERRACE -> R.string.quest_buildingType_terrace2
-    HOTEL -> R.string.quest_buildingType_hotel
-    DORMITORY -> R.string.quest_buildingType_dormitory
-    HOUSEBOAT -> R.string.quest_buildingType_houseboat
-    BUNGALOW -> R.string.quest_buildingType_bungalow
-    STATIC_CARAVAN -> R.string.quest_buildingType_static_caravan
-    HUT -> R.string.quest_buildingType_hut
-    INDUSTRIAL -> R.string.quest_buildingType_industrial
-    RETAIL -> R.string.quest_buildingType_retail
-    OFFICE -> R.string.quest_buildingType_office
-    WAREHOUSE -> R.string.quest_buildingType_warehouse
-    KIOSK -> R.string.quest_buildingType_kiosk
-    STORAGE_TANK -> R.string.quest_buildingType_storage_tank
-    KINDERGARTEN -> R.string.quest_buildingType_kindergarten
-    SCHOOL -> R.string.quest_buildingType_school
-    COLLEGE -> R.string.quest_buildingType_college
-    SPORTS_CENTRE -> R.string.quest_buildingType_sports_centre
-    HOSPITAL -> R.string.quest_buildingType_hospital
-    STADIUM -> R.string.quest_buildingType_stadium
-    TRAIN_STATION -> R.string.quest_buildingType_train_station
-    TRANSPORTATION -> R.string.quest_buildingType_transportation
-    UNIVERSITY -> R.string.quest_buildingType_university
-    GOVERNMENT -> R.string.quest_buildingType_government
-    CHURCH -> R.string.quest_buildingType_church
-    CHAPEL -> R.string.quest_buildingType_chapel
-    CATHEDRAL -> R.string.quest_buildingType_cathedral
-    MOSQUE -> R.string.quest_buildingType_mosque
-    TEMPLE -> R.string.quest_buildingType_temple
-    PAGODA -> R.string.quest_buildingType_pagoda
-    SYNAGOGUE -> R.string.quest_buildingType_synagogue
-    SHRINE -> R.string.quest_buildingType_shrine
-    CARPORT -> R.string.quest_buildingType_carport
-    GARAGE -> R.string.quest_buildingType_garage
-    GARAGES -> R.string.quest_buildingType_garages
-    PARKING -> R.string.quest_buildingType_parking
-    FARM -> R.string.quest_buildingType_farmhouse
-    FARM_AUXILIARY -> R.string.quest_buildingType_farm_auxiliary
-    SILO -> R.string.quest_buildingType_silo
-    GREENHOUSE -> R.string.quest_buildingType_greenhouse
-    SHED -> R.string.quest_buildingType_shed
-    ROOF -> R.string.quest_buildingType_roof
-    TOILETS -> R.string.quest_buildingType_toilets
-    SERVICE -> R.string.quest_buildingType_service
-    HANGAR -> R.string.quest_buildingType_hangar
-    BUNKER -> R.string.quest_buildingType_bunker
-    HISTORIC -> R.string.quest_buildingType_historic
-    ABANDONED -> R.string.quest_buildingType_abandoned
-    RUINS -> R.string.quest_buildingType_ruins
-    RESIDENTIAL -> R.string.quest_buildingType_residential
-    COMMERCIAL -> R.string.quest_buildingType_commercial
-    CIVIC -> R.string.quest_buildingType_civic
-    RELIGIOUS -> R.string.quest_buildingType_religious
-    FOR_CARS -> R.string.quest_buildingType_cars
-    FOR_FARMS -> R.string.quest_buildingType_farm
-    OTHER -> R.string.quest_buildingType_other
-}
-
-private fun BuildingType.getDescriptionResId(): Int? = when(this) {
-    HOUSE -> R.string.quest_buildingType_house_description
-    APARTMENTS -> R.string.quest_buildingType_apartments_description
-    DETACHED -> R.string.quest_buildingType_detached_description
-    SEMI_DETACHED -> R.string.quest_buildingType_semi_detached_description
-    TERRACE -> R.string.quest_buildingType_terrace_description
-    BUNGALOW -> R.string.quest_buildingType_bungalow_description2
-    HUT -> R.string.quest_buildingType_hut_description
-    INDUSTRIAL -> R.string.quest_buildingType_industrial_description
-    RETAIL -> R.string.quest_buildingType_retail_description
-    CARPORT -> R.string.quest_buildingType_carport_description
-    GARAGES -> R.string.quest_buildingType_garages_description
-    FARM -> R.string.quest_buildingType_farmhouse_description
-    FARM_AUXILIARY -> R.string.quest_buildingType_farm_auxiliary_description
-    SERVICE -> R.string.quest_buildingType_service_description
-    HANGAR -> R.string.quest_buildingType_hangar_description
-    HISTORIC -> R.string.quest_buildingType_historic_description
-    ABANDONED -> R.string.quest_buildingType_abandoned_description
-    RUINS -> R.string.quest_buildingType_ruins_description
-    RESIDENTIAL -> R.string.quest_buildingType_residential_description
-    COMMERCIAL -> R.string.quest_buildingType_commercial_generic_description
-    CIVIC -> R.string.quest_buildingType_civic_description
-    else -> null
+fun BuildingType.asItem(): Item<String> = when(this) {
+    HOUSE -> Item(this.osmTag, R.drawable.ic_building_house, R.string.quest_buildingType_house, R.string.quest_buildingType_house_description)
+    APARTMENTS -> Item(this.osmTag, R.drawable.ic_building_apartments, R.string.quest_buildingType_apartments, R.string.quest_buildingType_apartments_description)
+    DETACHED -> Item(this.osmTag, R.drawable.ic_building_detached, R.string.quest_buildingType_detached, R.string.quest_buildingType_detached_description)
+    SEMI_DETACHED -> Item(this.osmTag, R.drawable.ic_building_semi_detached, R.string.quest_buildingType_semi_detached, R.string.quest_buildingType_semi_detached_description)
+    TERRACE -> Item(this.osmTag, R.drawable.ic_building_terrace, R.string.quest_buildingType_terrace2, R.string.quest_buildingType_terrace_description)
+    HOTEL -> Item(this.osmTag, R.drawable.ic_building_hotel, R.string.quest_buildingType_hotel)
+    DORMITORY -> Item(this.osmTag, R.drawable.ic_building_dormitory, R.string.quest_buildingType_dormitory)
+    HOUSEBOAT -> Item(this.osmTag, R.drawable.ic_building_houseboat, R.string.quest_buildingType_houseboat)
+    BUNGALOW -> Item(this.osmTag, R.drawable.ic_building_bungalow, R.string.quest_buildingType_bungalow, R.string.quest_buildingType_bungalow_description2)
+    STATIC_CARAVAN -> Item(this.osmTag, R.drawable.ic_building_static_caravan, R.string.quest_buildingType_static_caravan)
+    HUT -> Item(this.osmTag, R.drawable.ic_building_hut, R.string.quest_buildingType_hut, R.string.quest_buildingType_hut_description)
+    INDUSTRIAL -> Item(this.osmTag, R.drawable.ic_building_industrial, R.string.quest_buildingType_industrial, R.string.quest_buildingType_industrial_description)
+    RETAIL -> Item(this.osmTag, R.drawable.ic_building_retail, R.string.quest_buildingType_retail, R.string.quest_buildingType_retail_description)
+    OFFICE -> Item(this.osmTag, R.drawable.ic_building_office, R.string.quest_buildingType_office)
+    WAREHOUSE -> Item(this.osmTag, R.drawable.ic_building_warehouse, R.string.quest_buildingType_warehouse)
+    KIOSK -> Item(this.osmTag, R.drawable.ic_building_kiosk, R.string.quest_buildingType_kiosk)
+    STORAGE_TANK -> Item(this.osmTag, R.drawable.ic_building_storage_tank, R.string.quest_buildingType_storage_tank)
+    KINDERGARTEN -> Item(this.osmTag, R.drawable.ic_building_kindergarten, R.string.quest_buildingType_kindergarten)
+    SCHOOL -> Item(this.osmTag, R.drawable.ic_building_school, R.string.quest_buildingType_school)
+    COLLEGE -> Item(this.osmTag, R.drawable.ic_building_college, R.string.quest_buildingType_college)
+    SPORTS_CENTRE -> Item(this.osmTag, R.drawable.ic_sport_volleyball, R.string.quest_buildingType_sports_centre)
+    HOSPITAL -> Item(this.osmTag, R.drawable.ic_building_hospital, R.string.quest_buildingType_hospital)
+    STADIUM -> Item(this.osmTag, R.drawable.ic_sport_volleyball, R.string.quest_buildingType_stadium)
+    TRAIN_STATION -> Item(this.osmTag, R.drawable.ic_building_train_station, R.string.quest_buildingType_train_station)
+    TRANSPORTATION -> Item(this.osmTag, R.drawable.ic_building_transportation, R.string.quest_buildingType_transportation)
+    UNIVERSITY -> Item(this.osmTag, R.drawable.ic_building_university, R.string.quest_buildingType_university)
+    GOVERNMENT -> Item(this.osmTag, R.drawable.ic_building_historic, R.string.quest_buildingType_government)
+    CHURCH -> Item(this.osmTag, R.drawable.ic_religion_christian, R.string.quest_buildingType_church)
+    CHAPEL -> Item(this.osmTag, R.drawable.ic_religion_christian, R.string.quest_buildingType_chapel)
+    CATHEDRAL -> Item(this.osmTag, R.drawable.ic_religion_christian, R.string.quest_buildingType_cathedral)
+    MOSQUE -> Item(this.osmTag, R.drawable.ic_religion_muslim, R.string.quest_buildingType_mosque)
+    TEMPLE -> Item(this.osmTag, R.drawable.ic_building_temple, R.string.quest_buildingType_temple)
+    PAGODA -> Item(this.osmTag, R.drawable.ic_building_temple, R.string.quest_buildingType_pagoda)
+    SYNAGOGUE -> Item(this.osmTag, R.drawable.ic_religion_jewish, R.string.quest_buildingType_synagogue)
+    SHRINE -> Item(this.osmTag, R.drawable.ic_building_temple, R.string.quest_buildingType_shrine)
+    CARPORT -> Item(this.osmTag, R.drawable.ic_building_carport, R.string.quest_buildingType_carport, R.string.quest_buildingType_carport_description)
+    GARAGE -> Item(this.osmTag, R.drawable.ic_building_garage, R.string.quest_buildingType_garage)
+    GARAGES -> Item(this.osmTag, R.drawable.ic_building_garages, R.string.quest_buildingType_garages, R.string.quest_buildingType_garages_description)
+    PARKING -> Item(this.osmTag, R.drawable.ic_building_parking, R.string.quest_buildingType_parking)
+    FARM -> Item(this.osmTag, R.drawable.ic_building_farm_house, R.string.quest_buildingType_farmhouse, R.string.quest_buildingType_farmhouse_description)
+    FARM_AUXILIARY -> Item(this.osmTag, R.drawable.ic_building_barn, R.string.quest_buildingType_farm_auxiliary, R.string.quest_buildingType_farm_auxiliary_description)
+    SILO -> Item(this.osmTag, R.drawable.ic_building_silo, R.string.quest_buildingType_silo)
+    GREENHOUSE -> Item(this.osmTag, R.drawable.ic_building_greenhouse, R.string.quest_buildingType_greenhouse)
+    SHED -> Item(this.osmTag, R.drawable.ic_building_shed, R.string.quest_buildingType_shed)
+    ROOF -> Item(this.osmTag, R.drawable.ic_building_roof, R.string.quest_buildingType_roof)
+    TOILETS -> Item(this.osmTag, R.drawable.ic_building_toilets, R.string.quest_buildingType_toilets)
+    SERVICE -> Item(this.osmTag, R.drawable.ic_building_service, R.string.quest_buildingType_service, R.string.quest_buildingType_service_description)
+    HANGAR -> Item(this.osmTag, R.drawable.ic_building_hangar, R.string.quest_buildingType_hangar, R.string.quest_buildingType_hangar_description)
+    BUNKER -> Item(this.osmTag, R.drawable.ic_building_bunker, R.string.quest_buildingType_bunker)
+    HISTORIC -> Item(this.osmTag, R.drawable.ic_building_historic, R.string.quest_buildingType_historic, R.string.quest_buildingType_historic_description)
+    ABANDONED -> Item(this.osmTag, R.drawable.ic_building_abandoned, R.string.quest_buildingType_abandoned, R.string.quest_buildingType_abandoned_description)
+    RUINS -> Item(this.osmTag, R.drawable.ic_building_ruins, R.string.quest_buildingType_ruins, R.string.quest_buildingType_ruins_description)
+    RESIDENTIAL -> Item(this.osmTag, R.drawable.ic_building_apartments, R.string.quest_buildingType_residential, R.string.quest_buildingType_residential_description)
+    COMMERCIAL -> Item(this.osmTag, R.drawable.ic_building_office, R.string.quest_buildingType_commercial, R.string.quest_buildingType_commercial_generic_description)
+    CIVIC -> Item(this.osmTag, R.drawable.ic_building_civic, R.string.quest_buildingType_civic, R.string.quest_buildingType_civic_description)
+    RELIGIOUS -> Item(this.osmTag, R.drawable.ic_building_temple, R.string.quest_buildingType_religious)
+    FOR_CARS -> Item(this.osmTag, R.drawable.ic_building_car, R.string.quest_buildingType_cars)
+    FOR_FARMS -> Item(this.osmTag, R.drawable.ic_building_farm, R.string.quest_buildingType_farm)
+    OTHER -> Item(this.osmTag, R.drawable.ic_building_other, R.string.quest_buildingType_other)
 }
--- app/src/main/java/de/westnordost/streetcomplete/quests/surface/SurfaceItem.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/SurfaceItem.kt
@@ -4,58 +4,29 @@ import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.quests.surface.Surface.*
 import de.westnordost.streetcomplete.view.image_select.Item
 
-fun Surface.asItem(): Item<Surface> {
-    return Item(this, getIconResId(), getTitleResId())
-}
-
 fun List<Surface>.toItems() = this.map { it.asItem() }
 
-private fun Surface.getIconResId(): Int = when(this) {
-    ASPHALT -> R.drawable.surface_asphalt
-    CONCRETE -> R.drawable.surface_concrete
-    FINE_GRAVEL -> R.drawable.surface_fine_gravel
-    PAVING_STONES -> R.drawable.surface_paving_stones
-    COMPACTED -> R.drawable.surface_compacted
-    DIRT -> R.drawable.surface_dirt
-    SETT -> R.drawable.surface_sett
-    UNHEWN_COBBLESTONE -> R.drawable.surface_cobblestone
-    GRASS_PAVER -> R.drawable.surface_grass_paver
-    WOOD -> R.drawable.surface_wood
-    METAL -> R.drawable.surface_metal
-    GRAVEL -> R.drawable.surface_gravel
-    PEBBLES -> R.drawable.surface_pebblestone
-    GRASS -> R.drawable.surface_grass
-    SAND -> R.drawable.surface_sand
-    ROCK -> R.drawable.surface_rock
-    CLAY -> R.drawable.surface_tennis_clay
-    ARTIFICIAL_TURF -> R.drawable.surface_artificial_turf
-    TARTAN -> R.drawable.surface_tartan
-    PAVED -> R.drawable.path_surface_paved
-    UNPAVED -> R.drawable.path_surface_unpaved
-    GROUND -> R.drawable.surface_ground
-}
-
-private fun Surface.getTitleResId(): Int = when(this) {
-    ASPHALT -> R.string.quest_surface_value_asphalt
-    CONCRETE -> R.string.quest_surface_value_concrete
-    FINE_GRAVEL -> R.string.quest_surface_value_fine_gravel
-    PAVING_STONES -> R.string.quest_surface_value_paving_stones
-    COMPACTED -> R.string.quest_surface_value_compacted
-    DIRT -> R.string.quest_surface_value_dirt
-    SETT -> R.string.quest_surface_value_sett
-    UNHEWN_COBBLESTONE -> R.string.quest_surface_value_unhewn_cobblestone
-    GRASS_PAVER -> R.string.quest_surface_value_grass_paver
-    WOOD -> R.string.quest_surface_value_wood
-    METAL -> R.string.quest_surface_value_metal
-    GRAVEL -> R.string.quest_surface_value_gravel
-    PEBBLES -> R.string.quest_surface_value_pebblestone
-    GRASS -> R.string.quest_surface_value_grass
-    SAND -> R.string.quest_surface_value_sand
-    ROCK -> R.string.quest_surface_value_rock
-    CLAY -> R.string.quest_surface_value_clay
-    ARTIFICIAL_TURF -> R.string.quest_surface_value_artificial_turf
-    TARTAN -> R.string.quest_surface_value_tartan
-    PAVED -> R.string.quest_surface_value_paved
-    UNPAVED -> R.string.quest_surface_value_unpaved
-    GROUND -> R.string.quest_surface_value_ground
+fun Surface.asItem(): Item<Surface> = when (this) {
+    ASPHALT -> Item(this, R.drawable.surface_asphalt, R.string.quest_surface_value_asphalt)
+    CONCRETE -> Item(this, R.drawable.surface_concrete, R.string.quest_surface_value_concrete)
+    FINE_GRAVEL -> Item(this, R.drawable.surface_fine_gravel, R.string.quest_surface_value_fine_gravel)
+    PAVING_STONES -> Item(this, R.drawable.surface_paving_stones, R.string.quest_surface_value_paving_stones)
+    COMPACTED -> Item(this, R.drawable.surface_compacted, R.string.quest_surface_value_compacted)
+    DIRT -> Item(this, R.drawable.surface_dirt, R.string.quest_surface_value_dirt)
+    SETT -> Item(this, R.drawable.surface_sett, R.string.quest_surface_value_sett)
+    UNHEWN_COBBLESTONE -> Item(this, R.drawable.surface_cobblestone, R.string.quest_surface_value_unhewn_cobblestone)
+    GRASS_PAVER -> Item(this, R.drawable.surface_grass_paver, R.string.quest_surface_value_grass_paver)
+    WOOD -> Item(this, R.drawable.surface_wood, R.string.quest_surface_value_wood)
+    METAL -> Item(this, R.drawable.surface_metal, R.string.quest_surface_value_metal)
+    GRAVEL -> Item(this, R.drawable.surface_gravel, R.string.quest_surface_value_gravel)
+    PEBBLES -> Item(this, R.drawable.surface_pebblestone, R.string.quest_surface_value_pebblestone)
+    GRASS -> Item(this, R.drawable.surface_grass, R.string.quest_surface_value_grass)
+    SAND -> Item(this, R.drawable.surface_sand, R.string.quest_surface_value_sand)
+    ROCK -> Item(this, R.drawable.surface_rock, R.string.quest_surface_value_rock)
+    CLAY -> Item(this, R.drawable.surface_tennis_clay, R.string.quest_surface_value_clay)
+    ARTIFICIAL_TURF -> Item(this, R.drawable.surface_artificial_turf, R.string.quest_surface_value_artificial_turf)
+    TARTAN -> Item(this, R.drawable.surface_tartan, R.string.quest_surface_value_tartan)
+    PAVED -> Item(this, R.drawable.path_surface_paved, R.string.quest_surface_value_paved)
+    UNPAVED -> Item(this, R.drawable.path_surface_unpaved, R.string.quest_surface_value_unpaved)
+    GROUND -> Item(this, R.drawable.surface_ground, R.string.quest_surface_value_ground)
 }
