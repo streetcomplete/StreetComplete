ef1091d0069d78b99b804df42330853e023ebc7c
Flo Edelmann <florian-edelmann@online.de>
2021-01-14 18:42:58 +0100

    Extract `PowerPolesMaterial` data model

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/powerpoles_material/PowerPolesMaterial.kt
@@ -0,0 +1,7 @@
+package de.westnordost.streetcomplete.quests.powerpoles_material
+
+enum class PowerPolesMaterial(val osmValue: String) {
+    WOOD("wood"),
+    STEEL("steel"),
+    CONCRETE("concrete"),
+}
