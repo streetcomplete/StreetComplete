331c30c904745494ab64f94dabe1e89eee34d0eb
Flo Edelmann <florian-edelmann@online.de>
2021-01-14 20:20:49 +0100

    Extract `TourismInformation` data model

--- app/src/main/java/de/westnordost/streetcomplete/quests/tourism_information/AddInformationForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/tourism_information/AddInformationForm.kt
@@ -4,18 +4,19 @@ import android.os.Bundle
 
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.quests.AImageListQuestAnswerFragment
+import de.westnordost.streetcomplete.quests.tourism_information.TourismInformation.*
 import de.westnordost.streetcomplete.view.image_select.Item
 
-class AddInformationForm : AImageListQuestAnswerFragment<String,String>() {
+class AddInformationForm : AImageListQuestAnswerFragment<TourismInformation, TourismInformation>() {
 
     override val itemsPerRow = 2
 
     override val items get() = listOf(
-        Item("office", R.drawable.tourism_information_office, R.string.quest_tourism_information_office),
-        Item("board", R.drawable.tourism_information_board, R.string.quest_tourism_information_board),
-        Item("terminal", R.drawable.tourism_information_terminal, R.string.quest_tourism_information_terminal),
-        Item("map", R.drawable.tourism_information_map, R.string.quest_tourism_information_map),
-        Item("guidepost", R.drawable.tourism_information_guidepost, R.string.quest_tourism_information_guidepost)
+        Item(OFFICE, R.drawable.tourism_information_office, R.string.quest_tourism_information_office),
+        Item(BOARD, R.drawable.tourism_information_board, R.string.quest_tourism_information_board),
+        Item(TERMINAL, R.drawable.tourism_information_terminal, R.string.quest_tourism_information_terminal),
+        Item(MAP, R.drawable.tourism_information_map, R.string.quest_tourism_information_map),
+        Item(GUIDEPOST, R.drawable.tourism_information_guidepost, R.string.quest_tourism_information_guidepost)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +24,7 @@ class AddInformationForm : AImageListQuestAnswerFragment<String,String>() {
         imageSelector.cellLayoutId = R.layout.cell_icon_select_with_label_below
     }
 
-    override fun onClickOk(selectedItems: List<String>) {
+    override fun onClickOk(selectedItems: List<TourismInformation>) {
         applyAnswer(selectedItems.single())
     }
 }
