5be5d0b2e1a80268a27f4813f98d1dbc508998ea
Tobias Zwick <newton@westnordost.de>
2021-01-30 01:35:54 +0100

    Revert "move some code around..."
    This reverts commit e712049be8c4219096eb4787453149bde3ac6dd0.

--- app/src/main/java/de/westnordost/streetcomplete/data/osmnotes/OsmAvatarsInNotesUpdater.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/osmnotes/OsmAvatarsInNotesUpdater.kt
@@ -1,22 +1,14 @@
 package de.westnordost.streetcomplete.data.osmnotes
 
 import de.westnordost.osmapi.notes.Note
-import de.westnordost.streetcomplete.data.UserApi
 import javax.inject.Inject
 
-class OsmAvatarsInNotesUpdater @Inject constructor(
-    private val userApi: UserApi,
-    private val downloader: OsmAvatarsDownloader
-) : NoteSource.Listener {
+class OsmAvatarsInNotesUpdater @Inject constructor(private val downloader: OsmAvatarsDownloader) :
+    NoteSource.Listener {
 
     override fun onUpdated(added: Collection<Note>, updated: Collection<Note>, deleted: Collection<Long>) {
         val noteCommentUserIds = (added + updated).flatMap { it.userIds }.toSet()
-        for (userId in noteCommentUserIds) {
-            val avatarUrl = userApi.get(userId)?.profileImageUrl
-            if (avatarUrl != null) {
-                downloader.download(userId, avatarUrl)
-            }
-        }
+        downloader.download(noteCommentUserIds)
     }
 }
 
