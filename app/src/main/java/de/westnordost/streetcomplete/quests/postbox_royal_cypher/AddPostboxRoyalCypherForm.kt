88d5b74602dd33548af0bbd001c5d12dc1943c9c
Tobias Zwick <newton@westnordost.de>
2021-02-17 03:00:36 +0100

    reorder by prevalence; add labels

--- app/src/main/java/de/westnordost/streetcomplete/quests/postbox_royal_cypher/AddPostboxRoyalCypherForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/postbox_royal_cypher/AddPostboxRoyalCypherForm.kt
@@ -1,21 +1,21 @@
 package de.westnordost.streetcomplete.quests.postbox_royal_cypher
 
+import android.os.Bundle
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.quests.AImageListQuestAnswerFragment
 import de.westnordost.streetcomplete.quests.OtherAnswer
-import de.westnordost.streetcomplete.quests.postbox_royal_cypher.PostboxRoyalCypher
 import de.westnordost.streetcomplete.view.image_select.Item
 
 class AddPostboxRoyalCypherForm : AImageListQuestAnswerFragment<PostboxRoyalCypher, PostboxRoyalCypher>() {
 
     override val items = listOf(
-        Item(PostboxRoyalCypher.ELIZABETH_II,   R.drawable.postbox_royal_cypher_eiir),
-        Item(PostboxRoyalCypher.GEORGE_VI,      R.drawable.postbox_royal_cypher_gvir),
-        Item(PostboxRoyalCypher.EDWARD_VIII,    R.drawable.postbox_royal_cypher_eviiir),
-        Item(PostboxRoyalCypher.GEORGE_V,       R.drawable.postbox_royal_cypher_gr),
-        Item(PostboxRoyalCypher.EDWARD_VII,     R.drawable.postbox_royal_cypher_eviir),
-        Item(PostboxRoyalCypher.VICTORIA,       R.drawable.postbox_royal_cypher_vr),
-        Item(PostboxRoyalCypher.SCOTTISH_CROWN, R.drawable.postbox_royal_cypher_scottish_crown, R.string.quest_postboxRoyalCypher_type_scottish_crown)
+        Item(PostboxRoyalCypher.ELIZABETH_II,   R.drawable.ic_postbox_royal_cypher_eiir,           R.string.quest_postboxRoyalCypher_type_eiir),
+        Item(PostboxRoyalCypher.GEORGE_V,       R.drawable.ic_postbox_royal_cypher_gr,             R.string.quest_postboxRoyalCypher_type_gr),
+        Item(PostboxRoyalCypher.GEORGE_VI,      R.drawable.ic_postbox_royal_cypher_gvir,           R.string.quest_postboxRoyalCypher_type_gvir),
+        Item(PostboxRoyalCypher.VICTORIA,       R.drawable.ic_postbox_royal_cypher_vr,             R.string.quest_postboxRoyalCypher_type_vr),
+        Item(PostboxRoyalCypher.EDWARD_VII,     R.drawable.ic_postbox_royal_cypher_eviir,          R.string.quest_postboxRoyalCypher_type_eviir),
+        Item(PostboxRoyalCypher.SCOTTISH_CROWN, R.drawable.ic_postbox_royal_cypher_scottish_crown, R.string.quest_postboxRoyalCypher_type_scottish_crown),
+        Item(PostboxRoyalCypher.EDWARD_VIII,    R.drawable.ic_postbox_royal_cypher_eviiir,         R.string.quest_postboxRoyalCypher_type_eviiir),
     )
 
     override val otherAnswers = listOf(
@@ -23,6 +23,11 @@ class AddPostboxRoyalCypherForm : AImageListQuestAnswerFragment<PostboxRoyalCyph
     )
 
     override val itemsPerRow = 3
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        imageSelector.cellLayoutId = R.layout.cell_icon_select_with_label_below
+    }
 
     override fun onClickOk(selectedItems: List<PostboxRoyalCypher>) {
         applyAnswer(selectedItems.single())
