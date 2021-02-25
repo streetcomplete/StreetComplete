216222a2882abeb7294547474339acd6594d4a53
Flo Edelmann <florian-edelmann@online.de>
2021-02-25 20:18:18 +0100

    Revert "Show name in `AddChargingStationOperator`"

--- app/src/main/java/de/westnordost/streetcomplete/quests/charging_station_operator/AddChargingStationOperator.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/charging_station_operator/AddChargingStationOperator.kt
@@ -3,7 +3,6 @@ package de.westnordost.streetcomplete.quests.charging_station_operator
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder
 import de.westnordost.streetcomplete.data.osm.osmquest.OsmFilterQuestType
-import de.westnordost.streetcomplete.ktx.containsAnyKey
 
 class AddChargingStationOperator : OsmFilterQuestType<String>() {
 
@@ -13,11 +12,7 @@ class AddChargingStationOperator : OsmFilterQuestType<String>() {
     override val icon = R.drawable.ic_quest_car_charger
     override val isDeleteElementEnabled = true
 
-    override fun getTitle(tags: Map<String, String>): Int =
-        if (tags.containsAnyKey("name", "brand"))
-            R.string.quest_charging_station_name_operator_title
-        else
-            R.string.quest_charging_station_operator_title
+    override fun getTitle(tags: Map<String, String>): Int = R.string.quest_charging_station_operator_title
 
     override fun createForm() = AddChargingStationOperatorForm()
 
