80eae28d6d4ffcbe952203be764317633d51831f
Tobias Zwick <newton@westnordost.de>
2021-03-04 21:04:22 +0100

    update code contributors, add links to github profiles

--- app/src/main/java/de/westnordost/streetcomplete/map/MapFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/map/MapFragment.kt
@@ -7,7 +7,6 @@ import android.graphics.PointF
 import android.graphics.RectF
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
