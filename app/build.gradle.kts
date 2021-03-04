0f96eb1fdd028b19affc6cddf0e7d0d6d4ba7e97
Tobias Zwick <newton@westnordost.de>
2021-03-04 21:12:14 +0100

    version up, update translations and metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "de.westnordost.streetcomplete"
         minSdkVersion(17)
         targetSdkVersion(30)
-        versionCode = 3100
-        versionName = "31.0-beta1"
+        versionCode = 3101
+        versionName = "31.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
