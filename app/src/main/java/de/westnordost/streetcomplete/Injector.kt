5465ef26c3b73f0f6240ef134d384fe1c4ef221c
Tobias Zwick <newton@westnordost.de>
2021-03-07 18:55:36 +0100

    remove explicit module initializations as they are not needed anymore now it seems

--- app/src/main/java/de/westnordost/streetcomplete/Injector.kt
+++ app/src/main/java/de/westnordost/streetcomplete/Injector.kt
@@ -1,22 +1,5 @@
 package de.westnordost.streetcomplete
 
-import de.westnordost.streetcomplete.data.DbModule
-import de.westnordost.streetcomplete.data.OsmApiModule
-import de.westnordost.streetcomplete.data.download.DownloadModule
-import de.westnordost.streetcomplete.data.meta.MetadataModule
-import de.westnordost.streetcomplete.data.osm.edits.ElementEditsModule
-import de.westnordost.streetcomplete.data.osm.osmquests.OsmQuestModule
-import de.westnordost.streetcomplete.data.osmnotes.NotesModule
-import de.westnordost.streetcomplete.data.osmnotes.edits.NoteEditsModule
-import de.westnordost.streetcomplete.data.osmnotes.notequests.OsmNoteQuestModule
-import de.westnordost.streetcomplete.data.upload.UploadModule
-import de.westnordost.streetcomplete.data.user.UserModule
-import de.westnordost.streetcomplete.data.user.achievements.AchievementsModule
-import de.westnordost.streetcomplete.data.visiblequests.VisibleQuestTypeModule
-import de.westnordost.streetcomplete.map.MapModule
-import de.westnordost.streetcomplete.quests.QuestModule
-import de.westnordost.streetcomplete.quests.oneway_suspects.data.TrafficFlowSegmentsModule
-
 object Injector {
 
     lateinit var applicationComponent: ApplicationComponent
@@ -25,22 +8,6 @@ object Injector {
     fun initializeApplicationComponent(app: StreetCompleteApplication?) {
         applicationComponent = DaggerApplicationComponent.builder()
             .applicationModule(ApplicationModule(app!!)) // not sure why it is necessary to add these all by hand, I must be doing something wrong
-            .achievementsModule(AchievementsModule)
-            .dbModule(DbModule)
-            .downloadModule(DownloadModule)
-            .metadataModule(MetadataModule)
-            .osmApiModule(OsmApiModule)
-            .notesModule(NotesModule)
-            .questModule(QuestModule)
-            .trafficFlowSegmentsModule(TrafficFlowSegmentsModule)
-            .uploadModule(UploadModule)
-            .userModule(UserModule)
-            .mapModule(MapModule)
-            .elementEditsModule(ElementEditsModule)
-            .noteEditsModule(NoteEditsModule)
-            .osmNoteQuestModule(OsmNoteQuestModule)
-            .osmQuestModule(OsmQuestModule)
-            .visibleQuestTypeModule(VisibleQuestTypeModule)
             .build()
     }
 }
