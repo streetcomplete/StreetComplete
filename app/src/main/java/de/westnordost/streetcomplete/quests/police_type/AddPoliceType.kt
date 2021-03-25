b4f27adf6112df574738ee14636eef2c2826fade
naposm <photonpwiki+githubnaposm@protonmail.com>
2021-03-25 12:12:03 +0100

    Fix

--- app/src/main/java/de/westnordost/streetcomplete/quests/police_type/AddPoliceType.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/police_type/AddPoliceType.kt
@@ -4,10 +4,8 @@ import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.osm.osmquest.OsmFilterQuestType
 import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder
 import de.westnordost.streetcomplete.data.quest.NoCountriesExcept
-import de.westnordost.streetcomplete.quests.police_type.AddPostboxRoyalCypherForm
-import de.westnordost.streetcomplete.quests.police_type.PostboxRoyalCypher
 
-class AddPoliceType : OsmFilterQuestType<PostboxRoyalCypher>() {
+class AddPoliceType : OsmFilterQuestType<PoliceType>() {
 
     override val elementFilter = """
         nodes, ways, relations with
@@ -16,16 +14,19 @@ class AddPoliceType : OsmFilterQuestType<PostboxRoyalCypher>() {
     """
     override val commitMessage = "Add police type"
     override val wikiLink = "Tag:amenity=police"
-    override val icon = R.drawable.ic_quest_crown
+    override val icon = R.drawable.ic_quest_police
     override val enabledInCountries = NoCountriesExcept(
         "IT"
     )
 
-    override fun getTitle(tags: Map<String, String>) = R.string.quest_postboxRoyalCypher_title
+    override fun getTitle(tags: Map<String, String>) = R.string.quest_policeType_title
 
-    override fun createForm() = AddPostboxRoyalCypherForm()
+    override fun createForm() = AddPoliceTypeForm()
 
-    override fun applyAnswerTo(answer: PostboxRoyalCypher, changes: StringMapChangesBuilder) {
-        changes.add("royal_cypher", answer.osmValue)
+    override fun applyAnswerTo(answer: PoliceType, changes: StringMapChangesBuilder) {
+        changes.add("name", answer.policeName);
+        changes.add("operator", answer.operator.operatorName);
+        if (!answer.operator.wikidata.isNullOrEmpty()) changes.add("operator:wikidata", answer.operator.wikidata);
+        if (!answer.operator.wikipedia.isNullOrEmpty()) changes.add("operator:wikipedia", answer.operator.wikipedia);
     }
 }
