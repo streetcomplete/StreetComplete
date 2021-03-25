12f74637d7ffcf0abbbcf430df64ea2c3a644ae9
Tobias Zwick <newton@westnordost.de>
2021-03-25 20:12:09 +0100

    add some todos

--- app/src/main/java/de/westnordost/streetcomplete/data/edithistory/Edit.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/edithistory/Edit.kt
@@ -2,8 +2,8 @@ package de.westnordost.streetcomplete.data.edithistory
 
 import de.westnordost.osmapi.map.data.LatLon
 
-// TODO make sealed as soon as kotlin supports this
-/* sealed*/ interface Edit {
+// TODO make sealed as soon as kotlin supports this (1.5)
+interface Edit {
     val createdTimestamp: Long
     val isUndoable: Boolean
     val position: LatLon
--- app/src/main/java/de/westnordost/streetcomplete/data/osm/edits/update_tags/StringMapEntryChange.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/osm/edits/update_tags/StringMapEntryChange.kt
@@ -1,5 +1,6 @@
 package de.westnordost.streetcomplete.data.osm.edits.update_tags
 
+// TODO make sealed as soon as Kotlin supports that (1.5)
 interface StringMapEntryChange {
     override fun toString(): String
     override fun equals(other: Any?): Boolean
