b366ed57a89fd36a662994446fa0f44ce15f06da
Tobias Zwick <newton@westnordost.de>
2021-04-09 13:07:11 +0200

    make it easier readable and performance improvements
    - exit early if no crossing nodes are left so to avoid iterating through almost all nodes of ways in the data set if possible
    - use sequences instead of copying each the whole data on each filter step
    - reduce the connectionsById map early (delete entries that will not be used)
    - don't use that enum. @matkoniecz : Sorry about that, I find it better readable without it and it is possible to implement this in a well readable form without using any magic numbers

--- app/src/main/java/de/westnordost/streetcomplete/quests/kerb_height/KerbUtil.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/kerb_height/KerbUtil.kt
@@ -23,12 +23,14 @@ private val waysFilter by lazy {"""
 
 fun MapData.findAllKerbNodes(): Iterable<Node> {
     val footwayNodes = mutableSetOf<Node>()
-    ways.filter { footwaysFilter.matches(it) }
+    ways.asSequence()
+        .filter { footwaysFilter.matches(it) }
         .flatMap { it.nodeIds }
         .mapNotNullTo(footwayNodes) { getNode(it) }
 
     val kerbBarrierNodeIds = mutableSetOf<Long>()
-    ways.filter { it.tags?.get("barrier") == "kerb" }
+    ways.asSequence()
+        .filter { it.tags?.get("barrier") == "kerb" }
         .flatMapTo(kerbBarrierNodeIds) { it.nodeIds }
 
     val anyWays = ways.filter { waysFilter.matches(it) }
@@ -47,60 +49,51 @@ fun MapData.findAllKerbNodes(): Iterable<Node> {
     }
 }
 
-enum class Crossing {
-    MORE_THAN_ONE_CROSSING_CONNECTION,
-    NO_CROSSING_CONNECTION,
-    ONE_CROSSING_CONNECTION,
-    ONE_CROSSING_AND_ONE_SIDEWALK_CONNECTION,
-    ONE_CROSSING_AND_MORE_THAN_ONE_SIDEWALK_CONNECTION
-}
-
 /** Find all node ids of end nodes of crossings that are (very probably) kerbs within the given
  *  collection of [ways] */
 private fun findCrossingKerbEndNodeIds(ways: Collection<Way>): Set<Long> {
-    val footways = ways.filter { footwaysFilter.matches(it) }
-    val crossingEndNodesConnectionCountByIds = mutableMapOf<Long, Crossing>()
+    /* using asSequence in this function so to not copy potentially huge amounts (f.e. almost all
+       nodes of all ways in the data set) of data into temporary lists */
 
-    val crossingEndNodeIds = footways
+    val footways = ways.filter { footwaysFilter.matches(it) }
+
+    val crossingEndNodeIds = footways.asSequence()
         .filter { it.tags?.get("footway") == "crossing" }
         .flatMap { it.nodeIds.firstAndLast() }
 
+    val connectionsById = mutableMapOf<Long, Int>()
     for (id in crossingEndNodeIds) {
-        val prevState = crossingEndNodesConnectionCountByIds[id] ?: Crossing.NO_CROSSING_CONNECTION
-        if (prevState != Crossing.NO_CROSSING_CONNECTION) {
-            // connected already, so invalid - marking rather removing it from map
-            // as it may be connected to three or more crossings
-            crossingEndNodesConnectionCountByIds[id] = Crossing.MORE_THAN_ONE_CROSSING_CONNECTION
-        } else {
-            crossingEndNodesConnectionCountByIds[id] = Crossing.ONE_CROSSING_CONNECTION
-        }
+        val count = connectionsById[id] ?: 0
+        connectionsById[id] = count + 1
     }
+    // skip nodes that have not exactly ONE connection to a crossing
+    connectionsById.entries.removeAll { it.value != 1 }
+    if (connectionsById.isEmpty()) return emptySet()
 
-    // skip nodes that share an end node with a way where it is not clear if it is a sidewalk, crossing or something else
-    val unknownEndNodeIds = ways
-        .filter { it.tags?.get("footway") != "sidewalk" && it.tags?.get("footway") != "crossing" }
-        .flatMap { it.nodeIds.firstAndLast() }
-    crossingEndNodesConnectionCountByIds.keys.removeAll(unknownEndNodeIds)
-
-    // skip nodes that share an end node with any node of a way that is not an end node
-    val waysMiddleNodeIds = ways.flatMap { it.nodeIds.subList(1, it.nodeIds.size - 1) }
-    crossingEndNodesConnectionCountByIds.keys.removeAll(waysMiddleNodeIds)
-
-    // count connections of the remaining crossing end node ids with end nodes of sidewalks
-    val sidewalkEndNodeIds = footways
+    val sidewalkEndNodeIds = footways.asSequence()
         .filter { it.tags?.get("footway") == "sidewalk" }
         .flatMap { it.nodeIds.firstAndLast() }
 
     for (id in sidewalkEndNodeIds) {
-        val prevState = crossingEndNodesConnectionCountByIds[id] ?: Crossing.NO_CROSSING_CONNECTION
-        if (prevState == Crossing.ONE_CROSSING_AND_ONE_SIDEWALK_CONNECTION) {
-            crossingEndNodesConnectionCountByIds[id] = Crossing.ONE_CROSSING_AND_MORE_THAN_ONE_SIDEWALK_CONNECTION
-        } else if (prevState == Crossing.ONE_CROSSING_CONNECTION) {
-            crossingEndNodesConnectionCountByIds[id] = Crossing.ONE_CROSSING_AND_ONE_SIDEWALK_CONNECTION
-        }
+        val count = connectionsById[id] ?: continue
+        connectionsById[id] = count + 1
     }
+    // skip nodes that have not exactly ONE connection to a sidewalk (1 to crossing + 1 to sidewalk = 2)
+    connectionsById.entries.removeAll { it.value != 2 }
+    if (connectionsById.isEmpty()) return emptySet()
 
-    // if there are exactly two connections (one to crossing way, one to sidewalk way), this'll be
-    // a node where there is a kerb
-    return crossingEndNodesConnectionCountByIds.filter { it.value == Crossing.ONE_CROSSING_AND_ONE_SIDEWALK_CONNECTION }.keys
+    // skip nodes that share an end node with a way where it is not clear if it is a sidewalk, crossing or something else
+    ways.asSequence()
+        .filter { it.tags?.get("footway") != "sidewalk" && it.tags?.get("footway") != "crossing" }
+        .flatMap { it.nodeIds.firstAndLast() }
+        .forEach { connectionsById.remove(it) }
+    if (connectionsById.isEmpty()) return emptySet()
+
+    // skip nodes that share an end node with any node of a way that is not an end node
+    ways.asSequence()
+        .flatMap { it.nodeIds.subList(1, it.nodeIds.size - 1) }
+        .forEach { connectionsById.remove(it) }
+    if (connectionsById.isEmpty()) return emptySet()
+
+    return connectionsById.keys
 }
