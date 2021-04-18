3bddcf6e141d6957f97a3914ccfa53f2fb9210a9
Tobias Zwick <newton@westnordost.de>
2021-04-18 23:57:48 +0200

    remove now unnecessary LightweightOsmMapDataFactory

--- app/src/main/java/de/westnordost/streetcomplete/data/osm/mapdata/MapDataApiImpl.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/osm/mapdata/MapDataApiImpl.kt
@@ -22,9 +22,7 @@ import java.util.Date
 
 class MapDataApiImpl(osm: OsmConnection) : MapDataApi {
 
-    // generally we are not interested in certain data returned by the OSM API, so we use a
-    // map data factory that does not include that data
-    private val mapDataDao: MapDataDao = MapDataDao(osm, LightweightOsmMapDataFactory())
+    private val mapDataDao: MapDataDao = MapDataDao(osm)
 
     override fun uploadChanges(changesetId: Long, elements: Collection<Element>): MapDataUpdates {
         val handler = UpdatedElementsHandler()
