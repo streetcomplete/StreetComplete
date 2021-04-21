cf04ff5107d989f9ac158acad965ee0238b0dfb0
Tobias Zwick <newton@westnordost.de>
2021-04-21 15:27:20 +0200

    fix imports

--- app/src/main/java/de/westnordost/streetcomplete/quests/police_type/AddPoliceType.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/police_type/AddPoliceType.kt
@@ -1,8 +1,8 @@
 package de.westnordost.streetcomplete.quests.police_type
 
 import de.westnordost.streetcomplete.R
-import de.westnordost.streetcomplete.data.osm.osmquest.OsmFilterQuestType
-import de.westnordost.streetcomplete.data.osm.changes.StringMapChangesBuilder
+import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapChangesBuilder
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
 import de.westnordost.streetcomplete.data.quest.NoCountriesExcept
 
 class AddPoliceType : OsmFilterQuestType<PoliceType>() {
@@ -15,9 +15,7 @@ class AddPoliceType : OsmFilterQuestType<PoliceType>() {
     override val commitMessage = "Add police type"
     override val wikiLink = "Tag:amenity=police"
     override val icon = R.drawable.ic_quest_police
-    override val enabledInCountries = NoCountriesExcept(
-        "IT"
-    )
+    override val enabledInCountries = NoCountriesExcept("IT")
 
     override fun getTitle(tags: Map<String, String>) = R.string.quest_policeType_title
 
