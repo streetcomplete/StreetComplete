streetcomplete/StreetComplete

commit 84f39d8124eb7f496992de3caf100a3f7baea069

theoretically periodically ask for existence of emergency access point(#2679)

app/src/main/java/de/westnordost/streetcomplete/quests/existence/CheckExistence.kt
@@ -42,6 +42,7 @@ class CheckExistence(
             or advertising ~ column|board|poster_box
             or traffic_calming ~ bump|hump|island|cushion|choker|rumble_strip|chicane|dip
             or traffic_calming = table and !highway and !crossing
+            or (highway = emergency_access_point or emergency = access_point) and ref
           )
           and (${lastChecked(4.0)})
         )) and access !~ no|private
@@ -73,7 +74,7 @@ class CheckExistence(
             R.string.quest_existence_title
 
     override fun getTitleArgs(tags: Map<String, String>, featureName: Lazy<String?>): Array<String> {
-        val name = tags["name"] ?: tags["brand"] ?: tags["operator"]
+        val name = tags["name"] ?: tags["ref"] ?: tags["brand"] ?: tags["operator"]
         return arrayOfNotNull(name, featureName.value)
     }
 

commit e7a4cf3d2e533a486571b894356a417c4486db39

Ensure the list of tags checked for to decide the title type matches those used to fetch the name
Fixes bug introduced in 84f39d8124eb7f496992de3caf100a3f7baea069

app/src/main/java/de/westnordost/streetcomplete/quests/existence/CheckExistence.kt
@@ -67,7 +67,7 @@ class CheckExistence(
     override val icon = R.drawable.ic_quest_check
 
     override fun getTitle(tags: Map<String, String>): Int =
-        if (tags.containsAnyKey("name", "brand", "operator"))
+        if (tags.containsAnyKey("name", "ref", "brand", "operator"))
             R.string.quest_existence_name_title
         else
             R.string.quest_existence_title