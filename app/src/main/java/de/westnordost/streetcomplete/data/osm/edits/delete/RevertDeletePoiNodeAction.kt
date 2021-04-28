d75ca3f99828228c5e74428488fa9e9579e6914f
Tobias Zwick <newton@westnordost.de>
2021-04-28 15:47:56 +0200

    correctly mark RevertDeletePoiNodeAction as RevertAction

--- app/src/main/java/de/westnordost/streetcomplete/data/osm/edits/delete/RevertDeletePoiNodeAction.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/osm/edits/delete/RevertDeletePoiNodeAction.kt
@@ -2,6 +2,7 @@ package de.westnordost.streetcomplete.data.osm.edits.delete
 
 import de.westnordost.streetcomplete.data.osm.edits.ElementEditAction
 import de.westnordost.streetcomplete.data.osm.edits.ElementIdProvider
+import de.westnordost.streetcomplete.data.osm.edits.IsRevertAction
 import de.westnordost.streetcomplete.data.osm.edits.NewElementsCount
 import de.westnordost.streetcomplete.data.osm.mapdata.*
 import de.westnordost.streetcomplete.data.upload.ConflictException
@@ -11,7 +12,7 @@ import java.lang.System.currentTimeMillis
 /** Action that restores a POI node to the previous state before deletion/clearing of tags
  */
 @Serializable
-object RevertDeletePoiNodeAction : ElementEditAction {
+object RevertDeletePoiNodeAction : ElementEditAction, IsRevertAction {
 
     /** No "new" elements are created, instead, an old one is being revived */
     override val newElementsCount get() = NewElementsCount(0,0,0)
