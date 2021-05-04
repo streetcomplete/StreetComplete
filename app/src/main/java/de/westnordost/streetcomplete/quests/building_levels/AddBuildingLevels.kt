320117340ae317e759d70388515534be7fc163d5
Mateusz Konieczny <matkoniecz@gmail.com>
2021-05-04 09:17:02 +0200

    exclude building=industrial from building levels
    fixes #2835

--- app/src/main/java/de/westnordost/streetcomplete/quests/building_levels/AddBuildingLevels.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/building_levels/AddBuildingLevels.kt
@@ -34,5 +34,5 @@ private val BUILDINGS_WITH_LEVELS = arrayOf(
     "house","residential","apartments","detached","terrace","dormitory","semi",
     "semidetached_house","bungalow","school","civic","college","university","public",
     "hospital","kindergarten","transportation","train_station", "hotel","retail",
-    "commercial","office","industrial","manufacture","parking","farm","farm_auxiliary",
+    "commercial","office","manufacture","parking","farm","farm_auxiliary",
     "cabin")
