ffa98e182ca93f3671024a5360910cfa4f8d5b46
JayTurnr <jaynicholasturner@gmail.com>
2021-05-05 15:38:21 +0100

    Add title and descriptions to bridge:structure quest

--- app/src/main/java/de/westnordost/streetcomplete/quests/bridge_structure/AddBridgeStructureForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/bridge_structure/AddBridgeStructureForm.kt
@@ -9,15 +9,15 @@ class AddBridgeStructureForm : AImageListQuestAnswerFragment<BridgeStructure, Br
 
     // structures sorted highest to lowest amount of values on taginfo, footbridge-types last
     override val items = listOf(
-        Item(BEAM, R.drawable.bridge_structure_beam),
-        Item(SUSPENSION, R.drawable.bridge_structure_suspension),
-        Item(ARCH, R.drawable.bridge_structure_arch),
-        Item(TIED_ARCH, R.drawable.bridge_structure_tied_arch),
-        Item(TRUSS, R.drawable.bridge_structure_truss),
-        Item(CABLE_STAYED, R.drawable.bridge_structure_cablestayed),
-        Item(HUMPBACK, R.drawable.bridge_structure_humpback),
-        Item(SIMPLE_SUSPENSION, R.drawable.bridge_structure_simple_suspension),
-        Item(FLOATING, R.drawable.bridge_structure_floating)
+        Item(BEAM, R.drawable.bridge_structure_beam, R.string.bridge_structure_beam_title, R.string.bridge_structure_beam_desc),
+        Item(SUSPENSION, R.drawable.bridge_structure_suspension, R.string.bridge_structure_suspension_title, R.string.bridge_structure_suspension_desc),
+        Item(ARCH, R.drawable.bridge_structure_arch, R.string.bridge_structure_arch_title),
+        Item(TIED_ARCH, R.drawable.bridge_structure_tied_arch, R.string.bridge_structure_tied_arch_title),
+        Item(TRUSS, R.drawable.bridge_structure_truss, R.string.bridge_structure_truss_title),
+        Item(CABLE_STAYED, R.drawable.bridge_structure_cablestayed, R.string.bridge_structure_cablestayed_title, R.string.bridge_structure_cablestayed_desc),
+        Item(HUMPBACK, R.drawable.bridge_structure_humpback, R.string.bridge_structure_humpback_title),
+        Item(SIMPLE_SUSPENSION, R.drawable.bridge_structure_simple_suspension, R.string.bridge_structure_simple_suspension_title),
+        Item(FLOATING, R.drawable.bridge_structure_floating, R.string.bridge_structure_floating_title, R.string.bridge_structure_floating_desc)
     )
 
     override val itemsPerRow = 2
