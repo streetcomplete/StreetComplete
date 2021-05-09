58a0bdd36f60f61e7124ea44101eca0828d313a7
Helium314 <43007630+Helium314@users.noreply.github.com>
2021-05-09 20:44:14 +0200

    fix text visiblity in cameraTypeForm

--- app/src/main/java/de/westnordost/streetcomplete/quests/camera_type/AddCameraTypeForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/camera_type/AddCameraTypeForm.kt
@@ -15,6 +15,11 @@ class AddCameraTypeForm : AImageListQuestAnswerFragment<CameraType, CameraType>(
 
     override val itemsPerRow = 3
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        imageSelector.cellLayoutId = R.layout.cell_icon_select_with_label_below
+    }
+
     override fun onClickOk(selectedItems: List<CameraType>) {
         applyAnswer(selectedItems.single())
     }
