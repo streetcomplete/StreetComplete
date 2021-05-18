ab624aa0a27fcd94e5487cdffca10117a58dcbc9
Tobias Zwick <newton@westnordost.de>
2021-05-18 13:58:30 +0200

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         val kotlinVersion = "1.4.30"
-        classpath("com.android.tools.build:gradle:4.2.0")
+        classpath("com.android.tools.build:gradle:4.2.1")
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
     }
 }
