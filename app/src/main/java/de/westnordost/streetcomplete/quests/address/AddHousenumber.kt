df17ebba32fdc6d4f2321d4204bb63688459fdfb
Mateusz Konieczny <matkoniecz@gmail.com>
2021-06-16 19:43:05 +0200

    add building=shop to buildings with address
    synonym of building=retail, not very popular but within top100

--- app/src/main/java/de/westnordost/streetcomplete/quests/address/AddHousenumber.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/address/AddHousenumber.kt
@@ -181,7 +181,7 @@ private val buildingsWithMissingAddressFilter by lazy { """
 private val buildingTypesThatShouldHaveAddresses = listOf(
     "house", "residential", "apartments", "detached", "terrace", "dormitory", "semi",
     "semidetached_house", "farm", "school", "civic", "college", "university", "public", "hospital",
-    "kindergarten", "train_station", "hotel", "retail", "commercial"
+    "kindergarten", "train_station", "hotel", "retail", "shop", "commercial"
 )
 
 private fun Element.containsAnyNode(nodeIds: Set<Long>, mapData: MapDataWithGeometry): Boolean =
