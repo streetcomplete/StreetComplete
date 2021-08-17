62bf99b095e264ea719a7923116dfa515e3c215f
Mateusz Konieczny <matkoniecz@gmail.com>
2021-08-17 10:10:17 +0200

    make stone stepover stiles less confusing
    fixes #2991

--- app/src/main/java/de/westnordost/streetcomplete/quests/barrier_type/AddStileTypeForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/barrier_type/AddStileTypeForm.kt
@@ -10,7 +10,8 @@ class AddStileTypeForm : AImageListQuestAnswerFragment<BarrierType, BarrierType>
     override val items = listOf(
         Item(STILE_SQUEEZER, R.drawable.barrier_stile_squeezer, R.string.quest_barrier_type_stile_squeezer),
         Item(STILE_LADDER, R.drawable.barrier_stile_ladder, R.string.quest_barrier_type_stile_ladder),
-        Item(STILE_STEPOVER, R.drawable.barrier_stile_stepover, R.string.quest_barrier_type_stepover),
+        Item(STILE_STEPOVER_WOODEN, R.drawable.barrier_stile_stepover_wooden, R.string.quest_barrier_type_stepover_wooden),
+        Item(STILE_STEPOVER_STONE, R.drawable.barrier_stile_stepover_stone, R.string.quest_barrier_type_stepover_stone),
         Item(KISSING_GATE, R.drawable.barrier_kissing_gate, R.string.quest_barrier_type_kissing_gate_conversion),
     )
 
