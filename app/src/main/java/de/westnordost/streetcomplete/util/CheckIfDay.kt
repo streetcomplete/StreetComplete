7afc96a1eed46e95bc8aae7343550f4199955240
Tobias Zwick <newton@westnordost.de>
2021-08-23 01:28:22 +0200

    simplify @TurnrDev's isDay function

--- app/src/main/java/de/westnordost/streetcomplete/util/CheckIfDay.kt
+++ app/src/main/java/de/westnordost/streetcomplete/util/CheckIfDay.kt
@@ -4,38 +4,24 @@ import com.luckycatlabs.sunrisesunset.Zenith
 import com.luckycatlabs.sunrisesunset.calculator.SolarEventCalculator
 import com.luckycatlabs.sunrisesunset.dto.Location
 import de.westnordost.streetcomplete.data.osm.mapdata.LatLon
-import java.time.LocalDate
 import java.time.LocalTime
-import java.time.ZoneId
-import java.time.ZonedDateTime
 import java.util.*
 
-fun localDateToCalendar(localDate: LocalDate): Calendar {
-    val calendar = Calendar.getInstance()
-    calendar.set(localDate.year, localDate.month.value, localDate.dayOfMonth)
-    return calendar
-}
-
+/** Returns whether it's currently light out. It will use the location of the node and check the
+ *  civil sunrise/sunset time.  */
 fun isDay(pos: LatLon): Boolean {
-    /* This functions job is to check if it's currently light out.
-    It will use the location of the node and check the civil sunrise/sunset time.
-
-    Sometimes sunset is after midnight. This would actually cause sunset to be before sunrise (as it's checking 00:00 to 23:59, it'll catch the day before!), so to gnt around this, we check the next day if that's the case.
-     */
-
-    val timezone = TimeZone.getDefault().id
     val location = Location(pos.latitude, pos.longitude)
-    val calculator = SolarEventCalculator(location, timezone)
-    val now = ZonedDateTime.now(ZoneId.of(timezone))
-    val today = now.toLocalDate()
+    val calculator = SolarEventCalculator(location, TimeZone.getDefault().id)
+    val today = Calendar.getInstance()
 
-    val sunrise = ZonedDateTime.of(today, LocalTime.parse(calculator.computeSunriseTime(Zenith.CIVIL, localDateToCalendar(today))), ZoneId.of(timezone))
-    val sunset = ZonedDateTime.of(today, LocalTime.parse(calculator.computeSunsetTime(Zenith.CIVIL, localDateToCalendar(today))), ZoneId.of(timezone))
+    val sunrise = LocalTime.parse(calculator.computeSunriseTime(Zenith.CIVIL, today))
+    val sunset = LocalTime.parse(calculator.computeSunsetTime(Zenith.CIVIL, today))
+    val now = LocalTime.now()
+
+    // if sunset is after midnight
     return if (sunset < sunrise) {
-
-        val sunset = ZonedDateTime.of(today.plusDays(1), LocalTime.parse(calculator.computeSunsetTime(Zenith.CIVIL, localDateToCalendar(today.plusDays(1)))), ZoneId.of(timezone))
-        now in sunrise..sunset
+        now.isAfter(sunrise) || now.isBefore(sunset)
     } else {
-        now in sunrise..sunset
+        now.isAfter(sunrise) && now.isBefore(sunset)
     }
 }
