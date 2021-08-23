5110e819a90cd7ea432a8e3f49f599bddf4c5e26
Tobias Zwick <newton@westnordost.de>
2021-08-23 02:02:59 +0200

    add cache for determining if it's night or day
    @TurnrDev @smichel17

--- app/src/main/java/de/westnordost/streetcomplete/data/visiblequests/DayNightQuestFilter.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/visiblequests/DayNightQuestFilter.kt
@@ -1,20 +1,32 @@
 package de.westnordost.streetcomplete.data.visiblequests
 
+import de.westnordost.streetcomplete.data.osm.mapdata.LatLon
 import de.westnordost.streetcomplete.data.quest.DayNightCycle.*
 import de.westnordost.streetcomplete.data.quest.Quest
-import de.westnordost.streetcomplete.util.isDay
+import de.westnordost.streetcomplete.util.DaylightTimes
+import de.westnordost.streetcomplete.util.getDaylightTimes
+import de.westnordost.streetcomplete.util.isDaylight
 import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.math.round
 
-class DayNightQuestFilter @Inject internal constructor() {
-    /*
-    Might be an idea to add a listener so this is reevaluated occasionally, or something like that.
-    However, I think it's reevaluated everytime the displayed quests are updated?
-     */
+@Singleton class DayNightQuestFilter @Inject internal constructor() {
+    /* This is a singleton because it owns an in-memory cache, no need to duplicate that */
+
+    private val cache: MutableMap<LatLon, DaylightTimes> = mutableMapOf()
+
+    private fun isDaylightAt(pos: LatLon): Boolean {
+        // using low precision lat lons (~ city level) because the sun and the earth is big
+        val lowPrecisionLatLon = LatLon(round(pos.latitude*10)/10, round(pos.longitude*10)/10)
+        val daylightTimes = cache.getOrPut(lowPrecisionLatLon) { getDaylightTimes(pos) }
+        return isDaylight(daylightTimes)
+    }
+
     fun isVisible(quest: Quest): Boolean {
         return when (quest.type.dayNightCycle) {
             DAY_AND_NIGHT -> true
-            ONLY_DAY -> isDay(quest.position)
-            ONLY_NIGHT -> !isDay(quest.position)
+            ONLY_DAY -> isDaylightAt(quest.position)
+            ONLY_NIGHT -> !isDaylightAt(quest.position)
         }
     }
 }
