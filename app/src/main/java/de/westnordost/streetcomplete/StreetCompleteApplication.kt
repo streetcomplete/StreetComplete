9130a0fc6fcf28a0f379c512de1174caf6b09351
Tobias Zwick <newton@westnordost.de>
2021-08-26 13:08:32 +0200

    doesn't really solve anything because that function is not called in production, but at least to document the intention in code
    https://youtrack.jetbrains.com/issue/KT-48306

--- app/src/main/java/de/westnordost/streetcomplete/StreetCompleteApplication.kt
+++ app/src/main/java/de/westnordost/streetcomplete/StreetCompleteApplication.kt
@@ -56,4 +56,9 @@ class StreetCompleteApplication : MultiDexApplication() {
         // on each new version, invalidate quest cache
         downloadedTilesDao.removeAll()
     }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        applicationScope.cancel()
+    }
 }
