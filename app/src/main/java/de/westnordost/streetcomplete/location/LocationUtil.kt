8604761d0645a123aea92f9087b2407f3f192ddc
Stephen Michel <git@smichel.me>
2021-09-08 17:05:43 -0400

    Naive LocationUtils conversion to kotlin
    Done with the automatic converter, and then reformatted to match the
    previous style as closely as possible.

--- app/src/main/java/de/westnordost/streetcomplete/location/LocationUtil.kt
+++ app/src/main/java/de/westnordost/streetcomplete/location/LocationUtil.kt
@@ -1,31 +1,26 @@
-package de.westnordost.streetcomplete.location;
+package de.westnordost.streetcomplete.location
 
-import android.Manifest;
-import android.content.Context;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
+import android.Manifest
+import android.content.Context
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.location.LocationManager
 
-import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
-import androidx.core.location.LocationManagerCompat;
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationManagerCompat
 
-public class LocationUtil
-{
-	public static boolean isLocationEnabled(@NonNull Context context)
-	{
-		return hasLocationPermission(context) && LocationManagerCompat
-				.isLocationEnabled(ContextCompat.getSystemService(context, LocationManager.class));
-	}
+object LocationUtil {
+    fun isLocationEnabled(context: Context): Boolean {
+        return hasLocationPermission(context) && LocationManagerCompat
+                .isLocationEnabled(ContextCompat.getSystemService(context, LocationManager::class.java)!!)
+    }
 
-	public static boolean hasLocationPermission(@NonNull Context context)
-	{
-		return ContextCompat.checkSelfPermission(context,
-				Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-	}
+    fun hasLocationPermission(context: Context): Boolean {
+        return ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
 
-	public static IntentFilter createLocationAvailabilityIntentFilter()
-	{
-		return new IntentFilter(LocationManager.MODE_CHANGED_ACTION);
-	}
+    fun createLocationAvailabilityIntentFilter(): IntentFilter {
+        return IntentFilter(LocationManager.MODE_CHANGED_ACTION)
+    }
 }
