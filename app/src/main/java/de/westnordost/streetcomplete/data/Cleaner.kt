29e7ab61d1b0a0295f9804698f38ce7e4a1ccfc6
Tobias Zwick <newton@westnordost.de>
2021-09-19 18:32:28 +0200

    clear new user achievements on restarting the app
    because if one doesn't read it the first time, it shouldn't block other notifications

--- app/src/main/java/de/westnordost/streetcomplete/data/Cleaner.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/Cleaner.kt
@@ -2,6 +2,7 @@ package de.westnordost.streetcomplete.data
 
 import android.util.Log
 import de.westnordost.streetcomplete.ApplicationConstants
+import de.westnordost.streetcomplete.data.notifications.NewUserAchievementsDao
 import de.westnordost.streetcomplete.data.osm.edits.ElementEditsController
 import de.westnordost.streetcomplete.data.osm.mapdata.MapDataController
 import de.westnordost.streetcomplete.data.osmnotes.NoteController
@@ -21,13 +22,15 @@ class Cleaner @Inject constructor(
     private val mapDataController: MapDataController,
     private val elementEditsController: ElementEditsController,
     private val noteEditsController: NoteEditsController,
-    private val questTypeRegistry: QuestTypeRegistry
+    private val questTypeRegistry: QuestTypeRegistry,
+    private val newUserAchievementsDao: NewUserAchievementsDao
 ) {
     suspend fun clean() {
         val time = currentTimeMillis()
         coroutineScope {
             launch { deleteOldData() }
             launch { deleteUndoHistory() }
+            launch { clearNewUserAchievements() }
         }
         Log.i(TAG, "Cleaning took ${((currentTimeMillis() - time) / 1000.0).format(1)}s")
     }
@@ -47,6 +50,10 @@ class Cleaner @Inject constructor(
         val undoableChangesTimestamp = currentTimeMillis() - ApplicationConstants.MAX_UNDO_HISTORY_AGE
         elementEditsController.deleteSyncedOlderThan(undoableChangesTimestamp)
         noteEditsController.deleteSyncedOlderThan(undoableChangesTimestamp)
+    }
+
+    private suspend fun clearNewUserAchievements() = withContext(Dispatchers.IO) {
+        newUserAchievementsDao.clear()
     }
 
     companion object {
