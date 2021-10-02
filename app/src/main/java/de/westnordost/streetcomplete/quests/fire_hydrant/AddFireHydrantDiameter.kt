f59ec6768f9b2e2fe46b43c16cbfbbc7ff857d5c
theFeiter <git@marvinfeiter.de>
2021-10-03 01:32:39 +0200

    Add Fire Hydrant Dimaeter Quest

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/fire_hydrant/AddFireHydrantDiameter.kt
@@ -0,0 +1,25 @@
+package de.westnordost.streetcomplete.quests.fire_hydrant
+
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
+import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapChangesBuilder
+import de.westnordost.streetcomplete.data.user.achievements.QuestTypeAchievement
+
+class AddFireHydrantDiameter : OsmFilterQuestType<Int>() {
+
+    override val elementFilter = "nodes with emergency = fire_hydrant and !fire_hydrant:diameter"
+    override val commitMessage = "Add fire hydrant diameter"
+    override val wikiLink = "Tag:emergency=fire_hydrant"
+    override val icon = R.drawable.ic_quest_fire_hydrant
+    override val isDeleteElementEnabled = true
+
+    override val questTypeAchievements = emptyList<QuestTypeAchievement>()
+
+    override fun getTitle(tags: Map<String, String>) = R.string.quest_fireHydrant_diameter_title
+
+    override fun createForm() = AddFireHydrantDiameterForm()
+
+    override fun applyAnswerTo(answer: Int, changes: StringMapChangesBuilder) {
+        changes.add("fire_hydrant:diameter", answer.toString())
+    }
+}
---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/fire_hydrant/AddFireHydrantDiameterForm.kt
@@ -0,0 +1,27 @@
+package de.westnordost.streetcomplete.quests.fire_hydrant
+
+import android.os.Bundle
+import android.view.View
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.databinding.QuestFireHydrantDiameterBinding
+import de.westnordost.streetcomplete.quests.AbstractQuestFormAnswerFragment
+import de.westnordost.streetcomplete.util.TextChangedWatcher
+
+class AddFireHydrantDiameterForm : AbstractQuestFormAnswerFragment<Int>() {
+
+    override val contentLayoutResId = R.layout.quest_fire_hydrant_diameter
+    private val binding by contentViewBinding(QuestFireHydrantDiameterBinding::bind)
+
+    private val diameter get() = binding.diameterInput.text?.toString().orEmpty().trim()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.diameterInput.addTextChangedListener(TextChangedWatcher { checkIsFormComplete() })
+    }
+
+    override fun isFormComplete() = diameter.isNotEmpty() && diameter.toInt() > 0
+
+    override fun onClickOk() {
+        applyAnswer(diameter.toInt())
+    }
+}
