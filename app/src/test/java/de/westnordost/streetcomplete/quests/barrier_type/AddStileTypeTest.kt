6a1e46c7a2c272f32c2633638a1190cdc2e00bb3
Mateusz Konieczny <matkoniecz@gmail.com>
2021-10-14 15:00:57 +0200

    add tests

---
+++ app/src/test/java/de/westnordost/streetcomplete/quests/barrier_type/AddStileTypeTest.kt
@@ -0,0 +1,52 @@
+package de.westnordost.streetcomplete.quests.barrier_type
+
+import de.westnordost.streetcomplete.data.meta.toCheckDateString
+import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapEntryAdd
+import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapEntryDelete
+import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapEntryModify
+import org.junit.Test
+import java.time.LocalDate
+import de.westnordost.streetcomplete.quests.verifyAnswer
+
+class AddStileTypeTest {
+    private val questType = AddStileType()
+
+    @Test
+    fun `set stile as squeezer`() {
+        questType.verifyAnswer(
+            mapOf(
+                "barrier" to "stile",
+            ),
+            BarrierType.STILE_SQUEEZER,
+            StringMapEntryAdd("stile", "squeezer"),
+        )
+    }
+
+    @Test
+    fun `set stile as still squeezer`() {
+        questType.verifyAnswer(
+            mapOf(
+                "barrier" to "stile",
+                "stile" to "squeezer",
+            ),
+            BarrierType.STILE_SQUEEZER,
+            StringMapEntryAdd("check_date:stile", LocalDate.now().toCheckDateString()),
+        )
+    }
+
+    @Test
+    fun `reset tags when marking stile as a rebuild stepover`() {
+        questType.verifyAnswer(
+            mapOf(
+                "barrier" to "stile",
+                "stile" to "stepover",
+                "material" to "wood",
+                "steps" to "5",
+            ),
+            BarrierType.STILE_STEPOVER_STONE,
+            StringMapEntryDelete("steps", "5"),
+            StringMapEntryModify("material", "wood", "stone"),
+            StringMapEntryAdd("check_date:stile", LocalDate.now().toCheckDateString()),
+        )
+    }
+}
