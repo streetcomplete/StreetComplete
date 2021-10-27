a5cc1d5d02f687afe99b6c082e7e3572a9118794
Tobias Zwick <newton@westnordost.de>
2021-10-27 15:54:58 +0200

    add pins for atm

--- app/src/main/java/de/westnordost/streetcomplete/quests/atm_operator/AddAtmOperatorForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/atm_operator/AddAtmOperatorForm.kt
@@ -1,10 +1,18 @@
 package de.westnordost.streetcomplete.quests.atm_operator
 
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.osm.mapdata.filter
 import de.westnordost.streetcomplete.quests.ANameWithSuggestionsForm
 
 class AddAtmOperatorForm : ANameWithSuggestionsForm<String>() {
 
     override val suggestions: List<String>? get() = countryInfo.atmOperators
+
+    override suspend fun addInitialMapMarkers() {
+        getMapData().filter("nodes with amenity = atm").forEach {
+            putMarker(it, R.drawable.ic_pin_money)
+        }
+    }
 
     override fun onClickOk() {
         applyAnswer(name)
