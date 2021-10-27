7848a35da06a12ef744fd448c93191a63718495d
Tobias Zwick <newton@westnordost.de>
2021-10-27 16:58:34 +0200

    add pins for (car) charger

--- app/src/main/java/de/westnordost/streetcomplete/quests/charging_station_capacity/AddChargingStationCapacityForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/charging_station_capacity/AddChargingStationCapacityForm.kt
@@ -3,6 +3,7 @@ package de.westnordost.streetcomplete.quests.charging_station_capacity
 import android.os.Bundle
 import android.view.View
 import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.osm.mapdata.filter
 import de.westnordost.streetcomplete.databinding.QuestChargingStationCapacityBinding
 import de.westnordost.streetcomplete.quests.AbstractQuestFormAnswerFragment
 import de.westnordost.streetcomplete.util.TextChangedWatcher
@@ -17,6 +18,12 @@ class AddChargingStationCapacityForm : AbstractQuestFormAnswerFragment<Int>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.capacityInput.addTextChangedListener(TextChangedWatcher { checkIsFormComplete() })
+    }
+
+    override suspend fun addInitialMapMarkers() {
+        getMapData().filter("nodes, ways with amenity = charging_station").forEach {
+            putMarker(it, R.drawable.ic_pin_car_charger)
+        }
     }
 
     override fun isFormComplete() = capacity.isNotEmpty() && capacity.toInt() > 0
--- app/src/main/java/de/westnordost/streetcomplete/quests/charging_station_operator/AddChargingStationOperatorForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/charging_station_operator/AddChargingStationOperatorForm.kt
@@ -1,10 +1,18 @@
 package de.westnordost.streetcomplete.quests.charging_station_operator
 
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.osm.mapdata.filter
 import de.westnordost.streetcomplete.quests.ANameWithSuggestionsForm
 
 class AddChargingStationOperatorForm : ANameWithSuggestionsForm<String>() {
 
     override val suggestions: List<String>? get() = countryInfo.chargingStationOperators
+
+    override suspend fun addInitialMapMarkers() {
+        getMapData().filter("nodes, ways with amenity = charging_station").forEach {
+            putMarker(it, R.drawable.ic_pin_car_charger)
+        }
+    }
 
     override fun onClickOk() {
         applyAnswer(name)
