e1fd42a555af6032d527573e3e0cdac6d3534ec1
Mateusz Konieczny <matkoniecz@gmail.com>
2021-11-01 14:35:37 +0100

    may be used in future not only for parkings

--- app/src/main/java/de/westnordost/streetcomplete/quests/parking_access/AddParkingAccessForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/parking_access/AddParkingAccessForm.kt
@@ -8,8 +8,8 @@ import de.westnordost.streetcomplete.quests.parking_access.ParkingAccess.*
 class AddParkingAccessForm : AListQuestAnswerFragment<ParkingAccess>() {
 
     override val items = listOf(
-        TextItem(YES, R.string.quest_parking_access_yes2),
-        TextItem(PRIVATE, R.string.quest_parking_access_private2),
-        TextItem(CUSTOMERS, R.string.quest_parking_access_customers2),
+        TextItem(YES, R.string.quest_access_yes),
+        TextItem(PRIVATE, R.string.quest_access_private),
+        TextItem(CUSTOMERS, R.string.quest_access_customers),
     )
 }
