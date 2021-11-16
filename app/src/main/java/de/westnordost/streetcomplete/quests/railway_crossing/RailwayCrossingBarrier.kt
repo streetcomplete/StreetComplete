ba1c0cebebe49b9a4cc1c8f60798490e0a00f796
Tobias Zwick <newton@westnordost.de>
2021-11-16 21:42:23 +0100

    Also ask for barrier types on pedestrian crossings with railway (fixes #3277)

--- app/src/main/java/de/westnordost/streetcomplete/quests/railway_crossing/RailwayCrossingBarrier.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/railway_crossing/RailwayCrossingBarrier.kt
@@ -1,8 +1,10 @@
 package de.westnordost.streetcomplete.quests.railway_crossing
 
-enum class RailwayCrossingBarrier(val osmValue: String) {
+enum class RailwayCrossingBarrier(val osmValue: String?) {
     NO("no"),
     HALF("half"),
     DOUBLE_HALF("double_half"),
     FULL("full"),
+    GATE("gate"),
+    CHICANE(null) // for some reason, it's crossing:chicane=yes, not crossing:barrier=chicane ¯\_(ツ)_/¯
 }
