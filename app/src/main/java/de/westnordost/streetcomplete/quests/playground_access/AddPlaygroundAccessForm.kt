b68d1fdc32d463a84176868208e25ed181121aa4
Mateusz Konieczny <matkoniecz@gmail.com>
2021-12-09 23:21:21 +0100

    allow access=customers for playgrounds
    fixes #3433

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/playground_access/AddPlaygroundAccessForm.kt
@@ -0,0 +1,15 @@
+package de.westnordost.streetcomplete.quests.playground_access;
+
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.quests.AListQuestAnswerFragment
+import de.westnordost.streetcomplete.quests.TextItem
+import de.westnordost.streetcomplete.quests.playground_access.PlaygroundAccess.*
+
+class AddPlaygroundAccessForm : AListQuestAnswerFragment<PlaygroundAccess>() {
+
+    override val items = listOf(
+        TextItem(YES, R.string.quest_access_yes),
+        TextItem(CUSTOMERS, R.string.quest_access_customers),
+        TextItem(PRIVATE, R.string.quest_access_private),
+    )
+}
