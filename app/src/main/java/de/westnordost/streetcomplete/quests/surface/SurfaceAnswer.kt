f5380c1be0d7c0db4bf3fc4be86252f1145e3b32
Tobias Zwick <newton@westnordost.de>
2021-12-18 22:23:38 +0100

    surface quests: remove smoothness if surface changed

--- app/src/main/java/de/westnordost/streetcomplete/quests/surface/SurfaceAnswer.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/SurfaceAnswer.kt
@@ -9,7 +9,15 @@ object IsActuallyStepsAnswer : SurfaceOrIsStepsAnswer()
 data class SurfaceAnswer(val value: Surface, val note: String? = null) : SurfaceOrIsStepsAnswer()
 
 fun SurfaceAnswer.applyTo(changes: StringMapChangesBuilder, key: String) {
-    changes.updateWithCheckDate(key, value.osmValue)
+    val osmValue = value.osmValue
+    val previousOsmValue = changes.getPreviousValue(key)
+
+    // update surface + check date
+    changes.updateWithCheckDate(key, osmValue)
+    // remove smoothness tag if surface was changed
+    if (previousOsmValue != null && previousOsmValue != osmValue) {
+        changes.deleteIfExists("smoothness")
+    }
     // add/remove note - used to describe generic surfaces
     if (note != null) {
         changes.addOrModify("$key:note", note)
