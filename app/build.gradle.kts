0d9ee29360a77a14b78e35d8dbb7d7475e65e67b
Tobias Zwick <newton@westnordost.de>
2022-01-08 17:53:20 +0100

    use new nsi version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -191,7 +191,7 @@ val bcp47ExportLanguages = setOf(
     "fa","fi","fr","gl","hr","hu","id","it", "ja","ko","lt","ml","nb","no","nl","nn",
     "pl","pt","pt-BR","ro","ru","sk","sr-cyrl","sv","th","tr","uk","zh","zh-CN","zh-HK","zh-TW"
 )
-val nsiVersion = "v6.0.20211117"
+val nsiVersion = "v6.0.20220104"
 val presetsVersion = "v3.1.0"
 
 tasks.register("updateAvailableLanguages") {
