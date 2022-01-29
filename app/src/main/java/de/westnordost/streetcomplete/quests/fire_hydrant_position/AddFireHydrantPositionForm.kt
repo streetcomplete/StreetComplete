6dee9b0d23893c0efea33a5c5b25320647a9f26c
Flo Edelmann <florian-edelmann@online.de>
2022-01-29 20:15:35 +0100

    Rename existing images to include "underground"

--- app/src/main/java/de/westnordost/streetcomplete/quests/fire_hydrant_position/AddFireHydrantPositionForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/fire_hydrant_position/AddFireHydrantPositionForm.kt
@@ -11,10 +11,10 @@ import de.westnordost.streetcomplete.view.image_select.Item
 class AddFireHydrantPositionForm : AImageListQuestAnswerFragment<FireHydrantPosition, FireHydrantPosition>() {
 
     override val items = listOf(
-        Item(GREEN, R.drawable.fire_hydrant_position_green, R.string.quest_fireHydrant_position_green),
-        Item(LANE, R.drawable.fire_hydrant_position_lane, R.string.quest_fireHydrant_position_lane),
-        Item(SIDEWALK, R.drawable.fire_hydrant_position_sidewalk, R.string.quest_fireHydrant_position_sidewalk),
-        Item(PARKING_LOT, R.drawable.fire_hydrant_position_parking, R.string.quest_fireHydrant_position_parking_lot)
+        Item(GREEN, R.drawable.fire_hydrant_position_underground_green, R.string.quest_fireHydrant_position_green),
+        Item(LANE, R.drawable.fire_hydrant_position_underground_lane, R.string.quest_fireHydrant_position_lane),
+        Item(SIDEWALK, R.drawable.fire_hydrant_position_underground_sidewalk, R.string.quest_fireHydrant_position_sidewalk),
+        Item(PARKING_LOT, R.drawable.fire_hydrant_position_underground_parking, R.string.quest_fireHydrant_position_parking_lot)
     )
 
     override val itemsPerRow = 2
