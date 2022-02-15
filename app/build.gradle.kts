2485f074385aeeaaedcd789fcfa890a3325225e5
Tobias Zwick <newton@westnordost.de>
2022-02-15 16:27:19 +0100

    update changelog for v41

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "de.westnordost.streetcomplete"
         minSdk = 21
         targetSdk = 31
-        versionCode = 4002
-        versionName = "40.1"
+        versionCode = 4100
+        versionName = "41.0-beta1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
