c0be74d6390659bfc26fb7442e2e1470befb4dd4
Peter Newman <peternewman@users.noreply.github.com>
2022-02-16 18:16:40 +0000

    Add GB

--- buildSrc/src/main/java/UpdateNsiPresetsTask.kt
+++ buildSrc/src/main/java/UpdateNsiPresetsTask.kt
@@ -102,7 +102,7 @@ val UN_M49_REGEX = Regex("[0-9]{3}")
 val ISO3166_1_ALPHA2_REGEX = Regex("([a-z]{2})")
 val ISO3166_2_REGEX = Regex("([a-z]{2})-([a-z0-9]{1,3})(\\.geojson)?")
 
-val SUPPORTED_SUBDIVISIONS = setOf("au", "ca", "cn", "in", "us")
+val SUPPORTED_SUBDIVISIONS = setOf("au", "ca", "cn", "gb", "in", "us")
 
 /** resolve M49 codes to country codes */
 private fun expandM49Codes(codes: MutableList<String>) {
