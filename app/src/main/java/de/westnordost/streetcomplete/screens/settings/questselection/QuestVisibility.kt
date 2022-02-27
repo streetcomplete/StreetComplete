d818ad37dc77998cdf169d417c81de01dd2a6f0e
Matija Nalis <mnalis-git@voyager.hr>
2022-02-27 02:16:10 +0100

    allow disabling/moving Notes quest

--- app/src/main/java/de/westnordost/streetcomplete/screens/settings/questselection/QuestVisibility.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/settings/questselection/QuestVisibility.kt
@@ -4,5 +4,5 @@ import de.westnordost.streetcomplete.data.osmnotes.notequests.OsmNoteQuestType
 import de.westnordost.streetcomplete.data.quest.QuestType
 
 data class QuestVisibility(val questType: QuestType<*>, var visible: Boolean) {
-    val isInteractionEnabled get() = questType !is OsmNoteQuestType
+    val isInteractionEnabled = true
 }
