66842178cb6170f96c1d2d16bc4dd53fba115755
Peter Newman <peternewman@users.noreply.github.com>
2022-03-21 10:32:26 +0000

    Make the linter happy (#3901)

--- app/src/main/java/de/westnordost/streetcomplete/quests/air_conditioning/AddAirConditioning.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/air_conditioning/AddAirConditioning.kt
@@ -7,9 +7,9 @@ import de.westnordost.streetcomplete.data.osm.mapdata.filter
 import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
 import de.westnordost.streetcomplete.data.osm.osmquests.Tags
 import de.westnordost.streetcomplete.data.user.achievements.QuestTypeAchievement.CITIZEN
+import de.westnordost.streetcomplete.osm.IS_SHOP_OR_DISUSED_SHOP_EXPRESSION
 import de.westnordost.streetcomplete.quests.YesNoQuestAnswerFragment
 import de.westnordost.streetcomplete.util.ktx.toYesNo
-import de.westnordost.streetcomplete.osm.IS_SHOP_OR_DISUSED_SHOP_EXPRESSION
 
 class AddAirConditioning : OsmFilterQuestType<Boolean>() {
 
