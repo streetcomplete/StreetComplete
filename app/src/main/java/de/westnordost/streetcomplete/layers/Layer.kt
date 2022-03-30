643872a56d63377d9c190bff005d20fe3789a9bf
Tobias Zwick <newton@westnordost.de>
2022-03-31 01:40:43 +0200

    put LineStyle in other file

--- app/src/main/java/de/westnordost/streetcomplete/layers/Layer.kt
+++ app/src/main/java/de/westnordost/streetcomplete/layers/Layer.kt
@@ -8,20 +8,6 @@ interface Layer {
 
     /** returns how to display the given element. Assumes it has been checked first if the element
      *  may be displayed at all. */
-    fun getStyle(element: Element): LineStyle
-}
-
-data class LineStyle(
-    /** color value as hex value, e.g. "#66ff00" */
-    val color: String,
-
-    /** dashes style. Null if no dashes */
-    val dashes: DashesStyle? = null,
-
-    /** side on which to draw. Null if no side (but center) */
-    val side: Side? = null,
-) {
-    enum class Side { LEFT, RIGHT }
-    enum class DashesStyle { DOTTED, DASHED }
+    fun getStyle(element: Element): Style
 }
 
