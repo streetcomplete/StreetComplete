04efbfae734f2f33c7d6de63d4b3eac70e17bf45
Mateusz Konieczny <matkoniecz@gmail.com>
2022-04-02 13:50:27 +0200

    switch to the text form

--- app/src/main/java/de/westnordost/streetcomplete/quests/building_entrance/AddEntranceForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/building_entrance/AddEntranceForm.kt
@@ -1,6 +1,9 @@
 package de.westnordost.streetcomplete.quests.building_entrance
 
+import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.quests.AImageListQuestAnswerFragment
+import de.westnordost.streetcomplete.quests.AListQuestAnswerFragment
+import de.westnordost.streetcomplete.quests.TextItem
 import de.westnordost.streetcomplete.quests.building_entrance.EntranceExistsAnswer.EMERGENCY_EXIT
 import de.westnordost.streetcomplete.quests.building_entrance.EntranceExistsAnswer.EXIT
 import de.westnordost.streetcomplete.quests.building_entrance.EntranceExistsAnswer.GARAGE
@@ -9,16 +12,17 @@ import de.westnordost.streetcomplete.quests.building_entrance.EntranceExistsAnsw
 import de.westnordost.streetcomplete.quests.building_entrance.EntranceExistsAnswer.SERVICE
 import de.westnordost.streetcomplete.quests.building_entrance.EntranceExistsAnswer.SHOP
 import de.westnordost.streetcomplete.quests.building_entrance.EntranceExistsAnswer.GENERIC
-import de.westnordost.streetcomplete.view.image_select.DisplayItem
 
-class AddEntranceForm : AImageListQuestAnswerFragment<EntranceAnswer, EntranceAnswer>() {
-
-    override val items: List<DisplayItem<EntranceAnswer>> = listOf(MAIN, SECONDARY, SERVICE,
-        GARAGE, EMERGENCY_EXIT, EXIT, SHOP, GENERIC, DeadEnd).toItems()
-
-    override val itemsPerRow = 3
-
-    override fun onClickOk(selectedItems: List<EntranceAnswer>) {
-        applyAnswer(selectedItems.single())
-    }
+class AddEntranceForm : AListQuestAnswerFragment<EntranceAnswer>() {
+    override val items: List<TextItem<EntranceAnswer>> = listOf(
+        TextItem(MAIN, R.string.quest_building_entrance_main),
+        TextItem(SECONDARY, R.string.quest_building_entrance_main),
+        TextItem(SERVICE, R.string.quest_building_entrance_service),
+        TextItem(GARAGE, R.string.quest_building_entrance_garage),
+        TextItem(EXIT, R.string.quest_building_entrance_exit),
+        TextItem(EMERGENCY_EXIT, R.string.quest_building_entrance_emergency_exit),
+        TextItem(SHOP, R.string.quest_building_entrance_shop),
+        TextItem(GENERIC, R.string.quest_building_entrance_yes),
+        TextItem(DeadEnd, R.string.quest_building_entrance_dead_end),
+    )
 }
