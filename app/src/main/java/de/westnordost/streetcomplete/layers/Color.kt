49df9744b1a2bcc9bd3a3a8b6972bfeebedfe9e2
Tobias Zwick <newton@westnordost.de>
2022-04-15 17:22:31 +0200

    fix display of transparent ways

--- app/src/main/java/de/westnordost/streetcomplete/layers/Color.kt
+++ app/src/main/java/de/westnordost/streetcomplete/layers/Color.kt
@@ -2,7 +2,7 @@ package de.westnordost.streetcomplete.layers
 
 /** Default and common colors for layers */
 object Color {
-    const val INVISIBLE = "#00ff0000"
+    const val INVISIBLE = "#00000000"
     const val UNSPECIFIED = "#ff0000"
     const val UNSUPPORTED = "#ff5500"
 }
