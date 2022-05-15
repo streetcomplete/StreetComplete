a8aaa7b044268fa7a5001d149fa31c1d16785d9a
Tobias Zwick <newton@westnordost.de>
2022-05-15 19:42:17 +0200

    hotfix update: force logout users

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "de.westnordost.streetcomplete"
         minSdk = 21
         targetSdk = 31
-        versionCode = 4302
-        versionName = "43.1"
+        versionCode = 4303
+        versionName = "43.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
