ad891e038076a82651a65b96aa103b14fa75e157
Mateusz Konieczny <matkoniecz@gmail.com>
2022-05-16 23:07:13 +0200

    fix tests, treat nodes as real

--- app/src/test/java/de/westnordost/streetcomplete/quests/building_entrance/AddEntranceTest.kt
+++ app/src/test/java/de/westnordost/streetcomplete/quests/building_entrance/AddEntranceTest.kt
@@ -1,6 +1,7 @@
 package de.westnordost.streetcomplete.quests.building_entrance
 
 import de.westnordost.streetcomplete.quests.TestMapDataWithGeometry
+import de.westnordost.streetcomplete.testutils.node
 import de.westnordost.streetcomplete.testutils.way
 import org.junit.Assert
 import org.junit.Test
@@ -13,14 +14,19 @@ class AddBuildingEntranceTest {
     fun `not applicable to building passage openings`() {
         val mapData = TestMapDataWithGeometry(
             listOf(
+                node(1),
+                node(2),
+                node(3),
+                node(4),
+                node(30),
                 way(1L, listOf(1, 2, 3, 4), mapOf(
                     "building" to "apartments"
                 )),
-                way(1L, listOf(1, 3), mapOf(
+                way(2L, listOf(1, 3), mapOf(
                     "highway" to "footway",
                     "tunnel" to "building_pasage",
                 )),
-                way(1L, listOf(3, 30), mapOf(
+                way(3L, listOf(3, 30), mapOf(
                     "highway" to "footway",
                 )),
             ),
@@ -32,10 +38,15 @@ class AddBuildingEntranceTest {
     fun `applicable to plausible entrances`() {
         val mapData = TestMapDataWithGeometry(
             listOf(
+                node(1),
+                node(2),
+                node(3),
+                node(4),
+                node(30),
                 way(1L, listOf(1, 2, 3, 4), mapOf(
                     "building" to "apartments"
                 )),
-                way(1L, listOf(3, 30), mapOf(
+                way(2L, listOf(3, 30), mapOf(
                     "highway" to "footway",
                 )),
             ),
