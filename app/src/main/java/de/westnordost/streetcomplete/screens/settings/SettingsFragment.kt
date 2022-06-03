2ef1b6cd055d633e3f9224327c0feb24a1792e2f
Helium <helium314@disroot.org>
2022-06-03 09:08:50 +0200

    add import and export for settings, presets and hidden quest

--- app/src/main/java/de/westnordost/streetcomplete/screens/settings/SettingsFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/settings/SettingsFragment.kt
@@ -2,11 +2,14 @@ package de.westnordost.streetcomplete.screens.settings
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.text.InputType
-import android.util.Log
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -26,15 +29,22 @@ import de.westnordost.streetcomplete.ApplicationConstants.REFRESH_DATA_AFTER
 import de.westnordost.streetcomplete.BuildConfig
 import de.westnordost.streetcomplete.Prefs
 import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.ConflictAlgorithm
+import de.westnordost.streetcomplete.data.Database
 import de.westnordost.streetcomplete.data.download.tiles.DownloadedTilesDao
 import de.westnordost.streetcomplete.data.osm.mapdata.MapDataController
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmQuestsHiddenTable
 import de.westnordost.streetcomplete.data.osmnotes.NoteController
+import de.westnordost.streetcomplete.data.osmnotes.notequests.NoteQuestsHiddenTable
 import de.westnordost.streetcomplete.data.quest.QuestController
 import de.westnordost.streetcomplete.data.quest.QuestTypeRegistry
 import de.westnordost.streetcomplete.data.visiblequests.DayNightQuestFilter
 import de.westnordost.streetcomplete.data.visiblequests.QuestPresetsSource
+import de.westnordost.streetcomplete.data.visiblequests.QuestPresetsTable
+import de.westnordost.streetcomplete.data.visiblequests.QuestTypeOrderTable
 import de.westnordost.streetcomplete.data.visiblequests.VisibleQuestTypeController
 import de.westnordost.streetcomplete.data.visiblequests.VisibleQuestTypeSource
+import de.westnordost.streetcomplete.data.visiblequests.VisibleQuestTypeTable
 import de.westnordost.streetcomplete.databinding.DialogDeleteCacheBinding
 import de.westnordost.streetcomplete.screens.HasTitle
 import de.westnordost.streetcomplete.screens.measure.MeasureActivity
@@ -56,6 +66,7 @@ import java.io.IOException
 import java.util.Locale
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
+import kotlin.system.exitProcess
 
 /** Shows the settings screen */
 class SettingsFragment :
@@ -74,6 +85,7 @@ class SettingsFragment :
     private val questPresetsSource: QuestPresetsSource by inject()
     private val visibleQuestTypeController: VisibleQuestTypeController by inject()
     private val dayNightQuestFilter: DayNightQuestFilter by inject()
+    private val db: Database by inject()
 
     interface Listener {
         fun onClickedQuestSelection()
@@ -219,12 +231,277 @@ class SettingsFragment :
             true
         }
 
+        findPreference<Preference>("export")?.setOnPreferenceClickListener {
+            fun export(name: String) {
+                val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                    putExtra(Intent.EXTRA_TITLE, "$name.txt")
+                    type = "application/text"
+                }
+                val requestCode = when (name) {
+                    "settings" -> REQUEST_CODE_SETTINGS_EXPORT
+                    "hidden_quests" ->  REQUEST_CODE_HIDDEN_EXPORT
+                    "presets" ->  REQUEST_CODE_PROFILES_EXPORT
+                    else -> 0
+                }
+                startActivityForResult(intent, requestCode)
+            }
+
+            AlertDialog.Builder(requireContext())
+                .setTitle(R.string.pref_export)
+                .setNegativeButton(R.string.import_export_settings) { _,_ -> export("settings") }
+                .setNeutralButton(R.string.import_export_hidden_quests) { _,_ -> export("hidden_quests") }
+                .setPositiveButton(R.string.import_export_profiles)  { _,_ -> export("presets") }
+                .show()
+
+            true
+        }
+
+        findPreference<Preference>("import")?.setOnPreferenceClickListener {
+            fun import(name: String) {
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                    type = "*/*" // can't select text file if setting to application/text
+                }
+                val requestCode = when (name) {
+                    "settings" -> REQUEST_CODE_SETTINGS_IMPORT
+                    "hidden_quests" ->  REQUEST_CODE_HIDDEN_IMPORT
+                    "presets" ->  REQUEST_CODE_PROFILES_IMPORT
+                    else -> 0
+                }
+                startActivityForResult(intent, requestCode)
+            }
+
+            AlertDialog.Builder(requireContext())
+                .setTitle(R.string.pref_import)
+                .setMessage(R.string.import_warning)
+                .setNegativeButton(R.string.import_export_settings) { _,_ -> import("settings") }
+                .setNeutralButton(R.string.import_export_hidden_quests) { _,_ -> import("hidden_quests") }
+                .setPositiveButton(R.string.import_export_profiles)  { _,_ -> import("presets") }
+                .show()
+
+            true
+        }
+
         buildLanguageSelector()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode != GPX_REQUEST_CODE || resultCode != Activity.RESULT_OK || data == null)
+        if (resultCode != Activity.RESULT_OK || data == null)
             return
+        when (requestCode) {
+            GPX_REQUEST_CODE -> saveGpx(data)
+            REQUEST_CODE_SETTINGS_EXPORT -> {
+                val f = File(context?.applicationInfo?.dataDir + File.separator + "shared_prefs" + File.separator + context?.applicationInfo?.packageName + "_preferences.xml")
+                val uri = data.data ?: return
+                if (!f.exists()) return
+                val os = activity?.contentResolver?.openOutputStream(uri)?.bufferedWriter() ?: return
+                val lines = f.readLines().filterNot {
+                    it.contains("TangramPinsSpriteSheet") || it.contains("oauth.") || it.contains("osm.")
+                }
+                os.write(lines.joinToString("\n"))
+                os.close()
+                // there is some SharedPreferencesBackupHelper, but can't access this without some app backup thing apparently
+            }
+            REQUEST_CODE_HIDDEN_EXPORT -> {
+                val uri = data.data ?: return
+                val os = activity?.contentResolver?.openOutputStream(uri)?.bufferedWriter() ?: return
+                val version = db.rawQuery("PRAGMA user_version;") { c -> c.getLong("user_version") }.single()
+
+                val hiddenQuests = db.query(OsmQuestsHiddenTable.NAME) { c ->
+                    c.getLong(OsmQuestsHiddenTable.Columns.ELEMENT_ID).toString() + "," +
+                    c.getString(OsmQuestsHiddenTable.Columns.ELEMENT_TYPE) + "," +
+                    c.getString(OsmQuestsHiddenTable.Columns.QUEST_TYPE) + "," +
+                    c.getLong(OsmQuestsHiddenTable.Columns.TIMESTAMP)
+                }
+                val hiddenNotes = db.query(NoteQuestsHiddenTable.NAME) { c->
+                    c.getLong(NoteQuestsHiddenTable.Columns.NOTE_ID).toString() + "," +
+                    c.getLong(NoteQuestsHiddenTable.Columns.TIMESTAMP)
+                }
+
+                os.use {
+                    it.write(version.toString())
+                    it.write("\nquests\n")
+                    it.write(hiddenQuests.joinToString("\n"))
+                    it.write("\nnotes\n")
+                    it.write(hiddenNotes.joinToString("\n"))
+                }
+                os.close()
+            }
+            REQUEST_CODE_PROFILES_EXPORT -> {
+                val uri = data.data ?: return
+                val os = activity?.contentResolver?.openOutputStream(uri)?.bufferedWriter() ?: return
+                val version = db.rawQuery("PRAGMA user_version;") { c -> c.getLong("user_version") }.single()
+
+                val presets = db.query(QuestPresetsTable.NAME) { c ->
+                    c.getLong(QuestPresetsTable.Columns.QUEST_PRESET_ID).toString() + "," +
+                    c.getString(QuestPresetsTable.Columns.QUEST_PRESET_NAME)
+                }
+                val orders = db.query(QuestTypeOrderTable.NAME) { c->
+                    c.getLong(QuestTypeOrderTable.Columns.QUEST_PRESET_ID).toString() + "," +
+                    c.getString(QuestTypeOrderTable.Columns.BEFORE) + "," +
+                    c.getString(QuestTypeOrderTable.Columns.AFTER)
+                }
+                val visibities = db.query(VisibleQuestTypeTable.NAME) { c ->
+                    c.getLong(VisibleQuestTypeTable.Columns.QUEST_PRESET_ID).toString() + "," +
+                    c.getString(VisibleQuestTypeTable.Columns.QUEST_TYPE) + "," +
+                    c.getLong(VisibleQuestTypeTable.Columns.VISIBILITY).toString()
+                }
+
+                os.use {
+                    it.write(version.toString())
+                    it.write("\npresets\n")
+                    it.write(presets.joinToString("\n"))
+                    it.write("\norders\n")
+                    it.write(orders.joinToString("\n"))
+                    it.write("\nvisibilities\n")
+                    it.write(visibities.joinToString("\n"))
+                }
+                os.close()
+            }
+            REQUEST_CODE_SETTINGS_IMPORT -> {
+                // how to make sure shared prefs are re-read from the new file?
+                // probably need to restart app on import
+                val f = File(context?.applicationInfo?.dataDir + File.separator + "shared_prefs" + File.separator + context?.applicationInfo?.packageName + "_preferences.xml")
+                val uri = data.data ?: return
+                val inputStream = activity?.contentResolver?.openInputStream(uri) ?: return
+                val t = inputStream.reader().readText()
+                if (!t.startsWith("<?xml version")) {
+                    context?.toast(getString(R.string.import_error), Toast.LENGTH_LONG)
+                    return
+                }
+                f.writeText(t)
+
+                restartApp()
+            }
+            REQUEST_CODE_HIDDEN_IMPORT -> {
+                // do not delete existing hidden quests
+                val uri = data.data ?: return
+                val input = activity?.contentResolver?.openInputStream(uri)?.bufferedReader() ?: return
+                val version = db.rawQuery("PRAGMA user_version;") { c -> c.getLong("user_version") }.single()
+                if (input.readLine() != version.toString()) return // TODO: handle this once the version changes
+                if (input.readLine() != "quests") {
+                    context?.toast(getString(R.string.import_error), Toast.LENGTH_LONG)
+                    return
+                }
+                val lines = input.readLines()
+
+                val quests = mutableListOf<Array<Any?>>()
+                val notes = mutableListOf<Array<Any?>>()
+                var currentThing = "quests"
+                for (line in lines) {
+                    if (line == "notes") {
+                        currentThing = "notes"
+                        continue
+                    }
+                    val split = line.split(",")
+                    if (split.size < 2) break
+                    when (currentThing) {
+                        "quests" -> quests.add(arrayOf(split[0].toLong(), split[1], split[2], split[3].toLong()))
+                        "notes" -> notes.add(arrayOf(split[0].toLong(), split[1].toLong()))
+                    }
+                }
+
+                db.insertMany(OsmQuestsHiddenTable.NAME,
+                    arrayOf(OsmQuestsHiddenTable.Columns.ELEMENT_ID,
+                        OsmQuestsHiddenTable.Columns.ELEMENT_TYPE,
+                        OsmQuestsHiddenTable.Columns.QUEST_TYPE,
+                        OsmQuestsHiddenTable.Columns.TIMESTAMP),
+                    quests,
+                    conflictAlgorithm = ConflictAlgorithm.REPLACE
+                )
+                db.insertMany(NoteQuestsHiddenTable.NAME,
+                    arrayOf(NoteQuestsHiddenTable.Columns.NOTE_ID,
+                        NoteQuestsHiddenTable.Columns.TIMESTAMP),
+                    notes,
+                    conflictAlgorithm = ConflictAlgorithm.REPLACE
+                )
+
+                // definitely need to reset visible quests
+                visibleQuestTypeController.onQuestTypeVisibilitiesChanged()
+                // maybe more?
+            }
+            REQUEST_CODE_PROFILES_IMPORT -> {
+                val uri = data.data ?: return
+                val input = activity?.contentResolver?.openInputStream(uri)?.bufferedReader() ?: return
+                val version = db.rawQuery("PRAGMA user_version;") { c -> c.getLong("user_version") }.single()
+                if (input.readLine() != version.toString()) return // TODO: handle this once the version changes
+                if (input.readLine() != "presets") {
+                    context?.toast(getString(R.string.import_error), Toast.LENGTH_LONG)
+                    return
+                }
+                val lines = input.readLines()
+
+                val presets = mutableListOf<Array<Any?>>()
+                val orders = mutableListOf<Array<Any?>>()
+                val visibilities = mutableListOf<Array<Any?>>()
+                var currentThing = "presets"
+                for (line in lines) {
+                    when (line) {
+                        "orders" -> {currentThing = "orders"; continue}
+                        "visibilities" -> {currentThing = "visibilities"; continue}
+                    }
+                    val split = line.split(",")
+                    if (split.size < 2) break
+                    when (currentThing) {
+                        "presets" -> presets.add(arrayOf(split[0].toLong(), split[1]))
+                        "orders" -> orders.add(arrayOf(split[0].toLong(), split[1], split[2]))
+                        "visibilities" -> visibilities.add(arrayOf(split[0].toLong(), split[1], split[2].toLong()))
+                    }
+                }
+
+                // delete existing data in both tables
+                db.delete(QuestPresetsTable.NAME)
+                db.delete(QuestTypeOrderTable.NAME)
+                db.delete(VisibleQuestTypeTable.NAME)
+
+                db.insertMany(QuestPresetsTable.NAME,
+                    arrayOf(QuestPresetsTable.Columns.QUEST_PRESET_ID, QuestPresetsTable.Columns.QUEST_PRESET_NAME),
+                    presets
+                )
+                db.insertMany(QuestTypeOrderTable.NAME,
+                    arrayOf(QuestTypeOrderTable.Columns.QUEST_PRESET_ID,
+                        QuestTypeOrderTable.Columns.BEFORE,
+                        QuestTypeOrderTable.Columns.AFTER),
+                    orders
+                )
+                db.insertMany(VisibleQuestTypeTable.NAME,
+                    arrayOf(VisibleQuestTypeTable.Columns.QUEST_PRESET_ID,
+                        VisibleQuestTypeTable.Columns.QUEST_TYPE,
+                        VisibleQuestTypeTable.Columns.VISIBILITY),
+                    visibilities
+                )
+
+                // set selected preset to default
+                prefs.edit().putLong(Prefs.SELECTED_QUESTS_PRESET, 0).apply()
+                visibleQuestTypeController.onQuestTypeVisibilitiesChanged()
+                // TODO: also allow adding the profile with new id(s)
+            }
+        }
+    }
+
+    private fun restartApp() {
+        val i = Intent(requireContext(), this.javaClass)
+        val pi = PendingIntent.getActivity(requireContext(), 234, i, PendingIntent.FLAG_CANCEL_CURRENT)
+        val a = requireContext().getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        // according to stackoverflow, restart does not work due to some restrictions added in Q
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            AlertDialog.Builder(requireContext())
+                .setCancelable(false)
+                .setTitle(R.string.restart_required)
+                .setMessage(R.string.restart_required_message)
+                .setPositiveButton(android.R.string.ok) { _,_ ->
+                    a.set(AlarmManager.RTC, System.currentTimeMillis() + 50, pi)
+                    exitProcess(0)
+                }
+                .show()
+        } else {
+            a.set(AlarmManager.RTC, System.currentTimeMillis() + 50, pi)
+            exitProcess(0)
+        }
+    }
+
+    private fun saveGpx(data: Intent) {
         val uri = data.data ?: return
         val os = activity?.contentResolver?.openOutputStream(uri)?.buffered() ?: return
         try {
@@ -251,9 +528,10 @@ class SettingsFragment :
                 zipStream.closeEntry()
             }
             zipStream.close()
+            os.close()
             files.forEach { it.delete() }
         } catch (e: IOException) {
-            Log.i("sctest", "writing didn't work", e)
+            context?.toast(getString(R.string.pref_save_gpx_error), Toast.LENGTH_LONG)
         }
     }
 
@@ -359,3 +637,9 @@ class SettingsFragment :
 }
 
 private const val GPX_REQUEST_CODE = 387532
+private const val REQUEST_CODE_SETTINGS_EXPORT = 532527
+private const val REQUEST_CODE_HIDDEN_EXPORT = 532528
+private const val REQUEST_CODE_PROFILES_EXPORT = 532529
+private const val REQUEST_CODE_SETTINGS_IMPORT = 67367487
+private const val REQUEST_CODE_HIDDEN_IMPORT = 67367488
+private const val REQUEST_CODE_PROFILES_IMPORT = 67367489
