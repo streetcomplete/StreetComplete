523e457d94b614cee94ca8c9663d4f10d4800ab0
Mateusz Konieczny <matkoniecz@gmail.com>
2022-06-12 14:14:35 +0200

    HF

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/memorial_type/MemorialType.kt
@@ -0,0 +1,36 @@
+package de.westnordost.streetcomplete.quests.memorial_type
+
+import de.westnordost.streetcomplete.data.osm.osmquests.Tags
+
+sealed interface MemorialTypeAnswer
+
+enum class MemorialType(val osmValue: String) : MemorialTypeAnswer {
+    STATUE("statue"),
+    BUST("bust"),
+    PLAQUE("plaque"),
+    WAR_MEMORIAL("war_memorial"),
+    STONE("stone"),
+    OBELISK("obelisk"),
+}
+
+enum class Stele(val osmMaterialValue: String) : MemorialTypeAnswer {
+    WOODEN_STELE("wood"),
+    STONE_STELE("stone"),
+    KHACHKAR_STELE("stone"),
+}
+
+fun MemorialTypeAnswer.applyTo(tags: Tags) {
+    when (this) {
+        is MemorialType -> {
+            tags["memorial"] = this.osmValue
+        }
+        is Stele -> {
+            tags["memorial"] = "stele"
+            tags["material"] = this.osmMaterialValue
+        }
+    }
+
+    if (this == Stele.KHACHKAR_STELE) {
+        tags["stele"] = "khachkar"
+    }
+}
