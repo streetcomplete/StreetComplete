e6814ce13fb18000ffdcfc2ace9b636a3c068bf1
Mateusz Konieczny <matkoniecz@gmail.com>
2022-06-17 20:57:13 +0200

    enable entrance address quest in more countries
    and give sources

--- app/src/main/java/de/westnordost/streetcomplete/quests/building_entrance_reference/AddEntranceReference.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/building_entrance_reference/AddEntranceReference.kt
@@ -29,7 +29,29 @@ class AddEntranceReference : OsmElementQuestType<EntranceAnswer> {
     override val wikiLink = "Key:ref"
     override val icon = R.drawable.ic_quest_door_address
     override val questTypeAchievements = listOf(CITIZEN, BLIND)
-    override val enabledInCountries = NoCountriesExcept("BG", "PL", "RU", "UA", "BY", "EE", "RO")
+    override val enabledInCountries = NoCountriesExcept(
+        "PL", // Poland - own knowledge of Mateusz Konieczny https://github.com/streetcomplete/StreetComplete/issues/3064#issuecomment-879447168
+        "RU", // Russia - https://github.com/streetcomplete/StreetComplete/issues/3064#issuecomment-880231076
+        "MK", // North Macedonia https://github.com/streetcomplete/StreetComplete/issues/3064#issuecomment-1158016740
+        "BG", // Bulgaria - https://github.com/streetcomplete/StreetComplete/issues/3064#issuecomment-879318531
+        "UA", // Ukraine - https://t.me/osmUA/123011
+        "BY", // Belarus - https://t.me/byosm/59969
+        "EE", // Estonia - https://t.me/byosm/59976
+        "HU", // Hungary https://github.com/streetcomplete/StreetComplete/issues/3064#issuecomment-1159088130
+        "RO", // Romania https://t.me/OSMRomania/7363
+
+        // not applicable (no matching address system)
+        // Kosovo (XK) -  https://t.me/osmkosovo/19219
+        // Serbia (RS) - https://t.me/osm_sr/6877
+        // Lithuania (LT) - https://lists.openstreetmap.org/pipermail/talk-lt/2022-June/thread.html#2723 https://github.com/streetcomplete/StreetComplete/issues/3064#issuecomment-1147833339
+        // Slovakia (SK) https://groups.google.com/g/osm_sk/c/IqZz0-iRK5I
+        // Czech Republic (CZ) https://t.me/openstreetmapcz/1242
+        // Croatia https://github.com/streetcomplete/StreetComplete/issues/3064#issuecomment-1157858833
+
+        // was unable to reach communities or get replies from Kazakhstan, Moldova, Bosnia and Herzegovina
+        // if you are sure that this quest should be enabled there or elsewhere
+        // please create an issue at https://github.com/streetcomplete/StreetComplete/issues
+    )
     override fun getTitle(tags: Map<String, String>) = R.string.quest_entrance_reference
 
     override fun getHighlightedElements(element: Element, getMapData: () -> MapDataWithGeometry) =
