92f46fdd298503545b50a4f767587f0ecb5afcba
Matija Nalis <mnalis-git@voyager.hr>
2022-06-18 13:09:32 +0200

    correct comment

--- app/src/main/java/de/westnordost/streetcomplete/data/osmtracks/TracksApi.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/osmtracks/TracksApi.kt
@@ -14,7 +14,7 @@ interface TracksApi {
      * @param trackpoints history of recorded trackpoints
      * @param noteText optional text appended to the track
      *
-     * @throws AuthorizationException if this application is not authorized to write notes
+     * @throws AuthorizationException if this application is not authorized to write traces
      *                                (Permission.READ_GPS_TRACES, Permission.WRITE_GPS_TRACES)
      * @throws ConnectionException if a temporary network connection problem occurs
      *
