7902b75cbe21dc57df9a20833bc7a4b108060bf1
Matija Nalis <mnalis-git@voyager.hr>
2022-06-23 01:44:40 +0200

    allows user to mark path as indoor=yes (if no available "external" surface answer)

--- app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddPathSurfaceForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddPathSurfaceForm.kt
@@ -13,6 +13,7 @@ class AddPathSurfaceForm : AImageListQuestForm<Surface, SurfaceOrIsStepsAnswer>(
 
     override val otherAnswers get() = listOfNotNull(
         createConvertToStepsAnswer(),
+        createMarkAsInsideAnswer(),
     )
 
     override val itemsPerRow = 3
@@ -40,6 +41,15 @@ class AddPathSurfaceForm : AImageListQuestForm<Surface, SurfaceOrIsStepsAnswer>(
 
         return AnswerItem(R.string.quest_generic_answer_is_actually_steps) {
             applyAnswer(IsActuallyStepsAnswer)
+        }
+    }
+
+    private fun createMarkAsInsideAnswer(): AnswerItem? {
+        val way = element as? Way ?: return null
+        if (way.isArea() || way.tags["inside"] == "yes") return null
+
+        return AnswerItem(R.string.quest_generic_answer_is_indoors) {
+            applyAnswer(IsIndoorsAnswer)
         }
     }
 }
