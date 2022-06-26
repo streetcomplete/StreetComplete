7b9774a78a980031a6d37f21173af45d8f0e351a
Helium <helium314@disroot.org>
2022-06-26 14:10:04 +0200

    update documentation

--- app/src/main/java/de/westnordost/streetcomplete/util/SpatialCache.kt
+++ app/src/main/java/de/westnordost/streetcomplete/util/SpatialCache.kt
@@ -11,11 +11,11 @@ import de.westnordost.streetcomplete.util.math.contains
 import de.westnordost.streetcomplete.util.math.isCompletelyInside
 
 /**
- * <Key, LatLon> cache based on tiles, inspired by Android LruCache
- * fetch needs get data from db and fill other caches, e.g. questKey -> Quest
- * onRemoveTile needs to clean other caches, e.g. questKey -> Quest
- * trim() needs to be called after get() and getting data from other caches
- * the bbox queried in get() must fit in cache, i.e. may not be larger than maxTiles at tileZoom
+ * <Key, LatLon> cache based on tiles, inspired by Android LruCache.
+ * fetch needs get data from db and fill other caches, e.g. questKey -> Quest.
+ * onRemoveTile needs to clean other caches, e.g. questKey -> Quest.
+ * trim() needs to be called after get() and getting data from other caches.
+ * the bbox queried in get() must fit in cache, i.e. may not be larger than maxTiles at tileZoom.
  */
 class SpatialCache<T>(
     private val maxTiles: Int,
@@ -26,12 +26,20 @@ class SpatialCache<T>(
     private val byTile = LinkedHashMap<TilePos, HashSet<T>>((maxTiles/0.75).toInt(), 0.75f, true)
     private val byKey = hashMapOf<T, LatLon>()
 
+    /**
+     * Removes the given key from cache.
+     * If a key was removed, onKeysRemoved is called.
+     */
     fun remove(key: T) {
         val pos = byKey.remove(key) ?: return
         byTile[getTilePosFor(pos)]?.remove(key)
         onKeysRemoved(listOf(key))
     }
 
+    /**
+     * Removes the given keys from cache.
+     * If any keys were removed, onKeysRemoved is called.
+     */
     fun removeAll(keys: Collection<T>) {
         val removed = keys.filter {
             val pos = byKey.remove(it) ?: return@filter false
@@ -41,7 +49,10 @@ class SpatialCache<T>(
         onKeysRemoved(removed)
     }
 
-    // returns whether the key was put in cache
+    /**
+     * Puts the entry to cache only if the containing tile is already cached.
+     * return whether the key was put in cache
+     */
     fun putIfTileExists(key: T, position: LatLon): Boolean {
         val previousPosition = byKey[key]
         // in cache already
@@ -59,8 +70,12 @@ class SpatialCache<T>(
         return false
     }
 
-    // returns keys that were not put to cache because they are not in fully contained tiles
-    // contrary to get(bbox), here the bbox may be larger than what can be put in cache
+    /**
+     * Replaces all tiles fully contained in the bounding box.
+     * If the number of tiles exceeds maxTiles, only the first maxSize tiles are cached.
+     * Return all keys not put to cache, either because the tile was not fully contained
+     * in the given bbox, or because the bbox contained too many tiles at tileZoom.
+     */
     fun replaceAllInBBox(entries: Collection<Pair<T, LatLon>>, bbox: BoundingBox): List<T> {
         val tiles = bbox.asListOfEnclosingTilePos()
         val completelyContainedTiles = tiles.filter { it.asBoundingBox(tileZoom).isCompletelyInside(bbox) }
@@ -110,8 +125,8 @@ class SpatialCache<T>(
     }
 
     /**
-     * get all data inside bbox, will be loaded from db if necessary using fetch
-     * call trim() after using get and getting data from other caches
+     * Get all data inside bbox, will be loaded from db if necessary using fetch.
+     * Call trim() after using get and getting data from other caches, as cache size may hav increased
      */
     fun get(bbox: BoundingBox): List<T> {
         val requiredTiles = bbox.asListOfEnclosingTilePos()
@@ -132,6 +147,10 @@ class SpatialCache<T>(
         return keys
     }
 
+    /**
+     * Reduces cache size to the given number of tiles.
+     * If any keys were removed, onKeysRemoved is called.
+     */
     fun trim(size: Int = maxTiles) {
         if (byTile.size <= size) return
 
@@ -145,6 +164,9 @@ class SpatialCache<T>(
         }
     }
 
+    /**
+     * clears the caches without calling onKeysRemoved
+     */
     fun clear() {
         byKey.clear()
         byTile.clear()
