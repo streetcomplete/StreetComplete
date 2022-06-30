60bfdaccea20a16eeffcf8689e6d9c628d97382a
Mateusz Konieczny <matkoniecz@gmail.com>
2022-06-30 09:21:59 +0200

    stop asking is toilet paid in USA and Canada
    fixes #4174

--- app/src/main/java/de/westnordost/streetcomplete/quests/toilets_fee/AddToiletsFee.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/toilets_fee/AddToiletsFee.kt
@@ -2,6 +2,7 @@ package de.westnordost.streetcomplete.quests.toilets_fee
 
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
+import de.westnordost.streetcomplete.data.quest.AllCountriesExcept
 import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement.CITIZEN
 import de.westnordost.streetcomplete.osm.Tags
 import de.westnordost.streetcomplete.quests.YesNoQuestForm
@@ -19,6 +20,7 @@ class AddToiletsFee : OsmFilterQuestType<Boolean>() {
     override val changesetComment = "Add toilets fee"
     override val wikiLink = "Key:fee"
     override val icon = R.drawable.ic_quest_toilet_fee
+    override val enabledInCountries = AllCountriesExcept("US", "CA")
     override val isDeleteElementEnabled = true
     override val achievements = listOf(CITIZEN)
 
