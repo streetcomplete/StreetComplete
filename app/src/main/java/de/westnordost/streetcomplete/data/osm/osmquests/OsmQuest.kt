cc1126c2ce542c9c6070b8c4794e40a267731d55
Helium <helium314@disroot.org>
2022-07-04 10:51:02 +0200

    change quest key and marker position to lazy
    since both are likely to be called never, or more than once

--- app/src/main/java/de/westnordost/streetcomplete/data/osm/osmquests/OsmQuest.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/osm/osmquests/OsmQuest.kt
@@ -17,13 +17,13 @@ data class OsmQuest(
     override val geometry: ElementGeometry
 ) : Quest, OsmQuestDaoEntry {
 
-    override val key: OsmQuestKey get() = OsmQuestKey(elementType, elementId, questTypeName)
+    override val key: OsmQuestKey by lazy { OsmQuestKey(elementType, elementId, questTypeName) }
 
     override val questTypeName: String get() = type.name
 
     override val position: LatLon get() = geometry.center
 
-    override val markerLocations: Collection<LatLon> get() {
+    override val markerLocations: Collection<LatLon> by lazy {
         if (geometry is ElementPolylinesGeometry) {
             val polyline = geometry.polylines[0]
             val length = polyline.measuredLength()
@@ -36,13 +36,13 @@ data class OsmQuest(
                 val between = (length - (2 * MARKER_FROM_END_DISTANCE)) / (count - 1)
                 // space markers `between` apart, starting with `MARKER_FROM_END_DISTANCE` (the
                 // final marker will end up at `MARKER_FROM_END_DISTANCE` from the other end)
-                return polyline.pointsOnPolylineFromStart(
+                return@lazy polyline.pointsOnPolylineFromStart(
                     (0 until count).map { MARKER_FROM_END_DISTANCE + (it * between) }
                 )
             }
         }
         // fall through to a single marker in the middle
-        return listOf(position)
+        listOf(position)
     }
 }
 
