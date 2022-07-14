7f0caeeca1c618850e2ecabfc2677e8e7a72f3a8
Tobias Zwick <newton@westnordost.de>
2022-07-14 13:41:30 +0200

    use assertFalse/assertTrue

--- app/src/test/java/de/westnordost/streetcomplete/osm/SurfaceKtTest.kt
+++ app/src/test/java/de/westnordost/streetcomplete/osm/SurfaceKtTest.kt
@@ -1,31 +1,32 @@
 package de.westnordost.streetcomplete.osm
 
-import org.junit.Assert
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class SurfaceKtTest {
     @Test
     fun `poor tracktype conflicts with paved surface`() {
-        Assert.assertEquals(true, isSurfaceAndTracktypeMismatching("asphalt", "grade5"))
+        assertTrue(isSurfaceAndTracktypeMismatching("asphalt", "grade5"))
     }
 
     @Test
     fun `high quality tracktype conflicts with poor surface`() {
-        Assert.assertEquals(true, isSurfaceAndTracktypeMismatching("gravel", "grade1"))
+        assertTrue(isSurfaceAndTracktypeMismatching("gravel", "grade1"))
     }
 
     @Test
     fun `high quality tracktype fits good surface`() {
-        Assert.assertEquals(false, isSurfaceAndTracktypeMismatching("paving_stones", "grade1"))
+        assertFalse(isSurfaceAndTracktypeMismatching("paving_stones", "grade1"))
     }
 
     @Test
     fun `unknown tracktype does not crash or conflict`() {
-        Assert.assertEquals(false, isSurfaceAndTracktypeMismatching("paving_stones", "lorem ipsum"))
+        assertFalse(isSurfaceAndTracktypeMismatching("paving_stones", "lorem ipsum"))
     }
 
     @Test
     fun `unknown surface does not crash or conflict`() {
-        Assert.assertEquals(false, isSurfaceAndTracktypeMismatching("zażółć", "grade1"))
+        assertFalse(isSurfaceAndTracktypeMismatching("zażółć", "grade1"))
     }
 }
