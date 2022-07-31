24d9dac6a4cb76aec8cf1979c6428cefda70cb29
Matija Nalis <mnalis-github@voyager.hr>
2022-07-31 11:57:09 +0200

    Update app/src/main/java/de/westnordost/streetcomplete/quests/camping/CampType.kt
    just ordering
    
    Co-authored-by: Peter Newman <peternewman@users.noreply.github.com>

--- app/src/main/java/de/westnordost/streetcomplete/quests/camping/CampType.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/camping/CampType.kt
@@ -2,7 +2,7 @@ package de.westnordost.streetcomplete.quests.camping
 
 enum class CampType(val tents: Boolean, val caravans: Boolean) {
     TENTS_AND_CARAVANS(true, true),
-    CARAVANS_ONLY(false, true),
     TENTS_ONLY(true, false),
+    CARAVANS_ONLY(false, true),
     BACKCOUNTRY(false, false),
 }
