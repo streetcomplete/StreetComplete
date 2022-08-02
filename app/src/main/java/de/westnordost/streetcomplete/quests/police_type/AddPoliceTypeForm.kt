3b97381ccf7c48fdfa422599785df57e5d01d82f
Mateusz Konieczny <matkoniecz@gmail.com>
2022-08-02 08:09:44 +0200

    match drawable and svg names for italian police
    it is in 'res/graphics/italian police type' folder

--- app/src/main/java/de/westnordost/streetcomplete/quests/police_type/AddPoliceTypeForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/police_type/AddPoliceTypeForm.kt
@@ -8,12 +8,12 @@ import de.westnordost.streetcomplete.view.image_select.Item
 class AddPoliceTypeForm : AImageListQuestForm<PoliceType, PoliceType>() {
 
     override val items = listOf(
-        Item(PoliceType.CARABINIERI,            R.drawable.ic_police_type_carabinieri,         R.string.quest_policeType_type_it_carabinieri),
-        Item(PoliceType.POLIZIA_DI_STATO,       R.drawable.ic_police_type_polizia,             R.string.quest_policeType_type_it_polizia_di_stato),
-        Item(PoliceType.POLIZIA_MUNICIPALE,     R.drawable.ic_police_type_municipale,          R.string.quest_policeType_type_it_polizia_municipale),
-        Item(PoliceType.POLIZIA_LOCALE,         R.drawable.ic_police_type_locale,              R.string.quest_policeType_type_it_polizia_locale),
-        Item(PoliceType.GUARDIA_DI_FINANZA,     R.drawable.ic_police_type_finanza,             R.string.quest_policeType_type_it_guardia_di_finanza),
-        Item(PoliceType.GUARDIA_COSTIERA,       R.drawable.ic_police_type_costiera,            R.string.quest_policeType_type_it_guardia_costiera)
+        Item(PoliceType.CARABINIERI,            R.drawable.ic_italian_police_type_carabinieri, R.string.quest_policeType_type_it_carabinieri),
+        Item(PoliceType.POLIZIA_DI_STATO,       R.drawable.ic_italian_police_type_polizia,     R.string.quest_policeType_type_it_polizia_di_stato),
+        Item(PoliceType.POLIZIA_MUNICIPALE,     R.drawable.ic_italian_police_type_municipale,  R.string.quest_policeType_type_it_polizia_municipale),
+        Item(PoliceType.POLIZIA_LOCALE,         R.drawable.ic_italian_police_type_locale,      R.string.quest_policeType_type_it_polizia_locale),
+        Item(PoliceType.GUARDIA_DI_FINANZA,     R.drawable.ic_italian_police_type_finanza,     R.string.quest_policeType_type_it_guardia_di_finanza),
+        Item(PoliceType.GUARDIA_COSTIERA,       R.drawable.ic_italian_police_type_costiera,    R.string.quest_policeType_type_it_guardia_costiera)
     )
 
     override val itemsPerRow = 3
