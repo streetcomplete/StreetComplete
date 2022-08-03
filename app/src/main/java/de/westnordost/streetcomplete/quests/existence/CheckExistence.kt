fe61b46fa470b8109287139d6908694e6a5a106e
Tobias Zwick <newton@westnordost.de>
2022-08-03 21:16:39 +0200

    only ask if a traffic calming is still there for types that can be added/removed relatively easily (fixes #4249)

--- app/src/main/java/de/westnordost/streetcomplete/quests/existence/CheckExistence.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/existence/CheckExistence.kt
@@ -53,7 +53,7 @@ class CheckExistence(
             amenity = bench
             or amenity = lounger
             or amenity = waste_basket
-            or traffic_calming ~ bump|hump|island|cushion|choker|rumble_strip|chicane|dip
+            or traffic_calming ~ bump|mini_bumps|hump|cushion|rumble_strip|dip|double_dip
             or traffic_calming = table and !highway and !crossing
             or amenity = recycling and recycling_type = container
             or amenity = toilets
