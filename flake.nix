cff7f9701652e5810c70f075ac07a158e4992eaf
Gustavo Porto <gustavo@portosanti.com>
2022-08-04 20:11:27 +0100

    chore: update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "StreetComplete";
   nixConfig = { bash-prompt = "[StreetComplete]$ "; };
   inputs = { flake-utils = { url = "github:numtide/flake-utils"; }; };
-  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
