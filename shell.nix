02346e572fd9053a7d92faa8a8c19ca0b8d02cf2
Gustavo Porto <gustavo@portosanti.com>
2022-08-04 20:10:43 +0100

    chore: remove checks from flake

--- shell.nix
+++ shell.nix
@@ -1,9 +1,12 @@
-{ pkgs ? import <nixpkgs> { }, shellHook ? ''
-  EDITOR=vim
-'' }:
+{ pkgs ? import <nixpkgs> { config.android_sdk.accept_license = true; } }:
 
-pkgs.mkShell {
-  buildInputs =
-    [ pkgs.android-udev-rules pkgs.androidenv.androidPkgs_9_0.platform-tools ];
-  inherit shellHook;
+let androidSdk = pkgs.androidenv.androidPkgs_9_0.androidsdk;
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    android-udev-rules
+    androidenv.androidPkgs_9_0.platform-tools
+    androidSdk
+  ];
+  GRADLE_OPTS =
+    "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/28.0.3/aapt2";
 }
