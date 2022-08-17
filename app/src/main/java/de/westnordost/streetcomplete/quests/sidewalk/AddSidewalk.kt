9579c513b32348b2faf2670e715f21443b12441f
Tobias Zwick <newton@westnordost.de>
2022-08-17 14:09:47 +0200

    disable by default sidewalk and lit quests

--- app/src/main/java/de/westnordost/streetcomplete/quests/sidewalk/AddSidewalk.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/sidewalk/AddSidewalk.kt
@@ -32,6 +32,7 @@ class AddSidewalk : OsmElementQuestType<SidewalkSides> {
     override val wikiLink = "Key:sidewalk"
     override val icon = R.drawable.ic_quest_sidewalk
     override val achievements = listOf(PEDESTRIAN)
+    override val defaultDisabledMessage = R.string.default_disabled_msg_overlay
 
     override fun getTitle(tags: Map<String, String>) = R.string.quest_sidewalk_title
 
