1ea1bdb4bf96fe302b569aef7f67902f945c816d
Tobias Zwick <newton@westnordost.de>
2022-09-05 15:08:08 +0200

    fix test

--- app/src/test/java/de/westnordost/streetcomplete/osm/housenumber/AddressNumberValidatorKtTest.kt
+++ app/src/test/java/de/westnordost/streetcomplete/osm/housenumber/AddressNumberValidatorKtTest.kt
@@ -1,37 +1,43 @@
 package de.westnordost.streetcomplete.osm.housenumber
 
-import org.junit.Assert
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-@Test fun `housenumber regex`() {
-    val r = VALID_HOUSE_NUMBER_REGEX
-    Assert.assertTrue("1".matches(r))
-    Assert.assertTrue("1234".matches(r))
+class AddressNumberValidatorKtTest {
 
-    Assert.assertTrue("1234a".matches(r))
-    Assert.assertTrue("1234/a".matches(r))
-    Assert.assertTrue("1234 / a".matches(r))
-    Assert.assertTrue("1234 / A".matches(r))
-    Assert.assertTrue("1234/ab".matches(r))
-    Assert.assertTrue("1234A".matches(r))
-    Assert.assertTrue("1234/9".matches(r))
-    Assert.assertTrue("1234 / 9".matches(r))
-    Assert.assertTrue("1234/55".matches(r))
+    @Test
+    fun `housenumber regex`() {
+        val r = VALID_HOUSE_NUMBER_REGEX
+        assertTrue("1".matches(r))
+        assertTrue("1234".matches(r))
 
-    Assert.assertTrue("12345".matches(r))
+        assertTrue("1234a".matches(r))
+        assertTrue("1234/a".matches(r))
+        assertTrue("1234 / a".matches(r))
+        assertTrue("1234 / A".matches(r))
+        assertTrue("1234/ab".matches(r))
+        assertTrue("1234A".matches(r))
+        assertTrue("1234/9".matches(r))
+        assertTrue("1234 / 9".matches(r))
+        assertTrue("1234/55".matches(r))
 
-    Assert.assertTrue("1234AB".matches(r))
-    Assert.assertFalse("123456".matches(r))
-    Assert.assertFalse("1234 5".matches(r))
-}
+        assertTrue("12345".matches(r))
 
-@Test fun `blocknumber regex`() {
-    val r = VALID_BLOCK_NUMBER_REGEX
-    Assert.assertTrue("1".matches(r))
-    Assert.assertTrue("1234".matches(r))
-    Assert.assertFalse("12345".matches(r))
+        assertTrue("1234AB".matches(r))
+        assertFalse("123456".matches(r))
+        assertFalse("1234 5".matches(r))
+    }
 
-    Assert.assertTrue("1234a".matches(r))
-    Assert.assertTrue("1234 a".matches(r))
-    Assert.assertFalse("1234 ab".matches(r))
+    @Test
+    fun `blocknumber regex`() {
+        val r = VALID_BLOCK_NUMBER_REGEX
+        assertTrue("1".matches(r))
+        assertTrue("1234".matches(r))
+        assertFalse("12345".matches(r))
+
+        assertTrue("1234a".matches(r))
+        assertTrue("1234 a".matches(r))
+        assertFalse("1234 ab".matches(r))
+    }
 }
