79df63ced6f1ded80a13d0430c4e4357afb7b81e
Tobias Zwick <newton@westnordost.de>
2022-09-05 23:48:20 +0200

    put house number and conscription number input logic into own classes

--- app/src/main/java/de/westnordost/streetcomplete/quests/address/AddHousenumberForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/address/AddHousenumberForm.kt
@@ -1,29 +1,24 @@
 package de.westnordost.streetcomplete.quests.address
 
-import android.content.res.ColorStateList
 import android.os.Bundle
-import android.text.InputType
-import android.text.method.DigitsKeyListener
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.isInvisible
 import androidx.core.widget.doAfterTextChanged
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.databinding.DialogQuestAddressNoHousenumberBinding
 import de.westnordost.streetcomplete.osm.housenumber.AddressNumber
-import de.westnordost.streetcomplete.osm.housenumber.ConscriptionNumber
 import de.westnordost.streetcomplete.osm.housenumber.HouseAndBlockNumber
 import de.westnordost.streetcomplete.osm.housenumber.HouseNumber
-import de.westnordost.streetcomplete.osm.housenumber.addToHouseNumber
 import de.westnordost.streetcomplete.osm.housenumber.looksInvalid
 import de.westnordost.streetcomplete.quests.AbstractOsmQuestForm
 import de.westnordost.streetcomplete.quests.AnswerItem
 import de.westnordost.streetcomplete.quests.building_type.BuildingType
 import de.westnordost.streetcomplete.quests.building_type.asItem
 import de.westnordost.streetcomplete.util.ktx.nonBlankTextOrNull
-import de.westnordost.streetcomplete.util.ktx.showKeyboard
+import de.westnordost.streetcomplete.view.controller.ConscriptionNumberInputViewController
+import de.westnordost.streetcomplete.view.controller.HouseNumberInputViewController
 import de.westnordost.streetcomplete.view.image_select.DisplayItem
 import de.westnordost.streetcomplete.view.image_select.ItemViewHolder
 
@@ -36,26 +31,15 @@ class AddHousenumberForm : AbstractOsmQuestForm<HouseNumberAnswer>() {
         AnswerItem(R.string.quest_housenumber_multiple_numbers) { showMultipleNumbersHint() }
     )
 
-    private var houseNumberInput: EditText? = null
     private var houseNameInput: EditText? = null
-    private var conscriptionNumberInput: EditText? = null
-    private var streetNumberInput: EditText? = null
-    private var blockNumberInput: EditText? = null
 
-    private var toggleKeyboardButton: Button? = null
-
-    private var addButton: View? = null
-    private var subtractButton: View? = null
+    private var houseNumberInputViewController: HouseNumberInputViewController? = null
+    private var conscriptionNumberInputViewController: ConscriptionNumberInputViewController? = null
 
     enum class InterfaceMode {
         HOUSENUMBER, HOUSENAME, HOUSENUMBER_AND_HOUSENAME
     }
     private var interfaceMode: InterfaceMode = InterfaceMode.HOUSENUMBER
-
-    private var houseNumberInputTextColors: ColorStateList? = null
-
-    // because the hint is implemented as a hack: it is actually the text proper but colored in light-gray
-    private val isShowingHouseNumberHint: Boolean get() = houseNumberInputTextColors != null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -121,112 +105,39 @@ class AddHousenumberForm : AbstractOsmQuestForm<HouseNumberAnswer>() {
     }
 
     private fun onContentViewCreated(view: View) {
-        toggleKeyboardButton = view.findViewById(R.id.toggleKeyboardButton)
-        houseNumberInput = view.findViewById(R.id.houseNumberInput)
         houseNameInput = view.findViewById(R.id.houseNameInput)
-        conscriptionNumberInput = view.findViewById(R.id.conscriptionNumberInput)
-        streetNumberInput = view.findViewById(R.id.streetNumberInput)
-        blockNumberInput = view.findViewById(R.id.blockNumberInput)
-        addButton = view.findViewById(R.id.addButton)
-        subtractButton = view.findViewById(R.id.subtractButton)
+        val houseNumberInput: EditText? = view.findViewById(R.id.houseNumberInput)
+        val toggleKeyboardButton: Button? = view.findViewById(R.id.toggleKeyboardButton)
+        val conscriptionNumberInput: EditText? = view.findViewById(R.id.conscriptionNumberInput)
+        val streetNumberInput: EditText? = view.findViewById(R.id.streetNumberInput)
 
-        addButton?.setOnClickListener { addToHouseNumberInput(+1) }
-        subtractButton?.setOnClickListener { addToHouseNumberInput(-1) }
+        houseNumberInputViewController = if (houseNumberInput != null && toggleKeyboardButton != null) {
+            HouseNumberInputViewController(
+                requireActivity(),
+                houseNumberInput,
+                view.findViewById(R.id.blockNumberInput),
+                toggleKeyboardButton,
+                view.findViewById(R.id.addButton),
+                view.findViewById(R.id.subtractButton),
+                lastBlockNumber,
+                lastHouseNumber
+            )
+        } else null
 
-        // must be called before registering the text changed watchers because it changes the text
-        prefillBlockNumber()
+        conscriptionNumberInputViewController = if (conscriptionNumberInput != null && toggleKeyboardButton != null && streetNumberInput != null) {
+            ConscriptionNumberInputViewController(
+                requireActivity(),
+                conscriptionNumberInput,
+                streetNumberInput,
+                toggleKeyboardButton
+            )
+        } else null
 
-        initKeyboardButton()
-        // must be after initKeyboardButton because it re-sets the onFocusListener
-        showHouseNumberHint()
-
-        listOfNotNull(
-            houseNumberInput, houseNameInput, conscriptionNumberInput,
-            streetNumberInput, blockNumberInput
-        ).forEach { it.doAfterTextChanged { checkIsFormComplete() } }
+        houseNumberInputViewController?.onInputChanged = { checkIsFormComplete() }
+        conscriptionNumberInputViewController?.onInputChanged = { checkIsFormComplete() }
+        houseNameInput?.doAfterTextChanged { checkIsFormComplete() }
 
         checkIsFormComplete()
-    }
-
-    private fun prefillBlockNumber() {
-        /* the block number likely does not change from one input to the other, so let's prefill it
-           with the last selected value */
-        lastBlockNumber?.let { blockNumberInput?.setText(it) }
-    }
-
-    private fun showHouseNumberHint() {
-        val input = houseNumberInput ?: return
-        val prev = lastHouseNumber ?: return
-
-        /* The Auto fit layout does not work with hints, so we workaround this by setting the "real"
-        *  text instead and make it look like it is a hint. This little hack is much less effort
-        *  than to fork and fix the external dependency. We need to revert back the color both on
-        *  focus and on text changed (tapping on +/- button) */
-        houseNumberInputTextColors = input.textColors
-        input.setTextColor(input.hintTextColors)
-        input.setText(prev)
-        input.doAfterTextChanged {
-            val colors = houseNumberInputTextColors
-            if (colors != null) input.setTextColor(colors)
-            houseNumberInputTextColors = null
-        }
-        input.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
-            updateKeyboardButtonVisibility()
-            if (hasFocus) input.showKeyboard()
-            val colors = houseNumberInputTextColors
-            if (hasFocus && colors != null) {
-                input.text = null
-                input.setTextColor(colors)
-                houseNumberInputTextColors = null
-            }
-        }
-    }
-
-    private fun addToHouseNumberInput(add: Int) {
-        val input = houseNumberInput ?: return
-        val prev = input.text.toString().ifBlank { lastHouseNumber } ?: return
-        val newHouseNumber = addToHouseNumber(prev, add) ?: return
-        input.setText(newHouseNumber)
-        input.setSelection(newHouseNumber.length)
-    }
-
-    private fun initKeyboardButton() {
-        toggleKeyboardButton?.text = "abc"
-        toggleKeyboardButton?.setOnClickListener {
-            val focus = requireActivity().currentFocus
-            if (focus != null && focus is EditText) {
-                val start = focus.selectionStart
-                val end = focus.selectionEnd
-                if (focus.inputType and InputType.TYPE_CLASS_NUMBER != 0) {
-                    focus.inputType = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
-                    toggleKeyboardButton?.text = "123"
-                } else {
-                    focus.inputType = InputType.TYPE_CLASS_NUMBER
-                    focus.keyListener = DigitsKeyListener.getInstance("0123456789.,- /")
-                    toggleKeyboardButton?.text = "abc"
-                }
-                // for some reason, the cursor position gets lost first time the input type is set (#1093)
-                focus.setSelection(start, end)
-                focus.showKeyboard()
-            }
-        }
-        updateKeyboardButtonVisibility()
-
-        val onFocusChange = View.OnFocusChangeListener { v, hasFocus ->
-            updateKeyboardButtonVisibility()
-            if (hasFocus) v.showKeyboard()
-        }
-        houseNumberInput?.onFocusChangeListener = onFocusChange
-        streetNumberInput?.onFocusChangeListener = onFocusChange
-        blockNumberInput?.onFocusChangeListener = onFocusChange
-    }
-
-    private fun updateKeyboardButtonVisibility() {
-        toggleKeyboardButton?.isInvisible = !(
-            houseNumberInput?.hasFocus() == true
-            || streetNumberInput?.hasFocus() == true
-            || blockNumberInput?.hasFocus() == true
-        )
     }
 
     /* ----------------------------------- Commit answer ---------------------------------------- */
@@ -265,24 +176,14 @@ class AddHousenumberForm : AbstractOsmQuestForm<HouseNumberAnswer>() {
 
     override fun isRejectingClose(): Boolean =
         houseNameInput?.nonBlankTextOrNull != null
-        || houseNumberInput?.nonBlankTextOrNull?.takeIf { !isShowingHouseNumberHint } != null
-        || conscriptionNumberInput?.nonBlankTextOrNull != null
-        || streetNumberInput?.nonBlankTextOrNull != null
-        || blockNumberInput?.nonBlankTextOrNull != null
+        || houseNumberInputViewController?.isEmpty == false
+        || conscriptionNumberInputViewController?.isEmpty == false
 
     private fun createAnswer(): HouseNumberAndHouseName {
         val houseName = houseNameInput?.nonBlankTextOrNull
-        val houseNumber = houseNumberInput?.nonBlankTextOrNull?.takeIf { !isShowingHouseNumberHint }
-        val conscriptionNumber = conscriptionNumberInput?.nonBlankTextOrNull
-        val streetNumber = streetNumberInput?.nonBlankTextOrNull
-        val blockNumber = blockNumberInput?.nonBlankTextOrNull
-
-        val addressNumber = when {
-            conscriptionNumber != null                 -> ConscriptionNumber(conscriptionNumber, streetNumber) // streetNumber is optional
-            blockNumber != null && houseNumber != null -> HouseAndBlockNumber(houseNumber, blockNumber)
-            houseNumber != null                        -> HouseNumber(houseNumber)
-            else                                       -> null
-        }
+        val addressNumber =
+            houseNumberInputViewController?.addressNumber
+            ?: conscriptionNumberInputViewController?.conscriptionNumber
         return HouseNumberAndHouseName(addressNumber, houseName)
     }
 
@@ -293,7 +194,7 @@ class AddHousenumberForm : AbstractOsmQuestForm<HouseNumberAnswer>() {
             "SK" -> R.layout.quest_housenumber_slovakia
             else -> R.layout.quest_housenumber
         }
-        InterfaceMode.HOUSENAME -> R.layout.quest_housename
+        InterfaceMode.HOUSENAME ->                 R.layout.quest_housename
         InterfaceMode.HOUSENUMBER_AND_HOUSENAME -> R.layout.quest_housename_and_housenumber
     }
 
---
+++ app/src/main/java/de/westnordost/streetcomplete/view/controller/HouseNumberInputViewController.kt
@@ -0,0 +1,106 @@
+package de.westnordost.streetcomplete.view.controller
+
+import android.app.Activity
+import android.content.res.ColorStateList
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.doAfterTextChanged
+import de.westnordost.streetcomplete.osm.housenumber.AddressNumber
+import de.westnordost.streetcomplete.osm.housenumber.HouseAndBlockNumber
+import de.westnordost.streetcomplete.osm.housenumber.HouseNumber
+import de.westnordost.streetcomplete.osm.housenumber.addToHouseNumber
+import de.westnordost.streetcomplete.util.ktx.nonBlankTextOrNull
+import de.westnordost.streetcomplete.util.ktx.showKeyboard
+
+/** Manages inputting a house number with optional block number (Japan) */
+class HouseNumberInputViewController(
+    activity: Activity,
+    private val houseNumberInput: EditText,
+    private val blockNumberInput: EditText?,
+    toggleKeyboardButton: Button,
+    addButton: View?,
+    subtractButton: View?,
+    private val lastBlockNumber: String?,
+    private val lastHouseNumber: String?
+) {
+
+    private val toggleKeyboardButtonViewController: SwitchKeyboardButtonViewController
+
+    private var houseNumberInputTextColors: ColorStateList? = null
+    // because the hint is implemented as a hack: it is actually the text proper but colored in light-gray
+    private val houseNumberInputText: String? =
+        houseNumberInput.nonBlankTextOrNull?.takeIf { houseNumberInputTextColors == null }
+
+    var onInputChanged: (() -> Unit)? = null
+
+    val isEmpty: Boolean get() =
+        houseNumberInputText == null
+        && blockNumberInput?.nonBlankTextOrNull == null
+
+    val addressNumber: AddressNumber? get() {
+        val houseNumber = houseNumberInputText ?: return null
+        val blockNumber = blockNumberInput?.nonBlankTextOrNull
+        return when {
+            blockNumber != null -> HouseAndBlockNumber(houseNumber, blockNumber)
+            else ->                HouseNumber(houseNumber)
+        }
+    }
+
+    init {
+        addButton?.setOnClickListener { addToHouseNumberInput(+1) }
+        subtractButton?.setOnClickListener { addToHouseNumberInput(-1) }
+
+        // must be called before registering the text changed watchers because it changes the text
+        prefillBlockNumber()
+
+        houseNumberInput.doAfterTextChanged { onInputChanged?.invoke() }
+        blockNumberInput?.doAfterTextChanged { onInputChanged?.invoke() }
+
+        toggleKeyboardButtonViewController = SwitchKeyboardButtonViewController(activity, toggleKeyboardButton, setOfNotNull(houseNumberInput, blockNumberInput))
+
+        // must be after initKeyboardButton+textchanged watchers because it re-sets the onFocusListener
+        showHouseNumberHint()
+    }
+
+    private fun prefillBlockNumber() {
+        /* the block number likely does not change from one input to the other, so let's prefill it
+           with the last selected value */
+        lastBlockNumber?.let { blockNumberInput?.setText(it) }
+    }
+
+    private fun showHouseNumberHint() {
+        val prev = lastHouseNumber ?: return
+
+        /* The Auto fit layout does not work with hints, so we workaround this by setting the "real"
+        *  text instead and make it look like it is a hint. This little hack is much less effort
+        *  than to fork and fix the external dependency. We need to revert back the color both on
+        *  focus and on text changed (tapping on +/- button) */
+        houseNumberInputTextColors = houseNumberInput.textColors
+        houseNumberInput.setTextColor(houseNumberInput.hintTextColors)
+        houseNumberInput.setText(prev)
+        houseNumberInput.doAfterTextChanged {
+            val colors = houseNumberInputTextColors
+            if (colors != null) houseNumberInput.setTextColor(colors)
+            houseNumberInputTextColors = null
+        }
+        houseNumberInput.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            toggleKeyboardButtonViewController.updateVisibility()
+            if (hasFocus) houseNumberInput.showKeyboard()
+            val colors = houseNumberInputTextColors
+            if (hasFocus && colors != null) {
+                houseNumberInput.text = null
+                houseNumberInput.setTextColor(colors)
+                houseNumberInputTextColors = null
+            }
+        }
+    }
+
+    private fun addToHouseNumberInput(add: Int) {
+        val input = houseNumberInput
+        val prev = input.text.toString().ifBlank { lastHouseNumber } ?: return
+        val newHouseNumber = addToHouseNumber(prev, add) ?: return
+        input.setText(newHouseNumber)
+        input.setSelection(newHouseNumber.length)
+    }
+}
