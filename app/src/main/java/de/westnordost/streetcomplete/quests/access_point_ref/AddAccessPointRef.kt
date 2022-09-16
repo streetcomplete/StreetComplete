f6bfcc64575e4573227fddcdf92646a201cbac79
mcliquid <info@mcliquid.de>
2022-09-16 21:46:40 +0200

    Cleaned unused things and added icon

--- app/src/main/java/de/westnordost/streetcomplete/quests/access_point_ref/AddAccessPointRef.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/access_point_ref/AddAccessPointRef.kt
@@ -2,9 +2,7 @@ package de.westnordost.streetcomplete.quests.access_point_ref
 
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
-import de.westnordost.streetcomplete.data.quest.NoCountriesExcept
 import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement
-import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement.PEDESTRIAN
 import de.westnordost.streetcomplete.osm.Tags
 
 class AddAccessPointRef : OsmFilterQuestType<AccessPointRefAnswer>() {
@@ -18,10 +16,10 @@ class AddAccessPointRef : OsmFilterQuestType<AccessPointRefAnswer>() {
         )
         and !name and !ref and noref != yes and ref:signed != no and !~"ref:.*"
     """
-    //override val enabledInCountries = NoCountriesExcept("US", "CA", "JE", "IE")
+
     override val changesetComment = "Determine emergency access point refs"
     override val wikiLink = "Tag:emergency=access_point"
-    override val icon = R.drawable.ic_quest_notes
+    override val icon = R.drawable.ic_quest_access_point
     override val achievements = listOf(EditTypeAchievement.LIFESAVER)
 
     override fun getTitle(tags: Map<String, String>) = R.string.quest_accessPointRef_title
