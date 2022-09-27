63289cf33fc2c826953ad2c26df945e481dacda3
Tobias Zwick <newton@westnordost.de>
2022-09-27 19:40:49 +0200

    remove selection ring around selected quest
    because:
    - when a quest is highlighted, only that quest is shown anyway, so superfluous now
    - that ring is also not shown for creating-a-note or creating-an-element, so inconsistent
    - when a quest is highlighted, the important part to highlight is the geometry, so slightly diverting attention from what is the most important

--- app/src/main/java/de/westnordost/streetcomplete/screens/main/map/components/SelectedPinsMapComponent.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/main/map/components/SelectedPinsMapComponent.kt
@@ -1,54 +1,23 @@
 package de.westnordost.streetcomplete.screens.main.map.components
 
 import android.content.Context
-import android.graphics.PointF
-import android.graphics.drawable.BitmapDrawable
 import androidx.annotation.DrawableRes
 import com.mapzen.tangram.MapData
 import com.mapzen.tangram.geometry.Point
-import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.osm.mapdata.LatLon
 import de.westnordost.streetcomplete.screens.main.map.tangram.KtMapController
-import de.westnordost.streetcomplete.screens.main.map.tangram.Marker
 import de.westnordost.streetcomplete.screens.main.map.tangram.toLngLat
-import de.westnordost.streetcomplete.util.ktx.getBitmapDrawable
-import de.westnordost.streetcomplete.util.ktx.pxToDp
 
 /** Takes care of displaying "selected" pins. Those pins are always shown on top of pins displayed
  *  by the [PinsMapComponent] */
 class SelectedPinsMapComponent(private val ctx: Context, private val ctrl: KtMapController) {
 
-    private val selectedPinsLayer: MapData
-    private val pinSelectionMarkers: MutableList<Marker> = mutableListOf()
-
-    private val selectionDrawable: BitmapDrawable
-    private val selectionDrawableSize: PointF
-
-    init {
-        selectionDrawable = ctx.resources.getBitmapDrawable(R.drawable.pin_selection_ring)
-        selectionDrawableSize = PointF(
-            ctx.pxToDp(selectionDrawable.bitmap.width),
-            ctx.pxToDp(selectionDrawable.bitmap.height)
-        )
-
-        selectedPinsLayer = ctrl.addDataLayer(SELECTED_PINS_LAYER)
-    }
+    private val selectedPinsLayer: MapData = ctrl.addDataLayer(SELECTED_PINS_LAYER)
 
     /** Show selected pins with the given icon at the given positions. "Selected pins" are not
      *  related to pins, they are just visuals that are displayed on top of the normal pins and look
      *  highlighted/selected. */
     fun set(@DrawableRes iconResId: Int, pinPositions: Collection<LatLon>) {
-        putSelectedPins(iconResId, pinPositions)
-        showPinSelectionMarkers(pinPositions)
-    }
-
-    /** Clear the display of any selected pins */
-    fun clear() {
-        selectedPinsLayer.clear()
-        clearPinSelectionMarkers()
-    }
-
-    private fun putSelectedPins(@DrawableRes iconResId: Int, pinPositions: Collection<LatLon>) {
         val points = pinPositions.map { position ->
             Point(position.toLngLat(), mapOf(
                 "type" to "point",
@@ -58,32 +27,9 @@ class SelectedPinsMapComponent(private val ctx: Context, private val ctrl: KtMap
         selectedPinsLayer.setFeatures(points)
     }
 
-    private fun showPinSelectionMarkers(positions: Collection<LatLon>) {
-        clearPinSelectionMarkers()
-        for (position in positions) {
-            pinSelectionMarkers.add(createPinSelectionMarker(position))
-        }
-    }
-
-    private fun clearPinSelectionMarkers() {
-        pinSelectionMarkers.forEach { ctrl.removeMarker(it) }
-        pinSelectionMarkers.clear()
-    }
-
-    private fun createPinSelectionMarker(pos: LatLon): Marker = ctrl.addMarker().also {
-        it.setStylingFromString("""
-        {
-            style: 'pin-selection',
-            color: 'white',
-            size: [${selectionDrawableSize.x}px, ${selectionDrawableSize.y}px],
-            flat: false,
-            collide: false,
-            offset: ['0px', '-38px']
-        }
-        """.trimIndent())
-        it.setDrawable(selectionDrawable)
-        it.isVisible = true
-        it.setPoint(pos)
+    /** Clear the display of any selected pins */
+    fun clear() {
+        selectedPinsLayer.clear()
     }
 
     companion object {
