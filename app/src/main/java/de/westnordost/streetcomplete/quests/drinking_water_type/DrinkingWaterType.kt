1eb59b0eef5e1fbb6d242ded3bd710152a2895a6
Mateusz Konieczny <matkoniecz@gmail.com>
2022-09-28 21:05:01 +0200

    get rid of no longer helpful enum parameters
    they have outgrown any usefullness

--- app/src/main/java/de/westnordost/streetcomplete/quests/drinking_water_type/DrinkingWaterType.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/drinking_water_type/DrinkingWaterType.kt
@@ -1,31 +1,59 @@
 package de.westnordost.streetcomplete.quests.drinking_water_type
 
 import de.westnordost.streetcomplete.osm.Tags
+import de.westnordost.streetcomplete.osm.hasCheckDate
+import de.westnordost.streetcomplete.osm.updateCheckDate
+import de.westnordost.streetcomplete.quests.drinking_water_type.DrinkingWaterType.*
 
-enum class DrinkingWaterType(val osmKey: String, val osmValue: String, val providesDrinkingWater: Boolean = true) {
-    WATER_FOUNTAIN_GENERIC("fountain", "drinking"),
-    WATER_FOUNTAIN_JET("fountain", "bubbler"),
-    WATER_FOUNTAIN_BOTTLE_REFILL_ONLY("fountain", "bottle_refill"),
-    WATER_TAP("man_made", "water_tap"),
-    HAND_PUMP("pump", "manual"),
-    WATER_WELL_WITHOUT_PUMP("pump", "no"),
-    SPRING("natural", "spring"),
-    DISUSED_DRINKING_WATER("disued:amenity", "drinking_water", false),
+enum class DrinkingWaterType {
+    WATER_FOUNTAIN_GENERIC,
+    WATER_FOUNTAIN_JET,
+    WATER_FOUNTAIN_BOTTLE_REFILL_ONLY,
+    WATER_TAP,
+    HAND_PUMP,
+    WATER_WELL_WITHOUT_PUMP,
+    SPRING,
+    DISUSED_DRINKING_WATER,
 }
 
 fun DrinkingWaterType.applyTo(tags: Tags) {
-    tags[this.osmKey] = this.osmValue
-    if (this == DrinkingWaterType.HAND_PUMP || this == DrinkingWaterType.WATER_WELL_WITHOUT_PUMP) {
-        tags["man_made"] = "water_well"
+    when (this) {
+        WATER_FOUNTAIN_GENERIC -> tags["fountain"] == "drinking"
+        WATER_FOUNTAIN_JET -> tags["fountain"] == "bubbler"
+        WATER_FOUNTAIN_BOTTLE_REFILL_ONLY -> tags["fountain"] == "bottle_refill"
+        WATER_TAP -> tags["man_made"] == "water_tap"
+        HAND_PUMP -> {
+            tags["man_made"] = "water_well"
+            tags["pump"] = "manual"
+        }
+        WATER_WELL_WITHOUT_PUMP -> {
+            tags["man_made"] = "water_well"
+            tags["pump"] = "no"
+        }
+        SPRING -> tags["natural"] == "spring"
+        DISUSED_DRINKING_WATER -> {
+            var changed = false
+            if (tags["amenity"] == "drinking_water") {
+                tags.remove("amenity")
+                changed = true
+            }
+            if (tags["amenity"] != "drinking_water") {
+                tags["disused:amenity"] = "drinking_water"
+                changed = true
+            }
+            if (!changed || tags.hasCheckDate()) {
+                tags.updateCheckDate()
+            }
+        }
     }
-    if (this.providesDrinkingWater) {
+
+    // handle change from disused:amenity=drinking_water to amenity=drinking_water
+    if (this != DISUSED_DRINKING_WATER) {
         if (tags["disused:amenity"] == "drinking_water") {
             tags.remove("disused:amenity")
-            tags["amenity"] = "drinking_water"
         }
-    } else {
-        if (tags["amenity"] == "drinking_water") {
-            tags.remove("amenity")
+        if (tags["amenity"] != "drinking_water") {
+            tags["amenity"] = "drinking_water"
         }
     }
 }
