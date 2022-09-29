75f9cd2fc32d1b1280a9446490b61880a06d1169
Tobias Zwick <newton@westnordost.de>
2022-09-29 20:05:29 +0200

    ShopsOverlay: getStyledElements

--- app/src/main/java/de/westnordost/streetcomplete/overlays/shops/ShopsOverlay.kt
+++ app/src/main/java/de/westnordost/streetcomplete/overlays/shops/ShopsOverlay.kt
@@ -1,16 +1,23 @@
 package de.westnordost.streetcomplete.overlays.shops
 
+import de.westnordost.osmfeatures.FeatureDictionary
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.osm.mapdata.Element
 import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
+import de.westnordost.streetcomplete.data.osm.mapdata.Node
+import de.westnordost.streetcomplete.data.osm.mapdata.filter
 import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement
+import de.westnordost.streetcomplete.osm.IS_SHOP_OR_DISUSED_SHOP_EXPRESSION
+import de.westnordost.streetcomplete.overlays.Color
 import de.westnordost.streetcomplete.overlays.Overlay
-import de.westnordost.streetcomplete.overlays.Style
+import de.westnordost.streetcomplete.overlays.PointStyle
+import de.westnordost.streetcomplete.overlays.PolygonStyle
 import de.westnordost.streetcomplete.quests.place_name.AddPlaceName
 import de.westnordost.streetcomplete.quests.shop_type.CheckShopType
 import de.westnordost.streetcomplete.quests.shop_type.SpecifyShopType
+import de.westnordost.streetcomplete.util.getNameLabel
 
-class ShopsOverlay: Overlay {
+class ShopsOverlay(private val featureDictionary: FeatureDictionary): Overlay {
 
     override val title = R.string.overlay_shops
     override val icon = R.drawable.ic_quest_shop
@@ -24,9 +31,22 @@ class ShopsOverlay: Overlay {
     )
     override val isCreateNodeEnabled = true
 
-    override fun getStyledElements(mapData: MapDataWithGeometry): Sequence<Pair<Element, Style>> {
-        TODO("Not yet implemented")
-    }
+    override fun getStyledElements(mapData: MapDataWithGeometry) =
+        mapData
+            .filter(IS_SHOP_OR_DISUSED_SHOP_EXPRESSION)
+            .map { element ->
+                val feature = featureDictionary.byTags(element.tags).find().firstOrNull()
+
+                val icon = feature?.icon ?: "ic_preset_make_shop" // TODO or dot
+                val label = getNameLabel(element.tags)
+
+                val style = if (element is Node) {
+                    PointStyle(icon, name)
+                } else {
+                    PolygonStyle(Color.INVISIBLE, icon, label)
+                }
+                element to style
+            }
 
     override fun createForm(element: Element?) = ShopsOverlayForm()
 }
