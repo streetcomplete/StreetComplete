6e4cd3d1351bae9068fb0d2032509fc021030d60
Tobias Zwick <newton@westnordost.de>
2022-10-03 00:35:07 +0200

    put polygons in front, but make them 20% transparent
    see https://github.com/streetcomplete/StreetComplete/commit/890fdc4bc7e6ccadcad78e4941d056225140bb32#commitcomment-85359473

--- app/src/main/java/de/westnordost/streetcomplete/util/ktx/Color.kt
+++ app/src/main/java/de/westnordost/streetcomplete/util/ktx/Color.kt
@@ -1,6 +1,7 @@
 package de.westnordost.streetcomplete.util.ktx
 
 import android.graphics.Color
+import androidx.annotation.ColorInt
 import androidx.core.graphics.alpha
 import androidx.core.graphics.blue
 import androidx.core.graphics.green
@@ -12,6 +13,9 @@ fun darken(color: Int, by: Float): Int {
     hsv[2] = hsv[2] * by
     return Color.HSVToColor(color.alpha, hsv)
 }
+
+fun alpha(@ColorInt color: Int, alpha: Float): Int =
+    Color.argb((alpha.coerceIn(0f, 1f)*255).toInt(), color.red, color.green, color.blue)
 
 fun toARGBString(color: Int): String =
     "#" + color.alpha.toString(16).padStart(2, '0') +
