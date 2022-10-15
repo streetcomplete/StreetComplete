f274e912318db0fded0d2c03a5c7a89cf540b542
Matija Nalis <mnalis-git@voyager.hr>
2022-10-15 15:55:03 +0200

    ordering test

--- app/src/main/java/de/westnordost/streetcomplete/quests/surface/SidewalkSurfaceAnswer.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/SidewalkSurfaceAnswer.kt
@@ -1,12 +1,13 @@
 package de.westnordost.streetcomplete.quests.surface
 
-data class SidewalkSurfaceSide(val surface: Surface)
-
 sealed interface SidewalkSurfaceAnswer
-object SidewalkIsDifferent : SidewalkSurfaceAnswer
 
 data class SidewalkSurface(
     val left: SurfaceAnswer?,
     val right: SurfaceAnswer?,
 ) : SidewalkSurfaceAnswer
+
+object SidewalkIsDifferent : SidewalkSurfaceAnswer
+
+data class SidewalkSurfaceSide(val surface: Surface)
 
