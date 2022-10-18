8645940a8e40a10167dcd54ed36423e9f2dcef6a
Matija Nalis <mnalis-git@voyager.hr>
2022-10-18 02:42:10 +0200

    try to compile test with SidewalkIsDifferent

--- app/src/test/java/de/westnordost/streetcomplete/quests/surface/AddSidewalkSurfaceTest.kt
+++ app/src/test/java/de/westnordost/streetcomplete/quests/surface/AddSidewalkSurfaceTest.kt
@@ -131,7 +131,7 @@ class AddSidewalkSurfaceTest {
                 "sidewalk:left" to "yes",
                 "sidewalk:right" to "yes",
             ),
-            SidewalkSurfaceAnswer(SidewalkIsDifferent),
+            SidewalkSurface(SidewalkIsDifferent),
             StringMapEntryDelete("sidewalk:left:surface", "asphalt"),
             StringMapEntryDelete("sidewalk:right:surface", "concrete"),
             StringMapEntryDelete("sidewalk:left:smoothness", "excellent"),
