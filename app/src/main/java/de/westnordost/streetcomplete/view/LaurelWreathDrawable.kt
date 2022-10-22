33619fb09696cfd35e4036787a3a05d2222b144e
Mateusz Konieczny <matkoniecz@gmail.com>
2022-10-22 19:07:38 +0200

    refactor, subtler stalk, draw arc once

--- app/src/main/java/de/westnordost/streetcomplete/view/LaurelWreathDrawable.kt
+++ app/src/main/java/de/westnordost/streetcomplete/view/LaurelWreathDrawable.kt
@@ -21,6 +21,7 @@ below: no decorative styling at all
 class LaurelWreathDrawable(val resources: Resources, private val percentageOfGrowth: Int) : Drawable() {
     private val laurelLeafOnStalk = resources.getBitmapDrawable(R.drawable.ic_laurel_leaf_rotated)
     private val horizontalEndingLeaf = resources.getBitmapDrawable(R.drawable.ic_laurel_leaf_ending)
+    private val niceSubtleGreen: Paint = Paint().apply { setARGB(255, 152, 184, 126) }
 
     private val antiAliasPaint: Paint = Paint().apply {
         isAntiAlias = true
@@ -28,12 +29,10 @@ class LaurelWreathDrawable(val resources: Resources, private val percentageOfGro
     }
 
     override fun draw(canvas: Canvas) {
-        // Get the drawable's bounds
         val canvasWidth: Int = bounds.width()
         val canvasHeight: Int = bounds.height()
         val circleRadius: Float = Math.min(canvasWidth, canvasHeight).toFloat() / 2f
 
-        val niceSubtleGreen: Paint = Paint().apply { setARGB(255, 152, 184, 126) }
         canvas.drawCircle((canvasWidth / 2).toFloat(), (canvasHeight / 2).toFloat(), circleRadius, niceSubtleGreen)
 
         if (percentageOfGrowth < 10) {
@@ -47,24 +46,26 @@ class LaurelWreathDrawable(val resources: Resources, private val percentageOfGro
         val circleCenterX = canvasWidth / 2f
         val shownSegments = ((regularSegmentCount - 1) * percentageOfGrowth / 100).toInt()
         val locationBetweenCenterAndEdge = 0.78f
+
+        val stalkPaint = Paint()
+        stalkPaint.isAntiAlias = true
+        stalkPaint.setARGB(255, 0, 106, 0)
+        stalkPaint.style = Paint.Style.STROKE
+        stalkPaint.strokeWidth = 2.5f
+        val stalkCircleSize = circleRadius * locationBetweenCenterAndEdge * 1.1f
+        val frame = RectF(canvasWidth.toFloat() / 2 - stalkCircleSize, canvasHeight.toFloat() / 2 - stalkCircleSize, canvasWidth.toFloat() / 2 + stalkCircleSize, canvasHeight.toFloat() / 2 + stalkCircleSize)
+        val stalkLengthInDegrees = 180f * percentageOfGrowth / 100f - 15f
+        canvas.drawArc(frame, 90f, stalkLengthInDegrees, false, stalkPaint )
+        val upperLimitOfRightStalk = (90f - stalkLengthInDegrees + 360) % 360
+        canvas.drawArc(frame, upperLimitOfRightStalk, stalkLengthInDegrees, false, stalkPaint )
+
         for (i in 1..shownSegments) {
             // https://developer.android.com/reference/kotlin/androidx/core/graphics/package-summary#(android.graphics.Canvas).withRotation(kotlin.Float,kotlin.Float,kotlin.Float,kotlin.Function1)
-
             var bitmap = laurelLeafOnStalk.bitmap
             if (i == shownSegments) {
                 bitmap = horizontalEndingLeaf.bitmap
             }
-            val stalkPaint = Paint()
-            stalkPaint.isAntiAlias = true
-            stalkPaint.setARGB(255, 0, 106, 0)
-            stalkPaint.style = Paint.Style.STROKE
-            stalkPaint.strokeWidth = 3f
-            val stalkCircleSize = circleRadius * locationBetweenCenterAndEdge * 1.1f
-            val frame = RectF(canvasWidth.toFloat() / 2 - stalkCircleSize, canvasHeight.toFloat() / 2 - stalkCircleSize, canvasWidth.toFloat() / 2 + stalkCircleSize, canvasHeight.toFloat() / 2 + stalkCircleSize)
-            val stalkLengthInDegrees = 180f * percentageOfGrowth / 100f - 10f
-            canvas.drawArc(frame, 90f, stalkLengthInDegrees, false, stalkPaint )
-            val upperLimitOfRightStalk = (90f - stalkLengthInDegrees + 360) % 360
-            canvas.drawArc(frame, upperLimitOfRightStalk, stalkLengthInDegrees, false, stalkPaint )
+
             // left side
             canvas.withRotation(i * 180.0f / regularSegmentCount, canvasWidth / 2f, canvasHeight / 2f) {
                 // drawBitmap takes corner of the bitmap, we care about centering segments
