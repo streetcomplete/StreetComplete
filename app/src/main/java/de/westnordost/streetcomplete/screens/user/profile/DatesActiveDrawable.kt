c6cedafad2fcc89aaa54b67b1d12f69029e7214e
Tobias Zwick <newton@westnordost.de>
2022-10-30 11:42:52 +0100

    nicer display in dark mode

--- app/src/main/java/de/westnordost/streetcomplete/screens/user/profile/DatesActiveDrawable.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/user/profile/DatesActiveDrawable.kt
@@ -31,7 +31,7 @@ class DatesActiveDrawable(
     private val textHeight: Float = boxSize * 0.8f
 
     private val greenBoxPaint = Paint().apply { setARGB(255, 152, 184, 126) }
-    private val emptyBoxPaint = Paint().apply { setARGB(20, 0, 0, 0) }
+    private val emptyBoxPaint = Paint().apply { setARGB(20, 128, 128, 128) }
     private val textPaint = Paint().apply {
         color = textColor
         textSize = textHeight
