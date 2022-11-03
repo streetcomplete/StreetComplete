ae283547eee8e1f701ebc9b7dd11c9f09a646f50
Mateusz Konieczny <matkoniecz@gmail.com>
2022-11-03 09:18:29 +0100

    fix tests

--- app/src/test/java/de/westnordost/streetcomplete/osm/surface/SurfaceKtTest.kt
+++ app/src/test/java/de/westnordost/streetcomplete/osm/surface/SurfaceKtTest.kt
@@ -1,4 +1,4 @@
-package de.westnordost.streetcomplete.osm
+package de.westnordost.streetcomplete.osm.surface
 
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
--- app/src/test/java/de/westnordost/streetcomplete/quests/surface/SurfaceAnswerKtTest.kt
+++ app/src/test/java/de/westnordost/streetcomplete/quests/surface/SurfaceAnswerKtTest.kt
@@ -6,6 +6,7 @@ import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapEntryCh
 import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapEntryDelete
 import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapEntryModify
 import de.westnordost.streetcomplete.osm.nowAsCheckDateString
+import de.westnordost.streetcomplete.osm.surface.Surface
 import org.assertj.core.api.Assertions
 import org.junit.Test
 
