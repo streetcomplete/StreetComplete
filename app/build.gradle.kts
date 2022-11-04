c044209de3f4c321b87941ed406939c4ef398110
Tobias Zwick <newton@westnordost.de>
2022-11-04 21:18:03 +0100

    update translations, metadata,...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -203,7 +203,7 @@ val bcp47ExportLanguages = setOf(
 )
 
 // see https://github.com/osmlab/name-suggestion-index/tags for latest version
-val nsiVersion = "v6.0.20221010"
+val nsiVersion = "v6.0.20221031"
 // see https://github.com/openstreetmap/id-tagging-schema/releases for latest version
 val presetsVersion = "v3.5.1"
 
