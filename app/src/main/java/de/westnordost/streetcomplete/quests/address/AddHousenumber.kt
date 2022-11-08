c3bd422a5b67561eca1a075648137f674ae61d67
Mateusz Konieczny <matkoniecz@gmail.com>
2022-11-08 10:10:49 +0100

    update link as package was renamed
    fixes #4620

--- app/src/main/java/de/westnordost/streetcomplete/quests/address/AddHousenumber.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/address/AddHousenumber.kt
@@ -26,7 +26,7 @@ class AddHousenumber : OsmElementQuestType<HouseNumberAnswer> {
     override val wikiLink = "Key:addr"
     override val icon = R.drawable.ic_quest_housenumber
     override val achievements = listOf(POSTMAN)
-    // See overview here: https://ent8r.github.io/blacklistr/?streetcomplete=housenumber/AddHousenumber.kt
+    // See overview here: https://ent8r.github.io/blacklistr/?streetcomplete=address/AddHousenumber.kt
     override val enabledInCountries = AllCountriesExcept(
         "LU", // https://github.com/streetcomplete/StreetComplete/pull/1943
         "LV", // https://github.com/streetcomplete/StreetComplete/issues/4597
