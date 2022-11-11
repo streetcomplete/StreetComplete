27c7625827e7d1d87a8a02d9dedda9c4ec992a79
Mateusz Konieczny <matkoniecz@gmail.com>
2022-11-11 15:34:19 +0100

    fix tests with overlays

--- app/src/test/java/de/westnordost/streetcomplete/quests/surface/AddFootwayPartSurfaceTest.kt
+++ app/src/test/java/de/westnordost/streetcomplete/quests/surface/AddFootwayPartSurfaceTest.kt
@@ -4,6 +4,7 @@ import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapEntryAd
 import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapEntryDelete
 import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapEntryModify
 import de.westnordost.streetcomplete.osm.nowAsCheckDateString
+import de.westnordost.streetcomplete.osm.surface.Surface
 import de.westnordost.streetcomplete.quests.TestMapDataWithGeometry
 import de.westnordost.streetcomplete.quests.verifyAnswer
 import de.westnordost.streetcomplete.testutils.way
