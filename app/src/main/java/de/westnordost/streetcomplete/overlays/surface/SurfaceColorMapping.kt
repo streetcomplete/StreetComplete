03ba0526dc84cb98318b950277270e04896e7220
Mateusz Konieczny <matkoniecz@gmail.com>
2022-11-19 17:08:46 +0100

    skipped for now, will get extra documentation of someone will be curious

--- app/src/main/java/de/westnordost/streetcomplete/overlays/surface/SurfaceColorMapping.kt
+++ app/src/main/java/de/westnordost/streetcomplete/overlays/surface/SurfaceColorMapping.kt
@@ -18,19 +18,4 @@ val Surface?.color get() = when (this) {
     GRAVEL, PEBBLES, ROCK -> "#999999"
     CLAY, ARTIFICIAL_TURF, TARTAN -> Color.BLACK // not encountered in normal situations, get the same as surface with surface:note
     PAVED_ROAD, PAVED_AREA, UNPAVED_ROAD, UNPAVED_AREA, null -> Color.DATA_REQUESTED
-}    /*
-    // TODO create graphs as illustration of design process
-    categorizing surface into 8+2 groups
-
-    missing surface (including paved/unpaved) vs missing surface (including paved/unpaved) with note vs surface
-
-    fundamental split:
-    all: paved vs unpaved
-    paved: flat and flattish vs unhewn_cobblestone
-    flat and flattish paved: asphalt/concrete/paving_stones/wood/metal vs sett/concrete:plates vs concrete:lanes
-    unpaved: extremely bad for cyclists (sand, grass paver), high quality (compacted), all other
-    extremely bad for cyclists: sand, grass paver
-    unpaved_other: dirt/gravel/pebblestone/rock/ground/woodchips/fine_gravel vs grass
-
-    special, not really on roads: clay, artificial_turf, tartan
-    */
+}
