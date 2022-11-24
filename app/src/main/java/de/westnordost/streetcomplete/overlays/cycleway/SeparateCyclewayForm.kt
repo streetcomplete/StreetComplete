f29fa95fda07d0db34926930cf2709d2e9a8e72d
Tobias Zwick <osm@westnordost.de>
2022-11-24 12:43:29 +0100

    init separate cycleway form with current tagging + treat "allowed" as "none"

--- app/src/main/java/de/westnordost/streetcomplete/overlays/cycleway/SeparateCyclewayForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/overlays/cycleway/SeparateCyclewayForm.kt
@@ -1,20 +1,32 @@
 package de.westnordost.streetcomplete.overlays.cycleway
 
+import android.os.Bundle
+import android.view.View
 import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapChangesBuilder
 import de.westnordost.streetcomplete.data.osm.edits.update_tags.UpdateElementTagsAction
 import de.westnordost.streetcomplete.osm.cycleway_separate.SeparateCycleway
 import de.westnordost.streetcomplete.osm.cycleway_separate.SeparateCycleway.*
 import de.westnordost.streetcomplete.osm.cycleway_separate.applyTo
 import de.westnordost.streetcomplete.osm.cycleway_separate.asItem
+import de.westnordost.streetcomplete.osm.cycleway_separate.createSeparateCycleway
 import de.westnordost.streetcomplete.overlays.AImageSelectOverlayForm
 import de.westnordost.streetcomplete.view.image_select.DisplayItem
 
 class SeparateCyclewayForm : AImageSelectOverlayForm<SeparateCycleway>() {
 
-    override val items: List<DisplayItem<SeparateCycleway>> =
-        listOf(NONE, NON_SEGREGATED, SEGREGATED, EXCLUSIVE, WITH_SIDEWALK).map { it.asItem() }
+    override val items: List<DisplayItem<SeparateCycleway>> get() =
+        listOf(NONE, NON_SEGREGATED, SEGREGATED, EXCLUSIVE, WITH_SIDEWALK).map {
+            it.asItemTreatingAllowedAsNone(countryInfo.isLeftHandTraffic)
+        }
 
     private var currentCycleway: SeparateCycleway? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        currentCycleway = createSeparateCycleway(element!!.tags)
+        selectedItem = currentCycleway?.asItemTreatingAllowedAsNone(countryInfo.isLeftHandTraffic)
+    }
 
     override fun hasChanges(): Boolean =
         selectedItem?.value != currentCycleway
@@ -25,3 +37,21 @@ class SeparateCyclewayForm : AImageSelectOverlayForm<SeparateCycleway>() {
         applyEdit(UpdateElementTagsAction(tagChanges.create()))
     }
 }
+
+/* Not displaying bicycle=yes and bicycle=no on footways and treating it the same because whether
+*  riding a bike on a footway is allowed by default (without requiring signs) or only under certain
+*  conditions (e.g. certain mininum width of sidewalk) is very much dependent on the country or
+*  state one is in.
+*  Hence, it is not verifiable well for the on-site surveyor: If there is no sign that specifically
+*  allows or forbids cycling on a footway, the user is left with his loose (mis)understanding of the
+*  local legislation to decide. After all, bicycle=yes/no is (usually) nothing physical, but merely
+*  describes what is legal. It is in that sense then not information surveyable on-the-ground,
+*  unless specifically signed. bicycle=yes/no does not make a statement about from where this info
+*  is derived.
+*
+*  So, from an on-site surveyor point of view, it is always better to record what is signed, instead
+*  of what follows from that signage.
+*  Signage, however, is out of scope of this overlay. It would be better fitted as a quest (in
+*  countries and states where frequent signage is to be expected) */
+private fun SeparateCycleway.asItemTreatingAllowedAsNone(isLeftHandTraffic: Boolean) =
+    (if (this == ALLOWED) NONE else this).asItem(isLeftHandTraffic)
