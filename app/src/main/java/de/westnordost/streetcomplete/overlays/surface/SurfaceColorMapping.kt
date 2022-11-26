e9db1e1e9594d41d9d56da4488ba0e865820774e
Mateusz Konieczny <matkoniecz@gmail.com>
2022-11-26 13:26:24 +0100

    describe design considerations for surface colours

--- app/src/main/java/de/westnordost/streetcomplete/overlays/surface/SurfaceColorMapping.kt
+++ app/src/main/java/de/westnordost/streetcomplete/overlays/surface/SurfaceColorMapping.kt
@@ -4,12 +4,41 @@ import de.westnordost.streetcomplete.osm.surface.Surface
 import de.westnordost.streetcomplete.osm.surface.Surface.*
 import de.westnordost.streetcomplete.overlays.Color
 
+/*
+ * Design considerations:
+ * - use standard colour set, for consistency and for better support for colour-blind people
+ *  - it is OK to use some extra colours if absolutely needed
+ *
+ * - red is reserved for missing data
+ * - black is reserved for generic surface with note tag
+ *
+ * - similar surface should have similar colours
+ *  - all paved ones are one such group
+ *  - all unpaved ones are nother
+ *  - grass paver and compacted being high quality unpaved, unhewn cobblestone being low quality unpaved
+ *    should be on border between this two groups
+ *
+ * - asphalt, concrete, paving stones are associated with grey
+ *   but colouring this surfaces as grey resulted in really depressing, unfun
+ *   and soul-crushing display in cities where everything is well paved.
+ *   Blue is more fun and less sad (while it will not convince anyone
+ *   that concrete desert is a good thing).
+ *
+ * - highly unusual (for roads and paths) surfaces also got black colour
+ * - due to running out of colors all well paved surface get the same colour
+ * - due to running out of colours fine gravel, gravel, pebbles, rock got gray surface
+ * - dashes were tested but due to limitation of Tangram were not working well and were
+ *   incapable of replacing colour coding
+ *
+ * - ideally, sand would have colour close to yellow and grass colour close to green caused
+ *   by extremely strong association between surface and colour
+ */
 val Surface?.color get() = when (this) {
     // https://davidmathlogic.com/colorblind/#%23444444-%23FF0000-%231A87E6-%232FACE8-%2330D4EE-%2310C1B8-%230DA082-%23F37D1E-%23EEBD0D-%23B6EF28-%23DDDDDD-%23999999
     ASPHALT, CONCRETE, PAVING_STONES, PAVING_STONES_WITH_WEIRD_SUFFIX, BRICK, BRICKS, WOOD, METAL -> Color.BLUE
     CONCRETE_PLATES, CONCRETE_LANES, SETT, COBLLESTONE_FLATTENED -> Color.SKY
-    COMPACTED -> Color.CYAN
-    UNHEWN_COBBLESTONE -> Color.AQUAMARINE
+    UNHEWN_COBBLESTONE -> Color.CYAN
+    COMPACTED -> Color.AQUAMARINE
     GRASS_PAVER -> Color.TEAL
     SAND  -> Color.ORANGE
     GRASS  -> Color.LIME
