259c32bb394b0b1ef16dbfb24219ae3715dd3509
Tobias Zwick <osm@westnordost.de>
2022-12-14 12:32:53 +0100

    maybe fix #4685

--- app/src/main/java/de/westnordost/streetcomplete/screens/main/map/QuestPinsManager.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/main/map/QuestPinsManager.kt
@@ -133,7 +133,7 @@ class QuestPinsManager(
         properties.toQuestKey()
 
     fun onNewScreenPosition() {
-        if (!isVisible) return
+        if (!isStarted || !isVisible) return
         val zoom = ctrl.cameraPosition.zoom
         // require zoom >= 14, which is the lowest zoom level where quests are shown
         if (zoom < 14) return
