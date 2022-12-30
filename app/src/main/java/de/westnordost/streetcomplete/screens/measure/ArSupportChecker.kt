f580a2b81366d3de5038b7f98514526d0fb87391
Tobias Zwick <newton@westnordost.de>
2022-12-30 14:51:06 +0100

    specifically check for google play

--- app/src/main/java/de/westnordost/streetcomplete/screens/measure/ArSupportChecker.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/measure/ArSupportChecker.kt
@@ -3,6 +3,8 @@ package de.westnordost.streetcomplete.screens.measure
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.NameNotFoundException
 import android.net.Uri
 import android.os.Build
 import androidx.core.content.getSystemService
@@ -15,8 +17,12 @@ private fun hasArMeasureSupport(context: Context): Boolean =
     // extra requirements for Sceneform: min Android SDK and OpenGL ES 3.1
     Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
     && context.getSystemService<ActivityManager>()!!.deviceConfigurationInfo.glEsVersion.toDouble() >= 3.1
-    /* Google Play is required to lead the user through installing the app */
+    // Google Play is required to lead the user through installing the app
     && (
-        context.packageManager.getLaunchIntentForPackage("de.westnordost.streetmeasure") != null
-        || context.packageManager.resolveActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=de.westnordost.streetmeasure")), 0) != null
-    )
+        // app is already installed
+        context.packageManager.isPackageInstalled("de.westnordost.streetmeasure")
+        // or at least google play is installed
+        || context.packageManager.isPackageInstalled("com.android.vending"))
+
+private fun PackageManager.isPackageInstalled(packageName: String): Boolean =
+    try { getPackageInfo(packageName, 0) != null } catch (e: NameNotFoundException) { false }
