9d5b1b9ae77443072b53f6f755c57b61fde48634
Helium314 <helium314@mailbox.org>
2023-01-06 21:38:56 +0100

    disable node creation in custom overlay if nodes are not shown

--- app/src/main/java/de/westnordost/streetcomplete/overlays/custom/CustomOverlay.kt
+++ app/src/main/java/de/westnordost/streetcomplete/overlays/custom/CustomOverlay.kt
@@ -24,7 +24,7 @@ class CustomOverlay(val prefs: SharedPreferences) : Overlay {
     override val icon = R.drawable.ic_custom_overlay_poi
     override val changesetComment = "Edit user-defined element selection"
     override val wikiLink: String = "Tags"
-    override val isCreateNodeEnabled = true
+    override val isCreateNodeEnabled get() = prefs.getString(Prefs.CUSTOM_OVERLAY_FILTER, "")!!.startsWith("nodes")
 
     override fun getStyledElements(mapData: MapDataWithGeometry): Sequence<Pair<Element, Style>> {
         val filter = try {
