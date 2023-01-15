f32b560dd9d1712802f259ba3aa0ad032debe619
Tobias Zwick <newton@westnordost.de>
2023-01-15 01:23:07 +0100

    update core lib desugaring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,7 +111,7 @@ dependencies {
     val kotlinxCoroutinesVersion = "1.6.4"
     val koinVersion = "3.2.3"
 
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.8")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.0")
 
     // tests
     testImplementation("junit:junit:4.13.2")
