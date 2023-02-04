2de76416efe9ae05b31bcf20c4295cb282dffab3
Tobias Zwick <newton@westnordost.de>
2023-02-04 23:30:59 +0100

    use common function to parse surface sides

--- app/src/main/java/de/westnordost/streetcomplete/osm/sidewalk_surface/SidewalkSurfaceParser.kt
+++ app/src/main/java/de/westnordost/streetcomplete/osm/sidewalk_surface/SidewalkSurfaceParser.kt
@@ -1,24 +1,30 @@
 package de.westnordost.streetcomplete.osm.sidewalk_surface
 
+import de.westnordost.streetcomplete.osm.expandSidesTags
 import de.westnordost.streetcomplete.osm.surface.Surface
 import de.westnordost.streetcomplete.osm.surface.SurfaceAndNote
 
 fun createSidewalkSurface(tags: Map<String, String>): LeftAndRightSidewalkSurface? {
-    val surfaceStr = tags["sidewalk:surface"]
-    val surfaceBothStr = tags["sidewalk:both:surface"]
-    val surfaceLeftStr = tags["sidewalk:left:surface"] ?: surfaceBothStr ?: surfaceStr
-    val surfaceRightStr = tags["sidewalk:right:surface"] ?: surfaceBothStr ?: surfaceStr
-    val surfaceLeft = surfaceMap[surfaceLeftStr]
-    val surfaceRight = surfaceMap[surfaceRightStr]
-    if (surfaceLeft == null && surfaceRight == null) return null
-    val note = tags["sidewalk:surface:note"]
-    val bothNote = tags["sidewalk:both:surface:note"]
-    val leftNote = tags["sidewalk:left:surface:note"] ?: bothNote ?: note
-    val rightNote = tags["sidewalk:right:surface:note"] ?: bothNote ?: note
+    val expandedTags = expandRelevantSidesTags(tags)
+
+    val left = SURFACE_MAP[expandedTags["sidewalk:left:surface"]]
+    val right = SURFACE_MAP[expandedTags["sidewalk:right:surface"]]
+    if (left == null && right == null) return null
+
+    val leftNote = expandedTags["sidewalk:left:surface:note"]
+    val rightNote = expandedTags["sidewalk:right:surface:note"]
+
     return LeftAndRightSidewalkSurface(
-        surfaceLeft?.let { SurfaceAndNote(it, leftNote) },
-        surfaceRight?.let { SurfaceAndNote(it, rightNote) }
+        left?.let { SurfaceAndNote(it, leftNote) },
+        right?.let { SurfaceAndNote(it, rightNote) }
     )
 }
 
-private val surfaceMap: Map<String, Surface> = Surface.values().associateBy { it.osmValue }
+private val SURFACE_MAP: Map<String, Surface> = Surface.values().associateBy { it.osmValue }
+
+private fun expandRelevantSidesTags(tags: Map<String, String>): Map<String, String> {
+    val result = tags.toMutableMap()
+    result.expandSidesTags("sidewalk", "surface", true)
+    result.expandSidesTags("sidewalk", "surface:note", true)
+    return result
+}
