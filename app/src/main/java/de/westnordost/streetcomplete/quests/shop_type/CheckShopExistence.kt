f00f71a00c9a51df1fd1362df8734205b9eff38b
Mateusz Konieczny <matkoniecz@gmail.com>
2023-02-24 08:52:01 +0100

    exclude some major features that change rarely from check shop existence quest

--- app/src/main/java/de/westnordost/streetcomplete/quests/shop_type/CheckShopExistence.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/shop_type/CheckShopExistence.kt
@@ -34,6 +34,8 @@ class CheckShopExistence : OsmFilterQuestType<Unit>() {
              and !attraction
              and !aeroway
              and !railway
+             and amenity !~ hospital|clinic|library|music_school|post_office
+             and healthcare !~ hospital|clinic
           )
         ) and (
           older today -2 years
