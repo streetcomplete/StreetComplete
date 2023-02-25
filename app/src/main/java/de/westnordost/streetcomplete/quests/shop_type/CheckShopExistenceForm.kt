25b9b7933fb2ccb7bc763fe6b2ea472faf1b3324
Mateusz Konieczny <matkoniecz@gmail.com>
2023-02-25 15:22:03 +0100

    simplify code
    Co-authored-by: Flo Edelmann <git@flo-edelmann.de>

--- app/src/main/java/de/westnordost/streetcomplete/quests/shop_type/CheckShopExistenceForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/shop_type/CheckShopExistenceForm.kt
@@ -5,13 +5,8 @@ import de.westnordost.streetcomplete.quests.AbstractOsmQuestForm
 import de.westnordost.streetcomplete.quests.AnswerItem
 
 class CheckShopExistenceForm : AbstractOsmQuestForm<Unit>() {
-    override val buttonPanelAnswers get() =
-        listOf(
-            AnswerItem(R.string.quest_generic_hasFeature_no) {
-                replaceShop()
-            },
-            AnswerItem(R.string.quest_generic_hasFeature_yes) {
-                applyAnswer(Unit)
-            }
-        )
+    override val buttonPanelAnswers = listOf(
+        AnswerItem(R.string.quest_generic_hasFeature_no) { replaceShop() },
+        AnswerItem(R.string.quest_generic_hasFeature_yes) { applyAnswer(Unit) },
+    )
 }
