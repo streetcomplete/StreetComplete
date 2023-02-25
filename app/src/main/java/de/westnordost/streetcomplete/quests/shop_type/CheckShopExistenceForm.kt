744cda3eb8b715daffb8c8d70d726a77f9bc0ad0
Mateusz Konieczny <matkoniecz@gmail.com>
2023-02-25 14:55:47 +0100

    simplify

--- app/src/main/java/de/westnordost/streetcomplete/quests/shop_type/CheckShopExistenceForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/shop_type/CheckShopExistenceForm.kt
@@ -6,18 +6,12 @@ import de.westnordost.streetcomplete.quests.AnswerItem
 
 class CheckShopExistenceForm : AbstractOsmQuestForm<Unit>() {
     override val buttonPanelAnswers get() =
-        if (isAskingAboutExistence) {
-            listOf(
-                AnswerItem(R.string.quest_generic_hasFeature_no) {
-                    replaceShop()
-                },
-                AnswerItem(R.string.quest_generic_hasFeature_yes) {
-                    applyAnswer(Unit)
-                }
-            )
-        } else {
-            emptyList()
-        }
-
-    private var isAskingAboutExistence: Boolean = true
+        listOf(
+            AnswerItem(R.string.quest_generic_hasFeature_no) {
+                replaceShop()
+            },
+            AnswerItem(R.string.quest_generic_hasFeature_yes) {
+                applyAnswer(Unit)
+            }
+        )
 }
