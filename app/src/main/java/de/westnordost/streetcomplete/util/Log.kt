2c878390d977e18d43cf8e01513a94352140e33d
Helium314 <helium314@mailbox.org>
2023-03-20 16:38:35 +0100

    fix crash

--- app/src/main/java/de/westnordost/streetcomplete/util/Log.kt
+++ app/src/main/java/de/westnordost/streetcomplete/util/Log.kt
@@ -7,47 +7,49 @@ import java.util.Collections
 object Log {
     fun e(tag: String, message: String, e: Throwable? = null) {
         if (e == null) {
-            log('E', tag, message)
+            synchronized(logLines) { log('E', tag, message) }
             android.util.Log.e(tag, message)
         } else {
-            log('E', tag, "$message\n${e.stackTraceToString()}")
+            synchronized(logLines) { log('E', tag, "$message\n${e.stackTraceToString()}") }
             android.util.Log.e(tag, message, e)
         }
     }
 
     fun w(tag: String, message: String, e: Throwable? = null) {
         if (e == null) {
-            log('W', tag, message)
+            synchronized(logLines) { log('W', tag, message) }
             android.util.Log.e(tag, message)
         } else {
-            log('W', tag, "$message\n${e.stackTraceToString()}")
+                synchronized(logLines) { log('W', tag, "$message\n${e.stackTraceToString()}") }
             android.util.Log.e(tag, message, e)
         }
     }
 
     fun i(tag: String, message: String) {
-        log('I', tag, message)
+        synchronized(logLines) { log('I', tag, message) }
         android.util.Log.i(tag, message)
     }
 
     fun d(tag: String, message: String) {
-        log('D', tag, message)
+        synchronized(logLines) { log('D', tag, message) }
         android.util.Log.d(tag, message)
     }
 
     fun v(tag: String, message: String) {
-        log('V', tag, message)
+        synchronized(logLines) { log('V', tag, message) }
         android.util.Log.v(tag, message)
     }
 
     private fun log(level: Char, tag: String, message: String) {
         val dateTime = LocalDateTime.now().toString().replace('T', ' ')
         val logLine = "$dateTime $level $tag: $message"
-        if (logLines.size > 12000) // clear oldest entries if list gets too long
-            logLines.subList(0, 2000).clear()
-        logLines.add(logLine)
-//        println(logLine)
+        synchronized(logLines) {
+            if (logLines.size > 12000) // clear oldest entries if list gets too long
+                logLines.subList(0, 2000).clear()
+            logLines.add(logLine)
+        }
+//        println(logLine) // would be nice for testing only, but how to do?
     }
 
-    val logLines: MutableList<String> = Collections.synchronizedList(ArrayList<String>())
+    val logLines: MutableList<String> = ArrayList(2000)
 }
