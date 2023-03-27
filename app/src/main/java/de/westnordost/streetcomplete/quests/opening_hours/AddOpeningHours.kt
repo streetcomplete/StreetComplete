ff3b1bd4e98a571c42670e0171679fee0f2a163c
Tobias Zwick <newton@westnordost.de>
2023-03-27 14:50:15 +0200

    add various *schools etc. to opening hours, wheelchair access and place name quests
    While these kind of schools you can not usually just walk in and attend a course, they often *do* have office hours.
    Also, no reason why gambling, brothels, etc. couldn't be wheelchair accessible

--- app/src/main/java/de/westnordost/streetcomplete/quests/opening_hours/AddOpeningHours.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/opening_hours/AddOpeningHours.kt
@@ -49,9 +49,12 @@ class AddOpeningHours(
                 "food_court", "nightclub",
                 "cinema", "planetarium", "casino",                                                  // amenities
                 "townhall", "courthouse", "embassy", "community_centre", "youth_centre", "library", // civic
+                "driving_school", "music_school", "prep_school", "language_school", "dive_centre",  // learning
+                "dancing_school", "ski_school", "flight_school", "surf_school", "sailing_school",
+                "cooking_school",
                 "bank", "bureau_de_change", "money_transfer", "post_office", "marketplace",         // commercial
                 "internet_cafe", "payment_centre",
-                "car_wash", "car_rental", "fuel","driving_school",                                  // car stuff
+                "car_wash", "car_rental", "fuel",                                                   // car stuff
                 "dentist", "doctors", "clinic", "pharmacy", "veterinary",                           // health
                 "animal_boarding", "animal_shelter", "animal_breeding",                             // animals
                 "coworking_space",                                                                  // work
@@ -79,7 +82,7 @@ class AddOpeningHours(
             "office" to arrayOf(
                 // common
                 "insurance", "government", "travel_agent", "tax_advisor", "religion",
-                "employment_agency", "diplomatic", "coworking",
+                "employment_agency", "diplomatic", "coworking", "energy_supplier",
                 "estate_agent", "lawyer", "telecommunication", "educational_institution",
                 "association", "ngo", "it", "accountant"
             ),
