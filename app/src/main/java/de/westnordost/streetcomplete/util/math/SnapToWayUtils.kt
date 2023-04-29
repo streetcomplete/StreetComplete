4d30e3cad6b05310a5124d2a2c5a0db49edf0a22
Tobias Zwick <newton@westnordost.de>
2023-04-29 22:30:56 +0200

    rather use ElementGeometry, more easy to use

--- app/src/main/java/de/westnordost/streetcomplete/util/math/SnapToWayUtils.kt
+++ app/src/main/java/de/westnordost/streetcomplete/util/math/SnapToWayUtils.kt
@@ -1,23 +1,30 @@
 package de.westnordost.streetcomplete.util.math
 
+import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
+import de.westnordost.streetcomplete.data.osm.geometry.ElementPolygonsGeometry
+import de.westnordost.streetcomplete.data.osm.geometry.ElementPolylinesGeometry
 import de.westnordost.streetcomplete.data.osm.mapdata.LatLon
+import de.westnordost.streetcomplete.data.osm.mapdata.Way
+import de.westnordost.streetcomplete.util.ktx.asSequenceOfPairs
 import kotlin.math.min
 
+sealed interface PositionOnWay {
+    val position: LatLon
+}
 
-sealed interface PointOnWay
-
-/** A vertex of a way */
+/** A vertex of one or several ways */
 data class VertexOfWay(
-    val wayId: Long,
-    val point: LatLon
-) : PointOnWay
+    val wayIds: Set<Long>,
+    override val position: LatLon,
+    val nodeId: Long,
+) : PositionOnWay
 
 /** A point on a segment of a way */
-data class PointOnWaySegment(
+data class PositionOnWaySegment(
     val wayId: Long,
-    val point: LatLon,
+    override val position: LatLon,
     val segment: Pair<LatLon, LatLon>
-): PointOnWay
+): PositionOnWay
 
 
 /** Returns the point on any of the given ways that is nearest to this point and at most
@@ -26,32 +33,84 @@ data class PointOnWaySegment(
  *  Additionally, if this point is closer than [snapToVertexDistance] near a vertex of a way, that
  *  vertex is chosen instead, even if it is not closest to this point.
  *   */
-fun LatLon.getPointOnWays(
-    geometriesByWayId: Map<Long, List<LatLon>>,
+fun LatLon.getPositionOnWays(
+    ways: Collection<Pair<Way, ElementGeometry>>,
     maxDistance: Double,
     snapToVertexDistance: Double = 0.0
-): PointOnWay? {
+): PositionOnWay? {
     if (snapToVertexDistance > 0.0) {
-        val (wayId, nearestVertex) = geometriesByWayId
-            .mapValues { getNearestVertex(it.value) }
-            .minBy { distanceTo(it.value) }
+        val nearestVertex = getNearestVertexOfWays(ways, min(maxDistance, snapToVertexDistance))
+        if (nearestVertex != null) return nearestVertex
+    }
 
-        if (distanceTo(nearestVertex) <= min(maxDistance, snapToVertexDistance)) {
-            return VertexOfWay(wayId, nearestVertex)
+    return getNearestPositionToWays(ways, maxDistance)
+}
+
+/** Returns the nearest vertex of any of the ways given in [mapData] that is at  most [maxDistance]
+ *  away from this point or null if there isn't any */
+private fun LatLon.getNearestVertexOfWays(
+    ways: Collection<Pair<Way, ElementGeometry>>,
+    maxDistance: Double,
+): VertexOfWay? {
+    var minDistance = Double.MAX_VALUE
+    var nearestNodeId: Long? = null
+    var nearestPoint: LatLon? = null
+    var nearestWayIds = mutableSetOf<Long>()
+    for ((way, geometry) in ways) {
+        val latLons = geometry.wayLatLons
+        for ((i, nodeId) in way.nodeIds.withIndex()) {
+            if (nodeId == nearestNodeId) {
+                nearestWayIds.add(way.id)
+                continue
+            }
+            val point = latLons[i]
+
+            val distance = distanceTo(point)
+            if (distance < minDistance && distance <= maxDistance) {
+                minDistance = distance
+                nearestNodeId = nodeId
+                nearestPoint = point
+                nearestWayIds = mutableSetOf()
+            }
         }
     }
 
-    val (wayId2, nearestArc) = geometriesByWayId
-        .mapValues { nearestArcOf(it.value) }
-        .minBy { distanceToArc(it.value.first, it.value.second) }
-
-    if (distanceToArc(nearestArc.first, nearestArc.second) <= maxDistance) {
-        val nearestPoint = nearestPointOnArc(nearestArc.first, nearestArc.second)
-        return PointOnWaySegment(wayId2, nearestPoint, nearestArc)
+    if (nearestNodeId != null && nearestPoint != null) {
+        return VertexOfWay(nearestWayIds, nearestPoint, nearestNodeId)
     }
-
     return null
 }
 
-private fun LatLon.getNearestVertex(polyline: List<LatLon>): LatLon =
-    polyline.minBy { distanceTo(it) }
+/** Returns the nearest point on any of the ways given in [ways] that is at most [maxDistance]
+ *  away from this point or null if there isn't any */
+private fun LatLon.getNearestPositionToWays(
+    ways: Collection<Pair<Way, ElementGeometry>>,
+    maxDistance: Double
+): PositionOnWaySegment? {
+    var minDistance = Double.MAX_VALUE
+    var nearestWay: Way? = null
+    var nearestSegment: Pair<LatLon, LatLon>? = null
+    for ((way, geometry) in ways) {
+        val latLons = geometry.wayLatLons
+        for (segment in latLons.asSequenceOfPairs()) {
+            val distance = distanceToArc(segment.first, segment.second)
+            if (distance < minDistance && distance <= maxDistance) {
+                minDistance = distance
+                nearestWay = way
+                nearestSegment = segment
+            }
+        }
+    }
+    if (nearestWay != null && nearestSegment != null) {
+        val nearestPoint = nearestPointOnArc(nearestSegment.first, nearestSegment.second)
+        return PositionOnWaySegment(nearestWay.id, nearestPoint, nearestSegment)
+    }
+    return null
+}
+
+private val ElementGeometry.wayLatLons: List<LatLon> get() = when (this) {
+    // single is safe because a way cannot have multiple polygons / polylines
+    is ElementPolygonsGeometry -> polygons.single()
+    is ElementPolylinesGeometry -> polylines.single()
+    else -> throw IllegalStateException()
+}
