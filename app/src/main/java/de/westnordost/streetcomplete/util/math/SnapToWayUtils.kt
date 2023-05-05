c0e0c261387ca731fa86809e75812bfe628021df
Tobias Zwick <newton@westnordost.de>
2023-05-06 01:49:25 +0200

    fix first way id is not added to result

--- app/src/main/java/de/westnordost/streetcomplete/util/math/SnapToWayUtils.kt
+++ app/src/main/java/de/westnordost/streetcomplete/util/math/SnapToWayUtils.kt
@@ -68,7 +68,7 @@ private fun LatLon.getNearestVertexOfWays(
                 minDistance = distance
                 nearestNodeId = nodeId
                 nearestPoint = point
-                nearestWayIds = mutableSetOf()
+                nearestWayIds = mutableSetOf(way.id)
             }
         }
     }
