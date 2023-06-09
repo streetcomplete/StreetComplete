1e96ced81e9d58c361d46ea809d2b693c170bc3e
Tobias Zwick <newton@westnordost.de>
2023-06-09 21:57:27 +0200

    remove geometry filtering from sidewalk and cycleway quest (fixes #5000)

--- app/src/main/java/de/westnordost/streetcomplete/quests/sidewalk/AddSidewalk.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/sidewalk/AddSidewalk.kt
@@ -3,34 +3,20 @@ package de.westnordost.streetcomplete.quests.sidewalk
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.elementfilter.toElementFilterExpression
 import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
-import de.westnordost.streetcomplete.data.osm.geometry.ElementPolylinesGeometry
 import de.westnordost.streetcomplete.data.osm.mapdata.Element
 import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
 import de.westnordost.streetcomplete.data.osm.osmquests.OsmElementQuestType
 import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement.PEDESTRIAN
 import de.westnordost.streetcomplete.osm.MAXSPEED_TYPE_KEYS
 import de.westnordost.streetcomplete.osm.Tags
-import de.westnordost.streetcomplete.osm.estimateCycleTrackWidth
-import de.westnordost.streetcomplete.osm.estimateParkingOffRoadWidth
-import de.westnordost.streetcomplete.osm.estimateRoadwayWidth
-import de.westnordost.streetcomplete.osm.guessRoadwayWidth
 import de.westnordost.streetcomplete.osm.sidewalk.LeftAndRightSidewalk
 import de.westnordost.streetcomplete.osm.sidewalk.Sidewalk.INVALID
 import de.westnordost.streetcomplete.osm.sidewalk.any
 import de.westnordost.streetcomplete.osm.sidewalk.applyTo
 import de.westnordost.streetcomplete.osm.sidewalk.createSidewalkSides
 import de.westnordost.streetcomplete.osm.surface.ANYTHING_UNPAVED
-import de.westnordost.streetcomplete.util.math.isNearAndAligned
 
 class AddSidewalk : OsmElementQuestType<LeftAndRightSidewalk> {
-    private val maybeSeparatelyMappedSidewalksFilter by lazy { """
-        ways with
-          highway ~ path|footway|cycleway|construction
-          and foot !~ no|private
-          and access !~ no|private
-    """.toElementFilterExpression() }
-    // highway=construction included, as situation often changes during and after construction
-
     override val changesetComment = "Specify whether roads have sidewalks"
     override val wikiLink = "Key:sidewalk"
     override val icon = R.drawable.ic_quest_sidewalk
@@ -39,67 +25,12 @@ class AddSidewalk : OsmElementQuestType<LeftAndRightSidewalk> {
 
     override fun getTitle(tags: Map<String, String>) = R.string.quest_sidewalk_title
 
-    override fun getApplicableElements(mapData: MapDataWithGeometry): Iterable<Element> {
-        val eligibleRoads = mapData.ways.filter { roadsFilter.matches(it) }
-        val roadsWithMissingSidewalks = eligibleRoads.filter { untaggedRoadsFilter.matches(it) }.toMutableList()
+    override fun getApplicableElements(mapData: MapDataWithGeometry): Iterable<Element> =
+        mapData.filter { isApplicableTo(it) }
 
-        /* Unfortunately, the filter above is not enough. In OSM, sidewalks may be mapped as
-         * separate ways as well and it is not guaranteed that in this case, sidewalk = separate
-         * (or foot = use_sidepath) is always tagged on the main road then. So, all roads should
-         * be excluded whose center is within several meters of a footway, to be on the safe side. */
-
-        if (roadsWithMissingSidewalks.isNotEmpty()) {
-
-            val maybeSeparatelyMappedSidewalkGeometries = mapData.ways
-                .filter { maybeSeparatelyMappedSidewalksFilter.matches(it) }
-                .mapNotNull { mapData.getWayGeometry(it.id) as? ElementPolylinesGeometry }
-            if (maybeSeparatelyMappedSidewalkGeometries.isEmpty()) {
-                return roadsWithMissingSidewalks
-            } else {
-                val minAngleToWays = 25.0
-                // filter out roads with missing sidewalks that are near footways
-                roadsWithMissingSidewalks.removeAll { road ->
-                    val minDistToWays = getMinDistanceToWays(road.tags).toDouble()
-                    val roadGeometry = mapData.getWayGeometry(road.id) as? ElementPolylinesGeometry
-                    roadGeometry?.isNearAndAligned(
-                        minDistToWays,
-                        minAngleToWays,
-                        maybeSeparatelyMappedSidewalkGeometries
-                    ) ?: true
-                }
-            }
-        }
-
-        // Also include any roads with invalid (overloaded) or incomplete sidewalk tagging
-        val roadsWithInvalidSidewalkTags = eligibleRoads.filter { way ->
-            way.hasInvalidOrIncompleteSidewalkTags()
-        }
-
-        return roadsWithMissingSidewalks + roadsWithInvalidSidewalkTags
-    }
-
-    /* Calculate when footway is too far away from the road to be considered its sidewalk.
-       It is an estimate, and we deliberately err on the side of showing the quest too often. */
-    private fun getMinDistanceToWays(tags: Map<String, String>): Float =
-        (
-            (estimateRoadwayWidth(tags) ?: guessRoadwayWidth(tags)) +
-            (estimateParkingOffRoadWidth(tags) ?: 0f) +
-            (estimateCycleTrackWidth(tags) ?: 0f) +
-            1.5f    // assumed sidewalk width
-        ) / 2f +
-        4f // + generous buffer for possible grass verge
-
-    override fun isApplicableTo(element: Element): Boolean? {
-        if (!roadsFilter.matches(element)) return false
-
-        /* can't determine for yet untagged roads by the tags alone because we need info about
-           surrounding geometry */
-        if (untaggedRoadsFilter.matches(element)) return null
-
-        /* but if already tagged and invalid or incomplete, we don't need to look at surrounding
-           geometry to see if it is applicable or not */
-        return element.hasInvalidOrIncompleteSidewalkTags()
-    }
+    override fun isApplicableTo(element: Element): Boolean =
+        roadsFilter.matches(element)
+        && (untaggedRoadsFilter.matches(element) || element.hasInvalidOrIncompleteSidewalkTags())
 
     override fun createForm() = AddSidewalkForm()
 
@@ -148,16 +79,11 @@ private val untaggedRoadsFilter by lazy { """
         lit = yes
         or highway = residential
         or ~"${(MAXSPEED_TYPE_KEYS + "maxspeed").joinToString("|")}" ~ ".*:(urban|.*zone.*|nsl_restricted)"
+        or maxspeed <= 60
         or (foot ~ yes|designated and highway ~ motorway|motorway_link|trunk|trunk_link|primary|primary_link|secondary|secondary_link)
       )
-      and foot != use_sidepath
-      and bicycle != use_sidepath
-      and bicycle:backward != use_sidepath
-      and bicycle:forward != use_sidepath
-      and cycleway != separate
-      and cycleway:left != separate
-      and cycleway:right != separate
-      and cycleway:both != separate
+      and ~foot|bicycle|bicycle:backward|bicycle:forward !~ use_sidepath
+      and ~cycleway|cycleway:left|cycleway:right|cycleway:both !~ separate
 """.toElementFilterExpression() }
 
 private fun Element.hasInvalidOrIncompleteSidewalkTags(): Boolean {
