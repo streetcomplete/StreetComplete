889815c7164eea5bc818fcfe7c2f5d038564d01c
Tobias Zwick <newton@westnordost.de>
2023-06-29 01:43:42 +0200

    update serialization plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ import java.util.Properties
 plugins {
     id("com.android.application")
     kotlin("android")
-    kotlin("plugin.serialization") version "1.8.0"
+    kotlin("plugin.serialization") version "1.8.21"
 }
 
 android {
