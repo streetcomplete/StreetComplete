f6c511e39076417bb9e7b1c7c01bc65abb524d62
Helium314 <helium314@mailbox.org>
2023-07-26 18:57:54 +0200

    ask AddCrossing for sidwalk and crossing ways

--- app/src/main/java/de/westnordost/streetcomplete/quests/crossing/AddCrossingForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/crossing/AddCrossingForm.kt
@@ -1,15 +1,32 @@
 package de.westnordost.streetcomplete.quests.crossing
 
 import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.osm.edits.MapDataWithEditsSource
 import de.westnordost.streetcomplete.quests.AListQuestForm
 import de.westnordost.streetcomplete.quests.TextItem
 import de.westnordost.streetcomplete.quests.crossing.CrossingAnswer.*
+import org.koin.android.ext.android.inject
 
 class AddCrossingForm : AListQuestForm<CrossingAnswer>() {
+    private val mapDataSource: MapDataWithEditsSource by inject()
 
-    override val items = listOf(
+    override val items get() = listOfNotNull(
         TextItem(YES, R.string.quest_crossing_yes),
         TextItem(NO, R.string.quest_crossing_no),
-        TextItem(PROHIBITED, R.string.quest_crossing_prohibited),
+        if (isOnSidewalkOrCrossing()) null else TextItem(PROHIBITED, R.string.quest_crossing_prohibited)
     )
+
+    /* PROHIBITED is neither available for sidewalks nor crossings (=separately mapped sidewalk infrastructure)
+    *  because a "no" answer would require to also delete/adapt the crossing ways, rather than just
+    *  tagging crossing=no on the vertex.
+    *  See https://github.com/streetcomplete/StreetComplete/pull/2999#discussion_r681516203
+    *  and https://github.com/streetcomplete/StreetComplete/issues/5160 */
+
+    private fun isOnSidewalkOrCrossing(): Boolean {
+        val ways = mapDataSource.getWaysForNode(element.id)
+        return ways.any {
+            val footway = it.tags["footway"]
+            footway == "sidewalk" || footway == "crossing"
+        }
+    }
 }
