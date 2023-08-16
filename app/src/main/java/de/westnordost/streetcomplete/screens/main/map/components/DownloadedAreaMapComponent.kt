b583bc22bc941a85c5b77003065a15758d2e98a5
Helium314 <helium314@mailbox.org>
2023-08-16 22:05:35 +0200

    maybe fix rare crash in DownloadedAreaMapComponent

--- app/src/main/java/de/westnordost/streetcomplete/screens/main/map/components/DownloadedAreaMapComponent.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/main/map/components/DownloadedAreaMapComponent.kt
@@ -10,12 +10,12 @@ import de.westnordost.streetcomplete.screens.main.map.tangram.KtMapController
 import de.westnordost.streetcomplete.screens.main.map.tangram.toLngLat
 
 class DownloadedAreaMapComponent(private val ctrl: KtMapController) {
-    private var layer: MapData? = null
+    private var layer: MapData = ctrl.addDataLayer(DOWNLOADED_AREA_LAYER)
 
     fun set(tiles: Collection<TilePos>) {
         // tangram does not clear a layer properly on re-setting features on it, so let's remove
         // and re-add the whole layer
-        layer?.remove()
+        layer.remove()
         val layer = ctrl.addDataLayer(DOWNLOADED_AREA_LAYER)
 
         val zoom = ApplicationConstants.DOWNLOAD_TILE_ZOOM
