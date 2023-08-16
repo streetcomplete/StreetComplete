c51f60f6270344240d5757c40744d8db92fac0c4
Helium314 <helium314@mailbox.org>
2023-08-16 15:36:26 +0200

    make flatDistanceTo(Arc) work across 180th longitude
    test updated to also cover this case

--- app/src/main/java/de/westnordost/streetcomplete/util/math/FlatEarthMath.kt
+++ app/src/main/java/de/westnordost/streetcomplete/util/math/FlatEarthMath.kt
@@ -15,6 +15,8 @@ import kotlin.math.sqrt
  *  0.03° difference between points (several km at common latitudes). */
 
 // ~5 times faster than spherical version
+/** Returns the approximate distance from this point to the other point.
+ *  Result is within 0.1 m of the spherical version for less than 0.3° difference and EARTH_RADIUS */
 fun LatLon.flatDistanceTo(pos: LatLon, globeRadius: Double = EARTH_RADIUS): Double =
     flatAngularDistance(
         latitude.toRadians(),
@@ -24,8 +26,13 @@ fun LatLon.flatDistanceTo(pos: LatLon, globeRadius: Double = EARTH_RADIUS): Doub
     ) * globeRadius
 
 // ~10 times faster than spherical version
-fun LatLon.flatDistanceToArc(start: LatLon, end: LatLon, globeRadius: Double = EARTH_RADIUS): Double =
-    abs(
+/** Returns the shortest distance between this point and the arc between the given points. */
+fun LatLon.flatDistanceToArc(start: LatLon, end: LatLon, globeRadius: Double = EARTH_RADIUS): Double {
+    if (abs(longitude - start.longitude) > 180 || abs(longitude - end.longitude) > 180 || abs(start.longitude - end.longitude) > 180) {
+        // fall back to spherical version
+        return distanceToArc(start, end, globeRadius)
+    }
+    return abs(
         flatAngularDistanceToArc(
             start.latitude.toRadians(),
             start.longitude.toRadians(),
@@ -35,7 +42,9 @@ fun LatLon.flatDistanceToArc(start: LatLon, end: LatLon, globeRadius: Double = E
             longitude.toRadians()
         )
     ) * globeRadius
+}
 
+/** Returns the shortest distance between this point and the arcs between the given points */
 fun LatLon.flatDistanceToArcs(polyLine: List<LatLon>, globeRadius: Double = EARTH_RADIUS): Double {
     require(polyLine.isNotEmpty()) { "Polyline must not be empty" }
     if (polyLine.size == 1) return flatDistanceTo(polyLine[0])
@@ -45,15 +54,21 @@ fun LatLon.flatDistanceToArcs(polyLine: List<LatLon>, globeRadius: Double = EART
         .minOf { flatDistanceToArc(it.first, it.second, globeRadius) }
 }
 
+/** Returns the approximate distance of two points on a sphere.
+ *  Takes into account wrapping at 180th longitude */
 private fun flatAngularDistance(φ1: Double, λ1: Double, φ2: Double, λ2: Double): Double {
     // https://en.wikipedia.org/wiki/Geographical_distance#Spherical_Earth_projected_to_a_plane
     val δφ = φ1 - φ2
-    val δλ = λ1 - λ2
-    val cosδλ = approxCos((φ1 + φ2) / 2) * δλ
+    var δλ = abs(λ1 - λ2)
+    if (δλ > PI)
+        δλ = 2 * PI - δλ
+    val cosδλ = approximateCos((φ1 + φ2) / 2) * δλ
     return sqrt(δφ * δφ + cosδλ * cosδλ)
 }
 
-// not an arc any more, just more or less a straight line
+/** Returns the shortest distance between point three and the arc/line between point one and two.
+ *  Does not take into account wrapping at 180th longitude (handled by flatDistanceToArc)
+ *  Contrary to spherical version, the sign is always positive */
 // from http://paulbourke.net/geometry/pointlineplane/ and the linked java implementation
 private fun flatAngularDistanceToArc(φ1: Double, λ1: Double, φ2: Double, λ2: Double, φ3: Double, λ3: Double): Double {
     val δφ12 = φ2 - φ1
@@ -62,22 +77,21 @@ private fun flatAngularDistanceToArc(φ1: Double, λ1: Double, φ2: Double, λ2:
     if (δφ12 == 0.0 && δλ12 == 0.0)
         return flatAngularDistance(φ1, λ1, φ3, λ3)
 
-    val c = approxCos(φ3)
+    val δλ13 = λ3 - λ1
+    val c = approximateCos(φ3)
     // need the cosine as sort of "weight factor", because λ distances are much shorter at high φ
-    val u = ((φ3 - φ1) * δφ12 + (λ3 - λ1) * δλ12 * c * c) / (δφ12 * δφ12 + δλ12 * δλ12 * c * c)
+    val u = ((φ3 - φ1) * δφ12 + δλ13 * δλ12 * c * c) / (δφ12 * δφ12 + δλ12 * δλ12 * c * c)
 
     val (closestPointφ, closestPointλ) = if (u < 0) φ1 to λ1
         else if (u > 1) φ2 to λ2
         else φ1 + u * δφ12 to λ1 + u * δλ12
 
-    // contrary to spherical version, the result is always positive. But sign is never uses anyway...
     return flatAngularDistance(closestPointφ, closestPointλ, φ3, λ3)
 }
 
-// approximation using Taylor expansion
-// ~10 times faster than cos
-// within -2.5E-5 of cos in range of interest [-PI/2, PI/2]
-private fun approxCos(radians: Double): Double {
+/** approximate cosine using Taylor expansion, ~10 times faster than Math.cos
+ * in interval [-PI/2, PI/2] it's within 2.5E-5 of Math.cos */
+private fun approximateCos(radians: Double): Double {
     // not using pow because it's really slow (integers are converted to double)
     val rSquared = radians * radians
     return 1.0 - rSquared / 2 + rSquared * rSquared / 24 - rSquared * rSquared * rSquared / 720 + rSquared * rSquared * rSquared * rSquared / 40320
--- app/src/test/java/de/westnordost/streetcomplete/util/math/FlatEarthMathTest.kt
+++ app/src/test/java/de/westnordost/streetcomplete/util/math/FlatEarthMathTest.kt
@@ -1,9 +1,8 @@
 package de.westnordost.streetcomplete.util.math
 
 import de.westnordost.streetcomplete.data.osm.mapdata.LatLon
-import org.junit.Assert.assertTrue
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import kotlin.math.abs
 import kotlin.random.Random
 
 class FlatEarthMathTest {
@@ -14,37 +13,37 @@ class FlatEarthMathTest {
         repeat(1000000) {
             val p1 = LatLon(
                 Random.nextDouble(-90 + deltaDegrees, 90 - deltaDegrees),
-                Random.nextDouble(-180 + deltaDegrees, 180 - deltaDegrees)
+                Random.nextDouble(-180.0, 180.0)
             )
             val p2 = LatLon(
                 p1.latitude + Random.nextDouble(-deltaDegrees, deltaDegrees),
-                p1.longitude + Random.nextDouble(-deltaDegrees, deltaDegrees)
+                normalizeLongitude(p1.longitude + Random.nextDouble(-deltaDegrees, deltaDegrees))
             )
             val dExact = p1.distanceTo(p2)
             val dApproximate = p1.flatDistanceTo(p2)
-            assertTrue(abs(dExact - dApproximate) < precisionInMeters)
+            assertEquals(dExact, dApproximate, precisionInMeters)
         }
     }
 
     @Test fun flatAngularDistanceToArcPrecision() {
         val deltaDegrees = 0.03
-        val precisionInMeters = 0.5
+        val precisionInMeters = 0.6
         repeat(1000000) {
             val p1 = LatLon(
                 Random.nextDouble(-90 + deltaDegrees, 90 - deltaDegrees),
-                Random.nextDouble(-180 + deltaDegrees, 180 - deltaDegrees)
+                Random.nextDouble(-180.0, 180.0)
             )
             val p2 = LatLon(
                 p1.latitude + Random.nextDouble(-deltaDegrees, deltaDegrees),
-                p1.longitude + Random.nextDouble(-deltaDegrees, deltaDegrees)
+                normalizeLongitude(p1.longitude + Random.nextDouble(-deltaDegrees, deltaDegrees))
             )
             val p3 = LatLon(
                 p1.latitude + Random.nextDouble(-deltaDegrees, deltaDegrees),
-                p1.longitude + Random.nextDouble(-deltaDegrees, deltaDegrees)
+                normalizeLongitude(p1.longitude + Random.nextDouble(-deltaDegrees, deltaDegrees))
             )
             val dExact = p3.distanceToArc(p1, p2)
             val dApproximate = p3.flatDistanceToArc(p1, p2)
-            assertTrue(abs(dExact - dApproximate) < precisionInMeters)
+            assertEquals(dExact, dApproximate, precisionInMeters)
         }
     }
 }
