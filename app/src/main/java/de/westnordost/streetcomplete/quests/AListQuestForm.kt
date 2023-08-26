4a0d34293f8ab24b6b11f1eb3da30c34c7c802a6
Helium314 <helium314@mailbox.org>
2023-08-26 07:35:36 +0200

    ask user to leave a note when crossing with a sidewalk or crossing path is not possible

--- app/src/main/java/de/westnordost/streetcomplete/quests/AListQuestForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/AListQuestForm.kt
@@ -19,6 +19,8 @@ abstract class AListQuestForm<T> : AbstractOsmQuestForm<T>() {
 
     private val radioButtonIds = HashMap<Int, TextItem<T>>()
 
+    val checkedItem get() = radioButtonIds[binding.radioButtonGroup.checkedRadioButtonId]
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         for (item in items) {
--- app/src/main/java/de/westnordost/streetcomplete/quests/crossing/AddCrossingForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/crossing/AddCrossingForm.kt
@@ -1,5 +1,6 @@
 package de.westnordost.streetcomplete.quests.crossing
 
+import androidx.appcompat.app.AlertDialog
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.osm.edits.MapDataWithEditsSource
 import de.westnordost.streetcomplete.quests.AListQuestForm
@@ -10,17 +11,31 @@ import org.koin.android.ext.android.inject
 class AddCrossingForm : AListQuestForm<CrossingAnswer>() {
     private val mapDataSource: MapDataWithEditsSource by inject()
 
-    override val items get() = listOfNotNull(
+    override val items get() = listOf(
         TextItem(YES, R.string.quest_crossing_yes),
         TextItem(NO, R.string.quest_crossing_no),
-        if (isOnSidewalkOrCrossing()) null else TextItem(PROHIBITED, R.string.quest_crossing_prohibited)
+        TextItem(PROHIBITED, R.string.quest_crossing_prohibited)
     )
 
-    /* PROHIBITED is neither available for sidewalks nor crossings (=separately mapped sidewalk infrastructure)
+    /* PROHIBITED is neither possible for sidewalks nor crossings (=separately mapped sidewalk infrastructure)
     *  because a "no" answer would require to also delete/adapt the crossing ways, rather than just
     *  tagging crossing=no on the vertex.
+    *  This situation needs to be solved in a different editor, so we ask the user to leave a note.
     *  See https://github.com/streetcomplete/StreetComplete/pull/2999#discussion_r681516203
     *  and https://github.com/streetcomplete/StreetComplete/issues/5160 */
+    override fun isFormComplete(): Boolean {
+        if (checkedItem?.value == PROHIBITED && isOnSidewalkOrCrossing()) {
+            AlertDialog.Builder(requireContext())
+                .setMessage(R.string.quest_crossing_prohibited_but_on_sidewalk_or_crossing)
+                .setPositiveButton(R.string.quest_leave_new_note_yes) { _, _ -> composeNote() }
+                .setNegativeButton(android.R.string.cancel, null)
+                .show()
+            return false
+        }
+        return super.isFormComplete()
+    }
+
+    override fun isRejectingClose() = super.isFormComplete()
 
     private fun isOnSidewalkOrCrossing(): Boolean {
         val ways = mapDataSource.getWaysForNode(element.id)
