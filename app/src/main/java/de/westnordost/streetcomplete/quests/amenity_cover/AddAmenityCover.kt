5c525eb7a9c45c37ab9ddbc1842e7ac44cb9a447
Helium314 <helium314@mailbox.org>
2023-08-29 00:06:54 +0200

    change AddAmenityCover back to filter quest
    makes use of default quest settings
    potentially more applicable elements
    faster scan

--- app/src/main/java/de/westnordost/streetcomplete/quests/amenity_cover/AddAmenityCover.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/amenity_cover/AddAmenityCover.kt
@@ -2,11 +2,10 @@ package de.westnordost.streetcomplete.quests.amenity_cover
 
 import de.westnordost.osmfeatures.FeatureDictionary
 import de.westnordost.streetcomplete.R
-import de.westnordost.streetcomplete.data.elementfilter.toElementFilterExpression
 import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
 import de.westnordost.streetcomplete.data.osm.mapdata.Element
 import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
-import de.westnordost.streetcomplete.data.osm.osmquests.OsmElementQuestType
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
 import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement.OUTDOORS
 import de.westnordost.streetcomplete.osm.Tags
 import de.westnordost.streetcomplete.quests.YesNoQuestForm
@@ -15,16 +14,16 @@ import java.util.concurrent.FutureTask
 
 class AddAmenityCover (
     private val featureDictionaryFuture: FutureTask<FeatureDictionary>
-) : OsmElementQuestType<Boolean> {
+) : OsmFilterQuestType<Boolean>() {
 
-    private val nodesFilter by lazy { """
+    override val elementFilter = """
         nodes with
           (leisure = picnic_table
            or amenity = bbq)
           and access !~ private|no
           and !covered
           and (!seasonal or seasonal = no)
-    """.toElementFilterExpression() }
+    """
     override val changesetComment = "Specify whether various amenities are covered"
     override val wikiLink = "Key:covered"
     override val icon = R.drawable.ic_quest_picnic_table_cover
@@ -32,14 +31,6 @@ class AddAmenityCover (
     override val achievements = listOf(OUTDOORS)
 
     override fun getTitle(tags: Map<String, String>) = R.string.quest_amenityCover_title
-    override fun getApplicableElements(mapData: MapDataWithGeometry): Iterable<Element> =
-        mapData.filter { isApplicableTo(it) }
-
-    override fun isApplicableTo(element: Element) =
-        nodesFilter.matches(element) && hasAnyName(element.tags)
-
-    private fun hasAnyName(tags: Map<String, String>): Boolean =
-        featureDictionaryFuture.get().byTags(tags).isSuggestion(false).find().isNotEmpty()
 
     override fun getHighlightedElements(element: Element, getMapData: () -> MapDataWithGeometry): Sequence<Element> {
         /* put markers for objects that are exactly the same as for which this quest is asking for
