2cb37fdabf27336a0ba25d3050a452998125fe27
mcliquid <info@mcliquid.de>
2023-09-21 22:39:59 +0200

    Fix for #453
    Fix for #453

--- app/src/main/java/de/westnordost/streetcomplete/quests/service_building/AddServiceBuildingTypeForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/service_building/AddServiceBuildingTypeForm.kt
@@ -9,7 +9,7 @@ class AddServiceBuildingTypeForm : AListQuestForm<String>() {
     override val items = listOf(
         TextItem("gas", R.string.quest_service_building_type_pressure),
         TextItem("substation", R.string.quest_service_building_type_substation),
-        TextItem("well", R.string.quest_service_building_type_well),
+        TextItem("water_well", R.string.quest_service_building_type_well),
         TextItem("reservoir_covered", R.string.quest_service_building_type_reservoir),
         TextItem("pumping_station", R.string.quest_service_building_type_pump),
     )
