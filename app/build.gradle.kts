5cc034cf6b9c47168b0a3e6ae94d51051ac0d8d5
Tim <tapetis@gmail.com>
2023-09-25 16:12:42 +0200

    update gradle and android gradle plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 
     bundle {
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         val kotlinVersion = "1.8.22"
-        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.android.tools.build:gradle:8.1.1")
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
     }
 }
