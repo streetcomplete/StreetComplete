7195781a6d28634fec504f49d65cc62e85c3d8e4
Tobias Zwick <newton@westnordost.de>
2023-09-25 16:43:00 +0200

    use kotlin 1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        val kotlinVersion = "1.8.22"
+        val kotlinVersion = "1.9.10"
         classpath("com.android.tools.build:gradle:8.1.1")
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
     }
