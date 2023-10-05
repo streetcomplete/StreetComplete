c6feb469c3ce9d7b73369affa2cf7d1194e89371
Flo Edelmann <git@flo-edelmann.de>
2023-10-05 23:11:44 +0200

    Enable searching by English quest titles (#5284)
    * Enable searching by English quest titles
    
    * Fix minor IDE warnings
    
    * Simplify `questTypeMatchesSearchWords` function
    
    Co-authored-by: Tobias Zwick <newton@westnordost.de>
    
    * Restore `applyChecked(b)`
    
    ---------
    
    Co-authored-by: Tobias Zwick <newton@westnordost.de>

--- app/src/main/java/de/westnordost/streetcomplete/util/ktx/String.kt
+++ app/src/main/java/de/westnordost/streetcomplete/util/ktx/String.kt
@@ -2,3 +2,5 @@ package de.westnordost.streetcomplete.util.ktx
 
 fun String.truncate(length: Int): String =
     if (this.length > length) substring(0, length - 1) + "…" else this
+
+fun String.containsAll(words: List<String>) = words.all { this.contains(it) }
