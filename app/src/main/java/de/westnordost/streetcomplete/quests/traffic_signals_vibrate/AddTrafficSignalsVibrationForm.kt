57e77915495125443c63670709aa9cc6dd91f148
Flo Edelmann <git@flo-edelmann.de>
2023-10-12 00:28:59 +0200

    Use non-MCC localization for vibrating button illustration

--- app/src/main/java/de/westnordost/streetcomplete/quests/traffic_signals_vibrate/AddTrafficSignalsVibrationForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/traffic_signals_vibrate/AddTrafficSignalsVibrationForm.kt
@@ -1,5 +1,8 @@
 package de.westnordost.streetcomplete.quests.traffic_signals_vibrate
 
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.quests.AbstractOsmQuestForm
 import de.westnordost.streetcomplete.quests.AnswerItem
@@ -8,8 +11,21 @@ class AddTrafficSignalsVibrationForm : AbstractOsmQuestForm<Boolean>() {
 
     override val contentLayoutResId = R.layout.quest_traffic_lights_vibration
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ImageView>(R.id.buttonIllustrationImageView)
+            .setImageResource(getVibratingButtonIllustrationResId(countryInfo.countryCode))
+    }
+
     override val buttonPanelAnswers = listOf(
         AnswerItem(R.string.quest_generic_hasFeature_no) { applyAnswer(false) },
         AnswerItem(R.string.quest_generic_hasFeature_yes) { applyAnswer(true) }
     )
+}
+
+private fun getVibratingButtonIllustrationResId(countryCode: String): Int = when (countryCode) {
+    "AU" -> R.drawable.vibrating_button_illustration_au
+    "GB" -> R.drawable.vibrating_button_illustration_gb
+    else -> R.drawable.vibrating_button_illustration
 }
