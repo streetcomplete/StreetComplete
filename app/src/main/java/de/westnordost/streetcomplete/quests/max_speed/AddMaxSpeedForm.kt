streetcomplete/StreetComplete

commit fbef023eedf509d5b95a71e36ed1a40e48243b2f

Drop MCC hack for layouts (#5298)
* Drop MCC logic from max speed quest form

* Drop MCC logic from max weight quest form

app/src/main/java/de/westnordost/streetcomplete/quests/max_speed/AddMaxSpeedForm.kt
@@ -156,8 +156,8 @@ class AddMaxSpeedForm : AbstractOsmQuestForm<MaxSpeedAnswer>() {
     }
 
     private val SpeedType.layoutResId get() = when (this) {
-        SIGN          -> R.layout.quest_maxspeed_sign
-        ZONE          -> R.layout.quest_maxspeed_zone_sign
+        SIGN          -> getMaxSpeedSignLayoutResId(countryInfo.countryCode)
+        ZONE          -> getMaxSpeedZoneSignLayoutResId(countryInfo.countryCode)
         LIVING_STREET -> R.layout.quest_maxspeed_living_street_sign
         NSL           -> R.layout.quest_maxspeed_national_speed_limit_sign
         ADVISORY      -> countryInfo.advisorySpeedLimitSignLayoutResId ?: R.layout.quest_maxspeed_advisory_blue
@@ -323,4 +323,15 @@ class AddMaxSpeedForm : AbstractOsmQuestForm<MaxSpeedAnswer>() {
 
 private enum class SpeedType {
     SIGN, ZONE, LIVING_STREET, ADVISORY, NO_SIGN, NSL
+}
+
+private fun getMaxSpeedSignLayoutResId(countryCode: String): Int = when (countryCode) {
+    "CA" -> R.layout.quest_maxspeed_sign_ca
+    "US" -> R.layout.quest_maxspeed_sign_us
+    else -> R.layout.quest_maxspeed_sign
+}
+
+private fun getMaxSpeedZoneSignLayoutResId(countryCode: String): Int = when (countryCode) {
+    "IL" -> R.layout.quest_maxspeed_zone_sign_il
+    else -> R.layout.quest_maxspeed_zone_sign
 }

app/src/main/java/de/westnordost/streetcomplete/quests/max_weight/AddMaxWeightForm.kt
@@ -84,7 +84,9 @@ class AddMaxWeightForm : AbstractOsmQuestForm<MaxWeightAnswer>() {
 
     private fun showSignSelectionDialog() {
         val ctx = context ?: return
-        val items = MaxWeightSign.values().map { it.asItem(layoutInflater) }
+        val items = MaxWeightSign.values().map {
+            it.asItem(layoutInflater, countryInfo.countryCode)
+        }
         ImageListPickerDialog(ctx, items, R.layout.cell_labeled_icon_select, 2) { selected ->
             selected.value?.let { setMaxWeightSign(it) }
             checkIsFormComplete()
@@ -97,7 +99,8 @@ class AddMaxWeightForm : AbstractOsmQuestForm<MaxWeightAnswer>() {
         binding.selectSignButton.isInvisible = true
         binding.inputSignContainer.removeAllViews()
 
-        layoutInflater.inflate(sign.layoutResourceId, binding.inputSignContainer)
+        val layoutResourceId = sign.getLayoutResourceId(countryInfo.countryCode)
+        layoutInflater.inflate(layoutResourceId, binding.inputSignContainer)
         initMaxWeightInput()
         focusMaxWeightInput()
 

app/src/main/java/de/westnordost/streetcomplete/quests/max_weight/MaxWeightSignItem.kt
@@ -13,23 +13,24 @@ import de.westnordost.streetcomplete.view.DrawableImage
 import de.westnordost.streetcomplete.view.image_select.DisplayItem
 import de.westnordost.streetcomplete.view.image_select.Item2
 
-fun MaxWeightSign.asItem(inflater: LayoutInflater): DisplayItem<MaxWeightSign> =
-    Item2(this, DrawableImage(BitmapDrawable(inflater.context.resources, createBitmap(inflater))))
+fun MaxWeightSign.asItem(
+    inflater: LayoutInflater,
+    countryCode: String,
+): DisplayItem<MaxWeightSign> = Item2(
+    this,
+    DrawableImage(BitmapDrawable(
+        inflater.context.resources,
+        createBitmap(inflater, countryCode)
+    )),
+)
 
-val MaxWeightSign.layoutResourceId get() = when (this) {
-    MaxWeightSign.MAX_WEIGHT             -> R.layout.quest_maxweight_sign
-    MaxWeightSign.MAX_GROSS_VEHICLE_MASS -> R.layout.quest_maxweight_mgv_sign
-    MaxWeightSign.MAX_AXLE_LOAD          -> R.layout.quest_maxweight_axleload_sign
-    MaxWeightSign.MAX_TANDEM_AXLE_LOAD   -> R.layout.quest_maxweight_tandem_axleload_sign
-}
-
-private fun MaxWeightSign.createBitmap(inflater: LayoutInflater): Bitmap {
+private fun MaxWeightSign.createBitmap(inflater: LayoutInflater, countryCode: String): Bitmap {
     val container = FrameLayout(inflater.context)
     container.layoutParams = ViewGroup.LayoutParams(
         ViewGroup.LayoutParams.WRAP_CONTENT,
         ViewGroup.LayoutParams.WRAP_CONTENT
     )
-    inflater.inflate(layoutResourceId, container)
+    inflater.inflate(getLayoutResourceId(countryCode), container)
 
     container.measure(
         View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
@@ -41,4 +42,33 @@ private fun MaxWeightSign.createBitmap(inflater: LayoutInflater): Bitmap {
     return createBitmap(w, h, Bitmap.Config.ARGB_8888).applyCanvas {
         container.draw(this)
     }
+}
+
+fun MaxWeightSign.getLayoutResourceId(countryCode: String) = when (this) {
+    MaxWeightSign.MAX_WEIGHT             -> getMaxWeightSignLayoutResId(countryCode)
+    MaxWeightSign.MAX_GROSS_VEHICLE_MASS -> getMaxWeightMgvSignLayoutResId(countryCode)
+    MaxWeightSign.MAX_AXLE_LOAD          -> getMaxWeightAxleLoadSignLayoutResId(countryCode)
+    MaxWeightSign.MAX_TANDEM_AXLE_LOAD   -> getMaxWeightTandemAxleLoadSignLayoutResId(countryCode)
+}
+
+private fun getMaxWeightSignLayoutResId(countryCode: String): Int = when (countryCode) {
+    "AU", "CA", "US" -> R.layout.quest_maxweight_us_sign
+    else -> R.layout.quest_maxweight_sign
+}
+
+private fun getMaxWeightMgvSignLayoutResId(countryCode: String): Int = when (countryCode) {
+    "AU", "CA", "US" -> R.layout.quest_maxweight_mgv_us_sign
+    "DE" -> R.layout.quest_maxweight_mgv_de_sign
+    "GB" -> R.layout.quest_maxweight_mgv_gb_sign
+    else -> R.layout.quest_maxweight_mgv_sign
+}
+
+private fun getMaxWeightAxleLoadSignLayoutResId(countryCode: String): Int = when (countryCode) {
+    "AU", "CA", "US" -> R.layout.quest_maxweight_axleload_us_sign
+    else -> R.layout.quest_maxweight_axleload_sign
+}
+
+private fun getMaxWeightTandemAxleLoadSignLayoutResId(countryCode: String): Int = when (countryCode) {
+    "AU", "CA", "US" -> R.layout.quest_maxweight_tandem_axleload_us_sign
+    else -> R.layout.quest_maxweight_tandem_axleload_sign
 }

app/src/main/res/layout-mcc234/quest_maxweight_mgv_sign.xml
@@ -1,9 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<FrameLayout
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    xmlns:android="http://schemas.android.com/apk/res/android">
-
-    <include layout="@layout/quest_maxweight_mgv_gb_sign"/>
-
-</FrameLayout>

app/src/main/res/layout-mcc262/quest_maxweight_mgv_sign.xml
@@ -1,8 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<FrameLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content">
-
-    <include layout="@layout/quest_maxweight_mgv_de_sign" />
-
-</FrameLayout>

app/src/main/res/layout-mcc302/quest_maxspeed_sign.xml
@@ -1,6 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<FrameLayout
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    xmlns:android="http://schemas.android.com/apk/res/android">
-    <include layout="@layout/quest_maxspeed_sign_ca"/></FrameLayout>

app/src/main/res/layout-mcc302/quest_maxweight_axleload_sign.xml
@@ -1,9 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<FrameLayout
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    xmlns:android="http://schemas.android.com/apk/res/android">
-
-    <include layout="@layout/quest_maxweight_axleload_us_sign"/>
-
-</FrameLayout>

app/src/main/res/layout-mcc302/quest_maxweight_mgv_sign.xml
@@ -1,9 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<FrameLayout
-    xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content">
-
-    <include layout="@layout/quest_maxweight_mgv_us_sign" />
-
-</FrameLayout>

commit 86fa1d04bb96e337d106d77f604b2d4eeac78efe

Localize slow zone image in confirm dialog
Regression in #5298

app/src/main/java/de/westnordost/streetcomplete/quests/max_speed/AddMaxSpeedForm.kt
@@ -228,6 +228,12 @@ class AddMaxSpeedForm : AbstractOsmQuestForm<MaxSpeedAnswer>() {
         activity?.let {
             val dialogBinding = QuestMaxspeedNoSignNoSlowZoneConfirmationBinding.inflate(layoutInflater)
             enableAppropriateLabelsForSlowZone(dialogBinding.slowZoneImage)
+            dialogBinding.slowZoneImage.removeAllViews()
+            layoutInflater.inflate(
+                getMaxSpeedZoneSignLayoutResId(countryInfo.countryCode),
+                dialogBinding.slowZoneImage,
+                true,
+            )
             val dialogSpeedInput: EditText = dialogBinding.slowZoneImage.findViewById(R.id.maxSpeedInput)
             dialogSpeedInput.setText("××")
             dialogSpeedInput.inputType = EditorInfo.TYPE_NULL