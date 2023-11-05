9f501df0a3b2fea7b9d6dd0f54143465e748ba34
Tobias Zwick <newton@westnordost.de>
2023-11-05 22:15:55 +0100

    do not ask for crossing kerb height if kerb:left and kerb:right are set
    https://community.openstreetmap.org/t/kerb-auf-crossings/105665/7

--- app/src/main/java/de/westnordost/streetcomplete/quests/crossing_kerb_height/AddCrossingKerbHeight.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/crossing_kerb_height/AddCrossingKerbHeight.kt
@@ -21,6 +21,7 @@ class AddCrossingKerbHeight : OsmElementQuestType<KerbHeight> {
           highway = crossing
           and foot != no
           and crossing
+          and !kerb:left and !kerb:right
           and (
             !kerb
             or kerb ~ yes|unknown
