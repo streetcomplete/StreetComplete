4965ca350a4437639e2a956326168a7f4f01b3ab
Matija Nalis <mnalis-git@voyager.hr>
2023-11-12 02:25:31 +0100

    add confirmNotBbq()

--- app/src/main/java/de/westnordost/streetcomplete/quests/bbq_fuel/AddBbqFuelForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/bbq_fuel/AddBbqFuelForm.kt
@@ -17,6 +17,15 @@ class AddBbqFuelForm : AListQuestForm<BbqFuel>() {
     )
 
     override val otherAnswers = listOfNotNull(
-        AnswerItem(R.string.quest_bbq_fuel_not_a_bbq) { applyAnswer(NOT_BBQ) },
+        AnswerItem(R.string.quest_bbq_fuel_not_a_bbq) { confirmNotBbq() },
     )
+
+    private fun confirmNotBbq() {
+        val ctx = context ?: return
+        AlertDialog.Builder(ctx)
+            .setTitle(R.string.quest_bbq_fuel_not_a_bbq_confirmation_title)
+            .setPositiveButton(R.string.quest_generic_confirmation_yes) { _, _ -> applyAnswer(NOT_BBQ) }
+            .setNegativeButton(R.string.quest_generic_confirmation_no, null)
+            .show()
+    }
 }
