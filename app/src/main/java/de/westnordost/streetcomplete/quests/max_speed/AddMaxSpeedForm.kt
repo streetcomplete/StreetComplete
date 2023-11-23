80dbc5a25d4c1f303818d294c5899b98762f81b5
Tobias Zwick <newton@westnordost.de>
2023-11-23 16:59:37 +0100

    fix  #5378

--- app/src/main/java/de/westnordost/streetcomplete/quests/max_speed/AddMaxSpeedForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/max_speed/AddMaxSpeedForm.kt
@@ -324,7 +324,7 @@ class AddMaxSpeedForm : AbstractOsmQuestForm<MaxSpeedAnswer>() {
     companion object {
         private val POSSIBLY_SLOWZONE_ROADS = listOf("residential", "unclassified", "tertiary" /*#1133*/)
         private val MAYBE_LIVING_STREET = listOf("residential", "unclassified")
-        private val ROADS_WITH_DEFINITE_SPEED_LIMIT = listOf("trunk", "motorway", "living_street")
+        private val ROADS_WITH_DEFINITE_SPEED_LIMIT = listOf("motorway", "living_street")
         private val ROADS_WITH_DEFINITE_SPEED_LIMIT_GB = listOf("motorway", "living_street") /*#2750*/
 
         private var LAST_INPUT_SLOW_ZONE: Int? = null
