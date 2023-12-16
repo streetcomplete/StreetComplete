2a1aad8d615e35eeaf39f21e41c2ae2a770e38aa
Tobias Zwick <newton@westnordost.de>
2023-12-16 17:31:47 +0100

    downgrade some deps again
    I don't feel like migrating to compile target SDK 34 right now...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -130,7 +130,7 @@ dependencies {
 
     // Android stuff
     implementation("com.google.android.material:material:1.6.1")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.annotation:annotation:1.7.1")
@@ -152,7 +152,7 @@ dependencies {
     api("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
     // scheduling background jobs
-    implementation("androidx.work:work-runtime:2.9.0")
+    implementation("androidx.work:work-runtime:2.8.1")
 
     // finding in which country we are for country-specific logic
     implementation("de.westnordost:countryboundaries:2.1")
