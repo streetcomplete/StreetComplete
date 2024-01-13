e16cbb2162dfd25c20facb94e25b1e5e230492ae
Flo Edelmann <git@flo-edelmann.de>
2024-01-13 16:53:18 +0100

    Tweak WebView styles to be indistinguishable from TextView

--- app/src/main/java/de/westnordost/streetcomplete/screens/about/ChangelogFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/about/ChangelogFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.webkit.WebView
 import androidx.annotation.ColorRes
+import androidx.annotation.DimenRes
 import androidx.appcompat.app.AlertDialog
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.databinding.DialogWhatsNewBinding
@@ -23,6 +24,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.math.roundToInt
 
 /** Shows the full changelog */
 class ChangelogFragment : TwoPaneDetailFragment(R.layout.fragment_changelog), HasTitle {
@@ -79,22 +81,41 @@ private suspend fun readChangelog(resources: Resources): String = withContext(Di
 private fun Resources.getHexColor(@ColorRes resId: Int) =
     String.format("#%06X", 0xffffff and getColor(resId))
 
+private fun Resources.getDimensionInSp(@DimenRes resId: Int) =
+    (getDimension(resId) / displayMetrics.scaledDensity).roundToInt()
+
+private fun Resources.getDimensionInDp(@DimenRes resId: Int) =
+    (getDimension(resId) / displayMetrics.density).roundToInt()
+
 private fun WebView.setHtmlFromString(html: String) {
     val textColor = resources.getHexColor(R.color.text)
-    val linkColor = resources.getHexColor(R.color.text_based_on_main_colors)
+    val linkColor = resources.getHexColor(R.color.accent)
     val dividerColor = resources.getHexColor(R.color.divider)
+
+    val textSize = resources.getDimensionInSp(androidx.appcompat.R.dimen.abc_text_size_body_1_material)
+    val h2Size = resources.getDimensionInSp(androidx.appcompat.R.dimen.abc_text_size_headline_material)
+    val h3Size = resources.getDimensionInSp(androidx.appcompat.R.dimen.abc_text_size_medium_material)
+    val h4Size = resources.getDimensionInSp(androidx.appcompat.R.dimen.abc_text_size_subhead_material)
+
+    val verticalMargin = resources.getDimensionInDp(R.dimen.activity_vertical_margin)
+    val horizontalMargin = resources.getDimensionInDp(R.dimen.activity_horizontal_margin)
 
     val style = """
         <meta name="color-scheme" content="dark light">
         <style>
-            :root {
+            body {
+                margin: ${verticalMargin}px ${horizontalMargin}px;
                 color: $textColor;
-                padding: 6px;
+                font-size: ${textSize}px;
             }
 
-            :link {
-                color: $linkColor;
-            }
+            :link { color: $linkColor; }
+
+            h2, h3, h4 { font-family: sans-serif-condensed; }
+
+            h2 { font-size: ${h2Size}px; }
+            h3 { font-size: ${h3Size}px; }
+            h4 { font-size: ${h4Size}px; }
 
             h2:not(:first-child) {
                 border-top: 1px solid $dividerColor;
