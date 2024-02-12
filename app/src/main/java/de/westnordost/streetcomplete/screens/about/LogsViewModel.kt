45b225966fcbb8df180e874c7cd0bd60e85cb5a5
Tobias Zwick <newton@westnordost.de>
2024-02-12 16:28:51 +0100

    Revert "solve performance issue in LogsViewModel (?)"
    This reverts commit 314f05f75df208a543a3566a5c81f6a31731ae8d.

--- app/src/main/java/de/westnordost/streetcomplete/screens/about/LogsViewModel.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/about/LogsViewModel.kt
@@ -4,12 +4,9 @@ import androidx.lifecycle.ViewModel
 import de.westnordost.streetcomplete.data.logs.LogMessage
 import de.westnordost.streetcomplete.data.logs.LogsFilters
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 
 abstract class LogsViewModel : ViewModel() {
     abstract val filters: MutableStateFlow<LogsFilters>
-    abstract val logs: SharedFlow<List<LogMessage>>
-
-    abstract val logsText: String
+    abstract val logs: StateFlow<List<LogMessage>>
 }
