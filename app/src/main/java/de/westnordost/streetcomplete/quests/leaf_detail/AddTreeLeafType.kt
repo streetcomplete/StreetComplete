b66f4e804bf8b44fae3575bb1cabb2dd87501ba4
Mateusz Konieczny <matkoniecz@gmail.com>
2024-02-22 18:09:26 +0100

    ask about leaf type of natural=tree
    partial implementation of #83

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/leaf_detail/AddTreeLeafType.kt
@@ -0,0 +1,34 @@
+package de.westnordost.streetcomplete.quests.leaf_detail
+
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.elementfilter.toElementFilterExpression
+import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
+import de.westnordost.streetcomplete.data.osm.mapdata.Element
+import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
+import de.westnordost.streetcomplete.data.osm.mapdata.filter
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
+import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement.OUTDOORS
+import de.westnordost.streetcomplete.osm.Tags
+
+class AddTreeLeafType : OsmFilterQuestType<TreeLeafType>() {
+    override val elementFilter = """
+        nodes with natural = tree and !leaf_type
+    """
+    override val changesetComment = "Specify leaf types"
+    override val wikiLink = "Key:leaf_type"
+    override val icon = R.drawable.ic_quest_leaf
+    override val achievements = listOf(OUTDOORS)
+
+    override fun getTitle(tags: Map<String, String>) = R.string.quest_leafType_tree_title
+
+    override fun getHighlightedElements(element: Element, getMapData: () -> MapDataWithGeometry) =
+        getMapData().filter("""
+            nodes with natural=tree
+        """.toElementFilterExpression())
+
+    override fun createForm() = AddTreeLeafTypeForm()
+
+    override fun applyAnswerTo(answer: TreeLeafType, tags: Tags, geometry: ElementGeometry, timestampEdited: Long) {
+        tags["leaf_type"] = answer.osmValue
+    }
+}
