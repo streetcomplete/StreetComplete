ae82fdbbce7c05942fd7567a4b37bf52913245a6
Flo Edelmann <git@flo-edelmann.de>
2024-03-02 00:15:43 +0100

    Standardize multiline comment formatting

--- app/src/main/java/de/westnordost/streetcomplete/overlays/cycleway/SeparateCyclewayForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/overlays/cycleway/SeparateCyclewayForm.kt
@@ -51,29 +51,30 @@ class SeparateCyclewayForm : AImageSelectOverlayForm<SeparateCycleway>() {
 
         val cycleway = parseSeparateCycleway(element!!.tags)
 
-        /* Not displaying bicycle=yes and bicycle=no on footways and treating it the same because
-           whether riding a bike on a footway is allowed by default (without requiring signs) or
-           only under certain conditions (e.g. certain minimum width of sidewalk) is very much
-           dependent on the country or state one is in.
+        /*
+            Not displaying bicycle=yes and bicycle=no on footways and treating it the same because
+            whether riding a bike on a footway is allowed by default (without requiring signs) or
+            only under certain conditions (e.g. certain minimum width of sidewalk) is very much
+            dependent on the country or state one is in.
 
-           Hence, it is not verifiable well for the on-site surveyor: If there is no sign that
-           specifically allows or forbids cycling on a footway, the user is left with his loose
-           (mis)understanding of the local legislation to decide. After all, bicycle=yes/no
-           is (usually) nothing physical, but merely describes what is legal. It is in that sense
-           then not information surveyable on-the-ground, unless specifically signed.
-           bicycle=yes/no does however not make a statement about from where this info is derived.
+            Hence, it is not verifiable well for the on-site surveyor: If there is no sign that
+            specifically allows or forbids cycling on a footway, the user is left with his loose
+            (mis)understanding of the local legislation to decide. After all, bicycle=yes/no
+            is (usually) nothing physical, but merely describes what is legal. It is in that sense
+            then not information surveyable on-the-ground, unless specifically signed.
+            bicycle=yes/no does however not make a statement about from where this info is derived.
 
-           So, from an on-site surveyor point of view, it is always better to record what is signed,
-           instead of what follows from that signage.
+            So, from an on-site surveyor point of view, it is always better to record what is signed,
+            instead of what follows from that signage.
 
-           Signage, however, is out of scope of this overlay because while the physical presence of
-           a cycleway can be validated at a glance, the presence of a sign requires to walk a bit up
-           or down the street in order to find (or not find) a sign.
-           More importantly, at the time of writing, there is no way to tag the information that a
-           bicycle=* access restriction is derived from the presence of a sign. This however is a
-           prerequisite for it being  displayed as a selectable option due to the reasons stated
-           above.
-        */
+            Signage, however, is out of scope of this overlay because while the physical presence of
+            a cycleway can be validated at a glance, the presence of a sign requires to walk a bit up
+            or down the street in order to find (or not find) a sign.
+            More importantly, at the time of writing, there is no way to tag the information that a
+            bicycle=* access restriction is derived from the presence of a sign. This however is a
+            prerequisite for it being  displayed as a selectable option due to the reasons stated
+            above.
+         */
         currentCycleway = if (cycleway == NOT_ALLOWED || cycleway == ALLOWED_ON_FOOTWAY) NON_DESIGNATED else cycleway
         selectedItem = currentCycleway?.asItem(countryInfo.isLeftHandTraffic)
     }
