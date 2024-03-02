826ca49c819b22d6b2b0e47702110b9714bd9d04
Isira Seneviratne <isirasen96@gmail.com>
2024-03-02 20:03:37 +0530

    Improve location elapsed time difference calculation

--- app/src/main/java/de/westnordost/streetcomplete/util/ktx/Location.kt
+++ app/src/main/java/de/westnordost/streetcomplete/util/ktx/Location.kt
@@ -1,6 +1,11 @@
 package de.westnordost.streetcomplete.util.ktx
 
 import android.location.Location
+import androidx.core.location.LocationCompat
 import de.westnordost.streetcomplete.data.osm.mapdata.LatLon
+import kotlin.time.Duration.Companion.milliseconds
 
 fun Location.toLatLon() = LatLon(latitude, longitude)
+
+val Location.elapsedDuration
+    get() = LocationCompat.getElapsedRealtimeMillis(this).milliseconds
