ef6eb16792a81c48ae3b24075d2af2b894e96ac4
Tobias Zwick <newton@westnordost.de>
2024-03-26 10:30:32 +0100

    move set html body to own file

--- app/src/main/java/de/westnordost/streetcomplete/screens/about/ChangelogFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/about/ChangelogFragment.kt
@@ -20,6 +20,7 @@ import de.westnordost.streetcomplete.util.ktx.getRawTextFile
 import de.westnordost.streetcomplete.util.ktx.indicesOf
 import de.westnordost.streetcomplete.util.ktx.pxToDp
 import de.westnordost.streetcomplete.util.ktx.pxToSp
+import de.westnordost.streetcomplete.util.ktx.setHtmlBody
 import de.westnordost.streetcomplete.util.ktx.viewLifecycleScope
 import de.westnordost.streetcomplete.util.viewBinding
 import kotlinx.coroutines.CoroutineScope
@@ -41,7 +42,7 @@ class ChangelogFragment : TwoPaneDetailFragment(R.layout.fragment_changelog), Ha
 
         viewLifecycleScope.launch {
             val changelog = readChangelog(resources)
-            binding.webView.setHtmlFromString(changelog)
+            binding.webView.setHtmlBody(changelog)
         }
     }
 }
@@ -67,7 +68,7 @@ class WhatsNewDialog(context: Context, sinceVersion: String) : AlertDialog(conte
             }
             val changelog = fullChangelog.substring(0, sinceVersionIndex)
 
-            binding.webView.setHtmlFromString(changelog)
+            binding.webView.setHtmlBody(changelog)
         }
     }
 
@@ -79,59 +80,4 @@ class WhatsNewDialog(context: Context, sinceVersion: String) : AlertDialog(conte
 
 private suspend fun readChangelog(resources: Resources): String = withContext(Dispatchers.IO) {
     resources.getRawTextFile(R.raw.changelog)
-}
-
-private fun Resources.getHexColor(@ColorRes resId: Int) =
-    String.format("#%06X", 0xffffff and getColor(resId))
-
-private fun Resources.getDimensionInSp(@DimenRes resId: Int) = pxToSp(getDimension(resId)).roundToInt()
-
-private fun Resources.getDimensionInDp(@DimenRes resId: Int) = pxToDp(getDimension(resId)).roundToInt()
-
-private fun WebView.setHtmlFromString(body: String) {
-    val textColor = resources.getHexColor(R.color.text)
-    val linkColor = resources.getHexColor(R.color.accent)
-    val dividerColor = resources.getHexColor(R.color.divider)
-
-    val textSize = resources.getDimensionInSp(androidx.appcompat.R.dimen.abc_text_size_body_1_material)
-    val h2Size = resources.getDimensionInSp(androidx.appcompat.R.dimen.abc_text_size_headline_material)
-    val h3Size = resources.getDimensionInSp(androidx.appcompat.R.dimen.abc_text_size_medium_material)
-    val h4Size = resources.getDimensionInSp(androidx.appcompat.R.dimen.abc_text_size_subhead_material)
-
-    val verticalMargin = resources.getDimensionInDp(R.dimen.activity_vertical_margin)
-    val horizontalMargin = resources.getDimensionInDp(R.dimen.activity_horizontal_margin)
-
-    val html = """
-        <html>
-            <head>
-                <meta name="color-scheme" content="dark light">
-                <style>
-                    body {
-                        margin: ${verticalMargin}px ${horizontalMargin}px;
-                        color: $textColor;
-                        font-size: ${textSize}px;
-                    }
-
-                    :link { color: $linkColor; }
-
-                    h2, h3, h4 { font-family: sans-serif-condensed; }
-
-                    h2 { font-size: ${h2Size}px; }
-                    h3 { font-size: ${h3Size}px; }
-                    h4 { font-size: ${h4Size}px; }
-
-                    h2:not(:first-child) {
-                        border-top: 1px solid $dividerColor;
-                        padding-top: 1rem;
-                    }
-
-                    @media (prefers-color-scheme: dark) {}
-                </style>
-            </head>
-            <body>$body</body>
-        </html>
-    """.trimIndent()
-
-    loadDataWithBaseURL(null, html, "text/html", "utf-8", null)
-    setBackgroundColor(Color.TRANSPARENT)
 }
