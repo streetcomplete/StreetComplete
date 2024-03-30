b13513ecb11c1f64aac618dde31d741e8ff54ca9
Tobias Zwick <newton@westnordost.de>
2024-03-30 01:13:19 +0100

    add viewmodel for QuestSelectionFragment and adapter

--- app/src/main/java/de/westnordost/streetcomplete/screens/settings/SettingsModule.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/settings/SettingsModule.kt
@@ -1,10 +1,14 @@
 package de.westnordost.streetcomplete.screens.settings
 
+import de.westnordost.streetcomplete.screens.settings.questselection.QuestSelectionViewModel
+import de.westnordost.streetcomplete.screens.settings.questselection.QuestSelectionViewModelImpl
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val settingsModule = module {
     single { ResurveyIntervalsUpdater(get()) }
 
     viewModel<SettingsViewModel> { SettingsViewModelImpl(get(), get(), get(), get(), get(), get(), get(), get()) }
+    viewModel<QuestSelectionViewModel> { QuestSelectionViewModelImpl(get(), get(), get(), get(), get(named("CountryBoundariesLazy")), get()) }
 }
--- app/src/main/java/de/westnordost/streetcomplete/screens/settings/questselection/QuestSelection.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/settings/questselection/QuestSelection.kt
@@ -3,6 +3,6 @@ package de.westnordost.streetcomplete.screens.settings.questselection
 import de.westnordost.streetcomplete.data.osmnotes.notequests.OsmNoteQuestType
 import de.westnordost.streetcomplete.data.quest.QuestType
 
-data class QuestVisibility(val questType: QuestType, var visible: Boolean) {
+data class QuestSelection(val questType: QuestType, val selected: Boolean) {
     val isInteractionEnabled get() = questType !is OsmNoteQuestType
 }
