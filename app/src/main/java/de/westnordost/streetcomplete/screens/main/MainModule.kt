bb1a9ecaed537294ef68aaecedf5a76216e7e81d
Tobias Zwick <newton@westnordost.de>
2024-04-04 11:39:10 +0200

    add viewmodel for UploadButtonFragment

--- app/src/main/java/de/westnordost/streetcomplete/screens/main/MainModule.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/main/MainModule.kt
@@ -1,10 +1,15 @@
 package de.westnordost.streetcomplete.screens.main
 
 import de.westnordost.streetcomplete.data.location.RecentLocationStore
+import de.westnordost.streetcomplete.screens.main.controls.UploadButtonViewModel
+import de.westnordost.streetcomplete.screens.main.controls.UploadButtonViewModelImpl
 import de.westnordost.streetcomplete.util.location.LocationAvailabilityReceiver
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val mainModule = module {
     single { LocationAvailabilityReceiver(get()) }
     single { RecentLocationStore() }
+
+    viewModel<UploadButtonViewModel> { UploadButtonViewModelImpl(get(), get(), get(), get(), get(), get()) }
 }
--- app/src/main/java/de/westnordost/streetcomplete/screens/main/controls/UploadButtonFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/main/controls/UploadButtonFragment.kt
@@ -1,105 +1,38 @@
 package de.westnordost.streetcomplete.screens.main.controls
 
-import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.View
-import androidx.core.content.getSystemService
 import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
-import com.russhwolf.settings.ObservableSettings
-import de.westnordost.streetcomplete.ApplicationConstants
-import de.westnordost.streetcomplete.Prefs
 import de.westnordost.streetcomplete.R
-import de.westnordost.streetcomplete.data.UnsyncedChangesCountSource
-import de.westnordost.streetcomplete.data.upload.UploadController
-import de.westnordost.streetcomplete.data.upload.UploadProgressSource
-import de.westnordost.streetcomplete.data.user.UserLoginStatusSource
+import de.westnordost.streetcomplete.util.ktx.observe
 import de.westnordost.streetcomplete.util.ktx.toast
-import de.westnordost.streetcomplete.util.ktx.viewLifecycleScope
 import de.westnordost.streetcomplete.view.dialogs.RequestLoginDialog
-import kotlinx.coroutines.launch
-import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /** Fragment that shows the upload button, including upload progress etc. */
 class UploadButtonFragment : Fragment(R.layout.fragment_upload_button) {
 
-    private val uploadController: UploadController by inject()
-    private val uploadProgressSource: UploadProgressSource by inject()
-    private val userLoginStatusSource: UserLoginStatusSource by inject()
-    private val unsyncedChangesCountSource: UnsyncedChangesCountSource by inject()
-    private val prefs: ObservableSettings by inject()
-
+    private val viewModel by viewModel<UploadButtonViewModel>()
     private val uploadButton get() = view as UploadButton
-
-    private val unsyncedChangesCountListener = object : UnsyncedChangesCountSource.Listener {
-        override fun onIncreased() { viewLifecycleScope.launch { updateCount() } }
-        override fun onDecreased() { viewLifecycleScope.launch { updateCount() } }
-    }
-
-    private val uploadProgressListener = object : UploadProgressSource.Listener {
-        override fun onStarted() { viewLifecycleScope.launch { updateProgress(true) } }
-        override fun onFinished() { viewLifecycleScope.launch { updateProgress(false) } }
-    }
-
-    /* --------------------------------------- Lifecycle ---------------------------------------- */
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         uploadButton.setOnClickListener {
-            if (isConnected()) {
-                uploadChanges()
+            if (viewModel.isConnected) {
+                if (viewModel.isLoggedIn) {
+                    viewModel.upload()
+                } else {
+                    context?.let { RequestLoginDialog(it).show() }
+                }
             } else {
                 context?.toast(R.string.offline)
             }
         }
-    }
 
-    override fun onStart() {
-        super.onStart()
-        /* Only show the button if autosync is off */
-        uploadButton.isGone = isAutosync
-        if (!isAutosync) {
-            viewLifecycleScope.launch { updateCount() }
-            updateProgress(uploadProgressSource.isUploadInProgress)
-            unsyncedChangesCountSource.addListener(unsyncedChangesCountListener)
-            uploadProgressSource.addListener(uploadProgressListener)
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        uploadProgressSource.removeListener(uploadProgressListener)
-        unsyncedChangesCountSource.removeListener(unsyncedChangesCountListener)
-    }
-
-    // ---------------------------------------------------------------------------------------------
-
-    private val isAutosync: Boolean get() =
-        Prefs.Autosync.valueOf(prefs.getStringOrNull(Prefs.AUTOSYNC) ?: ApplicationConstants.DEFAULT_AUTOSYNC) == Prefs.Autosync.ON
-
-    private suspend fun updateCount() {
-        uploadButton.uploadableCount = unsyncedChangesCountSource.getCount()
-    }
-
-    private fun updateProgress(isUploadInProgress: Boolean) {
-        uploadButton.isEnabled = !isUploadInProgress
-    }
-
-    private fun uploadChanges() {
-        // because the app should ask for permission even if there is nothing to upload right now
-        if (!userLoginStatusSource.isLoggedIn) {
-            context?.let { RequestLoginDialog(it).show() }
-        } else {
-            uploadController.upload(isUserInitiated = true)
-        }
-    }
-
-    /* Does not necessarily mean that the user has internet. But if he is not connected, he will
-     * not have internet */
-    private fun isConnected(): Boolean {
-        val connectivityManager = context?.getSystemService<ConnectivityManager>()
-        val activeNetworkInfo = connectivityManager?.activeNetworkInfo
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+        observe(viewModel.isAutosync) { uploadButton.isGone = it }
+        observe(viewModel.unsyncedChangesCount) { uploadButton.uploadableCount = it }
+        observe(viewModel.isUploadInProgress) { uploadButton.isEnabled = !it }
     }
 }
