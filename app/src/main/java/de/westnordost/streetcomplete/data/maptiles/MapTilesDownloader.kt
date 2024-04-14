c98e4d6a2ed10c7f259234379247aea413259242
Tobias Zwick <newton@westnordost.de>
2024-04-14 13:50:59 +0200

    catch and log exceptions thrown by OfflineManager, also use suspending API instead of callbacks

--- app/src/main/java/de/westnordost/streetcomplete/data/maptiles/MapTilesDownloader.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/maptiles/MapTilesDownloader.kt
@@ -3,11 +3,13 @@ package de.westnordost.streetcomplete.data.maptiles
 import android.content.Context
 import org.maplibre.android.geometry.LatLngBounds
 import org.maplibre.android.offline.OfflineManager
-import org.maplibre.android.offline.OfflineRegion
-import org.maplibre.android.offline.OfflineRegionError
-import org.maplibre.android.offline.OfflineRegionStatus
 import org.maplibre.android.offline.OfflineTilePyramidRegionDefinition
 import de.westnordost.streetcomplete.data.osm.mapdata.BoundingBox
+import de.westnordost.streetcomplete.screens.main.map.maplibre.awaitCreateOfflineRegion
+import de.westnordost.streetcomplete.screens.main.map.maplibre.awaitDelete
+import de.westnordost.streetcomplete.screens.main.map.maplibre.awaitDownload
+import de.westnordost.streetcomplete.screens.main.map.maplibre.awaitGetOfflineRegions
+import de.westnordost.streetcomplete.screens.main.map.maplibre.awaitResetDatabase
 import de.westnordost.streetcomplete.screens.main.map.maplibre.toLatLng
 import de.westnordost.streetcomplete.util.ktx.format
 import de.westnordost.streetcomplete.util.ktx.nowAsEpochMilliseconds
@@ -18,81 +20,57 @@ import org.maplibre.android.MapLibre
 
 class MapTilesDownloader(private val context: Context) {
 
-    private val offlineManager get() = OfflineManager.getInstance(context)
-    private val pixelRatio = context.resources.displayMetrics.density
-
-    private val offlineRegionCallback = object : OfflineManager.CreateOfflineRegionCallback {
-        override fun onCreate(offlineRegion: OfflineRegion) {
-            val time = nowAsEpochMilliseconds()
-
-            offlineRegion.setObserver(object : OfflineRegion.OfflineRegionObserver {
-                override fun mapboxTileCountLimitExceeded(limit: Long) {
-                    // was never called in a test with 10 tiles limit
-                    // ignore since we limit cache only by size
-                }
-
-                override fun onError(error: OfflineRegionError) {
-                    offlineRegion.setDownloadState(OfflineRegion.STATE_INACTIVE) // stop downloading
-                    Log.i(TAG, "Error during download: ${error.reason}, ${error.message}")
-                }
-
-                override fun onStatusChanged(status: OfflineRegionStatus) {
-                    if (status.isComplete) {
-                        offlineRegion.setDownloadState(OfflineRegion.STATE_INACTIVE) // also needed when download is complete
-                        val seconds = (nowAsEpochMilliseconds() - time) / 1000.0
-                        Log.i(TAG, "Downloaded ${status.completedTileCount} tiles (${status.completedTileSize / 1000}kB) in ${seconds.format(1)}s")
-                        // note that the numbers include tiles that were already on device
-                        //  no idea how to check which tiles were really downloaded (other than in android log for MapLibre)
-                        // status.requiredResourceCount and status.completedResourceSize might be interesting too
-                    }
-                }
-            })
-
-            // start the download by setting state to active
-            offlineRegion.setDownloadState(OfflineRegion.STATE_ACTIVE)
-        }
-
-        override fun onError(error: String) {
-            // creating the offline region failed, when can this happen?
-        }
-    }
-
     init {
-        MapLibre.getInstance(context) // must be called before getting OfflineManager instance
+        try {
+            // must be called before getting OfflineManager instance but will throw an exception if
+            // not called from the main thread
+            MapLibre.getInstance(context)
+        } catch (_: Exception) { }
     }
 
     /** delete regions, which allows contained tiles to be deleted if cache size is exceeded */
-    fun deleteRegionsOlderThan(olderThan: Long) {
-        offlineManager.listOfflineRegions(object : OfflineManager.ListOfflineRegionsCallback{
-            override fun onError(error: String) { }
-
-            override fun onList(offlineRegions: Array<OfflineRegion>?) {
-                offlineRegions?.forEach {
-                    val timestamp = it.metadata.toString(Charsets.UTF_8).toLongOrNull() ?: 0
-                    if (timestamp < olderThan) {
-                        it.delete(object : OfflineRegion.OfflineRegionDeleteCallback {
-                            override fun onDelete() {}
-
-                            override fun onError(error: String) {}
-                        })
-                    }
+    suspend fun deleteRegionsOlderThan(olderThan: Long) {
+        try {
+            val offlineRegions = OfflineManager.getInstance(context).awaitGetOfflineRegions()
+            for (offlineRegion in offlineRegions) {
+                val timestamp = offlineRegion.metadata.toString(Charsets.UTF_8).toLongOrNull() ?: 0
+                if (timestamp < olderThan) {
+                    offlineRegion.awaitDelete()
                 }
             }
-        })
+        } catch (e: Exception) {
+            Log.w(TAG, e.message.orEmpty(), e)
+        }
     }
 
-    fun clear() {
-        offlineManager.resetDatabase(null)
+    suspend fun clear() {
+        try {
+            OfflineManager.getInstance(context).awaitResetDatabase()
+        } catch (e: Exception) {
+            Log.w(TAG, e.message.orEmpty(), e)
+        }
     }
 
     suspend fun download(bbox: BoundingBox) = withContext(Dispatchers.IO) {
         val bounds = LatLngBounds.fromLatLngs(listOf(bbox.max.toLatLng(), bbox.min.toLatLng()))
+        val pixelRatio = context.resources.displayMetrics.density
         val regionDefinition = OfflineTilePyramidRegionDefinition(styleUrl, bounds, 0.0, 16.0, pixelRatio)
 
         // store timestamp as metadata for deleting areas older than
         // (could also be done directly from the long)
         val metadata = nowAsEpochMilliseconds().toString().toByteArray(Charsets.UTF_8)
-        offlineManager.createOfflineRegion(regionDefinition, metadata, offlineRegionCallback)
+        try {
+            val offlineRegion = OfflineManager.getInstance(context).awaitCreateOfflineRegion(regionDefinition, metadata)
+            val time = nowAsEpochMilliseconds()
+            val status = offlineRegion.awaitDownload()
+            val seconds = (nowAsEpochMilliseconds() - time) / 1000.0
+            Log.i(TAG, "Downloaded ${status.completedTileCount} tiles (${status.completedTileSize / 1000}kB) in ${seconds.format(1)}s")
+            // note that the numbers include tiles that were already on device
+            //  no idea how to check which tiles were really downloaded (other than in android log for MapLibre)
+            // status.requiredResourceCount and status.completedResourceSize might be interesting too
+        } catch (e: Exception) {
+            Log.w(TAG, e.message.orEmpty(), e)
+        }
     }
 
     companion object {
---
+++ app/src/main/java/de/westnordost/streetcomplete/screens/main/map/maplibre/OfflineManager.kt
@@ -0,0 +1,86 @@
+package de.westnordost.streetcomplete.screens.main.map.maplibre
+
+import org.maplibre.android.offline.OfflineManager
+import org.maplibre.android.offline.OfflineRegion
+import org.maplibre.android.offline.OfflineRegionDefinition
+import org.maplibre.android.offline.OfflineRegionError
+import org.maplibre.android.offline.OfflineRegionStatus
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class OfflineManagerException(message: String) : Exception(message)
+
+suspend fun OfflineManager.awaitGetOfflineRegions(): List<OfflineRegion> = suspendCoroutine { cont ->
+    listOfflineRegions(object : OfflineManager.ListOfflineRegionsCallback {
+        override fun onError(error: String) {
+            cont.resumeWithException(OfflineManagerException(error))
+        }
+
+        override fun onList(offlineRegions: Array<OfflineRegion>?) {
+            cont.resume(offlineRegions?.toList().orEmpty())
+        }
+    })
+}
+
+suspend fun OfflineManager.awaitCreateOfflineRegion(
+    definition: OfflineRegionDefinition,
+    metadata: ByteArray
+): OfflineRegion = suspendCoroutine { cont ->
+    createOfflineRegion(definition, metadata, object : OfflineManager.CreateOfflineRegionCallback {
+        override fun onCreate(offlineRegion: OfflineRegion) {
+            cont.resume(offlineRegion)
+        }
+
+        override fun onError(error: String) {
+            cont.resumeWithException(OfflineManagerException(error))
+        }
+    })
+}
+
+suspend fun OfflineManager.awaitResetDatabase(): Unit = suspendCoroutine { cont ->
+    resetDatabase(object : OfflineManager.FileSourceCallback {
+        override fun onError(message: String) {
+            cont.resumeWithException(OfflineManagerException(message))
+        }
+
+        override fun onSuccess() {
+            cont.resume(Unit)
+        }
+    })
+}
+
+suspend fun OfflineRegion.awaitDelete(): Unit = suspendCoroutine { cont ->
+    delete(object : OfflineRegion.OfflineRegionDeleteCallback {
+        override fun onDelete() {
+            cont.resume(Unit)
+        }
+
+        override fun onError(error: String) {
+            cont.resumeWithException(OfflineManagerException(error))
+        }
+    })
+}
+
+
+suspend fun OfflineRegion.awaitDownload(): OfflineRegionStatus = suspendCoroutine { cont ->
+    setObserver(object : OfflineRegion.OfflineRegionObserver {
+        override fun mapboxTileCountLimitExceeded(limit: Long) {
+            setDownloadState(OfflineRegion.STATE_INACTIVE)
+            cont.resumeWithException(OfflineManagerException("Tile count limit of $limit tiles exceeded"))
+        }
+
+        override fun onError(error: OfflineRegionError) {
+            setDownloadState(OfflineRegion.STATE_INACTIVE)
+            cont.resumeWithException(OfflineManagerException(error.message))
+        }
+
+        override fun onStatusChanged(status: OfflineRegionStatus) {
+            if (status.isComplete) {
+                setDownloadState(OfflineRegion.STATE_INACTIVE)
+                cont.resume(status)
+            }
+        }
+    })
+    setDownloadState(OfflineRegion.STATE_ACTIVE)
+}
