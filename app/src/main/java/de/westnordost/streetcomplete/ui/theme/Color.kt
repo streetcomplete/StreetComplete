c43c97bb63a3b7ce06b8cd2f46ded3802c34ccca
Tobias Zwick <newton@westnordost.de>
2024-04-18 17:43:08 +0200

    rather use other variant of defining per-theme colors
    This will only work if there is only dark theme and light theme, but nothing else is planned

--- app/src/main/java/de/westnordost/streetcomplete/ui/theme/Color.kt
+++ app/src/main/java/de/westnordost/streetcomplete/ui/theme/Color.kt
@@ -3,12 +3,9 @@ package de.westnordost.streetcomplete.ui.theme
 import androidx.compose.material.Colors
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-/* Contains colors independent of (dark/light) theme.
- *
- *  If the color (could) change between themes, define them in AppColors */
 object Color {
     /* Colors as they could be found on (illustrations of) traffic signs. */
     val TrafficRed = Color(0xffc1121c)
@@ -36,53 +33,37 @@ object Color {
     val Team11 = Color(0xff655555)
 }
 
-val LightColors = AppColors(
-    material = lightColors(
-        primary = Color(0xff4141ba),
-        primaryVariant = Color(0xff3939a3),
-        secondary = Color(0xffD14000),
-        secondaryVariant = Color(0xffF44336),
-        onPrimary = Color.White,
-        onSecondary = Color.White
-    ),
-    logVerbose = Color(0xff666666),
-    logDebug = Color(0xff2196f3),
-    logInfo = Color(0xff4caf50),
-    logWarning = Color(0xffff9800),
-    logError = Color(0xfff44336),
+val LightColors = lightColors(
+    primary = Color(0xff4141ba),
+    primaryVariant = Color(0xff3939a3),
+    secondary = Color(0xffD14000),
+    secondaryVariant = Color(0xffF44336),
+    onPrimary = Color.White,
+    onSecondary = Color.White
 )
 
-val DarkColors = AppColors(
-    material = darkColors(
-        primary = Color(0xff4141ba),
-        primaryVariant = Color(0xff3939a3),
-        secondary = Color(0xffff6600),
-        secondaryVariant = Color(0xffF44336),
-        onPrimary = Color.White,
-        onSecondary = Color.White
-    ),
-    // use lighter tones (200) for increased contrast with dark background
-    logVerbose = Color(0xff999999),
-    logDebug = Color(0xff90caf9),
-    logInfo = Color(0xffa5d6a7),
-    logWarning = Color(0xffffcc80),
-    logError = Color(0xffef9a9a),
+val DarkColors = darkColors(
+    primary = Color(0xff4141ba),
+    primaryVariant = Color(0xff3939a3),
+    secondary = Color(0xffff6600),
+    secondaryVariant = Color(0xffF44336),
+    onPrimary = Color.White,
+    onSecondary = Color.White
 )
 
-/* Colors specific to this app */
-@Immutable
-data class AppColors(
-    val material: Colors,
-    val logVerbose: Color,
-    val logDebug: Color,
-    val logInfo: Color,
-    val logWarning: Color,
-    val logError: Color,
-)
+// use lighter tones (200) for increased contrast with dark background
 
-// custom app colors are organized like suggested in
-// https://gustav-karlsson.medium.com/extending-the-jetpack-compose-material-theme-with-more-colors-e1b849390d50
-// This is slightly different than what can be found in an official example app on
-// https://github.com/android/compose-samples/tree/main/Jetsnack
-// but I am not sure whether for the latter, colors are automatically chosen by default for
-// composables.
+val Colors.logVerbose @Composable get() =
+    if (isLight) Color(0xff666666) else Color(0xff999999)
+
+val Colors.logDebug @Composable get() =
+    if (isLight) Color(0xff2196f3) else Color(0xff90caf9)
+
+val Colors.logInfo @Composable get() =
+    if (isLight) Color(0xff4caf50) else Color(0xffa5d6a7)
+
+val Colors.logWarning @Composable get() =
+    if (isLight) Color(0xffff9800) else Color(0xffffcc80)
+
+val Colors.logError @Composable get() =
+    if (isLight) Color(0xfff44336) else Color(0xffef9a9a)
--- app/src/main/java/de/westnordost/streetcomplete/ui/theme/Theme.kt
+++ app/src/main/java/de/westnordost/streetcomplete/ui/theme/Theme.kt
@@ -1,20 +1,8 @@
 package de.westnordost.streetcomplete.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Shapes
-import androidx.compose.material.Typography
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.ReadOnlyComposable
-import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun AppTheme(
@@ -22,17 +10,10 @@ fun AppTheme(
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) DarkColors else LightColors
-    CompositionLocalProvider(LocalColors provides colors) {
-        MaterialTheme(
-            colors = colors.material,
-            typography = Typography,
-            shapes = Shapes,
-            content = content
-        )
-    }
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
 }
-
-private val LocalColors = staticCompositionLocalOf { LightColors }
-
-val MaterialTheme.appColors: AppColors
-    @Composable @ReadOnlyComposable get() = LocalColors.current
