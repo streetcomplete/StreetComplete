a332c1e32b4444d6e0630b992ac719924e1c860f
Tobias Zwick <newton@westnordost.de>
2024-04-19 15:04:30 +0200

    lint

--- app/src/main/java/de/westnordost/streetcomplete/ui/user/profile/DatesActive.kt
+++ app/src/main/java/de/westnordost/streetcomplete/ui/user/profile/DatesActive.kt
@@ -2,14 +2,11 @@ package de.westnordost.streetcomplete.ui.user.profile
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
