1f4ffd46e59cd73b8190825838e9c5c4562a4872
Arthur-GYT <a.gayot@posteo.com>
2024-05-13 17:13:40 +0200

    Add Parcel Locker Brand quest
    Signed-off-by: Arthur-GYT <a.gayot@posteo.com>

--- app/src/main/java/de/westnordost/streetcomplete/quests/QuestsModule.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/QuestsModule.kt
@@ -112,6 +112,7 @@ import de.westnordost.streetcomplete.quests.oneway_suspects.data.WayTrafficFlowD
 import de.westnordost.streetcomplete.quests.opening_hours.AddOpeningHours
 import de.westnordost.streetcomplete.quests.opening_hours_signed.CheckOpeningHoursSigned
 import de.westnordost.streetcomplete.quests.orchard_produce.AddOrchardProduce
+import de.westnordost.streetcomplete.quests.parcel_locker_brand.AddParcelLockerBrand
 import de.westnordost.streetcomplete.quests.parking_access.AddBikeParkingAccess
 import de.westnordost.streetcomplete.quests.parking_access.AddParkingAccess
 import de.westnordost.streetcomplete.quests.parking_fee.AddBikeParkingFee
@@ -384,6 +385,8 @@ fun questTypeRegistry(
     72 to AddBikeParkingFee(),
     73 to AddBikeRentalCapacity(), // less ambiguous than bike parking
     74 to AddBikeParkingCapacity(), // used by cycle map layer on osm.org, OsmAnd
+
+    167 to AddParcelLockerBrand(),
 
     // address: usually only visible when just in front + sometimes requires to take "other answer"
     75 to AddHousenumber(),
