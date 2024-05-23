bfcf9b9649ab77b8d62c5f36d951b754ae962c1f
Tobias Zwick <newton@westnordost.de>
2024-05-23 21:15:53 +0200

    remove solved TODO

--- app/src/main/java/de/westnordost/streetcomplete/data/user/achievements/AchievementsModule.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/user/achievements/AchievementsModule.kt
@@ -444,7 +444,7 @@ val links = listOf(
         R.drawable.ic_link_osmlab,
         R.string.link_show_me_the_way_description
     ),
-    Link( // TODO map does not show since Nov 2023
+    Link(
         "osm-haiku",
         "https://satellitestud.io/osm-haiku/app",
         "OpenStreetMap Haiku",
