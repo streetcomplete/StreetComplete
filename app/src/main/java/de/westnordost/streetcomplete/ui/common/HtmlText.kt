5a971e90e6c1f4aa68d332fb61049d175a3f1911
Tobias Zwick <newton@westnordost.de>
2024-05-31 01:35:25 +0200

    preview more

--- app/src/main/java/de/westnordost/streetcomplete/ui/common/HtmlText.kt
+++ app/src/main/java/de/westnordost/streetcomplete/ui/common/HtmlText.kt
@@ -1,5 +1,6 @@
 package de.westnordost.streetcomplete.ui.common
 
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -7,6 +8,7 @@ import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import de.westnordost.streetcomplete.ui.util.toAnnotatedString
 import de.westnordost.streetcomplete.util.html.HtmlNode
 import de.westnordost.streetcomplete.util.html.parseHtml
@@ -60,17 +62,16 @@ fun HtmlText(
 @Preview
 @Composable
 private fun HtmlTextPreview() {
-    HtmlText("""
-    normal
+    HtmlText("""normal
     <b>bold</b>
     <i>italic</i>
     <s>strike</s>
     <u>underline</u>
-    <tt>code</tt><br>
+    <tt>code</tt>
     <sup>superspace</sup>
     <sub>subspace</sub>
     <big>big</big>
-    <small>small</small><br>
+    <small>small</small>
     <a href="url">link</a>
     <mark>mark</mark><br>
     <h1>h1</h1>
@@ -79,5 +80,12 @@ private fun HtmlTextPreview() {
     <h4>h4</h4>
     <h5>h5</h5>
     <h6>h6</h6>
-    """.trimIndent()) {}
+    <ul>
+    <li>The bullet symbol may take any of a variety of shapes such as circular, square, diamond or arrow.</li>
+    <li>Typical word processor software offers a wide selection of shapes and colors</li>
+    </ul>
+    <p>Paragraph</p>
+    <blockquote>A block quotation is a quotation in a written document that is set off from the main text as a paragraph, or block of text, and typically distinguished visually using indentation.</blockquote>
+    """,
+        modifier = Modifier.width(320.dp)) {}
 }
