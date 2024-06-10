babe2d91548c584008fffdaa4466024b9d7bff8a
jmizv <37914724+jmizv@users.noreply.github.com>
2024-06-10 09:56:00 +0200

    Optimization of elementFilter in AddAcceptsCards quest

--- app/src/main/java/de/westnordost/streetcomplete/quests/accepts_cards/AddAcceptsCards.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/accepts_cards/AddAcceptsCards.kt
@@ -20,7 +20,7 @@ class AddAcceptsCards : OsmFilterQuestType<CardAcceptance>() {
         and !payment:credit_cards and !payment:debit_cards and payment:others != no
         and !brand and !wikipedia:brand and !wikidata:brand
         and (!seasonal or seasonal = no)
-        and (name or brand or noname = yes or name:signed = no)
+        and (name or noname = yes or name:signed = no)
         and access !~ private|no
     """
     override val changesetComment = "Survey whether payment with cards is accepted"
