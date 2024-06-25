55d619f676a362e8530fba91a730d0360873bc08
Nick-Tallguy <nick.allen.54@gmail.com>
2024-06-25 14:45:38 +0100

    1st attempt at gates displaying only

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/barrier_opening/WidthAnswer.kt
@@ -0,0 +1,5 @@
+package de.westnordost.streetcomplete.quests.barrier_opening
+
+import de.westnordost.streetcomplete.osm.Length
+
+data class WidthAnswer(val width: Length, val isARMeasurement: Boolean)
