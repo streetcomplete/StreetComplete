6dba07fdd03972394ae5d2c104d291ac9336a7d7
Tobias Zwick <newton@westnordost.de>
2024-07-04 16:46:54 +0200

    add doc comment

--- app/src/main/java/de/westnordost/streetcomplete/ui/common/dialogs/ListPickerDialog.kt
+++ app/src/main/java/de/westnordost/streetcomplete/ui/common/dialogs/ListPickerDialog.kt
@@ -34,6 +34,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import de.westnordost.streetcomplete.ui.theme.AppTheme
 
+/** List picker dialog with OK and cancel button that expands to its maximum possible size in both
+ *  directions, scrollable.
+ *  (See explanation in ScrollableAlertDialog why it expands to the maximum possible size)*/
 @Composable
 fun <T> ListPickerDialog(
     onDismissRequest: () -> Unit,
