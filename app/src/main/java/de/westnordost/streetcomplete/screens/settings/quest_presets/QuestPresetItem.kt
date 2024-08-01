ba7350d962f04975d4d1a03d1f30728fd0a972d1
Tobias Zwick <newton@westnordost.de>
2024-08-01 20:59:31 +0200

    Put QuestPresetDropdownMenu into own file

--- app/src/main/java/de/westnordost/streetcomplete/screens/settings/quest_presets/QuestPresetItem.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/settings/quest_presets/QuestPresetItem.kt
@@ -1,6 +1,5 @@
 package de.westnordost.streetcomplete.screens.settings.quest_presets
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -8,9 +7,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
@@ -22,8 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -121,66 +115,6 @@ fun QuestPresetItem(
             onDismissRequest = { showShareDialog = false },
             url = item.url
         )
-    }
-}
-
-/** The dropdown menu that shows when tapping on the more button */
-@Composable
-private fun QuestPresetDropdownMenu(
-    expanded: Boolean,
-    onDismissRequest: () -> Unit,
-    onRename: () -> Unit,
-    onDuplicate: () -> Unit,
-    onShare: () -> Unit,
-    onDelete: () -> Unit,
-    isDefaultPreset: Boolean,
-    modifier: Modifier = Modifier
-) {
-    DropdownMenu(
-        expanded = expanded,
-        onDismissRequest = onDismissRequest,
-        modifier = modifier
-    ) {
-        if (!isDefaultPreset) {
-            DropdownMenuItem(onClick = { onDismissRequest(); onRename() }) {
-                TextWithIcon(
-                    text = stringResource(R.string.quest_presets_rename),
-                    painter = painterResource(R.drawable.ic_edit_24dp)
-                )
-            }
-        }
-        DropdownMenuItem(onClick = { onDismissRequest(); onDuplicate() }) {
-            TextWithIcon(
-                text = stringResource(R.string.quest_presets_duplicate),
-                painter = painterResource(R.drawable.ic_content_copy_24dp)
-            )
-        }
-        DropdownMenuItem(onClick = { onDismissRequest(); onShare() }) {
-            TextWithIcon(
-                text = stringResource(R.string.quest_presets_share),
-                painter = painterResource(R.drawable.ic_share_24dp)
-            )
-        }
-        if (!isDefaultPreset) {
-            DropdownMenuItem(onClick = { onDismissRequest(); onDelete() }) {
-                TextWithIcon(
-                    text = stringResource(R.string.quest_presets_delete),
-                    painter = painterResource(R.drawable.ic_delete_24dp)
-                )
-            }
-        }
-    }
-}
-
-@Composable
-private fun TextWithIcon(text: String, painter: Painter, modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(painter, text)
-        Text(text)
     }
 }
 
