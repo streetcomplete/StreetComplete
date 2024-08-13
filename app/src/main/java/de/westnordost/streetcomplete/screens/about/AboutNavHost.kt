fd31f5cc70919c79e75006dfadd0308620498a56
Tobias Zwick <newton@westnordost.de>
2024-08-14 00:49:49 +0200

    fix screen slide direction in about and settings screen for RTL layout

--- app/src/main/java/de/westnordost/streetcomplete/screens/about/AboutNavHost.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/about/AboutNavHost.kt
@@ -3,23 +3,26 @@ package de.westnordost.streetcomplete.screens.about
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import de.westnordost.streetcomplete.screens.about.logs.LogsScreen
+import de.westnordost.streetcomplete.ui.ktx.dir
 import org.koin.androidx.compose.koinViewModel
 import org.koin.compose.koinInject
 
 @Composable fun AboutNavHost(onClickBack: () -> Unit) {
     val navController = rememberNavController()
+    val dir = LocalLayoutDirection.current.dir
 
     NavHost(
         navController = navController,
         startDestination = AboutDestination.About,
-        enterTransition = { slideInHorizontally(initialOffsetX = { +it }) },
-        exitTransition = { slideOutHorizontally(targetOffsetX = { -it }) },
-        popEnterTransition = { slideInHorizontally(initialOffsetX = { -it }) },
-        popExitTransition = { slideOutHorizontally(targetOffsetX = { +it }) }
+        enterTransition = { slideInHorizontally(initialOffsetX = { +it * dir }) },
+        exitTransition = { slideOutHorizontally(targetOffsetX = { -it * dir }) },
+        popEnterTransition = { slideInHorizontally(initialOffsetX = { -it * dir }) },
+        popExitTransition = { slideOutHorizontally(targetOffsetX = { +it * dir }) }
     ) {
         composable(AboutDestination.About) {
             AboutScreen(
