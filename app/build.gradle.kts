5837c3f9285d1c4ee49c5b713032436d89619edf
Tobias Zwick <newton@westnordost.de>
2024-08-30 00:42:04 +0200

    remove debugging config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,10 +42,7 @@ android {
 
     buildTypes {
         all {
-            ndk {
-                debugSymbolLevel = "FULL"
-            }
-            isMinifyEnabled = false // TODO reenable
+            isMinifyEnabled = true
             isShrinkResources = false
             // don't use proguard-android-optimize.txt, it is too aggressive, it is more trouble than it is worth
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
