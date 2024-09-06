8ae245b4002111ef362c695ee461e00b3b969a11
Helium314 <helium314@mailbox.org>
2024-09-06 21:57:25 +0200

    don't crash if quest is not in questTypeRegistry
    fixes #639

--- app/src/main/java/de/westnordost/streetcomplete/screens/settings/quest_selection/QuestSelection.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/settings/quest_selection/QuestSelection.kt
@@ -9,5 +9,7 @@ import de.westnordost.streetcomplete.data.quest.QuestTypeRegistry
 
 data class QuestSelection(val questType: QuestType, var selected: Boolean, val enabledInCurrentCountry: Boolean, val prefs: Preferences) {
     fun isInteractionEnabled(questTypeRegistry: QuestTypeRegistry) = prefs.getBoolean(Prefs.EXPERT_MODE, false)
-        || (questType !is OsmNoteQuestType && questTypeRegistry.getOrdinalOf(questType)!! < EE_QUEST_OFFSET)
+        // not sure how questTypeRegistry can be empty / not contain a quest initially coming from that repository
+        // but it can happen, so just don't crash, see https://github.com/Helium314/SCEE/issues/639
+        || (questType !is OsmNoteQuestType && (questTypeRegistry.getOrdinalOf(questType) ?: EE_QUEST_OFFSET) < EE_QUEST_OFFSET)
 }
