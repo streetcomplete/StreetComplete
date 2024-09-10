c6ee10d1aa84b6e22d04a2e1e9230f6426516d72
Kai Michael Poppe <kai.michael.poppe@gmail.com>
2024-09-10 13:20:29 +0200

    Display object note for BusStopName
    Adding Bus Stop Name in the mix also.

--- app/src/main/java/de/westnordost/streetcomplete/quests/bus_stop_name/AddBusStopNameForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/bus_stop_name/AddBusStopNameForm.kt
@@ -1,6 +1,9 @@
 package de.westnordost.streetcomplete.quests.bus_stop_name
 
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
+import androidx.core.widget.doAfterTextChanged
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.databinding.QuestLocalizednameBinding
 import de.westnordost.streetcomplete.osm.LocalizedName
@@ -15,6 +18,17 @@ class AddBusStopNameForm : AAddLocalizedNameForm<BusStopNameAnswer>() {
     override val addLanguageButton get() = binding.addLanguageButton
     override val namesList get() = binding.namesList
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initStateFromTags()
+    }
+
+    private fun initStateFromTags() {
+        val objectNote = element.tags["note"]
+        if (objectNote != null) {
+            this.setHint(getString(R.string.note_for_object) + " " + objectNote)
+        }
+    }
     override val otherAnswers = listOf(
         AnswerItem(R.string.quest_placeName_no_name_answer) { confirmNoName() },
         AnswerItem(R.string.quest_streetName_answer_cantType) { showKeyboardInfo() }
