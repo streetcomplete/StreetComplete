fd73b78afc63852cb65f62a3560a465d072388b0
Tobias Zwick <newton@westnordost.de>
2024-09-18 11:10:53 +0200

    reduce rotate gesture angle threshold (#5844)

--- app/src/main/java/de/westnordost/streetcomplete/screens/main/map/MapFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/main/map/MapFragment.kt
@@ -157,6 +157,7 @@ open class MapFragment : Fragment(R.layout.fragment_map) {
         map.uiSettings.isDisableRotateWhenScaling = true
         // workaround for https://github.com/maplibre/maplibre-native/issues/2792
         map.gesturesManager.moveGestureDetector.moveThreshold = resources.dpToPx(5f)
+        map.gesturesManager.rotateGestureDetector.angleThreshold = 1.5f
 
         map.addOnMoveListener(object : MapLibreMap.OnMoveListener {
             override fun onMoveBegin(detector: MoveGestureDetector) { listener?.onPanBegin() }
