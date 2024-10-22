492d488e2b111a967fb2970eebde5a5f7696bdc4
Tobias Zwick <newton@westnordost.de>
2024-10-23 00:22:28 +0200

    redesign edit details display a bit

--- app/src/main/java/de/westnordost/streetcomplete/screens/main/edithistory/EditDetails.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/main/edithistory/EditDetails.kt
@@ -12,12 +12,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import de.westnordost.osmfeatures.FeatureDictionary
 import de.westnordost.streetcomplete.data.edithistory.Edit
 import de.westnordost.streetcomplete.data.osm.mapdata.Element
+import de.westnordost.streetcomplete.ui.util.toAnnotatedString
+import de.westnordost.streetcomplete.util.getNameAndLocationHtml
 import de.westnordost.streetcomplete.util.html.parseHtml
 import java.text.DateFormat
 
@@ -36,17 +39,22 @@ fun EditDetails(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Text(
-            text = DateFormat.getTimeInstance(DateFormat.SHORT).format(edit.createdTimestamp),
-            style = MaterialTheme.typography.body2,
-            color = LocalContentColor.current.copy(alpha = ContentAlpha.medium),
-        )
-        Row(horizontalArrangement = Arrangement.spacedBy(24.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             EditImage(
                 edit = edit,
                 modifier = Modifier.size(64.dp)
             )
             Column {
+                Text(
+                    text = DateFormat
+                        .getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT)
+                        .format(edit.createdTimestamp),
+                    style = MaterialTheme.typography.body2,
+                    color = LocalContentColor.current.copy(alpha = ContentAlpha.medium),
+                )
                 Text(
                     text = edit.getTitle(element?.tags),
                     style = MaterialTheme.typography.body1,
@@ -60,7 +68,7 @@ fun EditDetails(
                     if (nameAndLocation != null) {
                         Text(
                             text = nameAndLocation.toAnnotatedString(),
-                            style = MaterialTheme.typography.body1,
+                            style = MaterialTheme.typography.body2,
                             color = LocalContentColor.current.copy(alpha = ContentAlpha.medium),
                         )
                     }
