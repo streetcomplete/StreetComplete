147cb25a83ac7541ab40c729f032b4e8999ead7e
Tobias Zwick <osm@westnordost.de>
2024-11-18 16:24:19 +0100

    add bicycle repair station equipment quest (fixes #5910) (#6008)

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/bicycle_repair_station/BicycleRepairStationService.kt
@@ -0,0 +1,8 @@
+package de.westnordost.streetcomplete.quests.bicycle_repair_station
+
+enum class BicycleRepairStationService(val value: String) {
+    PUMP("pump"),
+    TOOLS("tools"),
+    STAND("stand"),
+    CHAIN_TOOL("chain_tool"),
+}
