49eb20355f2fbdc9da0fd962d7845f77dbfee8df
Tobias Zwick <newton@westnordost.de>
2024-11-26 21:08:16 +0100

    ask again if ambiguous rentals were specified (and remove them on answering)

--- app/src/main/java/de/westnordost/streetcomplete/quests/boat_rental/AddBoatRental.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/boat_rental/AddBoatRental.kt
@@ -12,7 +12,10 @@ class AddBoatRental : OsmFilterQuestType<List<BoatRental>>() {
     override val elementFilter = """
         nodes, ways with
         amenity = boat_rental
-        and ${BoatRental.entries.joinToString(" and ") { "!${it.osmValue}" }}
+        and (
+          ${BoatRental.entries.joinToString(" and ") { "!${it.osmValue}" }}
+          or ${DEPRECATED_RENTALS.joinToString(" or ")}
+        )
     """
     override val changesetComment = "Specify boats for rental"
     override val wikiLink = "Tag:amenity=boat_rental"
@@ -25,5 +28,13 @@ class AddBoatRental : OsmFilterQuestType<List<BoatRental>>() {
 
     override fun applyAnswerTo(answer: List<BoatRental>, tags: Tags, geometry: ElementGeometry, timestampEdited: Long) {
         answer.forEach { tags[it.osmValue] = "yes" }
+        // remove ambiguous ones that should have been specified correctly by the user's answer
+        DEPRECATED_RENTALS.forEach { tags.remove(it) }
     }
 }
+
+private val DEPRECATED_RENTALS = listOf(
+    // ambiguous:
+    // motor or rowing?        what kind of board?          rowing what?
+    "dinghy_rental", "paddleboard_rental", "board_rental", "rowing_rental"
+)
