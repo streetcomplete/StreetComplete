4b30199592827b08ba901c878e6a518638b77b03
Tobias Zwick <newton@westnordost.de>
2024-11-30 02:34:56 +0100

    main screen controls should only inset system bars (not IME)

--- app/src/main/java/de/westnordost/streetcomplete/screens/main/MainScreen.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/main/MainScreen.kt
@@ -13,12 +13,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -215,7 +217,7 @@ fun MainScreen(
 
         Column(Modifier
             .fillMaxSize()
-            .safeDrawingPadding()
+            .windowInsetsPadding(WindowInsets.systemBars)
             .onGloballyPositioned { pointerPinRects["frame"] = it.boundsInRoot() }
         ) {
             Box(Modifier
