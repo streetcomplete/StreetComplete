f61b5902b4220164f2175dbc3f7e8c8613df6dbf
Tobias Zwick <newton@westnordost.de>
2025-02-23 18:32:49 +0100

    add javadoc

--- app/src/main/java/de/westnordost/streetcomplete/osm/Feature.kt
+++ app/src/main/java/de/westnordost/streetcomplete/osm/Feature.kt
@@ -2,6 +2,8 @@ package de.westnordost.streetcomplete.osm
 
 import de.westnordost.osmfeatures.Feature
 
+/** Apply this feature to the given [tags], optionally removing a [previousFeature] first, i.e.
+ *  replacing it. */
 fun Feature.applyTo(tags: Tags, previousFeature: Feature? = null) {
     if (previousFeature != null) {
         for ((key, value) in previousFeature.removeTags) {
