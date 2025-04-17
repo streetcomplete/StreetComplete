c595dc24aed4f1dd74f4b6d9e6a83b934928345f
Tobias Zwick <newton@westnordost.de>
2025-04-17 22:42:38 +0200

    add all those new images

--- app/src/main/java/de/westnordost/streetcomplete/osm/mtb_scale/MtbScaleItem.kt
+++ app/src/main/java/de/westnordost/streetcomplete/osm/mtb_scale/MtbScaleItem.kt
@@ -9,7 +9,7 @@ import de.westnordost.streetcomplete.view.image_select.Item
 fun MtbScale.asItem(): GroupableDisplayItem<MtbScale> =
     Item(this, imageResId, titleResId, descriptionResId)
 
-private val MtbScale.titleResId: Int @StringRes get() = when (value) {
+private val MtbScale.titleResId: Int? @StringRes get() = when (value) {
     0 -> R.string.overlay_mtb_scale_0
     1 -> R.string.overlay_mtb_scale_1
     2 -> R.string.overlay_mtb_scale_2
@@ -17,10 +17,10 @@ private val MtbScale.titleResId: Int @StringRes get() = when (value) {
     4 -> R.string.overlay_mtb_scale_4
     5 -> R.string.overlay_mtb_scale_5
     6 -> R.string.overlay_mtb_scale_6
-    else -> 0
+    else -> null
 }
 
-private val MtbScale.descriptionResId: Int @StringRes get() = when (value) {
+private val MtbScale.descriptionResId: Int? @StringRes get() = when (value) {
     0 -> R.string.overlay_mtb_scale_0_description
     1 -> R.string.overlay_mtb_scale_1_description
     2 -> R.string.overlay_mtb_scale_2_description
@@ -28,16 +28,16 @@ private val MtbScale.descriptionResId: Int @StringRes get() = when (value) {
     4 -> R.string.overlay_mtb_scale_4_description
     5 -> R.string.overlay_mtb_scale_5_description
     6 -> R.string.overlay_mtb_scale_6_description
-    else -> 0
+    else -> null
 }
 
-private val MtbScale.imageResId: Int @DrawableRes get() = when (value) {
+private val MtbScale.imageResId: Int? @DrawableRes get() = when (value) {
     0 -> R.drawable.mtb_scale_0
     1 -> R.drawable.mtb_scale_1
     2 -> R.drawable.mtb_scale_2
     3 -> R.drawable.mtb_scale_3
     4 -> R.drawable.mtb_scale_4
     5 -> R.drawable.mtb_scale_5
-    6 -> R.drawable.mtb_scale_6
-    else -> 0
+    6 -> null
+    else -> null
 }
