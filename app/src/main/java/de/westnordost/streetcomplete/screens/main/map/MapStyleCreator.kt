a7b4eccfb2abc43c2ec7dcd5a11ce2f7f24d297f
Helium314 <helium314@mailbox.org>
2025-04-28 15:34:08 +0200

    update map style creator to match SC style

--- app/src/main/java/de/westnordost/streetcomplete/screens/main/map/MapStyleCreator.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/main/map/MapStyleCreator.kt
@@ -393,9 +393,9 @@ fun createMapStyle(name: String, accessToken: String, languages: List<String>, c
             minZoom = 5.0,
             paint = Fill(color = colors.green, opacity = byZoom(5.0 to 0.0, 6.0 to 1.0))
         ),
-        Layer("landuse-pitch-outline",
+        Layer("landuse-pitch-park-outline",
             src = "landuse",
-            filter = listOf(tagIs("class", "pitch")),
+            filter = listOf(tagIn("class", "pitch", "park")),
             minZoom = 16.0,
             paint = Line(
                 color = colors.earth,
@@ -575,7 +575,7 @@ fun createMapStyle(name: String, accessToken: String, languages: List<String>, c
 
         Layer("labels-housenumbers",
             src = "housenum_label",
-            minZoom = 18.0,
+            minZoom = 17.0,
             paint = defaultTextStyle.copy(text = "[\"get\", \"house_num\"]")
         ),
 
