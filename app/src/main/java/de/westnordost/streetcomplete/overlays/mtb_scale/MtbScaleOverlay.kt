fc33c38a9e3eda3b7aca25282a21c0ad66924cd5
Tobias Zwick <newton@westnordost.de>
2025-05-03 22:55:30 +0200

    don't allow editing of way where mtb == no

--- app/src/main/java/de/westnordost/streetcomplete/overlays/mtb_scale/MtbScaleOverlay.kt
+++ app/src/main/java/de/westnordost/streetcomplete/overlays/mtb_scale/MtbScaleOverlay.kt
@@ -33,6 +33,7 @@ class MtbScaleOverlay : Overlay {
                 or foot ~ yes|permissive|designated
                 or bicycle ~ yes|permissive|designated
               )
+              and mtb != no
               and (
                 surface ~ ${UNPAVED_SURFACES.joinToString("|")}|wood
                 or (highway = track and tracktype and tracktype != grade1)
