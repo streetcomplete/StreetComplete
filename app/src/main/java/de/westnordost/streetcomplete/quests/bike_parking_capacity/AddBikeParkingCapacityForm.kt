8074146b61358cd111dc43636e555aa889938096
Ercalvez <45692523+Ercalvez@users.noreply.github.com>
2025-05-22 00:16:36 +0200

    Migrate BikeParkingCapacityForm to Compose

--- app/src/main/java/de/westnordost/streetcomplete/quests/bike_parking_capacity/AddBikeParkingCapacityForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/bike_parking_capacity/AddBikeParkingCapacityForm.kt
@@ -2,10 +2,14 @@ package de.westnordost.streetcomplete.quests.bike_parking_capacity
 
 import android.os.Bundle
 import android.view.View
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.core.widget.doAfterTextChanged
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.databinding.QuestBikeParkingCapacityBinding
 import de.westnordost.streetcomplete.quests.AbstractOsmQuestForm
+import de.westnordost.streetcomplete.quests.step_count.StepCountForm
 import de.westnordost.streetcomplete.util.ktx.intOrNull
 
 class AddBikeParkingCapacityForm : AbstractOsmQuestForm<Int>() {
@@ -13,16 +17,25 @@ class AddBikeParkingCapacityForm : AbstractOsmQuestForm<Int>() {
     override val contentLayoutResId = R.layout.quest_bike_parking_capacity
     private val binding by contentViewBinding(QuestBikeParkingCapacityBinding::bind)
 
-    private val capacity get() = binding.capacityInput.intOrNull ?: 0
+    private lateinit var capacity: MutableState<Int>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.capacityInput.doAfterTextChanged { checkIsFormComplete() }
+        binding.questBikeParkingCapacityBase.setContent {
+            capacity = rememberSaveable { mutableIntStateOf(element.tags["capacity"]?.toIntOrNull() ?: 0) }
+            BikeParkingCapacityForm(
+                count = capacity.value,
+                onCountChange = {
+                    capacity.value = it
+                    checkIsFormComplete()
+                }
+            )
+        }
     }
 
-    override fun isFormComplete() = capacity > 0
+    override fun isFormComplete() = capacity.value > 0
 
     override fun onClickOk() {
-        applyAnswer(capacity)
+        applyAnswer(capacity.value)
     }
 }
