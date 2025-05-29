83fee3a7c9fad31dd538d9fe8cc5f25a082656cf
Tobias Zwick <newton@westnordost.de>
2025-05-29 14:54:14 +0200

    make shorter

--- app/src/iosMain/kotlin/de/westnordost/streetcomplete/ui/ktx/Res.ios.kt
+++ app/src/iosMain/kotlin/de/westnordost/streetcomplete/ui/ktx/Res.ios.kt
@@ -4,8 +4,7 @@ import de.westnordost.stretcomplete.resources.Res
 import platform.Foundation.NSBundle
 import platform.Foundation.NSFileManager
 
-actual fun Res.exists(path: String): Boolean {
-    val fileManager = NSFileManager.defaultManager
-    val path = NSBundle.mainBundle.resourcePath + "/compose-resources/" + path
-    return fileManager.fileExistsAtPath(path)
-}
+actual fun Res.exists(path: String): Boolean =
+    NSFileManager.defaultManager.fileExistsAtPath(
+        NSBundle.mainBundle.resourcePath + "/compose-resources/" + path
+    )
