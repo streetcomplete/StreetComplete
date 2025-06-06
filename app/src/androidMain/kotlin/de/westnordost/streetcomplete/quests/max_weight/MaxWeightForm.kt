f85f0064fe29ccaeb60492277a7d53443c88926c
Katrina Knight <kat.knight@newgaea.net>
2025-06-06 02:33:33 +0100

    feat: initial Form

---
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_weight/MaxWeightForm.kt
@@ -0,0 +1,7 @@
+package de.westnordost.streetcomplete.quests.max_weight
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MaxWeightForm(modifier: Modifier = Modifier) { }
