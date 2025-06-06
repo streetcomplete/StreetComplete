c951535ba772a5b35e20c5478a8adb42f8d116ac
Mateusz Konieczny <matkoniecz@gmail.com>
2025-06-06 19:21:54 +0200

    +

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/data/atp/atpquests/AtpQuestModule.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/data/atp/atpquests/AtpQuestModule.kt
@@ -1,7 +1,5 @@
 package de.westnordost.streetcomplete.data.atp.atpquests
 
-import de.westnordost.streetcomplete.data.atp.atpquests.AtpQuestController
-import de.westnordost.streetcomplete.data.osm.osmquests.OsmQuestsHiddenDao
 import org.koin.dsl.module
 
 val atpQuestModule = module {
