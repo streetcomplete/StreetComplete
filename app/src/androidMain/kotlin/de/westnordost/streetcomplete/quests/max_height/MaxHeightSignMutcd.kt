85393454a31ed0827038a0d92163df4ae6f1c513
Tobias Zwick <newton@westnordost.de>
2025-06-09 01:05:25 +0200

    fix layout for maxheight sign mutcd

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_height/MaxHeightSignMutcd.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_height/MaxHeightSignMutcd.kt
@@ -1,25 +1,19 @@
 package de.westnordost.streetcomplete.quests.max_height
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import de.westnordost.streetcomplete.data.meta.LengthUnit
-import de.westnordost.streetcomplete.ui.common.FootInchAppearance
-import de.westnordost.streetcomplete.ui.common.LengthInput
+import de.westnordost.streetcomplete.ui.theme.TrafficBlack
 import de.westnordost.streetcomplete.ui.theme.TrafficYellow
 
 @Composable
@@ -27,43 +21,17 @@ fun MaxHeightSignMutcd(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
-    CompositionLocalProvider(LocalTextStyle provides MaterialTheme.typography.h4) {
-        Box(
-            modifier = modifier
-                .width(IntrinsicSize.Max)
-                .height(IntrinsicSize.Min)
-        ) {
-            Surface(
-                color = TrafficYellow,
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .fillMaxWidth()
-                    .border(
-                        width = 2.dp,
-                        color = Color.Black,
-                        shape = RoundedCornerShape(10.dp)
-                    )
-            ) {
-                content()
-            }
-        }
-    }
-}
-
-@Composable
-@Preview(showBackground = true)
-fun MaxHeightSignMutcdPreview() {
-    CompositionLocalProvider(LocalTextStyle provides MaterialTheme.typography.h4) {
-        MaxHeightSignMutcd() {
-            LengthInput(
-                selectedUnit = LengthUnit.FOOT_AND_INCH,
-                currentLength = null,
-                syncLength = false,
-                onLengthChanged = {},
-                maxFeetDigits = 2,
-                maxMeterDigits = Pair(2, 2),
-                footInchAppearance = FootInchAppearance.UPPERCASE_ABBREVIATION
-            )
+    Box(
+        modifier = modifier
+            .width(IntrinsicSize.Max)
+            .height(IntrinsicSize.Min)
+            .background(TrafficYellow, RoundedCornerShape(10.dp))
+            .padding(4.dp)
+            .border(4.dp, TrafficBlack, RoundedCornerShape(6.dp))
+            .padding(horizontal = 12.dp, vertical = 16.dp)
+    ) {
+        CompositionLocalProvider(LocalContentColor provides TrafficBlack) {
+            content()
         }
     }
 }
