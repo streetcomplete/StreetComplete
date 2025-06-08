c3699222c3fcfcda5556684ed236ee1b46fb78ab
Tobias Zwick <newton@westnordost.de>
2025-06-09 01:08:25 +0200

    remove unused/unnecessary modifiers

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_height/MaxHeightSignMutcd.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_height/MaxHeightSignMutcd.kt
@@ -3,10 +3,7 @@ package de.westnordost.streetcomplete.quests.max_height
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.LocalContentColor
 import androidx.compose.runtime.Composable
@@ -23,8 +20,6 @@ fun MaxHeightSignMutcd(
 ) {
     Box(
         modifier = modifier
-            .width(IntrinsicSize.Max)
-            .height(IntrinsicSize.Min)
             .background(TrafficYellow, RoundedCornerShape(10.dp))
             .padding(4.dp)
             .border(4.dp, TrafficBlack, RoundedCornerShape(6.dp))
