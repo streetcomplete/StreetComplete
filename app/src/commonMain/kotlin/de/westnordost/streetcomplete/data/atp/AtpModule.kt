e53c17dfb24d15298bb821dad0d9d8df295f6d2d
Mateusz Konieczny <matkoniecz@gmail.com>
2025-06-24 12:18:55 +0200

    update TODO

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/atp/AtpModule.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/atp/AtpModule.kt
@@ -10,11 +10,7 @@ import org.koin.dsl.module
 val atpModule = module {
     factory { AtpDao(get()) }
     factory { AtpDownloader(get(), get()) }
-    // TODO: connect to actual global API
-    // current holders:
-    // https://gist.github.com/matkoniecz/163c0bca9d03efc33d744f6091c91904
-    // https://codeberg.org/matkoniecz/experimental_read_only_api_for_atp_osm_work
-    // https://matkoniecz.codeberg.page/improving_openstreetmap_using_alltheplaces_dataset/read_only_api_prototype/lat_50/lon_20_gathered.geojson
+    // TODO: connect to actual bidirectional API
     val OSM_ATP_COMPARISON_API_BASE_URL = "https://matkoniecz.codeberg.page/improving_openstreetmap_using_alltheplaces_dataset/read_only_api_prototype/"
     factory { AtpApiClient(get(), OSM_ATP_COMPARISON_API_BASE_URL, get()) }
     factory { AtpApiParser() }
