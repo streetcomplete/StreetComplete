9436302cb567015b3666151b256604df4e34a67a
Kai Michael Poppe <kai@poppe.dev>
2025-06-24 05:57:59 +0200

    Update HighwayUtils.kt
    Update naming as per discussion

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/HighwayUtils.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/HighwayUtils.kt
@@ -1,22 +1,24 @@
 package de.westnordost.streetcomplete.osm
 
-val MOTORWAYS = setOf("motorway", "motorway_link")
-val TRUNKS    = setOf("trunk", "trunk_link")
-val PRIMARY   = setOf("primary", "primary_link")
-val SECONDARY = setOf("secondary", "secondary_link")
-val TERTIARY  = setOf("tertiary", "tertiary_link")
+val MOTORWAYS   = setOf("motorway", "motorway_link")
+val TRUNKS      = setOf("trunk", "trunk_link")
+val HIGHWAYS    = MOTORWAYS + TRUNKS
 
-val CLASSIFIED_ROADS = PRIMARY + SECONDARY + TERTIARY
-val PEDESTRIAN_ROADS = setOf("residential", "living_street", "pedestrian")
-val OTHER_ROADS      = setOf("unclassified", "service", "track", "busway", "road")
+val PRIMARY     = setOf("primary", "primary_link")
+val SECONDARY   = setOf("secondary", "secondary_link")
+val TERTIARY    = setOf("tertiary", "tertiary_link")
+val MAJOR_ROADS = PRIMARY + SECONDARY + TERTIARY
 
-val ALL_ROADS = MOTORWAYS + TRUNKS + CLASSIFIED_ROADS +
-                PEDESTRIAN_ROADS + OTHER_ROADS
+val LOCAL_ACCESS_ROADS    = setOf("residential", "living_street")
+val PEDESTRIAN_ONLY_ROADS = setOf("pedestrian")
+
+val OTHER_ROADS = setOf("unclassified", "service", "track", "busway", "road")
+
+val ALL_ROADS = HIGHWAYS + MAJOR_ROADS + LOCAL_ACCESS_ROADS +
+                PEDESTRIAN_ONLY_ROADS + OTHER_ROADS
 
 val ALL_PATHS = setOf(
     "footway", "cycleway", "path", "bridleway", "steps"
 )
 
-val ROADS_ASSUMED_TO_BE_PAVED = arrayOf(
-    "trunk", "trunk_link", "motorway", "motorway_link"
-)
+val ROADS_ASSUMED_TO_BE_PAVED = HIGHWAYS
