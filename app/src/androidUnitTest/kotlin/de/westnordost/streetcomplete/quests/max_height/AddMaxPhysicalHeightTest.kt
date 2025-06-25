c745b724bffd19867839f42321583dd1929936fb
Kai Michael Poppe <kmpoppe@users.noreply.github.com>
2025-06-25 12:02:55 +0200

    MaxPhysicalHeight: Clarify wording, don't ask for highways (fixes #6362, fixes #6363)

--- app/src/androidUnitTest/kotlin/de/westnordost/streetcomplete/quests/max_height/AddMaxPhysicalHeightTest.kt
+++ app/src/androidUnitTest/kotlin/de/westnordost/streetcomplete/quests/max_height/AddMaxPhysicalHeightTest.kt
@@ -12,54 +12,73 @@ class AddMaxPhysicalHeightTest {
 
     private val questType = AddMaxPhysicalHeight(mock())
 
+    private val MAXHEIGHT_BELOW_DEFAULT    = mapOf("maxheight" to "below_default")
+    private val MAXHEIGHT_DEFAULT          = mapOf("maxheight" to "default")
+    private val MAXHEIGHT_NUMBER_VALUE     = mapOf("maxheight" to "3")
+    private val MAXHEIGHT_PHY_NUMBER_VALUE = mapOf("maxheight:physical" to "3")
+    private val MAXHEIGHT_SIGN_NO          = mapOf("maxheight:signed" to "no")
+    private val MAXHEIGHT_SOURCE_EST       = mapOf("source:maxheight" to "estimation")
+
     @Test fun `applicable if maxheight is below default`() {
-        assertTrue(isApplicableTo(mapOf("maxheight" to "below_default")))
+        assertTrue(isApplicableToHeightRestrictor(MAXHEIGHT_BELOW_DEFAULT))
+        assertTrue(isApplicableToRoad(MAXHEIGHT_BELOW_DEFAULT))
     }
 
     @Test fun `applicable if maxheight is only estimated`() {
-        assertTrue(isApplicableTo(mapOf(
-            "maxheight" to "3",
-            "source:maxheight" to "estimation"
-        )))
+        assertTrue(isApplicableToHeightRestrictor(MAXHEIGHT_NUMBER_VALUE + MAXHEIGHT_SOURCE_EST))
+        assertTrue(isApplicableToRoad(MAXHEIGHT_NUMBER_VALUE + MAXHEIGHT_SOURCE_EST))
     }
 
     @Test fun `not applicable if maxheight is only estimated but default`() {
-        assertFalse(isApplicableTo(mapOf(
-            "maxheight" to "default",
-            "source:maxheight" to "estimation"
-        )))
+        assertFalse(isApplicableToHeightRestrictor(MAXHEIGHT_DEFAULT + MAXHEIGHT_SOURCE_EST))
+        assertFalse(isApplicableToRoad(MAXHEIGHT_DEFAULT + MAXHEIGHT_SOURCE_EST))
     }
 
     @Test fun `applicable if maxheight is not signed`() {
-        assertTrue(isApplicableTo(mapOf("maxheight:signed" to "no")))
+        assertTrue(isApplicableToHeightRestrictor(MAXHEIGHT_SIGN_NO))
+        assertTrue(isApplicableToRoad(MAXHEIGHT_SIGN_NO))
     }
 
     @Test fun `not applicable if maxheight is not signed but maxheight is defined`() {
-        assertFalse(isApplicableTo(mapOf(
-            "maxheight:signed" to "no",
-            "maxheight" to "3"
-        )))
+        assertFalse(isApplicableToHeightRestrictor(MAXHEIGHT_SIGN_NO + MAXHEIGHT_NUMBER_VALUE))
+        assertFalse(isApplicableToRoad(MAXHEIGHT_SIGN_NO + MAXHEIGHT_NUMBER_VALUE))
     }
 
     @Test fun `not applicable if maxheight is default`() {
-        assertFalse(isApplicableTo(mapOf("maxheight" to "default")))
+        assertFalse(isApplicableToHeightRestrictor(MAXHEIGHT_DEFAULT))
+        assertFalse(isApplicableToRoad(MAXHEIGHT_DEFAULT))
     }
 
     @Test fun `not applicable if physical maxheight is already defined`() {
-        assertFalse(isApplicableTo(mapOf("maxheight:physical" to "3")))
+        assertFalse(isApplicableToHeightRestrictor(MAXHEIGHT_PHY_NUMBER_VALUE))
+        assertFalse(isApplicableToRoad(MAXHEIGHT_PHY_NUMBER_VALUE))
     }
 
-    private fun isApplicableTo(tags: Map<String, String>): Boolean {
-        // since the node and way filter is so similar, it makes sense to always test both
-        val nodes = questType.isApplicableTo(node(
+    @Test fun `not applicable when on motorroad`() {
+        assertTrue(isApplicableToHeightRestrictor(MAXHEIGHT_SIGN_NO))
+        assertFalse(isApplicableToRoad(mapOf("motorroad" to "yes")))
+    }
+    @Test fun `not applicable when on motorway`() {
+        assertTrue(isApplicableToHeightRestrictor(MAXHEIGHT_SIGN_NO))
+        assertFalse(isApplicableToRoad(mapOf("motorway" to "yes")))
+    }
+    @Test fun `not applicable when on expressway`() {
+        assertTrue(isApplicableToHeightRestrictor(MAXHEIGHT_SIGN_NO))
+        assertFalse(isApplicableToRoad(mapOf("expressway" to "yes")))
+    }
+    @Test fun `not applicable when on motorway_link`() {
+        assertTrue(isApplicableToHeightRestrictor(MAXHEIGHT_SIGN_NO))
+        assertFalse(isApplicableToRoad(mapOf("highway" to "motorway_link")))
+    }
+
+    private fun isApplicableToHeightRestrictor(tags: Map<String, String>): Boolean =
+        questType.isApplicableTo(node(
             tags = mapOf("barrier" to "height_restrictor") + tags
         ))
-        val ways = questType.isApplicableTo(way(
+
+    private fun isApplicableToRoad(tags: Map<String, String>): Boolean =
+        questType.isApplicableTo(way(
             tags = mapOf("highway" to "service") + tags
         ))
-        if (nodes != ways) {
-            fail("Result of isApplicableTo is not the same for nodes and ways")
-        }
-        return nodes
-    }
+
 }
