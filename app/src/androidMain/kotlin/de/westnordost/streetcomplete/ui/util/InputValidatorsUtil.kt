5096c485adc6a8663a79085bd90aa2eacde751ed
Tobias Zwick <newton@westnordost.de>
2025-06-26 20:21:19 +0200

    improve validators

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/ui/util/InputValidatorsUtil.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/ui/util/InputValidatorsUtil.kt
@@ -1,22 +1,31 @@
 package de.westnordost.streetcomplete.ui.util
 
-/** Only accept inputs that are numbers with the max number of digits before and after the decimal point */
-fun onlyDecimalDigits(string: String, beforeDecimalPoint: Int, afterDecimalPoint: Int): Boolean {
-    if (!string.all { it.isDigit() || it == '.' || it == ',' }) return false;
-    val texts = string.split(',', '.')
+/** Check if the [string] is a(n optional) decimal number with the specified number of
+ *  [maxIntegerDigits] and [maxFractionDigits]. */
+fun isOnlyDecimalDigits(
+    string: String,
+    decimalSeparator: Char,
+    maxIntegerDigits: Int,
+    maxFractionDigits: Int
+): Boolean {
+    if (!string.all { it.isDigit() || it == decimalSeparator }) return false
+    val texts = string.split(decimalSeparator)
     if (texts.size > 2 || texts.isEmpty()) return false
-    val before = texts[0]
-    val after = if (texts.size > 1) texts[1] else ""
-    return string.toDoubleOrNull() != null && after.length <= afterDecimalPoint && before.length <= beforeDecimalPoint
+    if (texts[0].length > maxIntegerDigits) return false
+    if (texts.size > 1 && texts[1].length > maxFractionDigits) return false
+    return true
 }
 
-/** Check if the input is a valid feet input with a maximum of maxFeetDigits digits */
-fun validFeetInput(string: String, maxFeetDigits: Int): Boolean {
-    return string.toIntOrNull() != null && string.length <= maxFeetDigits
+/** Check if the [string] is a valid feet input with a maximum of [maxFeetDigits] digits */
+fun isValidFeetInput(string: String, maxFeetDigits: Int): Boolean {
+    if (!string.all { it.isDigit() }) return false
+    if (string.length > maxFeetDigits) return false
+    return true
 }
 
-/** Check if the input is a valid inch input */
-fun validInchInput(string: String): Boolean {
-    val value = string.toIntOrNull()
-    return value != null && value >= 0 && value < 12
+/** Check if the [string] is a valid inch input */
+fun isValidInchesInput(string: String): Boolean {
+    if (!string.all { it.isDigit() }) return false
+    val value = string.toInt()
+    return value >= 0 && value < 12
 }
