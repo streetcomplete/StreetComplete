090a7b0ff7e9d72d09986fda0ba396400a157483
paukli <paulkliemann@posteo.net>
2025-06-28 02:14:55 +0200

    added unit test to ferry quests

--- app/src/androidUnitTest/kotlin/de/westnordost/streetcomplete/quests/ferry/AddFerryAccessTest.kt
+++ app/src/androidUnitTest/kotlin/de/westnordost/streetcomplete/quests/ferry/AddFerryAccessTest.kt
@@ -2,7 +2,6 @@ package de.westnordost.streetcomplete.quests.ferry
 
 import de.westnordost.streetcomplete.data.osm.mapdata.ElementType
 import de.westnordost.streetcomplete.quests.TestMapDataWithGeometry
-import de.westnordost.streetcomplete.quests.ferry.AddFerryAccessPedestrian
 import de.westnordost.streetcomplete.testutils.member
 import de.westnordost.streetcomplete.testutils.node
 import de.westnordost.streetcomplete.testutils.rel
@@ -12,10 +11,12 @@ import kotlin.test.assertEquals
 
 class AddFerryAccessTest {
 
-    private val questType = AddFerryAccessPedestrian()
+    private val pedestrianQuest = AddFerryAccessPedestrian()
+    private val motorQuest = AddFerryAccessMotorVehicle()
 
-    private fun generalTestData(): TestMapDataWithGeometry =
-        TestMapDataWithGeometry(
+    @Test
+    fun `test with one relation and one unassigned way`() {
+        val mapData = TestMapDataWithGeometry(
             listOf(
                 node(1),
                 node(2),
@@ -24,18 +25,61 @@ class AddFerryAccessTest {
                 way(5L, listOf(1, 2), mapOf(
                     "route" to "ferry",
                 )),
-                way(6L, listOf(3, 4), mapOf(
+                rel(6L, listOf(member(ElementType.WAY, 5L)), mapOf(
                     "route" to "ferry",
+                    "type" to "route"
                 )),
-                rel(7L, listOf(member(ElementType.WAY, 5L)), mapOf(
+                way(7L, listOf(3, 4), mapOf(
                     "route" to "ferry",
                 )),
             ),
         )
+        assertEquals(2, pedestrianQuest.getApplicableElements(mapData).toList().size)
+        assertEquals(2, motorQuest.getApplicableElements(mapData).toList().size)
+    }
 
     @Test
-    fun `applicable to cases where corridor through building is mapped`() {
-        val mapData = generalTestData()
-        assertEquals(4, questType.getApplicableElements(mapData).toList().size)
+    fun `test with one relation and no unassigned ways`() {
+        val mapData = TestMapDataWithGeometry(
+            listOf(
+                node(1),
+                node(2),
+                node(3),
+                node(4),
+                way(5L, listOf(1, 2), mapOf(
+                    "route" to "ferry",
+                )),
+                way(7L, listOf(3, 4), mapOf(
+                    "route" to "ferry",
+                )),
+                rel(6L, listOf(member(ElementType.WAY, 5L), member(ElementType.WAY, 7L)), mapOf(
+                    "route" to "ferry",
+                    "type" to "route"
+                )),
+            ),
+        )
+        assertEquals(1, pedestrianQuest.getApplicableElements(mapData).toList().size)
+        assertEquals(1, motorQuest.getApplicableElements(mapData).toList().size)
+    }
+
+    @Test
+    fun `test with two unassigned ways`() {
+        val mapData = TestMapDataWithGeometry(
+            listOf(
+                node(1),
+                node(2),
+                node(3),
+                node(4),
+                way(5L, listOf(1, 2), mapOf(
+                    "route" to "ferry",
+                )),
+                way(6L, listOf(2, 3)),
+                way(7L, listOf(3, 4), mapOf(
+                    "route" to "ferry",
+                )),
+            ),
+        )
+        assertEquals(2, pedestrianQuest.getApplicableElements(mapData).toList().size)
+        assertEquals(2, motorQuest.getApplicableElements(mapData).toList().size)
     }
 }
