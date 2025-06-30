28e42c6b982d358b3db1c279cc44f794b08d9474
Tobias Zwick <newton@westnordost.de>
2025-06-30 23:13:48 +0200

    remove input

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/ui/common/input/DecimalInput.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/ui/common/input/DecimalInput.kt
@@ -14,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
