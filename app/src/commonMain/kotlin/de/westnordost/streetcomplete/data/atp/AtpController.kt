cbef44ea1240c8277376338d960700e45d95c43c
Mateusz Konieczny <matkoniecz@gmail.com>
2025-07-09 19:49:28 +0200

    not notes

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/atp/AtpController.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/atp/AtpController.kt
@@ -7,20 +7,18 @@ import de.westnordost.streetcomplete.util.ktx.format
 import de.westnordost.streetcomplete.util.ktx.nowAsEpochMilliseconds
 import de.westnordost.streetcomplete.util.logs.Log
 
-/** Manages access to the notes storage */
+/** Manages access to the ATP data storage */
 class AtpController(
     private val dao: AtpDao
 ) {
-    // TODO rework into ATP version
-
     /* Must be a singleton because there is a listener that should respond to a change in the
      * database table */
 
-    /** Interface to be notified of new notes, updated notes and notes that have been deleted */
+    /** Interface to be notified of new atp entries, updated atp entries and atp entries that have been deleted */
     interface Listener {
         /** called when a number of ATP entries has been added, updated or deleted */
         fun onUpdated(added: Collection<AtpEntry>, updated: Collection<AtpEntry>, deleted: Collection<Long>)
-        /** called when all notes have been cleared */
+        /** called when all atp entries have been cleared */
         fun onCleared()
     }
     private val listeners = Listeners<Listener>()
@@ -75,7 +73,7 @@ class AtpController(
             deletedCount = dao.deleteAll(ids)
         }
 
-        Log.i(TAG, "Deleted $deletedCount old notes")
+        Log.i(TAG, "Deleted $deletedCount old atp entries")
 
         this@AtpController.onUpdated(deleted = ids)
 
@@ -89,7 +87,7 @@ class AtpController(
 
     fun getAllPositions(bbox: BoundingBox): List<LatLon> = dao.getAllPositions(bbox)
     fun getAll(bbox: BoundingBox): List<AtpEntry> = dao.getAll(bbox)
-    fun getAll(noteIds: Collection<Long>): List<AtpEntry> = dao.getAll(noteIds)
+    fun getAll(atpEntries: Collection<Long>): List<AtpEntry> = dao.getAll(atpEntries)
 
     /* ------------------------------------ Listeners ------------------------------------------- */
 
