293645cfa8f0d401f3534bd541cca65b3e39e9d7
Tobias Zwick <newton@westnordost.de>
2025-07-23 12:43:51 +0200

    make SelectButton more flexible

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/ui/common/SelectButton.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/ui/common/SelectButton.kt
@@ -1,16 +1,19 @@
 package de.westnordost.streetcomplete.ui.common
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonElevation
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,6 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
@@ -39,32 +44,56 @@ import kotlin.collections.forEach
 @Composable
 fun <T> SelectButton(
     items: List<T>,
-    selectedItem: T,
     onSelectedItem: (T) -> Unit,
     modifier: Modifier = Modifier,
+    selectedItem: T? = null,
+    style: ButtonStyle = ButtonStyle.Default,
     enabled: Boolean = true,
     interactionSource: MutableInteractionSource? = null,
-    elevation: ButtonElevation? = null,
+    elevation: ButtonElevation? = style.elevation,
+    shape: Shape = style.shape,
+    border: BorderStroke? = style.border,
+    colors: ButtonColors = style.buttonColors,
+    contentPadding: PaddingValues = style.contentPadding,
+    showDropDownArrow: Boolean = true,
     itemContent: @Composable (RowScope.(item: T) -> Unit),
+    content: @Composable (RowScope.() -> Unit) = { selectedItem?.let { itemContent(it) } }
 ) {
     var expanded by remember { mutableStateOf(false) }
 
+    val revisedContentPadding = if (showDropDownArrow) {
+        val layoutDirection = LocalLayoutDirection.current
+        PaddingValues(
+            start = contentPadding.calculateStartPadding(layoutDirection),
+            top = contentPadding.calculateTopPadding(),
+            end = contentPadding.calculateEndPadding(layoutDirection) - 8.dp,
+            bottom = contentPadding.calculateBottomPadding(),
+        )
+    } else {
+        contentPadding
+    }
+
     Box(modifier = modifier) {
-        OutlinedButton(
+        Button2(
             onClick = { expanded = !expanded },
-            contentPadding = PaddingValues(start = 16.dp, top = 8.dp, end = 8.dp, bottom = 8.dp),
             enabled = enabled,
             interactionSource = interactionSource,
             elevation = elevation,
+            shape = shape,
+            border = border,
+            colors = colors,
+            contentPadding = revisedContentPadding,
         ) {
-            itemContent(selectedItem)
-            Icon(
-                painter = painterResource(Res.drawable.ic_arrow_drop_down_24),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(start = 8.dp)
-                    .rotate(if (expanded) 180f else 0f)
-            )
+            content()
+            if (showDropDownArrow) {
+                Icon(
+                    painter = painterResource(Res.drawable.ic_arrow_drop_down_24),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .rotate(if (expanded) 180f else 0f)
+                )
+            }
         }
         DropdownMenu(
             expanded = expanded,
@@ -95,5 +124,6 @@ private fun LengthInputSelectorPreview() {
         items = words,
         selectedItem = selected,
         onSelectedItem = { selected = it },
-    ) { Text(it) }
+        itemContent = { Text(it) }
+    )
 }
