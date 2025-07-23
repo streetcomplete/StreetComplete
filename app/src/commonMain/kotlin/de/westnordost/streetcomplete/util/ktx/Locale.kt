ad070846e0286268c35fb5310f77465136600364
Tobias Zwick <newton@westnordost.de>
2025-07-23 23:19:27 +0200

    rename Locale name functions

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/util/ktx/Locale.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/util/ktx/Locale.kt
@@ -3,25 +3,25 @@ package de.westnordost.streetcomplete.util.ktx
 import androidx.compose.ui.text.intl.Locale
 
 /** Human-readable locale name in current locale. Returns `null` if no name is available. */
-val Locale.name: String? get() = getName(Locale.current)
+val Locale.displayName: String? get() = getDisplayName(Locale.current)
 
 /** Human-readable language name in current locale. Returns `null` if no name is available. */
-val Locale.languageName: String? get() = getLanguageName(Locale.current)
+val Locale.displayLanguage: String? get() = getDisplayLanguage(Locale.current)
 
-/** Human-readable country name in current locale. Returns `null` if no name is available. */
-val Locale.countryName: String? get() = getRegionName(Locale.current)
+/** Human-readable region name in current locale. Returns `null` if no name is available. */
+val Locale.displayRegion: String? get() = getDisplayRegion(Locale.current)
 
 /** Human-readable script name in current locale. Returns `null` if no name is available. */
-val Locale.scriptName: String? get() = getScriptName(Locale.current)
+val Locale.displayScript: String? get() = getDisplayScript(Locale.current)
 
 /** Human-readable locale name in given [locale]. Returns `null` if no name is available. */
-expect fun Locale.getName(locale: Locale): String?
+expect fun Locale.getDisplayName(locale: Locale): String?
 
 /** Human-readable language name in given [locale]. Returns `null` if no name is available. */
-expect fun Locale.getLanguageName(locale: Locale): String?
+expect fun Locale.getDisplayLanguage(locale: Locale): String?
 
-/** Human-readable country name in given [locale]. Returns `null` if no name is available. */
-expect fun Locale.getRegionName(locale: Locale): String?
+/** Human-readable region name in given [locale]. Returns `null` if no name is available. */
+expect fun Locale.getDisplayRegion(locale: Locale): String?
 
 /** Human-readable script name in given [locale]. Returns `null` if no name is available. */
-expect fun Locale.getScriptName(locale: Locale): String?
+expect fun Locale.getDisplayScript(locale: Locale): String?
