02d777b8a89621a0a5a6cfb4af5ebb257f5e2939
Tobias Zwick <newton@westnordost.de>
2025-07-24 16:32:35 +0200

    add classes to format a date or time locale-aware

---
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/util/locale/LocalTimeFormatter.android.kt
@@ -0,0 +1,22 @@
+package de.westnordost.streetcomplete.util.locale
+
+import androidx.compose.ui.text.intl.Locale
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toJavaLocalTime
+import kotlinx.datetime.toJavaZoneId
+import java.time.format.DateTimeFormatter
+
+actual class LocalTimeFormatter actual constructor(
+    locale: Locale?,
+    timeZone: TimeZone,
+    style: DateFormatStyle,
+) {
+    private val formatter = DateTimeFormatter
+        .ofLocalizedTime(style.toFormatStyle())
+        .let { if (locale != null) it.withLocale(locale.platformLocale) else it }
+        .withZone(timeZone.toJavaZoneId())
+
+    actual fun format(time: LocalTime): String =
+        time.toJavaLocalTime().format(formatter)
+}
