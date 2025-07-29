130b3d22b4a5f8480bb4c14668eae59d5048420f
Tobias Zwick <newton@westnordost.de>
2025-07-30 00:30:44 +0200

    implement StructuredHouseNumbers.minorStep (for #5479)

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/address/StructuredHouseNumbersParser.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/address/StructuredHouseNumbersParser.kt
@@ -6,19 +6,19 @@ import de.westnordost.streetcomplete.util.StringWithCursor
  * parses 99999/a, 9/a, 99/9, 99a, 99 a, 9 / a, "95-98", "5,5a,6", "5d-5f, 7", "5-1" etc. into an
  * appropriate data structure or return null if it cannot be parsed
  */
-fun parseHouseNumbers(string: String): HouseNumbers? {
-    return HouseNumbers(string.split(",").map { part ->
+fun parseHouseNumbers(string: String): StructuredHouseNumbers? {
+    return StructuredHouseNumbers(string.split(",").map { part ->
         val range = part.split("-")
         when (range.size) {
-            1 -> SingleHouseNumbersPart(parseHouseNumberParts(range[0]) ?: return null)
+            1 -> StructuredHouseNumbersPart.Single(parseHouseNumberParts(range[0]) ?: return null)
             2 -> {
                 val start = parseHouseNumberParts(range[0]) ?: return null
                 val end = parseHouseNumberParts(range[1]) ?: return null
                 if (start < end) {
-                    HouseNumbersPartsRange(start, end)
+                    StructuredHouseNumbersPart.Range(start, end)
                 } else {
                     // reverse ranges are interpreted like sub-housenumbers, i.e. 4-2 is about the same as 4/2
-                    SingleHouseNumbersPart(parseHouseNumberParts(part) ?: return null)
+                    StructuredHouseNumbersPart.Single(parseHouseNumberParts(part) ?: return null)
                 }
             }
             else -> return null
@@ -29,12 +29,12 @@ fun parseHouseNumbers(string: String): HouseNumbers? {
 private fun parseHouseNumberParts(string: String): StructuredHouseNumber? {
     val c = StringWithCursor(string)
     val houseNumber = c.nextMatchesAndAdvance("\\p{N}{1,5}".toRegex())?.value?.toIntOrNull() ?: return null
-    if (c.isAtEnd()) return SimpleHouseNumber(houseNumber)
+    if (c.isAtEnd()) return StructuredHouseNumber.Simple(houseNumber)
 
     val separatorWithNumber = c.nextMatchesAndAdvance("(\\s?[/-]\\s?)(\\p{N})".toRegex())
     if (separatorWithNumber != null) {
         if (!c.isAtEnd()) return null
-        return HouseNumberWithNumber(
+        return StructuredHouseNumber.WithNumber(
             houseNumber,
             separatorWithNumber.groupValues[1],
             separatorWithNumber.groupValues[2].toInt()
@@ -44,7 +44,7 @@ private fun parseHouseNumberParts(string: String): StructuredHouseNumber? {
     val separatorWithLetter = c.nextMatchesAndAdvance("(\\s?/?\\s?)(\\p{L})".toRegex())
     if (separatorWithLetter != null) {
         if (!c.isAtEnd()) return null
-        return HouseNumberWithLetter(
+        return StructuredHouseNumber.WithLetter(
             houseNumber,
             separatorWithLetter.groupValues[1],
             separatorWithLetter.groupValues[2]
