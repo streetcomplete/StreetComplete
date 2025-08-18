47fda93460b7e756e0bb7842a7a7c0096eb5980b
Tobias Zwick <newton@westnordost.de>
2025-08-18 18:05:15 +0200

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ import java.util.Properties
 
 
 /** App version name, code and flavor */
-val appVersionName = "61.2"
-val appVersionCode = 6103
+val appVersionName = "61.3"
+val appVersionCode = 6104
 
 /** Localizations the app should be available in */
 val bcp47ExportLanguages = setOf(
