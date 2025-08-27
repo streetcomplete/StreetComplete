ef200fca4e8002f25d6513fc9a22f6172507da75
Tobias Zwick <newton@westnordost.de>
2025-08-27 21:25:22 +0200

    remove prior implementation

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_weight/MaxWeightSign.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_weight/MaxWeightSign.kt
@@ -3,6 +3,7 @@ package de.westnordost.streetcomplete.quests.max_weight
 enum class MaxWeightSign {
     MAX_WEIGHT,
     MAX_GROSS_VEHICLE_MASS,
+    MAX_GROSS_VEHICLE_MASS_HGV,
     MAX_AXLE_LOAD,
     MAX_TANDEM_AXLE_LOAD,
 }
--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_weight/Weight.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_weight/Weight.kt
@@ -1,22 +1,27 @@
 package de.westnordost.streetcomplete.quests.max_weight
 
 import de.westnordost.streetcomplete.util.ktx.toShortString
+import kotlinx.serialization.Serializable
 
+@Serializable
 sealed interface Weight {
     fun toMetricTons(): Double
     override fun toString(): String
 }
 
+@Serializable
 data class MetricTons(val tons: Double) : Weight {
     override fun toMetricTons() = tons
     override fun toString() = tons.toShortString()
 }
 
+@Serializable
 data class ShortTons(val tons: Double) : Weight {
     override fun toMetricTons() = tons * 0.90718474
     override fun toString() = tons.toShortString() + " st"
 }
 
+@Serializable
 data class ImperialPounds(val pounds: Int) : Weight {
     override fun toMetricTons() = pounds * 0.45359237 / 1000
     override fun toString() = "$pounds lbs"
