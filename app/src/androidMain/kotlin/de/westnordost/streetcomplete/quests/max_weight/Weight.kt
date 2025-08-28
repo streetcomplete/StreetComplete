6a04986806ba45aaee0cd213a45addac3d0fcc31
Tobias Zwick <newton@westnordost.de>
2025-08-29 01:22:28 +0200

    refactor Weight

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_weight/Weight.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_weight/Weight.kt
@@ -1,28 +1,24 @@
 package de.westnordost.streetcomplete.quests.max_weight
 
+import de.westnordost.streetcomplete.data.meta.WeightMeasurementUnit
+import de.westnordost.streetcomplete.data.meta.WeightMeasurementUnit.*
 import de.westnordost.streetcomplete.util.ktx.toShortString
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed interface Weight {
-    fun toMetricTons(): Double
-    override fun toString(): String
-}
+data class Weight(
+    val value: Double,
+    val unit: WeightMeasurementUnit,
+) {
+    fun toMetricTons(): Double = when (unit) {
+        METRIC_TON -> value
+        SHORT_TON -> value * 0.90718474
+        POUND -> value * 0.45359237 / 1000.0
+    }
 
-@Serializable
-data class MetricTons(val tons: Double) : Weight {
-    override fun toMetricTons() = tons
-    override fun toString() = tons.toShortString()
-}
-
-@Serializable
-data class ShortTons(val tons: Double) : Weight {
-    override fun toMetricTons() = tons * 0.90718474
-    override fun toString() = tons.toShortString() + " st"
-}
-
-@Serializable
-data class ImperialPounds(val pounds: Int) : Weight {
-    override fun toMetricTons() = pounds * 0.45359237 / 1000
-    override fun toString() = "$pounds lbs"
+    fun toOsmString(): String = when (unit) {
+        METRIC_TON -> value.toShortString()
+        SHORT_TON -> value.toShortString() + " st"
+        POUND -> "$value lbs"
+    }
 }
