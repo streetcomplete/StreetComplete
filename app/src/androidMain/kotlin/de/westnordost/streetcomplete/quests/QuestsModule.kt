efb59f9dc11a9eba8c881b39749d2f173fb836bc
Naria <nariareynhard@gmail.com>
2025-09-21 16:51:40 +0200

    Saving for pull

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/QuestsModule.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/QuestsModule.kt
@@ -45,6 +45,7 @@ import de.westnordost.streetcomplete.quests.board_name.AddBoardName
 import de.westnordost.streetcomplete.quests.board_type.AddBoardType
 import de.westnordost.streetcomplete.quests.boat_rental.AddBoatRental
 import de.westnordost.streetcomplete.quests.bollard_type.AddBollardType
+import de.westnordost.streetcomplete.quests.bothway.AddBothway
 import de.westnordost.streetcomplete.quests.bridge_structure.AddBridgeStructure
 import de.westnordost.streetcomplete.quests.building_entrance.AddEntrance
 import de.westnordost.streetcomplete.quests.building_entrance_reference.AddEntranceReference
@@ -441,7 +442,7 @@ fun questTypeRegistry(
 
     166 to AddMopedAccess(),
 
-    182 to Bothway
+    182 to AddBothway(),
     /* ↓ 3.quests that may need some exploration / walking around --------------------------- */
 
     // ferry: usually visible from looking at the boat, but not always...
