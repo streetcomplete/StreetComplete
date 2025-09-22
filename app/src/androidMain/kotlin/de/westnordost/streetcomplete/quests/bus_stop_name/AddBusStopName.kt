7a288703b947225756a2772545454bc779fd3cdc
paukli <paulkliemann@posteo.net>
2025-09-22 17:45:09 +0200

    disabled bus stop name quest in sweden

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/bus_stop_name/AddBusStopName.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/bus_stop_name/AddBusStopName.kt
@@ -23,7 +23,7 @@ class AddBusStopName : OsmFilterQuestType<BusStopNameAnswer>(), AndroidQuest {
         and !name and noname != yes and name:signed != no
     """
 
-    override val enabledInCountries = AllCountriesExcept("US", "CA")
+    override val enabledInCountries = AllCountriesExcept("US", "CA", "SE")
     override val changesetComment = "Determine public transport stop names"
     override val wikiLink = "Tag:public_transport=platform"
     override val icon = R.drawable.ic_quest_bus_stop_name
