a2c0f911f847bff290b721939c1f8fe170d64412
paukli <paulkliemann@posteo.net>
2025-09-29 19:28:38 +0200

    also removed foodcourt

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/accepts_cash/AddAcceptsCash.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/accepts_cash/AddAcceptsCash.kt
@@ -19,9 +19,9 @@ class AddAcceptsCash : OsmFilterQuestType<Boolean>(), AndroidQuest {
 
     override val elementFilter: String get() {
         val amenities = listOf(
-            "bar", "cafe", "fast_food", "food_court", "ice_cream", "pub", "biergarten",
-            "restaurant", "cinema", "nightclub", "planetarium", "theatre", "internet_cafe",
-            "car_wash", "fuel", "pharmacy", "telephone", "vending_machine"
+            "bar", "cafe", "fast_food", "ice_cream", "pub", "biergarten", "restaurant", "fuel",
+            "cinema", "nightclub", "planetarium", "theatre", "internet_cafe", "car_wash",
+            "pharmacy", "telephone", "vending_machine"
         )
         val tourismsWithImpliedFees = listOf(
             "zoo", "aquarium", "theme_park", "hotel", "hostel", "motel", "guest_house",
