7a01cb20bd06ed9592cb4bc26196370d11331c1f
NariaReynhard <nariareynhard@gmail.com>
2025-09-29 10:23:47 +0200

    completed logic files, mostly patch coordinates <-> latlon logic. in progress : actual polylabel implementation

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/osm/geometry/polygons/Point.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/osm/geometry/polygons/Point.kt
@@ -1,5 +1,4 @@
 package de.westnordost.streetcomplete.data.osm.geometry.polygons
 
 /* Simple 2D point for algorithm logic */
-class Point(val x: Double, val y: Double) {
-}
+class Point(val x: Double, val y: Double)
--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/osm/geometry/polygons/PolygonAlgorithms.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/osm/geometry/polygons/PolygonAlgorithms.kt
@@ -1,2 +1,22 @@
 package de.westnordost.streetcomplete.data.osm.geometry.polygons
 
+object PolygonAlgorithms {
+
+    /* Simple centroid algorithm */
+    fun centroid(polygon: Polygon): Point {
+        val points = polygon.shape
+        var sumX = 0.0
+        var sumY = 0.0
+        for (p in points) {
+            sumX += p.x
+            sumY += p.y
+        }
+        return Point(sumX / points.size, sumY / points.size)
+    }
+
+    /* Core of the problem : visual center (within the polygon) */
+    fun polylabel(polygon: Polygon, precision: Double = 1.0): Point {
+        // TODO: Implement polylabel algorithm
+        return centroid(polygon)
+    }
+}
