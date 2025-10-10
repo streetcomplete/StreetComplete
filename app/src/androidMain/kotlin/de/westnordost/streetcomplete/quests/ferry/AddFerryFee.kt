7107921a6c532ea723cae949d22b103d8013d4f9
paukli <paulkliemann@posteo.net>
2025-10-11 00:18:35 +0200

    removed duplicate comment

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/ferry/AddFerryFee.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/ferry/AddFerryFee.kt
@@ -39,9 +39,6 @@ class AddFerryFee : OsmElementQuestType<Boolean>, AndroidQuest {
     }
 
     override fun getApplicableElements(mapData: MapDataWithGeometry): Iterable<Element> {
-        // the quest shall not be asked for ways tagged with route=ferry that are part of a relation
-        // also tagged with route=ferry because that makes the former not actually a "real" ferry
-        // route (╯°□°）╯︵ ┻━┻. Tagging mistake or not, it is very common tagging (#6373)
         val wayIdsInFerryRoutes = wayIdsInFerryRoutes(mapData.relations)
         return mapData
             .filter(filter)
