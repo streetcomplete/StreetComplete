d7d170ca60ba80e6fb7c385beb9c7af40ee38880
paukli <paulkliemann@posteo.net>
2025-10-14 21:03:11 +0200

    filtered out steps that are areas from AddSepsIncline

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/incline_direction/AddStepsIncline.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/incline_direction/AddStepsIncline.kt
@@ -1,21 +1,28 @@
 package de.westnordost.streetcomplete.quests.incline_direction
 
 import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.elementfilter.toElementFilterExpression
 import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
+import de.westnordost.streetcomplete.data.osm.mapdata.Element
+import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
+import de.westnordost.streetcomplete.data.osm.mapdata.Node
+import de.westnordost.streetcomplete.data.osm.mapdata.Way
+import de.westnordost.streetcomplete.data.osm.mapdata.filter
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmElementQuestType
 import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
 import de.westnordost.streetcomplete.data.quest.AndroidQuest
 import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement.PEDESTRIAN
 import de.westnordost.streetcomplete.osm.Tags
 
-class AddStepsIncline : OsmFilterQuestType<Incline>(), AndroidQuest {
+class AddStepsIncline : OsmElementQuestType<Incline>, AndroidQuest {
 
-    override val elementFilter = """
-        ways with highway = steps
+    private val filter by lazy { """
+         ways with highway = steps
          and (!indoor or indoor = no)
          and area != yes
          and access !~ private|no
          and !incline
-    """
+    """.toElementFilterExpression() }
     override val changesetComment = "Specify which way leads up for steps"
     override val wikiLink = "Key:incline"
     override val icon = R.drawable.ic_quest_steps
@@ -24,6 +31,16 @@ class AddStepsIncline : OsmFilterQuestType<Incline>(), AndroidQuest {
     override val hint = R.string.quest_arrow_tutorial
 
     override fun getTitle(tags: Map<String, String>) = R.string.quest_steps_incline_title
+
+    override fun getApplicableElements(mapData: MapDataWithGeometry): Iterable<Element> = mapData
+        .filter(filter)
+        .filter { it is Way && !it.isClosed }
+        .asIterable()
+
+    override fun isApplicableTo(element: Element): Boolean? {
+        if (!filter.matches(element)) return false
+        return null
+    }
 
     override fun createForm() = AddInclineForm()
 
