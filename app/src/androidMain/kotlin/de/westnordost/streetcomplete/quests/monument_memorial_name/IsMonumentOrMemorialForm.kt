f84584598190691abed239cf474f91081f856d66
HugoTHOLLON <hugo.thollon@gmail.com>
2025-10-23 21:29:58 +0200

    removing unused imports in IsMonumentOrMemorialForm

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/monument_memorial_name/IsMonumentOrMemorialForm.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/monument_memorial_name/IsMonumentOrMemorialForm.kt
@@ -1,12 +1,8 @@
 package de.westnordost.streetcomplete.quests.monument_memorial_name
 
 import de.westnordost.streetcomplete.R
-import de.westnordost.streetcomplete.osm.localized_name.LocalizedName
-import de.westnordost.streetcomplete.quests.AAddLocalizedNameForm
 import de.westnordost.streetcomplete.quests.AbstractOsmQuestForm
 import de.westnordost.streetcomplete.quests.AnswerItem
-import de.westnordost.streetcomplete.view.localized_name.confirmNoName
-import de.westnordost.streetcomplete.view.localized_name.showKeyboardInfo
 
 class IsMonumentOrMemorialForm : AbstractOsmQuestForm<Boolean>() {
 
