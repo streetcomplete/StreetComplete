c3111f82c656247fa8b8ca21dd607eec290dd4b1
RaphaLLamothe <raphael.lamothe@etu.univ-tlse2.fr>
2025-10-23 11:39:46 +0200

    adding a new svg for the quest, trying to follow the suggestions of westnordost, deleting the unnecessary failes, and updating the answers of the quest

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/aerialway/AerialwayBicycleAccessForm.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/aerialway/AerialwayBicycleAccessForm.kt
@@ -8,11 +8,11 @@ import de.westnordost.streetcomplete.quests.amenity_indoor.IsAmenityIndoorAnswer
 class AerialwayBicycleAccessForm : AbstractOsmQuestForm<AerialwayBicycleAccessAnswer>() {
     override val buttonPanelAnswers = listOf(
         AnswerItem(R.string.quest_generic_hasFeature_no) { applyAnswer(AerialwayBicycleAccessAnswer.NO) },
+        AnswerItem(R.string.quest_aerialway_bicycle_summer) { applyAnswer(AerialwayBicycleAccessAnswer.SUMMER) },
         AnswerItem(R.string.quest_generic_hasFeature_yes) { applyAnswer(AerialwayBicycleAccessAnswer.YES) }
     )
 
     override val otherAnswers = listOf(
-        AnswerItem(R.string.quest_aerialway_bicycle_summer) { applyAnswer(AerialwayBicycleAccessAnswer.SUMMER) },
         AnswerItem(R.string.quest_hairdresser_not_signed) { applyAnswer(AerialwayBicycleAccessAnswer.NO_SIGN) }
     )
 }
