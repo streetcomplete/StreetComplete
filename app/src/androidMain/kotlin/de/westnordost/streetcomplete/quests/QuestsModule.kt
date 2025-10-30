0efddcb558bcec6f3b01260f075b5fa390fecb79
RaphaLLamothe <161585200+RaphaLLamothe@users.noreply.github.com>
2025-10-30 14:12:43 +0100

    Aerialway bicycle quest (#6544)

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/QuestsModule.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/QuestsModule.kt
@@ -14,6 +14,7 @@ import de.westnordost.streetcomplete.quests.accepts_cash.AddAcceptsCash
 import de.westnordost.streetcomplete.quests.access_point_ref.AddAccessPointRef
 import de.westnordost.streetcomplete.quests.address.AddAddressStreet
 import de.westnordost.streetcomplete.quests.address.AddHousenumber
+import de.westnordost.streetcomplete.quests.aerialway.AddAerialwayBicycleAccess
 import de.westnordost.streetcomplete.quests.air_conditioning.AddAirConditioning
 import de.westnordost.streetcomplete.quests.air_pump.AddAirCompressor
 import de.westnordost.streetcomplete.quests.amenity_cover.AddAmenityCover
@@ -443,6 +444,9 @@ fun questTypeRegistry(
     // ferry: usually visible from looking at the boat, but not always...
     101 to AddFerryAccessPedestrian(),
     102 to AddFerryAccessMotorVehicle(),
+
+    //aerial way: usually visible from looking at the aerial way, but not always...
+    184 to AddAerialwayBicycleAccess(),
 
     103 to AddProhibitedForPedestrians(), // need to understand the pedestrian situation
 
