f30635f7e9c021fcfdd3059903438ea9b80ae732
Tobias Zwick <newton@westnordost.de>
2025-11-04 15:09:09 +0100

    add "customers" as selectable option

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/tower_access/TowerAccess.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/tower_access/TowerAccess.kt
@@ -2,16 +2,19 @@ package de.westnordost.streetcomplete.quests.tower_access
 
 import de.westnordost.streetcomplete.quests.tower_access.TowerAccess.*
 import de.westnordost.streetcomplete.resources.Res
+import de.westnordost.streetcomplete.resources.quest_access_customers
 import de.westnordost.streetcomplete.resources.quest_access_private
 import de.westnordost.streetcomplete.resources.quest_access_yes
 import org.jetbrains.compose.resources.StringResource
 
 enum class TowerAccess(val osmValue: String) {
     YES("yes"),
+    CUSTOMERS("customers"),
     PRIVATE("private"),
 }
 
 val TowerAccess.text: StringResource get() = when (this) {
     YES -> Res.string.quest_access_yes
+    CUSTOMERS -> Res.string.quest_access_customers
     PRIVATE -> Res.string.quest_access_private
 }
