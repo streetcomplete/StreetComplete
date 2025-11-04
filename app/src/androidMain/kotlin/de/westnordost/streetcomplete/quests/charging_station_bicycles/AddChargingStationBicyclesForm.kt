b2b3b1e2cbf647ebcbc0da39536c7bf63c27d717
Tobias Zwick <newton@westnordost.de>
2025-11-04 19:34:06 +0100

    move "only" answer into other answers

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/charging_station_bicycles/AddChargingStationBicyclesForm.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/charging_station_bicycles/AddChargingStationBicyclesForm.kt
@@ -6,9 +6,12 @@ import de.westnordost.streetcomplete.quests.AnswerItem
 import de.westnordost.streetcomplete.quests.charging_station_bicycles.ChargingStationBicycles.*
 
 class AddChargingStationBicyclesForm : AbstractOsmQuestForm<ChargingStationBicycles>() {
+    override val otherAnswers = listOf(
+        AnswerItem(R.string.quest_charging_station_bicycles_answer_only) { applyAnswer(ONLY) }
+    )
+
     override val buttonPanelAnswers = listOf(
         AnswerItem(R.string.quest_generic_hasFeature_no) { applyAnswer(NO) },
         AnswerItem(R.string.quest_generic_hasFeature_yes) { applyAnswer(YES) },
-        AnswerItem(R.string.quest_hasFeature_only) { applyAnswer(ONLY) },
     )
 }
