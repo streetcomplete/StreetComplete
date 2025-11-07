cd3fdbf0062235ea6a44b1ee02d73e5e6815e07c
Tobias Zwick <newton@westnordost.de>
2025-11-07 01:22:45 +0100

    big input

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/ANameWithSuggestionsForm.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/ANameWithSuggestionsForm.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -13,6 +14,7 @@ import androidx.compose.ui.text.input.TextFieldValue
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.databinding.ComposeViewBinding
 import de.westnordost.streetcomplete.ui.common.AutoCompleteTextField
+import de.westnordost.streetcomplete.ui.theme.largeInput
 import de.westnordost.streetcomplete.ui.util.content
 
 abstract class ANameWithSuggestionsForm<T> : AbstractOsmQuestForm<T>() {
@@ -43,6 +45,7 @@ abstract class ANameWithSuggestionsForm<T> : AbstractOsmQuestForm<T>() {
                         ?.takeIf { name.length >= 3 }
                         ?.filter { it.startsWith(name, ignoreCase = true) }
                         .orEmpty(),
+                    textStyle = MaterialTheme.typography.largeInput,
                 )
             }
         } }
