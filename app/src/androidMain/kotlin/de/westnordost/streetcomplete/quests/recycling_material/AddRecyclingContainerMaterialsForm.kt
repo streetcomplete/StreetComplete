1b8a0f6eb583955e0b13957f5c1cf3d391ff973e
Tobias Zwick <osm@westnordost.de>
2025-11-13 17:17:53 +0100

    Recycling quest UI change (fixes #6613, fixes #6608)
    * add class for tree structure
    
    * recycling materials in that tree structure + PET -> pet bottles
    
    * implement stuff
    
    * and solve todo...
    
    * actually, let's remove the special case for Czechia - just show "any glass" always. It's just one more item in the list (as opposed to opening of a dialog in the old UI)
    
    * no need to convert to list
    
    * typo + fix test
    
    * remove now unused graphic
    
    * order items by common-ness, add last-picked-ordering

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/recycling_material/AddRecyclingContainerMaterialsForm.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/recycling_material/AddRecyclingContainerMaterialsForm.kt
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AlertDialog
 import androidx.compose.material.Surface
 import androidx.compose.runtime.mutableStateOf
 import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.preferences.Preferences
 import de.westnordost.streetcomplete.databinding.ComposeViewBinding
 import de.westnordost.streetcomplete.quests.AbstractOsmQuestForm
 import de.westnordost.streetcomplete.quests.AnswerItem
 import de.westnordost.streetcomplete.ui.util.content
-
+import de.westnordost.streetcomplete.util.takeFavorites
+import org.koin.android.ext.android.inject
+import kotlin.getValue
 
 class AddRecyclingContainerMaterialsForm : AbstractOsmQuestForm<RecyclingContainerMaterialsAnswer>() {
 
@@ -18,31 +21,39 @@ class AddRecyclingContainerMaterialsForm : AbstractOsmQuestForm<RecyclingContain
     private val binding by contentViewBinding(ComposeViewBinding::bind)
     override val defaultExpanded = false
 
+    private val prefs: Preferences by inject()
+
     override val otherAnswers = listOf(
         AnswerItem(R.string.quest_recycling_materials_answer_waste) { confirmJustTrash() }
     )
 
-    private val items = RecyclingMaterial.selectableValues.map { listOf(it) }
-    private val selectedItems = mutableStateOf(emptySet<RecyclingMaterialsItem>())
+    private lateinit var reorderedItems: List<RecyclingMaterial>
+    private val selectedItems = mutableStateOf(emptySet<RecyclingMaterial>())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        reorderedItems = moveFavouritesToFront(RecyclingMaterial.entries)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.composeViewBase.content { Surface {
             RecyclingContainerMaterialsForm(
-                items = items,
+                items = reorderedItems,
+                tree = RecyclingMaterial.tree,
                 selectedItems = selectedItems.value,
                 onSelectedItems = {
                     selectedItems.value = it
                     checkIsFormComplete()
                 },
-                isAnyGlassRecyclable = countryInfo.isUsuallyAnyGlassRecyclableInContainers,
             )
         } }
     }
 
     override fun onClickOk() {
-        applyAnswer(RecyclingMaterials(selectedItems.value.flatten()))
+        prefs.addLastPicked(this::class.simpleName!!, selectedItems.value.toList())
+        applyAnswer(RecyclingMaterials(selectedItems.value))
     }
 
     override fun isFormComplete() = selectedItems.value.isNotEmpty()
@@ -54,5 +65,12 @@ class AddRecyclingContainerMaterialsForm : AbstractOsmQuestForm<RecyclingContain
             .setNegativeButton(R.string.quest_generic_confirmation_no, null)
             .show()
         }
+    }
+
+    private fun moveFavouritesToFront(originalList: List<RecyclingMaterial>): List<RecyclingMaterial> {
+        val favourites = prefs
+            .getLastPicked<RecyclingMaterial>(this::class.simpleName!!)
+            .takeFavorites(4)
+        return (favourites + originalList).distinct()
     }
 }
