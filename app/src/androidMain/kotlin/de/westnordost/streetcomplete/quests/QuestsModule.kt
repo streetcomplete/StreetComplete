571cf8c0562c491307a271c0e0e94b4afaeb0997
NariaReynhard <nariareynhard@gmail.com>
2025-12-01 14:28:49 +0100

    Deleted a comment that came from another PR of my group and should not appear yet

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/QuestsModule.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/QuestsModule.kt
@@ -437,8 +437,6 @@ fun questTypeRegistry(
     101 to AddFerryAccessPedestrian(),
     102 to AddFerryAccessMotorVehicle(),
 
-    // aerial way: usually visible from looking at the aerial way, but not always...
-
     103 to AddProhibitedForPedestrians(), // need to understand the pedestrian situation
 
     104 to MarkCompletedHighwayConstruction(), // need to look the whole way
