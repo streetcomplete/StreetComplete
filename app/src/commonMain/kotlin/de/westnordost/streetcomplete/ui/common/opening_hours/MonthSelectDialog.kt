8ecafe5f5809c93401c397d958ef34e0251c026a
Tobias Zwick <newton@westnordost.de>
2025-12-09 01:05:13 +0100

    make use of checkbox list in weekday and months select dialog

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/opening_hours/MonthSelectDialog.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/opening_hours/MonthSelectDialog.kt
@@ -1,23 +1,16 @@
 package de.westnordost.streetcomplete.ui.common.opening_hours
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateSet
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import de.westnordost.osm_opening_hours.model.Month
 import de.westnordost.osm_opening_hours.model.MonthsOrDateSelector
 import de.westnordost.streetcomplete.osm.opening_hours.getMonths
@@ -26,7 +19,8 @@ import de.westnordost.streetcomplete.resources.Res
 import de.westnordost.streetcomplete.resources.cancel
 import de.westnordost.streetcomplete.resources.ok
 import de.westnordost.streetcomplete.resources.quest_openingHours_chooseWeekdaysTitle
-import de.westnordost.streetcomplete.ui.common.dialogs.AlertDialogLayout
+import de.westnordost.streetcomplete.ui.common.CheckboxList
+import de.westnordost.streetcomplete.ui.common.dialogs.ScrollableAlertDialog
 import de.westnordost.streetcomplete.ui.ktx.fadingVerticalScrollEdges
 import org.jetbrains.compose.resources.stringResource
 
@@ -34,61 +28,45 @@ import org.jetbrains.compose.resources.stringResource
 @Composable
 fun MonthsSelectDialog(
     onDismissRequest: () -> Unit,
-    selectedMonths: List<MonthsOrDateSelector>,
+    initialMonths: List<MonthsOrDateSelector>,
     onSelected: (months: List<MonthsOrDateSelector>) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     val locale = Locale.current
     val scrollState = rememberScrollState()
-    val monthsSelection = remember(selectedMonths) {
-        SnapshotStateSet<Month>().also { it.addAll(selectedMonths.getMonths()) }
+    val selection = remember(initialMonths) {
+        SnapshotStateSet<Month>().also { it.addAll(initialMonths.getMonths()) }
     }
 
-    fun toggleMonth(checked: Boolean, month: Month) {
-        if (checked) monthsSelection.add(month)
-        else monthsSelection.remove(month)
-    }
-
-    Dialog(
+    ScrollableAlertDialog(
         onDismissRequest = onDismissRequest,
-    ) {
-        AlertDialogLayout(
-            modifier = modifier,
-            title = { Text(stringResource(Res.string.quest_openingHours_chooseWeekdaysTitle)) },
-            content = {
-                Column(Modifier
-                    .fadingVerticalScrollEdges(scrollState, 64.dp)
+        modifier = modifier,
+        title = { Text(stringResource(Res.string.quest_openingHours_chooseWeekdaysTitle)) },
+        content = {
+            CheckboxList(
+                options = Month.entries,
+                onToggle = { month, checked ->
+                    if (checked) selection.add(month)
+                    else selection.remove(month)
+                },
+                selectedOptions = selection,
+                itemContent = { Text(it.getDisplayName(locale = locale)) },
+                modifier = Modifier
+                    .fadingVerticalScrollEdges(scrollState, 32.dp)
+                    .padding(horizontal = 24.dp)
                     .verticalScroll(scrollState)
-                    .padding(horizontal = 24.dp),
-                ) {
-                    for (month in Month.entries) {
-                        val checked = month in monthsSelection
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            modifier = Modifier
-                                .toggleable(checked) { toggleMonth(it, month) }
-                        ) {
-                            Checkbox(
-                                checked = checked,
-                                onCheckedChange = { toggleMonth(it, month) },
-                            )
-                            Text(month.getDisplayName(locale = locale))
-                        }
-                    }
-                }
-            },
-            buttons = {
-                TextButton(onClick = onDismissRequest) {
-                    Text(stringResource(Res.string.cancel))
-                }
-                TextButton(onClick = {
-                    onDismissRequest()
-                    onSelected(monthsSelection.toMonthsSelectors())
-                }) {
-                    Text(stringResource(Res.string.ok))
-                }
+            )
+        },
+        buttons = {
+            TextButton(onClick = onDismissRequest) {
+                Text(stringResource(Res.string.cancel))
             }
-        )
-    }
+            TextButton(onClick = {
+                onDismissRequest()
+                onSelected(selection.toMonthsSelectors())
+            }) {
+                Text(stringResource(Res.string.ok))
+            }
+        }
+    )
 }
--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/opening_hours/WeekdaySelectDialog.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/opening_hours/WeekdaySelectDialog.kt
@@ -1,132 +1,98 @@
 package de.westnordost.streetcomplete.ui.common.opening_hours
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.toggleable
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Checkbox
-import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateSet
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.intl.Locale
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import de.westnordost.osm_opening_hours.model.Holiday
 import de.westnordost.osm_opening_hours.model.HolidaySelector
-import de.westnordost.osm_opening_hours.model.HolidayWithOffset
 import de.westnordost.osm_opening_hours.model.Weekday
 import de.westnordost.osm_opening_hours.model.WeekdaysSelector
+import de.westnordost.streetcomplete.osm.opening_hours.getHolidays
 import de.westnordost.streetcomplete.osm.opening_hours.getWeekdays
 import de.westnordost.streetcomplete.osm.opening_hours.toWeekdaysSelectors
 import de.westnordost.streetcomplete.resources.Res
 import de.westnordost.streetcomplete.resources.cancel
 import de.westnordost.streetcomplete.resources.ok
 import de.westnordost.streetcomplete.resources.quest_openingHours_chooseWeekdaysTitle
-import de.westnordost.streetcomplete.ui.common.dialogs.AlertDialogLayout
-import de.westnordost.streetcomplete.ui.ktx.fadingVerticalScrollEdges
+import de.westnordost.streetcomplete.ui.common.CheckboxList
+import de.westnordost.streetcomplete.ui.common.dialogs.ScrollableAlertDialog
 import org.jetbrains.compose.resources.stringResource
+import kotlin.jvm.JvmInline
 
 /** Dialog in which to select a number of weekdays */
 @Composable
 fun WeekdaySelectDialog(
     onDismissRequest: () -> Unit,
-    selectedWeekdays: List<WeekdaysSelector>,
-    selectedHolidays: List<HolidaySelector>,
+    initialWeekdays: List<WeekdaysSelector>,
+    initialHolidays: List<HolidaySelector>,
     onSelected: (weekdays: List<WeekdaysSelector>, holidays: List<HolidaySelector>) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     val locale = Locale.current
     val scrollState = rememberScrollState()
-    val weekdaysSelection = remember(selectedWeekdays) {
-        SnapshotStateSet<Weekday>().also { it.addAll(selectedWeekdays.getWeekdays()) }
-    }
-    var isPublicHolidaySelected by remember(selectedHolidays) {
-        mutableStateOf(selectedHolidays.isPublicHolidaySelected())
-    }
-
-    fun toggleWeekday(checked: Boolean, weekday: Weekday) {
-        if (checked) weekdaysSelection.add(weekday)
-        else weekdaysSelection.remove(weekday)
-    }
-
-    Dialog(
-        onDismissRequest = onDismissRequest,
-    ) {
-        AlertDialogLayout(
-            modifier = modifier,
-            title = { Text(stringResource(Res.string.quest_openingHours_chooseWeekdaysTitle)) },
-            content = {
-                Column(Modifier
-                    .fadingVerticalScrollEdges(scrollState, 64.dp)
-                    .verticalScroll(scrollState)
-                    .padding(horizontal = 24.dp),
-                ) {
-                    for (weekday in Weekday.entries) {
-                        val checked = weekday in weekdaysSelection
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            modifier = Modifier
-                                .toggleable(checked) { toggleWeekday(it, weekday) }
-                        ) {
-                            Checkbox(
-                                checked = checked,
-                                onCheckedChange = { toggleWeekday(it, weekday) },
-                            )
-                            Text(weekday.getDisplayName(locale = locale))
-                        }
-                    }
-                    Divider()
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(4.dp),
-                        modifier = Modifier
-                            .toggleable(isPublicHolidaySelected) { isPublicHolidaySelected = it }
-                    ) {
-                        Checkbox(
-                            checked = isPublicHolidaySelected,
-                            onCheckedChange = { isPublicHolidaySelected = it }
-                        )
-                        Text(stringResource(Holiday.PublicHoliday.getDisplayNameResource()))
-                    }
-                }
-            },
-            buttons = {
-                TextButton(onClick = onDismissRequest) {
-                    Text(stringResource(Res.string.cancel))
-                }
-                TextButton(onClick = {
-                    onDismissRequest()
-                    onSelected(
-                        weekdaysSelection.toWeekdaysSelectors(),
-                        if (isPublicHolidaySelected) listOf(Holiday.PublicHoliday) else listOf()
-                    )
-                }) {
-                    Text(stringResource(Res.string.ok))
-                }
-            }
-        )
-    }
-}
-
-fun List<HolidaySelector>.isPublicHolidaySelected(): Boolean {
-    for (selector in this) {
-        when (selector) {
-            Holiday.PublicHoliday -> return true
-            Holiday.SchoolHoliday -> throw UnsupportedOperationException()
-            is HolidayWithOffset -> throw UnsupportedOperationException()
+    val items =
+        Weekday.entries.map { WeekdayOrHoliday.Wd(it) } +
+        WeekdayOrHoliday.Hd(Holiday.PublicHoliday)
+    val selection = remember(initialWeekdays, initialHolidays) {
+        SnapshotStateSet<WeekdayOrHoliday>().also { set ->
+            set.addAll(initialWeekdays.getWeekdays().map { WeekdayOrHoliday.Wd(it) })
+            set.addAll(initialHolidays.getHolidays().map { WeekdayOrHoliday.Hd(it) })
         }
     }
-    return false
+
+    ScrollableAlertDialog(
+        onDismissRequest = onDismissRequest,
+        modifier = modifier,
+        title = { Text(stringResource(Res.string.quest_openingHours_chooseWeekdaysTitle)) },
+        content = {
+            CheckboxList(
+                options = items,
+                onToggle = { w, checked ->
+                    if (checked) selection.add(w)
+                    else selection.remove(w)
+                },
+                selectedOptions = selection,
+                itemContent = { w ->
+                    val text = when (w) {
+                        is WeekdayOrHoliday.Hd ->
+                            stringResource(w.value.getDisplayNameResource())
+                        is WeekdayOrHoliday.Wd ->
+                            w.value.getDisplayName(locale = locale)
+                    }
+                    Text(text)
+                }
+            )
+        },
+        buttons = {
+            TextButton(onClick = onDismissRequest) {
+                Text(stringResource(Res.string.cancel))
+            }
+            TextButton(onClick = {
+                val selectedWeekdays = selection
+                    .filterIsInstance<WeekdayOrHoliday.Wd>()
+                    .mapTo(HashSet()) { it.value }
+                val selectedHolidays = selection
+                    .filterIsInstance<WeekdayOrHoliday.Hd>()
+                    .map { it.value }
+
+                onDismissRequest()
+                onSelected(
+                    selectedWeekdays.toWeekdaysSelectors(),
+                    selectedHolidays
+                )
+            }) {
+                Text(stringResource(Res.string.ok))
+            }
+        }
+    )
+}
+
+private sealed interface WeekdayOrHoliday {
+    @JvmInline value class Wd(val value: Weekday): WeekdayOrHoliday
+    @JvmInline value class Hd(val value: Holiday): WeekdayOrHoliday
 }
