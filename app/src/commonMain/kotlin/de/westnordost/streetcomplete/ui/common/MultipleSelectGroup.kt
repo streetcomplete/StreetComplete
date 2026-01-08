61a679bd58dc47e8f7ef8fef8f43756fc7812f62
arthedain <73854742+paulklie@users.noreply.github.com>
2026-01-08 23:54:57 +0100

    Update app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/MultipleSelectGroup.kt
    Co-authored-by: Tobias Zwick <newton@westnordost.de>

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/MultipleSelectGroup.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/MultipleSelectGroup.kt
@@ -30,7 +30,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 fun <T> MultipleSelectGroup(
     options: List<T>,
-    onSelectionChange: (T) -> Unit,
+    onSelectionChange: (item: T, selected: Boolean)) -> Unit,
     selectedOptions: Set<T>,
     itemContent: @Composable BoxScope.(T) -> Unit,
     modifier: Modifier = Modifier
