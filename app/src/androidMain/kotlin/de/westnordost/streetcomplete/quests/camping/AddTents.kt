07aa39e96997a0ba18511ad87321039e1e4e2573
paukli <paulkliemann@posteo.net>
2026-01-10 01:31:36 +0100

    split up camp type into 2 quests and added cabin

---
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/camping/AddTents.kt
@@ -0,0 +1,45 @@
+package de.westnordost.streetcomplete.quests.camping
+
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
+import de.westnordost.streetcomplete.data.osm.mapdata.Element
+import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
+import de.westnordost.streetcomplete.data.osm.mapdata.filter
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmElementQuestType
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
+import de.westnordost.streetcomplete.data.quest.AndroidQuest
+import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement.OUTDOORS
+import de.westnordost.streetcomplete.osm.Tags
+import de.westnordost.streetcomplete.quests.YesNoQuestForm
+import de.westnordost.streetcomplete.util.ktx.toYesNo
+
+class AddTents : OsmFilterQuestType<Boolean>(), AndroidQuest {
+
+    override val elementFilter = """
+        nodes, ways with
+          tourism ~ caravan_site|camp_site
+          and !tents
+          and !backcountry
+    """
+    override val changesetComment = "Survey who may camp here"
+    override val wikiLink = "Key:caravans"
+    override val icon = R.drawable.quest_tent
+    // you can often see tents from the outside, so usually there is no need for this quest to be disabled by default
+    override val achievements = listOf(OUTDOORS)
+
+    override fun getTitle(tags: Map<String, String>) = R.string.quest_camp_tent_title
+
+    override fun getHighlightedElements(element: Element, getMapData: () -> MapDataWithGeometry) =
+        getMapData().filter("nodes, ways with tourism ~ caravan_site|camp_site")
+
+    override fun applyAnswerTo(
+        answer: Boolean,
+        tags: Tags,
+        geometry: ElementGeometry,
+        timestampEdited: Long,
+    ) {
+        tags["tents"] = answer.toYesNo()
+    }
+
+    override fun createForm() = YesNoQuestForm()
+}
