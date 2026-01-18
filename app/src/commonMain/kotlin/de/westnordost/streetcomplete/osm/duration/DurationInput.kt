3b8460d2c20e8ec7c867426826a377c6e4173a00
Tobias Zwick <newton@westnordost.de>
2026-01-19 00:08:17 +0100

    put duration unit dropdown into own file

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/duration/DurationInput.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/duration/DurationInput.kt
@@ -2,10 +2,8 @@ package de.westnordost.streetcomplete.osm.duration
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,17 +12,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import de.westnordost.streetcomplete.resources.Res
-import de.westnordost.streetcomplete.resources.unit_days
-import de.westnordost.streetcomplete.resources.unit_hours
-import de.westnordost.streetcomplete.resources.unit_minutes
-import de.westnordost.streetcomplete.ui.common.ButtonStyle
-import de.westnordost.streetcomplete.ui.common.DropdownButton
 import de.westnordost.streetcomplete.ui.common.input.DecimalInput
-import de.westnordost.streetcomplete.ui.theme.extraLargeInput
 import de.westnordost.streetcomplete.ui.theme.largeInput
-import org.jetbrains.compose.resources.StringResource
-import org.jetbrains.compose.resources.stringResource
 
 /** Input field to input the elements of a duration (value + unit) */
 @Composable
@@ -57,21 +46,12 @@ fun DurationInput(
             textStyle = MaterialTheme.typography.largeInput,
             modifier = Modifier.width(96.dp),
         )
-        DropdownButton(
-            items = DurationUnit.entries,
-            onSelectedItem = { newUnit ->
+        DurationUnitDropdown(
+            selectedDuration = unit,
+            onSelectedDuration = { newUnit ->
                 unit = newUnit
                 onChange(value?.let { Duration(it, newUnit) })
-            },
-            selectedItem = unit,
-            style = ButtonStyle.Outlined,
-            itemContent = { Text(stringResource(it.text)) }
+            }
         )
     }
-}
-
-private val DurationUnit.text: StringResource get() = when (this) {
-    DurationUnit.MINUTES -> Res.string.unit_minutes
-    DurationUnit.HOURS -> Res.string.unit_hours
-    DurationUnit.DAYS -> Res.string.unit_days
 }
