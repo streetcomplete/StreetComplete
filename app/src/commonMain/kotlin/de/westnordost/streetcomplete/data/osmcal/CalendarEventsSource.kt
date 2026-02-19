450206b75fcf30c413e80abca97ce9af37480b34
Tobias Zwick <newton@westnordost.de>
2026-02-19 18:11:24 +0100

    some comments

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/osmcal/CalendarEventsSource.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/osmcal/CalendarEventsSource.kt
@@ -6,7 +6,9 @@ interface CalendarEventsSource {
         fun onUnreadCountChanged()
     }
 
+    /** for how many calendar events the user didn't see the message yet */
     fun getUnreadCount(): Int
+    /** get the earliest unread calendar event or null if there is none */
     fun getFirstUnread(): CalendarEvent?
 
     fun addListener(listener: Listener)
