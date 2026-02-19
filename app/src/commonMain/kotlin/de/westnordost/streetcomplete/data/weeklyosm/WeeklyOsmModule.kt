608b091e31d4fa39ebbbd1182b4009be64742cb8
Tobias Zwick <newton@westnordost.de>
2026-02-19 17:47:42 +0100

    no need for parameter

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/weeklyosm/WeeklyOsmModule.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/weeklyosm/WeeklyOsmModule.kt
@@ -2,10 +2,8 @@ package de.westnordost.streetcomplete.data.weeklyosm
 
 import org.koin.dsl.module
 
-private const val WEEKLY_OSM_FEED_URL = "https://weeklyosm.eu/feed"
-
 val weeklyOsmModule = module {
-    factory { WeeklyOsmApiClient(get(), WEEKLY_OSM_FEED_URL, get()) }
+    factory { WeeklyOsmApiClient(get(), get()) }
     factory { WeeklyOsmUpdater(get(), get()) }
     factory { WeeklyOsmRssFeedParser() }
 }
