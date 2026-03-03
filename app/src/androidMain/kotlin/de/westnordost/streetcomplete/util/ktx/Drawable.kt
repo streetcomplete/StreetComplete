df074f4e9828120f20948a942d01a3a3769a21ce
Tobias Zwick <newton@westnordost.de>
2026-03-04 00:23:26 +0100

    remove unused functions

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/util/ktx/Drawable.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/util/ktx/Drawable.kt
@@ -15,14 +15,3 @@ fun Drawable.createBitmap(width: Int = intrinsicWidth, height: Int = intrinsicHe
         setBounds(0, 0, this.width, this.height)
         draw(this)
     }
-
-fun Drawable.asBitmapDrawable(resources: Resources, width: Int = intrinsicWidth, height: Int = intrinsicHeight): BitmapDrawable =
-    if (this is BitmapDrawable) this else createBitmap(width, height).toDrawable(resources)
-
-fun Drawable.asImageSpan(width: Int = intrinsicWidth, height: Int = intrinsicHeight): ImageSpan {
-    this.mutate()
-    this.setBounds(0, 0, width, height)
-    // ALIGN_CENTER does not work correctly on SDK 29, see #3736
-    val alignment = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) ImageSpan.ALIGN_CENTER else ImageSpan.ALIGN_BASELINE
-    return ImageSpan(this, alignment)
-}
