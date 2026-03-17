d3eb5baf647e9b97aca76683205b3b061f3bb0a4
Tobias Zwick <newton@westnordost.de>
2026-03-17 18:22:58 +0100

    rather use rememberPath, I think it's cleaner (no global variables)

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/screens/tutorial/OverlayPainter.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/screens/tutorial/OverlayPainter.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.graphics.vector.VectorComposable
 import androidx.compose.ui.graphics.vector.VectorPainter
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
-import de.westnordost.streetcomplete.ui.util.svgPath
+import de.westnordost.streetcomplete.ui.util.rememberPath
 import kotlin.math.PI
 import kotlin.math.cos
 
@@ -20,7 +20,7 @@ import kotlin.math.cos
 fun overlayPainter(progress: Float): VectorPainter = rememberMapOverlayPainter {
     val translate = -(1f - progress.coerceIn(0f, 1f)) * 317f
     Group(
-        clipPathData = clipPath,
+        clipPathData = rememberPath("M-111,111 l317,317 l317,-317 l-317,-317z"),
         translationX = translate,
         translationY = translate,
     ) {
@@ -29,21 +29,21 @@ fun overlayPainter(progress: Float): VectorPainter = rememberMapOverlayPainter {
             translationY = -translate,
         ) {
             Path(
-                pathData = way1Path,
+                pathData = rememberPath("m48.72,10.05 l-8.5,28.23 17.99,6.25 7.75,36.23 -20.99,22.24 8.99,10.49"),
                 strokeLineJoin = StrokeJoin.Round,
                 strokeLineCap = StrokeCap.Round,
                 strokeLineWidth = 8f,
                 stroke = SolidColor(Color(0xffeebd0d))
             )
             Path(
-                pathData = way2Path,
+                pathData = rememberPath(animatedWayPath),
                 strokeLineJoin = StrokeJoin.Round,
                 strokeLineCap = StrokeCap.Round,
                 strokeLineWidth = 8f,
                 stroke = SolidColor(Color(0xffff0000))
             )
             Path(
-                pathData = way3Path,
+                pathData = rememberPath("M94.19,215.44 l2.5,-13.24 l12.49,-27.73 10.99,-7 27.48,15.74 20.49,-3.75 -0.25,-15.74 -10.24,-6 12.74,-26.23 5.75,-3.75 38.73,-9.99"),
                 strokeLineJoin = StrokeJoin.Round,
                 strokeLineCap = StrokeCap.Round,
                 strokeLineWidth = 8f,
@@ -57,7 +57,7 @@ fun overlayPainter(progress: Float): VectorPainter = rememberMapOverlayPainter {
 fun overlayEditHighlightedPainter(progress: Float): VectorPainter = rememberMapOverlayPainter {
     val breathing = -cos(progress * 2 * PI) / 2.0 + 0.5 // 0..1
     Path(
-        pathData = way2Path,
+        pathData = rememberPath(animatedWayPath),
         strokeLineJoin = StrokeJoin.Round,
         strokeLineCap = StrokeCap.Round,
         strokeLineWidth = ((breathing + 1) * 8).toFloat(), // 8..16
@@ -69,7 +69,7 @@ fun overlayEditHighlightedPainter(progress: Float): VectorPainter = rememberMapO
 @Composable
 fun overlayEditDonePainter(progress: Float): VectorPainter = rememberMapOverlayPainter {
     Path(
-        pathData = way2Path,
+        pathData = rememberPath(animatedWayPath),
         strokeLineJoin = StrokeJoin.Round,
         strokeLineCap = StrokeCap.Round,
         strokeLineWidth = 8 + (1 - progress.coerceIn(0f, 1f)) * 16,
@@ -88,7 +88,4 @@ private fun rememberMapOverlayPainter(content: @Composable @VectorComposable () 
         autoMirror = false
     ) { _, _ -> content() }
 
-private val clipPath = svgPath("M-111,111 l317,317 l317,-317 l-317,-317z")
-private val way1Path = svgPath("m48.72,10.05 l-8.5,28.23 17.99,6.25 7.75,36.23 -20.99,22.24 8.99,10.49")
-private val way2Path = svgPath("m53.97,113.51 l-11.99,11.49 0.5,4.5 20.24,24.49 13.99,-6.75 20.49,18.49 -10.49,28.23 10.24,8.5")
-private val way3Path = svgPath("M94.19,215.44 l2.5,-13.24 l12.49,-27.73 10.99,-7 27.48,15.74 20.49,-3.75 -0.25,-15.74 -10.24,-6 12.74,-26.23 5.75,-3.75 38.73,-9.99")
+private val animatedWayPath = "m53.97,113.51 l-11.99,11.49 0.5,4.5 20.24,24.49 13.99,-6.75 20.49,18.49 -10.49,28.23 10.24,8.5"
