01cff5c6f3d719b91c48b581e4ec40c8ba4fd031
Tobias Zwick <newton@westnordost.de>
2026-04-02 11:36:42 +0200

    add todo comment

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/util/IsImeVisible.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/util/IsImeVisible.kt
@@ -4,4 +4,5 @@ import androidx.compose.runtime.Composable
 
 /** Whether the software keyboard (IME) is currently visible. */
 @Composable
+// TODO CMP: this is necessary as long as https://youtrack.jetbrains.com/issue/CMP-9906/Commonize-WindowInsets.isVisible-functions is not implemented
 expect fun isImeVisible(): Boolean
