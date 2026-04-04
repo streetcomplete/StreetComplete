e56aa1ee17cadc4f1ffa71752520bcb0836c38e3
Tobias Zwick <newton@westnordost.de>
2026-04-04 02:02:55 +0200

    remove unused imports

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/SwitchKeyboardPopupButton.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/SwitchKeyboardPopupButton.kt
@@ -6,17 +6,14 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.IntOffset
