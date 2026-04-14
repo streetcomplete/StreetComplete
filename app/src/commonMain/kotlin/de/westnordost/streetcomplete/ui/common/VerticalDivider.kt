caa11d711567481c37f9ca1fb0280347950e326d
Tobias Zwick <newton@westnordost.de>
2026-04-14 02:19:43 +0200

    copy new implementation from M3

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/VerticalDivider.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/VerticalDivider.kt
@@ -1,14 +1,13 @@
 package de.westnordost.streetcomplete.ui.common
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -19,13 +18,16 @@ fun VerticalDivider(
     color: Color = MaterialTheme.colors.onSurface.copy(alpha = DividerAlpha),
     thickness: Dp = 1.dp
 ) {
-    val targetThickness =
-        if (thickness == Dp.Hairline) {
-            (1f / LocalDensity.current.density).dp
-        } else {
-            thickness
-        }
-    Box(modifier.fillMaxHeight().width(targetThickness).background(color = color))
+    Canvas(modifier.fillMaxHeight().width(thickness)) {
+        val width = thickness.toPx()
+        val offset = width / 2f
+        drawLine(
+            color = color,
+            strokeWidth = width,
+            start = Offset(offset, 0f),
+            end = Offset(offset, size.height),
+        )
+    }
 }
 
 private const val DividerAlpha = 0.12f
