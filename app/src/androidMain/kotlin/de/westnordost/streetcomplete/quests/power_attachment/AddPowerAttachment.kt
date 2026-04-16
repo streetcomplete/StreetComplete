b84c36398d8addc476da86f4707591e9327fdbdd
Tobias Zwick <newton@westnordost.de>
2026-04-16 19:40:10 +0200

    don't ask for power line attachment on poles that have more than one power line going through (fixes #6547)

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/power_attachment/AddPowerAttachment.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/power_attachment/AddPowerAttachment.kt
@@ -1,29 +1,62 @@
 package de.westnordost.streetcomplete.quests.power_attachment
 
 import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.elementfilter.toElementFilterExpression
 import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
 import de.westnordost.streetcomplete.data.osm.mapdata.Element
 import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
 import de.westnordost.streetcomplete.data.osm.mapdata.filter
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmElementQuestType
 import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
 import de.westnordost.streetcomplete.data.quest.AndroidQuest
 import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement.BUILDING
 import de.westnordost.streetcomplete.osm.Tags
 import de.westnordost.streetcomplete.resources.*
 
-class AddPowerAttachment : OsmFilterQuestType<PowerAttachment>(), AndroidQuest {
+class AddPowerAttachment : OsmElementQuestType<PowerAttachment>, AndroidQuest {
 
-    override val elementFilter = """
+    private val polesFilter by lazy { """
         nodes with
           power ~ tower|pole|insulator
           and !line_attachment
-          and disused != yes and ruined != yes and abandoned != yes
-    """
+          and disused != yes
+          and ruined != yes
+          and abandoned != yes
+    """.toElementFilterExpression() }
+
+    private val powerLinesFilter by lazy { """
+        ways with power ~ line|minor_line
+    """.toElementFilterExpression() }
+
     override val changesetComment = "Specify line_attachment power support"
     override val wikiLink = "Key:line_attachment"
     override val icon = R.drawable.quest_power
     override val title = Res.string.quest_powerAttachment_title
     override val achievements = listOf(BUILDING)
+
+    override fun getApplicableElements(mapData: MapDataWithGeometry): Iterable<Element> {
+        // when several power lines are attached to any one power pole, it is very likely that that
+        // power pole has *multiple* power line attachments, which requires a more complex tagging
+        // that the UI for this quest does not support. Hence, we filter out all power poles that
+        // are nodes of more than one power lines. See #6547
+        val poleNodeIdsWithSeveralPowerLines = mapData.ways
+            .filter { powerLinesFilter.matches(it) }
+            .flatMap { it.nodeIds }
+            .groupingBy { it }.eachCount()
+            .filter { (id, count) -> count != 1 }
+            .mapTo(HashSet<Long>()) { (id, count) -> id }
+
+        return mapData.nodes.filter {
+            polesFilter.matches(it) && it.id !in poleNodeIdsWithSeveralPowerLines
+        }
+    }
+
+    override fun isApplicableTo(element: Element): Boolean? {
+        if (!polesFilter.matches(element)) return false
+        // otherwise, we can't say, because the power pole may be part of several power lines
+        // (see comment in getApplicableElements)
+        return null
+    }
 
     override fun getHighlightedElements(element: Element, mapData: MapDataWithGeometry) =
         // and also show the (power) lines themselves
