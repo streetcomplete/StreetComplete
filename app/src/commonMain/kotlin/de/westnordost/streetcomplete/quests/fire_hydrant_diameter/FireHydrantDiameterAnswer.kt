bb63da05bbcf51940bec6f7336c0dad1513e12b7
Tobias Zwick <newton@westnordost.de>
2026-04-27 21:14:21 +0200

    refactor some quest forms to new layout

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/quests/fire_hydrant_diameter/FireHydrantDiameterAnswer.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/quests/fire_hydrant_diameter/FireHydrantDiameterAnswer.kt
@@ -1,13 +1,27 @@
 package de.westnordost.streetcomplete.quests.fire_hydrant_diameter
 
+import de.westnordost.streetcomplete.quests.fire_hydrant_diameter.FireHydrantDiameter.Unit.Inch
+import de.westnordost.streetcomplete.quests.fire_hydrant_diameter.FireHydrantDiameter.Unit.Millimeter
+
 sealed interface FireHydrantDiameterAnswer {
     data object NoSign : FireHydrantDiameterAnswer
 }
 
 data class FireHydrantDiameter(val value: Int, val unit: Unit) : FireHydrantDiameterAnswer {
-    fun toOsmValue() = value.toString() + when (unit) {
-        Unit.Millimeter -> ""
-        Unit.Inch -> "\""
+    fun toOsmValue(): String =
+        value.toString() + when (unit) {
+            Unit.Millimeter -> ""
+            Unit.Inch -> "\""
+        }
+    enum class Unit {
+        Millimeter, Inch;
+
+        fun usualRange(): IntProgression = when (this) {
+            Millimeter -> 50..600 step 5
+            Inch -> 1..25
+        }
     }
-    enum class Unit { Millimeter, Inch }
+
+    fun isUnusual(): Boolean =
+        value !in unit.usualRange()
 }
