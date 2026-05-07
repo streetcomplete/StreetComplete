ddad5de18440266da99a62c968d49c5b78ccb308
Tobias Zwick <newton@westnordost.de>
2026-05-07 17:35:58 +0200

    migrate SidewalkOverlayForm

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/overlays/sidewalk/SidewalkOverlayForm.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/overlays/sidewalk/SidewalkOverlayForm.kt
@@ -1,17 +1,15 @@
 package de.westnordost.streetcomplete.overlays.sidewalk
 
-import android.os.Bundle
-import android.view.View
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material.Surface
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
-import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapChangesBuilder
 import de.westnordost.streetcomplete.data.osm.edits.update_tags.UpdateElementTagsAction
 import de.westnordost.streetcomplete.data.preferences.Preferences
-import de.westnordost.streetcomplete.databinding.ComposeViewBinding
 import de.westnordost.streetcomplete.osm.Sides
 import de.westnordost.streetcomplete.osm.sidewalk.Sidewalk
 import de.westnordost.streetcomplete.osm.sidewalk.applyTo
@@ -19,41 +17,40 @@ import de.westnordost.streetcomplete.osm.sidewalk.parseSidewalkSides
 import de.westnordost.streetcomplete.osm.sidewalk.validOrNullValues
 import de.westnordost.streetcomplete.overlays.AbstractOverlayForm
 import de.westnordost.streetcomplete.quests.sidewalk.SidewalkForm
-import de.westnordost.streetcomplete.ui.util.content
+import de.westnordost.streetcomplete.ui.common.overlay.OverlayForm
+import de.westnordost.streetcomplete.ui.util.rememberSerializable
 import org.koin.android.ext.android.inject
 
 class SidewalkOverlayForm : AbstractOverlayForm() {
 
-    override val contentLayoutResId = R.layout.compose_view
-    private val binding by contentViewBinding(ComposeViewBinding::bind)
-
     private val prefs: Preferences by inject()
 
-    override val contentPadding = false
-
-    private var originalSidewalks: Sides<Sidewalk> = Sides(null, null)
-    private val sidewalks: MutableState<Sides<Sidewalk>> = mutableStateOf(Sides(null, null))
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        originalSidewalks = parseSidewalkSides(element!!.tags)?.validOrNullValues() ?: Sides(null, null)
-        if (savedInstanceState == null) {
-            sidewalks.value = originalSidewalks
+    @Composable
+    override fun Content() {
+        val lastPicked = remember {
+            prefs.getLastPicked<Sides<Sidewalk>>("SidewalkOverlayForm")
         }
-    }
+        val originalSidewalks = remember {
+            parseSidewalkSides(element!!.tags)
+                ?.validOrNullValues()
+                ?: Sides<Sidewalk>(null, null)
+        }
+        var sidewalks by rememberSerializable { mutableStateOf(originalSidewalks) }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val lastPicked by lazy { prefs.getLastPicked<Sides<Sidewalk>>(this::class.simpleName!!) }
-
-        binding.composeViewBase.content { Surface {
+        OverlayForm(
+            isComplete = sidewalks.left != null || sidewalks.right != null,
+            hasChanges = sidewalks != originalSidewalks,
+            onClickOk = {
+                prefs.setLastPicked("SidewalkOverlayForm", listOf(sidewalks))
+                val tagChanges = StringMapChangesBuilder(element!!.tags)
+                sidewalks.applyTo(tagChanges)
+                applyEdit(UpdateElementTagsAction(element!!, tagChanges.create()))
+            },
+            contentPadding = PaddingValues.Zero,
+        ) {
             SidewalkForm(
-                value = sidewalks.value,
-                onValueChanged = {
-                    sidewalks.value = it
-                    checkIsFormComplete()
-                },
+                value = sidewalks,
+                onValueChanged = { sidewalks = it },
                 geometryRotation = geometryRotation.floatValue,
                 mapRotation = mapRotation.floatValue,
                 mapTilt = mapTilt.floatValue,
@@ -61,22 +58,6 @@ class SidewalkOverlayForm : AbstractOverlayForm() {
                 lastPicked = lastPicked,
                 lastPickedContentPadding = PaddingValues(start = 48.dp, end = 56.dp),
             )
-        } }
-
-        checkIsFormComplete()
-    }
-
-    override fun hasChanges(): Boolean =
-        sidewalks.value != originalSidewalks
-
-    override fun isFormComplete(): Boolean =
-        sidewalks.value.left != null || sidewalks.value.right != null
-
-    override fun onClickOk() {
-        prefs.setLastPicked(this::class.simpleName!!, listOf(sidewalks.value))
-
-        val tagChanges = StringMapChangesBuilder(element!!.tags)
-        sidewalks.value.applyTo(tagChanges)
-        applyEdit(UpdateElementTagsAction(element!!, tagChanges.create()))
+        }
     }
 }
