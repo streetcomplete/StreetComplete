5a309c481de5e6d84144709477418f2e784a3017
Tobias Zwick <newton@westnordost.de>
2026-05-18 18:35:47 +0200

    ar based quests pt 1

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_height/AddMaxPhysicalHeightForm.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/max_height/AddMaxPhysicalHeightForm.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import de.westnordost.streetcomplete.data.meta.CountryInfo
+import de.westnordost.streetcomplete.data.osm.mapdata.Element
 import de.westnordost.streetcomplete.osm.Length
 import de.westnordost.streetcomplete.quests.AbstractArMeasureQuestForm
 import de.westnordost.streetcomplete.quests.LengthForm
@@ -15,68 +20,57 @@ import de.westnordost.streetcomplete.resources.quest_maxheight_below_bridge_titl
 import de.westnordost.streetcomplete.resources.quest_maxheight_title
 import de.westnordost.streetcomplete.screens.measure.ArSupportChecker
 import de.westnordost.streetcomplete.ui.common.quest.QuestForm
+import de.westnordost.streetcomplete.ui.util.rememberSerializable
 import org.jetbrains.compose.resources.stringResource
 import org.koin.android.ext.android.inject
+import org.koin.compose.koinInject
+import kotlin.text.get
 
-class AddMaxPhysicalHeightForm : AbstractArMeasureQuestForm<MaxPhysicalHeightAnswer>() {
+@Composable
+fun AddMaxPhysicalHeightForm(
+    onAnswer: (MaxPhysicalHeightAnswer) -> Unit,
+    element: Element,
+    countryInfo: CountryInfo,
+    checkArSupport: ArSupportChecker = koinInject()
+) {
+    val arIsSupported = remember { checkArSupport() }
 
-    private val checkArSupport: ArSupportChecker by inject()
-    private var isARMeasurement: Boolean = false
-    private var length: MutableState<Length?> = mutableStateOf(null)
-
-    @Composable
-    override fun Content() {
-        val arIsSupported = remember { checkArSupport() }
-
-        val isBelowBridge = remember {
-            element.tags["amenity"] != "parking_entrance"
-            && element.tags["barrier"] != "height_restrictor"
-            && element.tags["tunnel"] == null
-            && element.tags["covered"] == null
-            && element.tags["man_made"] != "pipeline"
-        }
-
-        QuestForm(
-            title = stringResource(
-                // only the "below the bridge" situation may need some context
-                if (isBelowBridge) Res.string.quest_maxheight_below_bridge_title
-                else Res.string.quest_maxheight_title
-            ),
-            isComplete = length.value != null,
-            onClickOk = {
-                applyAnswer(MaxPhysicalHeightAnswer(length.value!!, isARMeasurement))
-            }
-        ) {
-            LengthForm(
-                length = length.value,
-                onChange = {
-                    isARMeasurement = false
-                    length.value = it
-                },
-                selectableUnits = countryInfo.lengthUnits,
-                showMeasureButton = arIsSupported,
-                onClickMeasure = { takeMeasurement(it, measureVertical = true) },
-                modifier = Modifier.fillMaxWidth(),
-            )
-        }
+    val isBelowBridge = remember {
+        element.tags["amenity"] != "parking_entrance"
+        && element.tags["barrier"] != "height_restrictor"
+        && element.tags["tunnel"] == null
+        && element.tags["covered"] == null
+        && element.tags["man_made"] != "pipeline"
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        savedInstanceState?.let { isARMeasurement = it.getBoolean(AR) }
+    var length by rememberSerializable { mutableStateOf<Length?>(null) }
+    var isArMeasurement by rememberSaveable { mutableStateOf<Boolean>(false) }
+
+    // TODO compose-quest-form not called from anywhere yet
+    fun onTookMeasurement(len: Length) {
+        length = len
+        isArMeasurement = true
     }
 
-    override fun onMeasured(length: Length) {
-        isARMeasurement = true
-        this.length.value = length
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putBoolean(AR, isARMeasurement)
-    }
-
-    companion object {
-        private const val AR = "ar"
+    QuestForm(
+        title = stringResource(
+            // only the "below the bridge" situation may need some context
+            if (isBelowBridge) Res.string.quest_maxheight_below_bridge_title
+            else Res.string.quest_maxheight_title
+        ),
+        isComplete = length != null,
+        onClickOk = { onAnswer(MaxPhysicalHeightAnswer(length!!, isArMeasurement)) }
+    ) {
+        LengthForm(
+            length = length,
+            onChange = {
+                isArMeasurement = false
+                length = it
+            },
+            selectableUnits = countryInfo.lengthUnits,
+            showMeasureButton = arIsSupported,
+            onClickMeasure = { takeMeasurement(it, measureVertical = true) },
+            modifier = Modifier.fillMaxWidth(),
+        )
     }
 }
--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/width/AddWidthForm.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/width/AddWidthForm.kt
@@ -9,8 +9,11 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import de.westnordost.streetcomplete.data.meta.CountryInfo
+import de.westnordost.streetcomplete.data.osm.mapdata.Element
 import de.westnordost.streetcomplete.osm.ALL_ROADS
 import de.westnordost.streetcomplete.osm.Length
 import de.westnordost.streetcomplete.osm.hasDubiousRoadWidth
@@ -20,79 +23,69 @@ import de.westnordost.streetcomplete.resources.*
 import de.westnordost.streetcomplete.screens.measure.ArSupportChecker
 import de.westnordost.streetcomplete.ui.common.dialogs.QuestConfirmationDialog
 import de.westnordost.streetcomplete.ui.common.quest.QuestForm
+import de.westnordost.streetcomplete.ui.util.rememberSerializable
 import org.jetbrains.compose.resources.stringResource
 import org.koin.android.ext.android.inject
+import org.koin.compose.koinInject
 
-class AddWidthForm : AbstractArMeasureQuestForm<WidthAnswer>() {
+@Composable
+fun AddWidthForm(
+    onAnswer: (WidthAnswer) -> Unit,
+    element: Element,
+    countryInfo: CountryInfo,
+    checkArSupport: ArSupportChecker = koinInject()
+) {
+    var confirmDubiousRoadWidth by remember { mutableStateOf(false) }
 
-    private val checkArSupport: ArSupportChecker by inject()
-    private var isARMeasurement: Boolean = false
-    private var length: MutableState<Length?> = mutableStateOf(null)
+    val isRoad = remember(element) { element.tags["highway"] in ALL_ROADS }
+    val arIsSupported = remember { checkArSupport() }
 
-    @Composable
-    override fun Content() {
-        var confirmDubiousRoadWidth by remember { mutableStateOf(false) }
+    var length by rememberSerializable { mutableStateOf<Length?>(null) }
+    var isArMeasurement by rememberSaveable { mutableStateOf<Boolean>(false) }
 
-        val isRoad = remember { element.tags["highway"] in ALL_ROADS }
-        val arIsSupported = remember { checkArSupport() }
+    // TODO compose-quest-form not called from anywhere yet
+    fun onTookMeasurement(len: Length) {
+        length = len
+        isArMeasurement = true
+    }
 
-        QuestForm(
-            isComplete = length.value != null,
-            onClickOk = {
-                val length = length.value!!
-                val newTags = element.tags + ("width" to length.toMeters().toString())
-                if (hasDubiousRoadWidth(newTags) != true) {
-                    applyAnswer(WidthAnswer(length, isARMeasurement))
-                } else {
-                    confirmDubiousRoadWidth = true
+    QuestForm(
+        isComplete = length != null,
+        onClickOk = {
+            val length = length!!
+            val newTags = element.tags + ("width" to length.toMeters().toString())
+            if (hasDubiousRoadWidth(newTags) != true) {
+                onAnswer(WidthAnswer(length, isArMeasurement))
+            } else {
+                confirmDubiousRoadWidth = true
+            }
+        },
+        content = {
+            Column {
+                if(isRoad) {
+                    Text(stringResource(Res.string.quest_road_width_explanation))
                 }
-            },
-            content = {
-                Column {
-                    if(isRoad) {
-                        Text(stringResource(Res.string.quest_road_width_explanation))
-                    }
 
-                    LengthForm(
-                        length = length.value,
-                        onChange = {
-                            isARMeasurement = false
-                            length.value = it
-                        },
-                        selectableUnits = countryInfo.lengthUnits,
-                        showMeasureButton = arIsSupported,
-                        onClickMeasure = { takeMeasurement(it, measureVertical = false) },
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                }
-            },
+                LengthForm(
+                    length = length,
+                    onChange = {
+                        isArMeasurement = false
+                        length = it
+                    },
+                    selectableUnits = countryInfo.lengthUnits,
+                    showMeasureButton = arIsSupported,
+                    onClickMeasure = { takeMeasurement(it, measureVertical = false) },
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+        },
+    )
+
+    if (confirmDubiousRoadWidth) {
+        QuestConfirmationDialog(
+            onDismissRequest = { confirmDubiousRoadWidth = false },
+            onConfirmed = { onAnswer(WidthAnswer(length!!, isArMeasurement)) },
+            text = { Text(stringResource(Res.string.quest_road_width_unusualInput_confirmation_description)) }
         )
-
-        if (confirmDubiousRoadWidth) {
-            QuestConfirmationDialog(
-                onDismissRequest = { confirmDubiousRoadWidth = false },
-                onConfirmed = { applyAnswer(WidthAnswer(length.value!!, isARMeasurement)) },
-                text = { Text(stringResource(Res.string.quest_road_width_unusualInput_confirmation_description)) }
-            )
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        savedInstanceState?.let { isARMeasurement = it.getBoolean(AR) }
-    }
-
-    override fun onMeasured(length: Length) {
-        isARMeasurement = true
-        this.length.value = length
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putBoolean(AR, isARMeasurement)
-    }
-
-    companion object {
-        private const val AR = "ar"
     }
 }
