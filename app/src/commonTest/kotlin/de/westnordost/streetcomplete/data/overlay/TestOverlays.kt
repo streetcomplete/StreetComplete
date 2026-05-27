86118934db28dfb7303494be1223a67ffed02f3f
Tobias Zwick <newton@westnordost.de>
2026-05-27 17:44:04 +0200

    move most quest tests to common

--- app/src/commonTest/kotlin/de/westnordost/streetcomplete/data/overlay/TestOverlays.kt
+++ app/src/commonTest/kotlin/de/westnordost/streetcomplete/data/overlay/TestOverlays.kt
@@ -1,5 +1,9 @@
 package de.westnordost.streetcomplete.data.overlay
 
+import androidx.compose.runtime.Composable
+import de.westnordost.streetcomplete.data.meta.CountryInfo
+import de.westnordost.streetcomplete.data.osm.edits.ElementEditAction
+import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
 import de.westnordost.streetcomplete.data.osm.mapdata.Element
 import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
 import de.westnordost.streetcomplete.data.overlays.Overlay
@@ -8,12 +12,22 @@ import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement
 import de.westnordost.streetcomplete.resources.*
 
 open class TestOverlayA : Overlay {
+
     override fun getStyledElements(mapData: MapDataWithGeometry): Sequence<Pair<Element, OverlayStyle>> = sequenceOf()
     override val changesetComment = "test"
-    override val icon = 0
+    override val icon = Res.drawable.quest_access_point
     override val title = Res.string.quest_address_title
     override val wikiLink = null
     override val achievements = emptyList<EditTypeAchievement>()
+
+    @Composable
+    override fun Form(
+        onEdit: (ElementEditAction) -> Unit,
+        element: Element?,
+        geometry: ElementGeometry,
+        countryInfo: CountryInfo,
+    ) {}
+
 }
 
 class TestOverlayB : TestOverlayA()
