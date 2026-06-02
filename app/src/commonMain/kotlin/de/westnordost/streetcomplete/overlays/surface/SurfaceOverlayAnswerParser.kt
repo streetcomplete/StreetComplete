91ecc09c238027aff9d31dc2811868612cf57ab1
Tobias Zwick <newton@westnordost.de>
2026-06-02 19:04:28 +0200

    formatting and test

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/overlays/surface/SurfaceOverlayAnswerParser.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/overlays/surface/SurfaceOverlayAnswerParser.kt
@@ -6,16 +6,16 @@ import de.westnordost.streetcomplete.osm.surface.parseSurface
 /** Returns either a [SingleSurface] or a [SegregatedSurface] if the footway is segregated between
  *  pedestrians and bicyclists */
 fun parseSurfaceOverlayAnswer(tags: Map<String, String>): SurfaceOverlayAnswer {
-    val originalSurface = parseSurface(tags["surface"])
-    val originalCyclewaySurface = parseSurface(tags["cycleway:surface"])
-    val originalFootwaySurface = parseSurface(tags["footway:surface"])
+    val surface = parseSurface(tags["surface"])
+    val cyclewaySurface = parseSurface(tags["cycleway:surface"])
+    val footwaySurface = parseSurface(tags["footway:surface"])
     val isSegregated =
         tags["highway"] in ALL_PATHS &&
-        (tags["segregated"] == "yes" || originalCyclewaySurface != null || originalFootwaySurface != null)
+        (tags["segregated"] == "yes" || cyclewaySurface != null || footwaySurface != null)
 
     return if (isSegregated) {
-        SegregatedSurface(footway = originalFootwaySurface, cycleway = originalCyclewaySurface)
+        SegregatedSurface(footway = footwaySurface, cycleway = cyclewaySurface)
     } else {
-        SingleSurface(originalSurface)
+        SingleSurface(surface)
     }
 }
