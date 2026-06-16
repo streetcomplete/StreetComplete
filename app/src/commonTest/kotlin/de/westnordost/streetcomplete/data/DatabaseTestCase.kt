b266a49cb40714657261a20a69c380113d42380b
Tobias Zwick <newton@westnordost.de>
2026-06-16 17:56:09 +0200

    move dao tests to common

---
+++ app/src/commonTest/kotlin/de/westnordost/streetcomplete/data/DatabaseTestCase.kt
@@ -0,0 +1,27 @@
+package de.westnordost.streetcomplete.data
+
+import androidx.sqlite.SQLiteConnection
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import kotlinx.io.files.Path
+import kotlinx.io.files.SystemFileSystem
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+
+open class DatabaseTestCase {
+    protected lateinit var database: Database
+    private lateinit var connection: SQLiteConnection
+
+    @BeforeTest fun setUpHelper() {
+        val connection = BundledSQLiteDriver().open(DATABASE_NAME)
+        database = DatabaseImpl(connection)
+    }
+
+    @AfterTest fun tearDownHelper() {
+        connection.close()
+        SystemFileSystem.delete(Path(DATABASE_NAME), mustExist = false)
+    }
+
+    companion object {
+        private const val DATABASE_NAME = "streetcomplete_test.db"
+    }
+}
