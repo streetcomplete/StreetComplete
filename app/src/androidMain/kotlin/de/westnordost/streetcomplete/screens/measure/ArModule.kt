3561e58652190d00f6f3f8fb2bc5adbd918f864a
Tobias Zwick <newton@westnordost.de>
2026-06-18 23:15:14 +0200

    make ArSupportChecker an interface

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/screens/measure/ArModule.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/screens/measure/ArModule.kt
@@ -3,6 +3,6 @@ package de.westnordost.streetcomplete.screens.measure
 import org.koin.dsl.module
 
 val arModule = module {
-    factory { ArSupportChecker(get()) }
+    factory<ArSupportChecker> { ArSupportCheckerImpl(get()) }
     factory { ArQuestsDisabler(get(), get()) }
 }
