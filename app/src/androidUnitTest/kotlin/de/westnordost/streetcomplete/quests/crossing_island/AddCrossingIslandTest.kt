9a524a5532a84e1c0afa45478a0fbae89ffbe5b5
Tobias Zwick <newton@westnordost.de>
2026-06-24 22:42:07 +0200

    Crossing island: Don't ask if there is a way tagged with footway=traffic_island adjacent to any way that includes the crossing node (fixes #6874)

--- app/src/androidUnitTest/kotlin/de/westnordost/streetcomplete/quests/crossing_island/AddCrossingIslandTest.kt
+++ app/src/androidUnitTest/kotlin/de/westnordost/streetcomplete/quests/crossing_island/AddCrossingIslandTest.kt
@@ -40,4 +40,22 @@ class AddCrossingIslandTest {
         assertEquals(0, questType.getApplicableElements(mapData).toList().size)
         assertNull(questType.isApplicableTo(crossing))
     }
+
+    @Test fun `not applicable to crossing with adjacent footway = traffic_island`() {
+        val crossing = node(id = 2, tags = mapOf(
+            "highway" to "crossing",
+            "crossing" to "something"
+        ))
+        val crossingWay = way(id = 1, nodes = listOf(1, 2, 3), tags = mapOf(
+            "highway" to "footway",
+            "footway" to "crossing"
+        ))
+        val adjacentTrafficIslandWay = way(id = 2, nodes = listOf(3, 4, 5), tags = mapOf(
+            "highway" to "footway",
+            "footway" to "traffic_island"
+        ))
+        val mapData = TestMapDataWithGeometry(listOf(crossing, crossingWay, adjacentTrafficIslandWay))
+        assertEquals(0, questType.getApplicableElements(mapData).toList().size)
+        assertNull(questType.isApplicableTo(crossing))
+    }
 }
