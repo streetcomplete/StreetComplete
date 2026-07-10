6bbfbc03e333d18c0168b022df52f17686b2f4f4
Kilian Koeltzsch <me@kilian.io>
2026-07-10 08:03:42 +0200

    fix number conversion in NumberFormatter on iOS

--- app/src/iosMain/kotlin/de/westnordost/streetcomplete/util/locale/NumberFormatter.ios.kt
+++ app/src/iosMain/kotlin/de/westnordost/streetcomplete/util/locale/NumberFormatter.ios.kt
@@ -26,11 +26,11 @@ actual class NumberFormatter actual constructor(
     }
 
     actual fun format(value: Number): String {
-        return format.stringFromNumber(value as NSNumber)!!
+        return format.stringFromNumber(value.toNSNumber())!!
     }
 
     actual fun parse(text: String): Number? {
-        return format.numberFromString(text) as? Number
+        return format.numberFromString(text)?.toNumber()
     }
 
     actual val decimalSeparator: Char
@@ -38,4 +38,23 @@ actual class NumberFormatter actual constructor(
 
     actual val groupingSeparator: Char
         get() = format.groupingSeparator.single()
+}
+
+// Kotlin's number types are not NSNumbers in Kotlin/Native, they must be converted explicitly
+
+private fun Number.toNSNumber(): NSNumber = when (this) {
+    is Byte -> NSNumber(char = this)
+    is Short -> NSNumber(short = this)
+    is Int -> NSNumber(int = this)
+    is Long -> NSNumber(longLong = this)
+    is Float -> NSNumber(float = this)
+    is Double -> NSNumber(double = this)
+    else -> NSNumber(double = toDouble())
+}
+
+/** Like NumberFormat.parse on the JVM, returns a Long if the parsed number is integral */
+private fun NSNumber.toNumber(): Number {
+    val double = doubleValue
+    val long = longLongValue
+    return if (long.toDouble() == double) long else double
 }
