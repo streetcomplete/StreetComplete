48e08c246a27a1b11c11aec2482a79c2a8400692
Tobias Zwick <newton@westnordost.de>
2026-07-15 19:56:35 +0200

    use correct functions according to documentation

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/ui/util/measure/ArModule.android.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/ui/util/measure/ArModule.android.kt
@@ -1,11 +1,14 @@
 package de.westnordost.streetcomplete.ui.util.measure
 
 import androidx.activity.ComponentActivity
+import org.koin.androidx.scope.dsl.activityRetainedScope
+import org.koin.androidx.scope.dsl.activityScope
 import org.koin.dsl.module
+import org.koin.core.module.dsl.viewModel
 
 actual val arPlatformModule = module {
     factory<ArSupportChecker> { AndroidArSupportChecker(get()) }
-    scope<ComponentActivity> {
-        factory<ArMeasureAppLauncher> { AndroidArMeasureAppLauncher(get()) }
+    activityScope {
+        scoped<ArMeasureAppLauncher> { AndroidArMeasureAppLauncher({ get() }) }
     }
 }
