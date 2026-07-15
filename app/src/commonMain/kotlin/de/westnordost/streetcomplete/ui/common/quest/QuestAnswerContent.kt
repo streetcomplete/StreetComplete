4c53d867ae8c8cbb7a348e649daf95591ccf6cc3
Tobias Zwick <newton@westnordost.de>
2026-07-15 20:12:14 +0200

    clip quest answer content to bounds

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/quest/QuestAnswerContent.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/quest/QuestAnswerContent.kt
@@ -13,6 +13,7 @@ import androidx.compose.material.ProvideTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.unit.dp
 
 /** Content area for the quest answer, i.e. [content] and/or button bar answers */
@@ -30,7 +31,8 @@ fun QuestAnswerContent(
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(contentPadding),
+                        .padding(contentPadding)
+                        .clipToBounds(),
                     contentAlignment = Alignment.Center,
                     content = content
                 )
