0720543661e8a6b2f7ac6bf7c88e1a011c257e93
Enno Hermann <Eginhard@users.noreply.github.com>
2026-08-05 14:57:11 +0200

    Don't ask for islands on continuous crossings

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/crossing_island/AddCrossingIsland.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/crossing_island/AddCrossingIsland.kt
@@ -24,6 +24,7 @@ class AddCrossingIsland : OsmElementQuestType<Boolean>, AndroidQuest {
           and foot != no
           and !crossing:island
           and crossing != island
+          and crossing:continuous != yes
     """.toElementFilterExpression() }
 
     private val excludedWaysFilter by lazy { """
