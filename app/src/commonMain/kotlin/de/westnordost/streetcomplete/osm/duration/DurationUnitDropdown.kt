c17a9941f60c766b6e5cb4f4d26e36cd9e59deef
Marek Krug <62391949+marekkrug@users.noreply.github.com>
2026-08-16 23:46:08 +0200

    Quest: How much do you have to pay to use this parking spot? (#6653)

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/duration/DurationUnitDropdown.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/duration/DurationUnitDropdown.kt
@@ -14,7 +14,8 @@ import org.jetbrains.compose.resources.stringResource
 fun DurationUnitDropdown(
     selectedDuration: DurationUnit,
     onSelectedDuration: (DurationUnit) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    alwaysSingular: Boolean = false,
 ) {
     DropdownButton(
         items = DurationUnit.entries,
@@ -22,12 +23,11 @@ fun DurationUnitDropdown(
         modifier = modifier,
         selectedItem = selectedDuration,
         style = ButtonStyle.Outlined,
-        itemContent = { Text(stringResource(it.text)) }
+        itemContent = { Text(stringResource(it.getText(alwaysSingular))) }
     )
 }
-
-private val DurationUnit.text: StringResource get() = when (this) {
-    DurationUnit.MINUTES -> Res.string.unit_minutes
-    DurationUnit.HOURS -> Res.string.unit_hours
-    DurationUnit.DAYS -> Res.string.unit_days
+private fun DurationUnit.getText(useSingular: Boolean): StringResource = when (this) {
+    DurationUnit.MINUTES -> if (useSingular) Res.string.unit_minute else Res.string.unit_minutes
+    DurationUnit.HOURS -> if (useSingular) Res.string.unit_hour else Res.string.unit_hours
+    DurationUnit.DAYS -> if (useSingular) Res.string.unit_day else Res.string.unit_days
 }
---
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/quests/charge/Charge.kt
@@ -0,0 +1,15 @@
+package de.westnordost.streetcomplete.quests.charge
+
+import de.westnordost.streetcomplete.osm.duration.DurationUnit
+/**
+ * Represents a simple charge for a specific time unit.
+ *
+ * @property amount the cost value, e.g. 1.50
+ * @property currency the currency of the charge, e.g. "EUR"
+ * @property timeUnit the unit of time the amount applies to (e.g. per hour, per day)
+ */
+data class Charge(
+    val amount: String,
+    val currency: String,
+    val timeUnit: DurationUnit
+)
