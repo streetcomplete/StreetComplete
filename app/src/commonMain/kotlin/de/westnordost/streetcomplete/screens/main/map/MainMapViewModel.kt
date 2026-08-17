d7001a0d45b431a7c934b58b29601e32432a84aa
Tobias Zwick <newton@westnordost.de>
2026-08-17 19:17:05 +0200

    properly initialize

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/screens/main/map/MainMapViewModel.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/screens/main/map/MainMapViewModel.kt
@@ -1,13 +1,14 @@
 package de.westnordost.streetcomplete.screens.main.map
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import de.westnordost.streetcomplete.util.ktx.launch
 import de.westnordost.streetcomplete.ApplicationConstants
 import de.westnordost.streetcomplete.data.download.tiles.DownloadedTilesSource
 import de.westnordost.streetcomplete.data.download.tiles.TilePos
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.callbackFlow
@@ -22,18 +23,24 @@ abstract class MainMapViewModel : ViewModel() {
 class MainMapViewModelImpl(
     private val downloadedTilesSource: DownloadedTilesSource
 ) : MainMapViewModel() {
-    override val downloadedTiles: StateFlow<Collection<TilePos>> = callbackFlow {
-        val listener = object : DownloadedTilesSource.Listener {
-            override fun onUpdated() {
-                launch {
-                    val tiles = withContext(Dispatchers.IO) {
-                        downloadedTilesSource.getAll(ApplicationConstants.DELETE_OLD_DATA_AFTER)
-                    }
-                    trySend(tiles)
-                }
-            }
+    override val downloadedTiles = MutableStateFlow<Collection<TilePos>>(emptyList())
+
+    private val downloadedTilesListener = object : DownloadedTilesSource.Listener {
+        override fun onUpdated() { updateDownloadedTiles() }
+    }
+
+    init {
+        updateDownloadedTiles()
+        downloadedTilesSource.addListener(downloadedTilesListener)
+    }
+
+    override fun onCleared() {
+        downloadedTilesSource.removeListener(downloadedTilesListener)
+    }
+
+    private fun updateDownloadedTiles() {
+        launch(Dispatchers.IO) {
+            downloadedTiles.value = downloadedTilesSource.getAll(ApplicationConstants.DELETE_OLD_DATA_AFTER)
         }
-        downloadedTilesSource.addListener(listener)
-        awaitClose { downloadedTilesSource.removeListener(listener) }
-    }.stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+    }
 }
