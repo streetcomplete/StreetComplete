44b6f83814bf33f2a3b8f55db0917271c04eee7c
paukli <paulkliemann@posteo.net>
2026-08-18 18:14:42 +0200

    added confirmation to certain aerialway oneway answers

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/quests/oneway/AddOnewayAerialway.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/quests/oneway/AddOnewayAerialway.kt
@@ -45,7 +45,7 @@ class AddOnewayAerialway : OsmElementQuestType<OnewayAnswer> {
 
     @Composable
     override fun Form(on: (QuestAction<OnewayAnswer>) -> Unit, element: Element, geometry: ElementGeometry, countryInfo: CountryInfo) {
-        AddOnewayForm(on, geometry)
+        AddOnewayForm(on, element, geometry)
     }
 
     override fun applyAnswerTo(answer: OnewayAnswer, tags: Tags, geometry: ElementGeometry, timestampEdited: Long) {
