8a52663651fe72407a0f2427bd0aae95efb2681a
Amaan Qureshi <138896458+Amaanprobably@users.noreply.github.com>
2026-08-26 11:11:20 +0530

    Add: building:use parsing and serialization

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/building/BuildingUseTypeCreator.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/building/BuildingUseTypeCreator.kt
@@ -1,14 +1,19 @@
 package de.westnordost.streetcomplete.osm.building
 
 import de.westnordost.streetcomplete.osm.Tags
-import de.westnordost.streetcomplete.osm.building.BuildingType.*
-import de.westnordost.streetcomplete.osm.updateCheckDate
+import de.westnordost.streetcomplete.osm.updateCheckDateForKey
 
 fun BuildingType.applyToBuildingUse(tags: Tags) {
     require(osmKey != null && osmValue != null)
+    // if user selects building:use same as building tag, remove redundant tagging
     if(osmValue == tags["building"]){
         tags.remove("building:use")
         return
     }
     tags["building:use"] = osmValue
+    // we set the check_date:building:use and not check_date because this is about the secondary feature,
+    // and updateCheckDateForKey() internally updates check_date if it existed before..
+    if (!tags.hasChanges) {
+        tags.updateCheckDateForKey("building:use")
+    }
 }
--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/building/BuildingUseTypeParser.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/osm/building/BuildingUseTypeParser.kt
@@ -2,7 +2,7 @@ package de.westnordost.streetcomplete.osm.building
 
 import de.westnordost.streetcomplete.osm.building.BuildingType.*
 fun createBuildingUseType(tags: Map<String,String>): BuildingType? {
-    // Excluded building types that shouldn't belong with building:use
+    // Excluded building types that shouldn't belong with building:use (subject to future changes)
     val excluded = listOf(RUINS, ABANDONED, HISTORIC)
     val buildingUseValue = tags["building:use"] ?: return null
     val buildingUse = BuildingType.entries.find { it.osmValue == buildingUseValue && it !in excluded }
