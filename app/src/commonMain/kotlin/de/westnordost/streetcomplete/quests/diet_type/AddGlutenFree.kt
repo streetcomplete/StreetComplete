4061a321666ccd34932b9bc6059317e6a8dabc7c
Flo Edelmann <git@flo-edelmann.de>
2026-08-30 14:34:39 +0200

    Use `=` instead of `~` for single values in quest filters

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/quests/diet_type/AddGlutenFree.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/quests/diet_type/AddGlutenFree.kt
@@ -21,7 +21,7 @@ class AddGlutenFree : OsmFilterQuestType<DietAvailabilityAnswer>() {
           amenity ~ restaurant|cafe|fast_food|food_court and food != no
           or amenity ~ pub|nightclub|biergarten|bar and food = yes
           or shop ~ supermarket|convenience|deli|bakery|pastry
-          or tourism ~ alpine_hut and food != no
+          or tourism = alpine_hut and food != no
         )
         and (
           !diet:gluten_free
--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/quests/diet_type/AddVegan.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/quests/diet_type/AddVegan.kt
@@ -25,7 +25,7 @@ class AddVegan : OsmFilterQuestType<DietAvailabilityAnswer>() {
           (
             amenity ~ restaurant|cafe|fast_food|food_court and food != no
             or amenity ~ pub|nightclub|biergarten|bar and food = yes
-            or tourism ~ alpine_hut and food != no
+            or tourism = alpine_hut and food != no
           )
         )
         and (
