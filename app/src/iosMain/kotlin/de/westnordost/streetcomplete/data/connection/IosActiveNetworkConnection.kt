047a641e9311410a28a913d3fe3600536a2d228d
Max Ward <max.jp.ward@gmail.com>
2026-09-01 19:20:09 -0700

    refactor IosActiveNetworkConnection to use withTimeoutOrNull to get current value from capabilitiesFlow, and distinguish between invalid state and no internet

--- app/src/iosMain/kotlin/de/westnordost/streetcomplete/data/connection/IosActiveNetworkConnection.kt
+++ app/src/iosMain/kotlin/de/westnordost/streetcomplete/data/connection/IosActiveNetworkConnection.kt
@@ -1,14 +1,10 @@
 package de.westnordost.streetcomplete.data.connection
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
 import platform.Network.nw_path_get_status
 import platform.Network.nw_path_is_expensive
 import platform.Network.nw_path_monitor_cancel
@@ -16,24 +12,26 @@ import platform.Network.nw_path_monitor_create
 import platform.Network.nw_path_monitor_set_queue
 import platform.Network.nw_path_monitor_set_update_handler
 import platform.Network.nw_path_monitor_start
+import platform.Network.nw_path_status_invalid
 import platform.Network.nw_path_status_satisfied
 import platform.Network.nw_path_t
 import platform.darwin.dispatch_queue_create
+import kotlin.time.Duration.Companion.milliseconds
 
 class IosActiveNetworkConnection : ActiveNetworkConnection {
     private val queue = dispatch_queue_create("network-monitor", null)
 
     private fun mapPath(path: nw_path_t): NetworkCapabilities? {
-        if(path == null) return null
-
+        if (path == null) return null
         val status = nw_path_get_status(path)
-        if (status != nw_path_status_satisfied) return null
-        val isMetered = nw_path_is_expensive(path)
-        return NetworkCapabilities(hasInternet = true, isMetered = isMetered)
+        if (status == nw_path_status_invalid) return null
+        return NetworkCapabilities(
+            hasInternet = status == nw_path_status_satisfied,
+            isMetered = nw_path_is_expensive(path),
+        )
     }
 
-    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
-    private val state: StateFlow<NetworkCapabilities?> = callbackFlow {
+    override val capabilities = callbackFlow {
         val monitor = nw_path_monitor_create()
         nw_path_monitor_set_queue(monitor, queue)
         nw_path_monitor_set_update_handler(monitor) { path ->
@@ -42,7 +40,5 @@ class IosActiveNetworkConnection : ActiveNetworkConnection {
         }
         nw_path_monitor_start(monitor)
         awaitClose { nw_path_monitor_cancel(monitor) }
-    }.stateIn(scope, SharingStarted.WhileSubscribed(), null)
-
-    override val capabilities: Flow<NetworkCapabilities?> get() = state
+    }
 }
