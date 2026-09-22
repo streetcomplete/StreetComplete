698259e6d6570b8cbb8f987bdff5e7be6e31c4c3
Amaan Qureshi <138896458+Amaanprobably@users.noreply.github.com>
2026-09-22 21:12:24 +0530

    Feat: Increase drum-roll size for TimePicker wheel

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/opening_hours/TimeSpansSelectorsSelectDialog.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/ui/common/opening_hours/TimeSpansSelectorsSelectDialog.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import de.westnordost.osm_opening_hours.model.ClockTime
 import de.westnordost.osm_opening_hours.model.ExtendedClockTime
@@ -37,7 +38,7 @@ import de.westnordost.streetcomplete.resources.*
 import de.westnordost.streetcomplete.ui.common.TimePicker
 import de.westnordost.streetcomplete.ui.common.dialogs.ScrollableAlertDialog
 import de.westnordost.streetcomplete.ui.common.rememberTimePickerState
-import de.westnordost.streetcomplete.ui.theme.largeInput
+import de.westnordost.streetcomplete.ui.theme.headlineLarge
 import de.westnordost.streetcomplete.util.locale.TimeFormatElements
 import org.jetbrains.compose.resources.stringResource
 
@@ -89,7 +90,7 @@ fun TimeSpansSelectorSelectDialog(
                     val timePickerState = if (step == 0) startTimePickerState else endTimePickerState
                     val openEnd = if (step == 0) startOpenEnd else endOpenEnd
 
-                    ProvideTextStyle(MaterialTheme.typography.largeInput) {
+                    ProvideTextStyle(MaterialTheme.typography.headlineLarge.copy(textAlign = TextAlign.Center)) {
                         TimePicker(
                             state = timePickerState,
                             timeFormatElements = timeFormatElements,
